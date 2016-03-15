ddf023876db575beeaa21c78a39d65a1b9d2fa73
wireswiss <github@wire.com>
2016-03-15 17:39:05 +0100

    Wire iOS release 2.1

--- Source/ZMSActivity.h
+++ Source/ZMSActivity.h
@@ -1,3 +1,4 @@
+// 
 // Wire
 // Copyright (C) 2016 Wire Swiss GmbH
 // 
@@ -13,6 +14,7 @@
 // 
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
+// 
 
 
 #import <os/activity.h>
