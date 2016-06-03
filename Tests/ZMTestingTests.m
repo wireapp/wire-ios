dcb9b695693c647534c255c924740bee59719059
wireswiss <github@wire.com>
2016-06-03 16:53:26 +0200

    iOS Wire version 2.7

--- Tests/ZMTestingTests.m
+++ Tests/ZMTestingTests.m
@@ -17,22 +17,22 @@
 // 
 
 
-#import <XCTest/XCTest.h>
+#import <ZMTesting/ZMTesting.h>
 
-@interface ZMTestingTests : XCTestCase
+@interface ZMTestingTests : ZMTBaseTest
 
 @end
 
 @implementation ZMTestingTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+- (void)testWaitForAllGroupsToBeEmpty {
+    XCTAssertTrue([self waitForAllGroupsToBeEmptyWithTimeout:100]);
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+- (void)testWaitForAllGroupsToBeEmptyFail {
+    [self.dispatchGroup enter];
+    XCTAssertFalse([self waitForAllGroupsToBeEmptyWithTimeout:0.01]);
+    [self.dispatchGroup leave];
 }
 
 @end
