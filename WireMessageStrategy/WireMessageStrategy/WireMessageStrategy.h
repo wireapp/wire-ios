115258bf6eded8f2b430ee24e1f2a07f3fc1787e
Jacob Persson <jacob.persson@wire.com>
2016-08-25 18:15:51 +0200

    Add empty project

---
+++ WireMessageStrategy/WireMessageStrategy/WireMessageStrategy.h
@@ -0,0 +1,19 @@
+//
+//  WireMessageStrategy.h
+//  WireMessageStrategy
+//
+//  Created by Jacob on 25/08/16.
+//  Copyright © 2016 Wire GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WireMessageStrategy.
+FOUNDATION_EXPORT double WireMessageStrategyVersionNumber;
+
+//! Project version string for WireMessageStrategy.
+FOUNDATION_EXPORT const unsigned char WireMessageStrategyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WireMessageStrategy/PublicHeader.h>
+
+
