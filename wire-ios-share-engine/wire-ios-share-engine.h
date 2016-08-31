a75e3dde02c719d0876aef1fa0abcbcb3bfba88e
Marco Conti <marcoconti83@gmail.com>
2016-08-31 15:10:34 +0200

    Initial commit

---
+++ wire-ios-share-engine/wire-ios-share-engine.h
@@ -0,0 +1,19 @@
+//
+//  wire-ios-share-engine.h
+//  wire-ios-share-engine
+//
+//  Created by Marco Conti on 31/08/16.
+//  Copyright © 2016 com.wire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for wire-ios-share-engine.
+FOUNDATION_EXPORT double wire_ios_share_engineVersionNumber;
+
+//! Project version string for wire-ios-share-engine.
+FOUNDATION_EXPORT const unsigned char wire_ios_share_engineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <wire_ios_share_engine/PublicHeader.h>
+
+
