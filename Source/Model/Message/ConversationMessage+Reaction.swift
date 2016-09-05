8b1a8bb329971a8ca144871c0f788347d4f69852
Silvan Dähn <silvandaehn@me.com>
2016-09-05 11:57:23 +0200

    Actually delete the reaction object from the database when calling `clearAllReactions` on a message

--- Source/Model/Message/ConversationMessage+Reaction.swift
+++ Source/Model/Message/ConversationMessage+Reaction.swift
@@ -66,6 +66,8 @@ extension ZMMessage {
 
     @objc public func clearAllReactions() {
         reactions.removeAll()
+        guard let moc = managedObjectContext else { return }
+        reactions.forEach(moc.deleteObject)
     }
     
 }
