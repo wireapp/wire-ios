b19f7d6332129fbacb51f981e56c670dd7496531
Silvan Daehn <silvandaehn@me.com>
2016-09-14 10:52:05 +0200

    Only allow valid center coordinates and region spans (#207)

---
+++ Wire-iOS Tests/LocationDataTests.swift
@@ -0,0 +1,61 @@
+//
+// Wire
+// Copyright (C) 2016 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+
+@testable import Wire
+import ZMCDataModel
+import XCTest
+import MapKit
+
+
+class LocationDataTests: XCTestCase {
+    
+    func testThatLocationDataCanBeConvertedToADictionary() {
+        // given
+        let sut = LocationData.locationData(
+            withLatitude: 45,
+            longitude: 75,
+            name: name,
+            zoomLevel: 5
+        )
+        
+        // when
+        let dict = sut.toDictionary()
+        
+        // then
+        XCTAssertEqual(dict["LastLocationLatitudeKey"] as? Float, Float(45))
+        XCTAssertEqual(dict["LastLocationLongitudeKey"] as? Float, Float(75))
+        XCTAssertEqual(dict["LastLocationZoomLevelKey"] as? Int, 5)
+    }
+    
+    func testThatLocationDataCanBeCreatedFromADictionary() {
+        // when
+        let sut = LocationData.locationData(fromDictionary: [
+            "LastLocationLatitudeKey": 45,
+            "LastLocationLongitudeKey": 75,
+            "LastLocationZoomLevelKey": 5
+            ])
+
+        // then
+        XCTAssertEqual(sut?.latitude, 45)
+        XCTAssertEqual(sut?.longitude, 75)
+        XCTAssertEqual(sut?.zoomLevel, 5)
+        XCTAssertNil(sut?.name)
+    }
+
+}
--- Wire-iOS/Sources/UserInterface/Location/MapKit+Helper.swift
+++ Wire-iOS/Sources/UserInterface/Location/MapKit+Helper.swift
@@ -52,8 +52,10 @@ extension MKMapView {
     }
     
     func setCenterCoordinate(coordinate: CLLocationCoordinate2D, zoomLevel: Int, animated: Bool = false) {
-        let span = MKCoordinateSpanMake(360 / pow(2, Double(zoomLevel)) * Double(frame.height) / 256, 0)
-        setRegion(MKCoordinateRegionMake(coordinate, span), animated: animated)
+        guard CLLocationCoordinate2DIsValid(coordinate) else { return }
+        let latitudeDelta = min(360 / pow(2, Double(zoomLevel)) * Double(frame.height) / 256, 180)
+        let region = MKCoordinateRegionMake(coordinate, MKCoordinateSpanMake(latitudeDelta, 0))
+        setRegion(region, animated: animated)
     }
 
 }
