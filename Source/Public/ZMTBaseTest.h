41e762b8f38770ff010ba7885e492fc7bfd7a02e
Sabine Geithner <sabine@wire.com>
2016-09-15 14:52:18 +0200

    add nullability indicators and uuid extension (#5)

--- Source/Public/ZMTBaseTest.h
+++ Source/Public/ZMTBaseTest.h
@@ -22,7 +22,7 @@
 #import "ZMTFailureRecorder.h"
 
 
-extern void PrintTimeoutWarning(XCTestCase *test, NSTimeInterval const maxTimeout, NSTimeInterval const actualTimeout);
+extern void PrintTimeoutWarning(XCTestCase *_Nonnull test, NSTimeInterval const maxTimeout, NSTimeInterval const actualTimeout);
 
 typedef BOOL(^VerificationBlock)(void);
 
@@ -30,17 +30,17 @@ typedef BOOL(^VerificationBlock)(void);
 @interface ZMTBaseTest : XCTestCase
 
 /// Calls @c -verify during @c -tearDown
-- (void)verifyMockLater:(id)mock;
+- (void)verifyMockLater:(nonnull id)mock;
 - (void)verifyMocksNow;
 
 - (void)setUp ZM_REQUIRES_SUPER;
 - (void)tearDown ZM_REQUIRES_SUPER;
 
 /// Should be wrapped in call to @c XCTAssert()
-- (BOOL)waitOnMainLoopUntilBlock:(VerificationBlock)block timeout:(NSTimeInterval)timeout ZM_MUST_USE_RETURN;
+- (BOOL)waitOnMainLoopUntilBlock:(nonnull VerificationBlock)block timeout:(NSTimeInterval)timeout ZM_MUST_USE_RETURN;
 
-- (BOOL)waitWithTimeout:(NSTimeInterval)timeout verificationBlock:(VerificationBlock)block ZM_MUST_USE_RETURN;
-- (BOOL)waitUntilDate:(NSDate *)runUntil verificationBlock:(VerificationBlock)block ZM_MUST_USE_RETURN;
+- (BOOL)waitWithTimeout:(NSTimeInterval)timeout verificationBlock:(nonnull VerificationBlock)block ZM_MUST_USE_RETURN;
+- (BOOL)waitUntilDate:(nonnull NSDate *)runUntil verificationBlock:(nonnull VerificationBlock)block ZM_MUST_USE_RETURN;
 
 /// Returns whether we are debugging the tests. This is enabled by setting the "DEBUG_TESTS" environment variable to 1
 + (BOOL)isDebuggingTests;
@@ -52,39 +52,39 @@ typedef BOOL(^VerificationBlock)(void);
 
 /// If this is set to true, we will ignore the debug flag for tests in timer (use this to test timer test failures)
 @property (nonatomic) BOOL ignoreTestDebugFlagForTestTimers;
-@property (nonatomic, readonly) ZMSDispatchGroup *dispatchGroup;
+@property (nonatomic, readonly, nonnull) ZMSDispatchGroup *dispatchGroup;
 
 - (void)spinMainQueueWithTimeout:(NSTimeInterval)timeout;
 - (BOOL)waitForAllGroupsToBeEmptyWithTimeout:(NSTimeInterval)timeout ZM_MUST_USE_RETURN;
 
-- (BOOL)waitForCustomExpectationsWithTimeout:(NSTimeInterval)timeout handler:(XCWaitCompletionHandler)handlerOrNil ZM_MUST_USE_RETURN;
+- (BOOL)waitForCustomExpectationsWithTimeout:(NSTimeInterval)timeout handler:(nullable XCWaitCompletionHandler)handlerOrNil ZM_MUST_USE_RETURN;
 - (BOOL)waitForCustomExpectationsWithTimeout:(NSTimeInterval)timeout ZM_MUST_USE_RETURN;
 - (BOOL)verifyAllExpectationsNow ZM_MUST_USE_RETURN;
 
 /// perform operations while not considering ZMLogErrors as test failure
-- (void)performIgnoringZMLogError:(void(^)(void))block;
+- (void)performIgnoringZMLogError:(nonnull void(^)(void))block;
 
 /// Returns the data of a small JPEG image
-- (NSData *)verySmallJPEGData;
+- (nonnull NSData *)verySmallJPEGData;
 /// Returns the data of a small JPEG image
-+ (NSData *)verySmallJPEGData;
++ (nonnull NSData *)verySmallJPEGData;
 /// Returns the data of a medium JPEG image
-- (NSData *)mediumJPEGData;
+- (nonnull NSData *)mediumJPEGData;
 /// Returns the data of a medium JPEG image
-+ (NSData *)mediumJPEGData;
++ (nonnull NSData *)mediumJPEGData;
 
-@property (nonatomic, strong) id<ZMSGroupQueue> fakeUIContext;
-@property (nonatomic, strong) id<ZMSGroupQueue> fakeSyncContext;
+@property (nonatomic, strong, nonnull) id<ZMSGroupQueue> fakeUIContext;
+@property (nonatomic, strong, nonnull) id<ZMSGroupQueue> fakeSyncContext;
 
 @end
 
 
 @interface ZMTBaseTest (Asserts)
 
-- (void)assertDictionary:(NSDictionary *)d1 isEqualToDictionary:(NSDictionary *)d2 name1:(char const *)name1 name2:(char const *)name2 failureRecorder:(ZMTFailureRecorder *)failureRecorder;
-- (void)assertDictionary:(NSDictionary *)d1 isEqualToDictionary:(NSDictionary *)d2 name1:(char const *)name1 name2:(char const *)name2 ignoreKeys:(NSArray *)ignoredKeys failureRecorder:(ZMTFailureRecorder *)failureRecorder;
+- (void)assertDictionary:(nonnull NSDictionary *)d1 isEqualToDictionary:(nonnull NSDictionary *)d2 name1:(nonnull char const *)name1 name2:(nonnull char const *)name2 failureRecorder:(nonnull ZMTFailureRecorder *)failureRecorder;
+- (void)assertDictionary:(nonnull NSDictionary *)d1 isEqualToDictionary:(nonnull NSDictionary *)d2 name1:(nonnull char const *)name1 name2:(nonnull char const *)name2 ignoreKeys:(nonnull NSArray *)ignoredKeys failureRecorder:(nonnull ZMTFailureRecorder *)failureRecorder;
 
-- (void)assertArray:(NSArray *)a1 hasSameElementsAsArray:(NSArray *)a2 name1:(char const *)name1 name2:(char const *)name2 failureRecorder:(ZMTFailureRecorder *)failureRecorder;
+- (void)assertArray:(nonnull NSArray *)a1 hasSameElementsAsArray:(nonnull NSArray *)a2 name1:(nonnull char const *)name1 name2:(nonnull char const *)name2 failureRecorder:(nonnull ZMTFailureRecorder *)failureRecorder;
 
 @end
 
