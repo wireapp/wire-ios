c903f7b63fd9436a86773150231c6bfd6d2a5121
Sabine Geithner <sabine.geithner@wearezeta.com>
2016-09-22 18:13:10 +0200

    Move message transcoders and tests

---
+++ Source/WireMessageStrategy.h
@@ -0,0 +1,17 @@
+//
+//  WireMessageStrategy.h
+//  WireMessageStrategy
+//
+//  Created by Sabine Geithner on 22/09/16.
+//  Copyright © 2016 Wire Swiss GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <WireMessageStrategy/ZMClientMessageTranscoder.h>
+#import <WireMessageStrategy/ZMMessageTranscoder.h>
+#import <WireMessageStrategy/ZMMessageTranscoder+Internal.h>
+
+#import <WireMessageStrategy/ZMPushMessageHandler.h>
+#import <WireMessageStrategy/CBCryptoBox+UpdateEvents.h>
+
