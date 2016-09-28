5844d9985e3c6cd85e942cc16dc3dd838df3324c
Silvan Daehn <silvandaehn@me.com>
2016-09-28 11:24:46 +0200

    Fix/initial observer state (#86)
    * Start observation system according to current application state
    
    * Add missing import

--- Source/UserSession/ZMUserSession.m
+++ Source/UserSession/ZMUserSession.m
@@ -322,6 +322,8 @@ ZM_EMPTY_ASSERTING_INIT()
         [self registerForRequestToOpenConversationNotification];
         [self enablePushNotifications];
         [self enableBackgroundFetch];
+        
+        self.managedObjectContext.globalManagedObjectContextObserver.propagateChanges = self.application.applicationState != UIApplicationStateBackground;
         ZM_ALLOW_MISSING_SELECTOR([[NSNotificationCenter defaultCenter] addObserver:self
                                                                            selector:@selector(didEnterEventProcessingState:)
                                                                                name:ZMApplicationDidEnterEventProcessingStateNotificationName
