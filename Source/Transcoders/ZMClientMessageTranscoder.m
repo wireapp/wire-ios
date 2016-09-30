58c5906d0fb4d01732c30da6e009326a6fd1462c
Gerasimenko Mike <github@gerasimenko.me>
2016-09-30 14:32:46 +0200

    Guard callbacks for completion of deleted message confirmation

--- Source/Transcoders/ZMClientMessageTranscoder.m
+++ Source/Transcoders/ZMClientMessageTranscoder.m
@@ -72,7 +72,8 @@
     [(id)message parseUploadResponse:response clientDeletionDelegate:self.clientRegistrationStatus];
     
     // if it's reaction
-    if ([message isKindOfClass:[ZMClientMessage class]]){
+    if ([message isKindOfClass:[ZMClientMessage class]] && !message.isZombieObject) {
+        
         ZMClientMessage *clientMessage = (id)message;
         if (clientMessage.genericMessage.hasReaction) {
             [message.managedObjectContext deleteObject:clientMessage];
