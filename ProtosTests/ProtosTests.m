81cf67dbf3c45f11b2998c0d1fd12f3cdcf13f57
Marco Conti <marcoconti83@gmail.com>
2016-10-17 18:28:02 +0200

    Add dummy test

---
+++ ProtosTests/ProtosTests.m
@@ -0,0 +1,22 @@
+//
+//  ProtosTests.m
+//  ZMProtos
+//
+//  Created by Marco Conti on 17/10/16.
+//  Copyright © 2016 Wire. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ProtosTests : XCTestCase
+
+@end
+
+@implementation ProtosTests
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
