c6ab7138360d6b2e0677ae95af36e2d5eadb1b51
Marco Conti <marcoconti83@gmail.com>
2016-10-19 11:38:58 +0200

    Add logs for crypto box operations

---
+++ Cryptobox/Logs.swift
@@ -0,0 +1,12 @@
+//
+//  Logs.swift
+//  Cryptobox
+//
+//  Created by Marco Conti on 19/10/16.
+//  Copyright © 2016 Cryptobox. All rights reserved.
+//
+
+import Foundation
+import ZMCSystem
+
+let zmLog = ZMSLog(tag: "cryptobox")
