a2d4d2c6943c15d4cec0baf362926983664abaec
Silvan Daehn <silvandaehn@me.com>
2016-11-10 16:48:38 +0100

    Add support to receive assets sent using the /assets/v3 endpoint (#17)
    * Add initial logic to download assets from the v3 endpoint
    
    * Add comments and rename test helper
    
    * Add asset v3 preview image download request strategy
    
    This adds a new strategy which is only used to download preview images for files uploaded using the v3 endpoint.
    The reasoning not to put this logic in the v3 file download strategy is as follows:
    
    * There are multiple things we want for file downloads but not for the image preview, like progress etc.
    * The AssetDownloadStrategy already has to deal with v2 and v3 assets, increasing this to preview images makes the code hard to reason about and unnecessarily bloats this class.
    
    Update tests for a new data model with a asset client message version identifier.
    
    * Use assetId as cache key for v3 assets
    
    * Add version 3 checks to download strategy predicates
    
    * Extract v3 asset downloading in separate request strategy
    
    * Add whitelist to AssetV3PreviewDownloadRequestStrategy and fix predicate
    
    The `AssetV3PreviewDownloadRequestStrategy` used a wrong predicate only downloading the preview when the assets `transferState` was `.downloading`, instead we want to whitelist the image download when the UI calls `requestImageDownload`.
    Thus this PR changes the implementation to use a `ZMDownstreamObjectSyncWithWhitelist` and listen to image download notifications as well as  adjusts the predicate for objects that need to be downloaded.
    
    * Use correct cache key
    
    * Fix asset v3 download cancellation and add test
    
    * Add cache changes ported from v3 sending branch
    
    * Remove unneeded code
    
    * Bump dependencies

---
+++ Tests/Source/AssetV3DownloadRequestStrategyTests.swift
@@ -0,0 +1,350 @@
+//
+// Wire
+// Copyright (C) 2016 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+
+import Foundation
+@testable import WireMessageStrategy
+
+private let testDataURL = Bundle(for: AssetV3DownloadRequestStrategyTests.self).url(forResource: "Lorem Ipsum", withExtension: "txt")!
+
+
+class AssetV3DownloadRequestStrategyTests: MessagingTest {
+
+    var authStatus: MockClientRegistrationStatus!
+    var cancellationProvider: MockTaskCancellationProvider!
+    var sut: AssetV3DownloadRequestStrategy!
+    var conversation: ZMConversation!
+
+    override func setUp() {
+        super.setUp()
+        authStatus = MockClientRegistrationStatus()
+        cancellationProvider = MockTaskCancellationProvider()
+        sut = AssetV3DownloadRequestStrategy(
+            authStatus: authStatus,
+            taskCancellationProvider: cancellationProvider,
+            managedObjectContext: syncMOC
+        )
+        conversation = createConversation()
+    }
+
+    fileprivate func createConversation() -> ZMConversation {
+        let conversation = ZMConversation.insertNewObject(in: syncMOC)
+        conversation.remoteIdentifier = UUID.create()
+        return conversation
+    }
+
+    fileprivate func createFileMessageWithAssetId(
+        in conversation: ZMConversation,
+        otrKey: Data = Data.randomEncryptionKey(),
+        sha: Data  = Data.randomEncryptionKey()
+        ) -> (message: ZMAssetClientMessage, assetId: String, assetToken: String)? {
+
+        let message = conversation.appendMessage(with: ZMFileMetadata(fileURL: testDataURL), version3: true) as! ZMAssetClientMessage
+
+        let (assetId, token) = (UUID.create().transportString(), UUID.create().transportString())
+
+        // TODO: We should replace this manual update with inserting a v3 asset as soon as we have sending support
+        let uploaded = ZMGenericMessage.genericMessage(
+            withUploadedOTRKey: otrKey,
+            sha256: sha,
+            messageID: message.nonce.transportString(),
+            expiresAfter: NSNumber(value: conversation.messageDestructionTimeout)
+        )
+
+        guard let uploadedWithId = uploaded.updated(withAssetId: assetId, token: token) else {
+            XCTFail("Failed to update asset")
+            return nil
+        }
+
+        message.add(uploadedWithId)
+        configureForDownloading(message: message)
+        XCTAssertEqual(message.version, 3)
+        return (message, assetId, token)
+    }
+
+    fileprivate func configureForDownloading(message: ZMAssetClientMessage) {
+        message.fileMessageData?.transferState = .downloading
+        self.syncMOC.saveOrRollback()
+
+        self.sut.contextChangeTrackers.forEach { tracker in
+            tracker.objectsDidChange(Set(arrayLiteral: message))
+        }
+
+        XCTAssertTrue(self.waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+    }
+
+    func testThatItGeneratesARequestToTheV3EndpointIfTheProtobufContainsAnAssetID_V3() {
+        // Given
+        guard let (message, assetId, token) = createFileMessageWithAssetId(in: createConversation()) else { return XCTFail("No message") }
+
+        guard let assetData = message.genericAssetMessage?.assetData else { return XCTFail("No assetData found") }
+        XCTAssert(assetData.hasUploaded())
+        XCTAssertEqual(assetData.uploaded.assetId, assetId)
+        XCTAssertEqual(assetData.uploaded.assetToken, token)
+
+        // When
+        guard let request = sut.nextRequest() else { return XCTFail("No request generated") }
+
+        // Then
+        XCTAssertEqual(request.method, .methodGET)
+        XCTAssertEqual(request.path, "/assets/v3/\(assetId)")
+        XCTAssert(request.needsAuthentication)
+    }
+
+    func testThatItGeneratesARequestToTheV3EndpointITheProtobufContainsAnAssetID_EphemeralConversation_V3() {
+        // Given
+        let conversation = createConversation()
+        conversation.messageDestructionTimeout = 5
+        guard let (message, assetId, token) = createFileMessageWithAssetId(in: conversation) else { return XCTFail("No message") }
+
+        guard let assetData = message.genericAssetMessage?.assetData else { return XCTFail("No assetData found") }
+        XCTAssert(assetData.hasUploaded())
+        XCTAssertEqual(assetData.uploaded.assetId, assetId)
+        XCTAssertEqual(assetData.uploaded.assetToken, token)
+        XCTAssert(message.genericAssetMessage!.hasEphemeral())
+
+        // When
+        guard let request = sut.nextRequest() else { return XCTFail("No request generated") }
+
+        // Then
+        XCTAssertEqual(request.method, .methodGET)
+        XCTAssertEqual(request.path, "/assets/v3/\(assetId)")
+        XCTAssert(request.needsAuthentication)
+    }
+
+    func testThatItGeneratesARequestOnlyOnceForAssetMessages_V3() {
+        // Given
+        guard let _ = createFileMessageWithAssetId(in: createConversation()) else { return XCTFail("No message") }
+
+        // When
+        guard let _ = sut.nextRequest() else { return XCTFail("No request generated") }
+
+        // Then
+        XCTAssertNil(sut.nextRequest())
+    }
+
+    func testThatItGeneratesNoRequestsIfNotAuthenticated_V3() {
+        // given
+        authStatus.mockClientIsReadyForRequests = false
+        _ = createFileMessageWithAssetId(in: conversation)! // V3
+
+        // then
+        XCTAssertNil(sut.nextRequest())
+    }
+
+    func testThatItGeneratesNoRequestsIfMessageIsUploading_V3() {
+        // given
+        guard let (message , _, _) = createFileMessageWithAssetId(in: conversation) else { return XCTFail() } // V3
+        message.fileMessageData?.transferState = .uploaded
+        syncMOC.saveOrRollback()
+
+        sut.contextChangeTrackers.forEach { tracker in
+            tracker.objectsDidChange(Set(arrayLiteral: message))
+        }
+
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+
+        // then
+        XCTAssertNil(sut.nextRequest())
+    }
+
+}
+
+// tests on result of request
+extension AssetV3DownloadRequestStrategyTests {
+
+    func testThatItMarksDownloadAsSuccessIfSuccessfulDownloadAndDecryption_V3() {
+        // given
+        let plainTextData = Data.secureRandomData(length: 500)
+        let key = Data.randomEncryptionKey()
+        let encryptedData = plainTextData.zmEncryptPrefixingPlainTextIV(key: key)
+        let sha = encryptedData.zmSHA256Digest()
+
+        let (message, _, _) = createFileMessageWithAssetId(in: conversation, otrKey: key, sha: sha)!
+
+        let request = sut.nextRequest()
+        let response = ZMTransportResponse(imageData: encryptedData, httpStatus: 200, transportSessionError: .none, headers: [:])
+
+        // when
+        request?.complete(with: response)
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+
+        // then
+        XCTAssertEqual(message.fileMessageData?.transferState.rawValue, ZMFileTransferState.downloaded.rawValue)
+    }
+
+    func testThatItMarksDownloadAsFailedIfCannotDownload_PermanentError_V3() {
+        // given
+        let (message, _, _) = createFileMessageWithAssetId(in: conversation)!
+        let request = sut.nextRequest()
+        let response = ZMTransportResponse(payload: [] as ZMTransportData, httpStatus: 404, transportSessionError: .none)
+
+        // when
+        request?.complete(with: response)
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+
+        // then
+        XCTAssertEqual(message.fileMessageData?.transferState.rawValue, ZMFileTransferState.failedDownload.rawValue)
+    }
+
+    func testThatItMarksDownloadAsFailedIfCannotDownload_TemporaryError_V3() {
+        // given
+        let (message, _, _) = createFileMessageWithAssetId(in: conversation)!
+        let request = sut.nextRequest()
+        let response = ZMTransportResponse(payload: [] as ZMTransportData, httpStatus: 500, transportSessionError: .none)
+
+        // when
+        request?.complete(with: response)
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+
+        // then
+        XCTAssertEqual(message.fileMessageData?.transferState.rawValue, ZMFileTransferState.failedDownload.rawValue)
+    }
+
+    func testThatItMarksDownloadAsFailedIfCannotDownload_CannotDecrypt_V3() {
+        // given
+        let (message, _, _) = createFileMessageWithAssetId(in: conversation)!
+        let request = sut.nextRequest()
+        let response = ZMTransportResponse(payload: [] as ZMTransportData, httpStatus: 200, transportSessionError: .none)
+
+        // when
+        request?.complete(with: response)
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+
+        // then
+        XCTAssertEqual(message.fileMessageData?.transferState.rawValue, ZMFileTransferState.failedDownload.rawValue)
+    }
+
+    func testThatItDoesNotMarkDownloadAsFailedWhenNotDownloading_V3() {
+        // given
+        let (message, _, _) = createFileMessageWithAssetId(in: conversation)!
+        let request = sut.nextRequest()
+        let response = ZMTransportResponse(payload: [] as ZMTransportData, httpStatus: 500, transportSessionError: .none)
+
+        // when
+        message.transferState = .uploaded
+        request?.complete(with: response)
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+
+        // then
+        XCTAssertEqual(message.fileMessageData?.transferState.rawValue, ZMFileTransferState.uploaded.rawValue)
+    }
+
+    func testThatItUpdatesFileDownloadProgress_V3() {
+        // given
+        let expectedProgress: Float = 0.5
+        let (message, _, _) = createFileMessageWithAssetId(in: conversation)!
+        let request = sut.nextRequest()
+
+        XCTAssertEqual(message.fileMessageData?.progress, 0)
+
+        // when
+        request?.updateProgress(expectedProgress)
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+
+        // then
+        XCTAssertEqual(message.fileMessageData?.progress, expectedProgress)
+    }
+
+    func testThatItSendsTheNotificationIfSuccessfulDownloadAndDecryption_V3() {
+
+        // given
+        let plainTextData = Data.secureRandomData(length: 500)
+        let key = Data.randomEncryptionKey()
+        let encryptedData = plainTextData.zmEncryptPrefixingPlainTextIV(key: key)
+        let sha = encryptedData.zmSHA256Digest()
+
+        let _ = createFileMessageWithAssetId(in: conversation, otrKey: key, sha: sha)!
+
+        let notificationExpectation = self.expectation(description: "Notification fired")
+
+        let _ = NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: AssetDownloadRequestStrategyNotification.downloadFinishedNotificationName), object: nil, queue: .main) { notification in
+            XCTAssertNotNil(notification.userInfo![AssetDownloadRequestStrategyNotification.downloadStartTimestampKey])
+            notificationExpectation.fulfill()
+        }
+
+        let request = sut.nextRequest()
+        request?.markStartOfUploadTimestamp()
+        let response = ZMTransportResponse(imageData: encryptedData, httpStatus: 200, transportSessionError: .none, headers: [:])
+
+        // when
+        request?.complete(with: response)
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+
+        // then
+        XCTAssertTrue(waitForCustomExpectations(withTimeout: 0.5))
+
+    }
+
+    func testThatItSendsTheNotificationIfCannotDownload_V3() {
+        // given
+
+        let notificationExpectation = self.expectation(description: "Notification fired")
+
+        let _ = NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: AssetDownloadRequestStrategyNotification.downloadFailedNotificationName), object: nil, queue: .main) { notification in
+            XCTAssertNotNil(notification.userInfo![AssetDownloadRequestStrategyNotification.downloadStartTimestampKey])
+            notificationExpectation.fulfill()
+        }
+
+        let _ = createFileMessageWithAssetId(in: conversation)!
+        let request = sut.nextRequest()
+        request?.markStartOfUploadTimestamp()
+        let response = ZMTransportResponse(payload: [] as ZMTransportData, httpStatus: 404, transportSessionError: .none)
+        
+        // when
+        request?.complete(with: response)
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+        
+        // then
+        XCTAssertTrue(waitForCustomExpectations(withTimeout: 0.5))
+    }
+}
+
+// MARK : - Download Cancellation
+
+extension AssetV3DownloadRequestStrategyTests {
+
+    func testThatItInformsTheTaskCancellationProviderToCancelARequestForAnAssetMessageWhenItReceivesTheNotification_V3() {
+        // given
+        let (message, _, _) = createFileMessageWithAssetId(in: conversation)!
+        XCTAssertNotNil(message.objectID)
+
+        // given the task has been created
+        guard let request = sut.nextRequest() else { return XCTFail("No request created") }
+
+        request.callTaskCreationHandlers(withIdentifier: 42, sessionIdentifier: name!)
+        XCTAssertTrue(syncMOC.saveOrRollback())
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+        let identifier = message.associatedTaskIdentifier
+        XCTAssertNotNil(identifier)
+
+        // when the transfer is cancelled
+        message.fileMessageData?.cancelTransfer()
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+
+        // then the cancellation provider should be informed to cancel the request
+        XCTAssertEqual(cancellationProvider.cancelledIdentifiers.count, 1)
+        let cancelledIdentifier = cancellationProvider.cancelledIdentifiers.first
+        XCTAssertEqual(cancelledIdentifier, identifier)
+
+        // It should nil-out the identifier as it has been cancelled
+        XCTAssertNil(message.associatedTaskIdentifier)
+    }
+    
+}
+
---
+++ Tests/Source/AssetV3PreviewDownloadRequestStrategyTests.swift
@@ -0,0 +1,247 @@
+//
+// Wire
+// Copyright (C) 2016 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+
+import Foundation
+import ZMCDataModel
+import ZMTesting
+@testable import WireMessageStrategy
+
+
+private let testDataURL = Bundle(for: AssetV3PreviewDownloadRequestStrategyTests.self).url(forResource: "Lorem Ipsum", withExtension: "txt")!
+
+
+class AssetV3PreviewDownloadRequestStrategyTests: MessagingTest {
+
+    var authStatus: MockClientRegistrationStatus!
+    var sut: AssetV3PreviewDownloadRequestStrategy!
+    var conversation: ZMConversation!
+
+    typealias PreviewMeta = (otr: Data, sha: Data, assetId: String, token: String)
+
+    override func setUp() {
+        super.setUp()
+        authStatus = MockClientRegistrationStatus()
+        sut = AssetV3PreviewDownloadRequestStrategy(
+            authStatus: authStatus,
+            managedObjectContext: syncMOC
+        )
+        conversation = createConversation()
+    }
+
+    fileprivate func createConversation() -> ZMConversation {
+        let conversation = ZMConversation.insertNewObject(in: syncMOC)
+        conversation.remoteIdentifier = UUID.create()
+        return conversation
+    }
+
+    fileprivate func createMessage(in conversation: ZMConversation) -> (message: ZMAssetClientMessage, assetId: String, assetToken: String)? {
+
+        let message = conversation.appendMessage(with: ZMFileMetadata(fileURL: testDataURL), version3: true) as! ZMAssetClientMessage
+        let (otrKey, sha) = (Data.randomEncryptionKey(), Data.randomEncryptionKey())
+        let (assetId, token) = (UUID.create().transportString(), UUID.create().transportString())
+
+        // TODO: We should replace this manual update with inserting a v3 asset as soon as we have sending support
+        let uploaded = ZMGenericMessage.genericMessage(
+            withUploadedOTRKey: otrKey,
+            sha256: sha,
+            messageID: message.nonce.transportString(),
+            expiresAfter: NSNumber(value: conversation.messageDestructionTimeout)
+        )
+
+        guard let uploadedWithId = uploaded.updated(withAssetId: assetId, token: token) else {
+            XCTFail("Failed to update asset")
+            return nil
+        }
+
+        message.add(uploadedWithId)
+        message.fileMessageData?.transferState = .downloading
+
+        prepareDownload(of: message)
+        return (message, assetId, token)
+    }
+
+    func prepareDownload(of message: ZMAssetClientMessage) {
+        syncMOC.saveOrRollback()
+
+        sut.contextChangeTrackers.forEach { tracker in
+            tracker.objectsDidChange(Set(arrayLiteral: message))
+        }
+
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+    }
+
+    func createPreview(with nonce: String, otr: Data = .randomEncryptionKey(), sha: Data = .randomEncryptionKey()) -> (ZMGenericMessage, PreviewMeta) {
+        let (assetId, token) = (UUID.create().transportString(), UUID.create().transportString())
+        let assetBuilder = ZMAsset.builder()
+        let previewBuilder = ZMAssetPreview.builder()
+        let remoteBuilder = ZMAssetRemoteData.builder()
+
+        _ = remoteBuilder?.setOtrKey(otr)
+        _ = remoteBuilder?.setSha256(sha)
+        _ = remoteBuilder?.setAssetId(assetId)
+        _ = remoteBuilder?.setAssetToken(token)
+        _ = previewBuilder?.setSize(512)
+        _ = previewBuilder?.setMimeType("image/jpg")
+        _ = previewBuilder?.setRemote(remoteBuilder)
+        _ = assetBuilder?.setPreview(previewBuilder)
+
+        let previewMeta = (otr, sha, assetId, token)
+        return (ZMGenericMessage.genericMessage(asset: assetBuilder!.build(), messageID: nonce), previewMeta)
+    }
+
+    func testThatItGeneratesNoRequestsIfTheStatusIsEmpty() {
+        XCTAssertNil(sut.nextRequest())
+    }
+
+    func testThatItGeneratesNoRequestsIfNotAuthenticated() {
+        // given
+        authStatus.mockClientIsReadyForRequests = false
+        let _ = createMessage(in: conversation)
+
+        // then
+        XCTAssertNil(sut.nextRequest())
+    }
+
+    func testThatItGeneratesNoRequestForAV3FileMessageWithPreviewThatHasNotBeenDownloadedYet_WhenNotWhitelisted() {
+        // given
+        let (message, _, _) = createMessage(in: conversation)!
+        let (previewGenericMessage, previewMeta) = createPreview(with: message.nonce.transportString())
+
+        message.add(previewGenericMessage)
+        prepareDownload(of: message)
+
+        guard let asset = message.genericAssetMessage?.assetData else { return XCTFail() }
+        XCTAssertTrue(asset.hasPreview())
+        XCTAssertTrue(asset.preview.hasRemote())
+        XCTAssertTrue(asset.preview.remote.hasAssetId())
+        XCTAssertEqual(asset.preview.remote.assetId, previewMeta.assetId)
+        XCTAssertFalse(message.hasDownloadedImage)
+        XCTAssertEqual(message.version, 3)
+        XCTAssertNotNil(message.fileMessageData)
+
+        // then
+        XCTAssertNil(sut.nextRequest())
+    }
+
+    func testThatItGeneratesARequestForAV3FileMessageWithPreviewThatHasNotBeenDownloadedYet() {
+        // given
+        let (message, _, _) = createMessage(in: conversation)!
+        let (previewGenericMessage, previewMeta) = createPreview(with: message.nonce.transportString())
+
+        message.add(previewGenericMessage)
+        prepareDownload(of: message)
+
+        guard let asset = message.genericAssetMessage?.assetData else { return XCTFail() }
+        XCTAssertTrue(asset.hasPreview())
+        XCTAssertTrue(asset.preview.hasRemote())
+        XCTAssertTrue(asset.preview.remote.hasAssetId())
+        XCTAssertEqual(asset.preview.remote.assetId, previewMeta.assetId)
+        XCTAssertFalse(message.hasDownloadedImage)
+        XCTAssertEqual(message.version, 3)
+        XCTAssertNotNil(message.fileMessageData)
+
+        // when
+        message.requestImageDownload()
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+
+        guard let request = sut.nextRequest() else { return XCTFail("No request generated") }
+
+        // then
+        XCTAssertEqual(request.path, "/assets/v3/\(previewMeta.assetId)")
+        XCTAssertEqual(request.method, .methodGET)
+    }
+
+    func testThatItDoesNotGenerateARequestForAV3FileMessageWithPreviewTwice() {
+        // given
+        let (message, _, _) = createMessage(in: conversation)!
+        let (previewGenericMessage, previewMeta) = createPreview(with: message.nonce.transportString())
+
+        message.add(previewGenericMessage)
+        prepareDownload(of: message)
+
+        // when
+        message.requestImageDownload()
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+
+        // then
+        guard let request = sut.nextRequest() else { return XCTFail("No request generated") }
+        XCTAssertEqual(request.path, "/assets/v3/\(previewMeta.assetId)")
+        XCTAssertEqual(request.method, .methodGET)
+
+        // when
+        message.requestImageDownload()
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+
+        // then
+        XCTAssertNil(sut.nextRequest())
+    }
+
+    func testThatItDoesNotGenerateARequestForAV3FileMessageWithPreviewThatAlreadyHasBeenDownloaded() {
+        // given
+        let (message, _, _) = createMessage(in: conversation)!
+        let (previewGenericMessage, _) = createPreview(with: message.nonce.transportString())
+
+        // when
+        syncMOC.zm_imageAssetCache.storeAssetData(
+            message.nonce,
+            format: .medium,
+            encrypted: false,
+            data: Data.secureRandomData(length: 512)
+        )
+
+        message.add(previewGenericMessage)
+        prepareDownload(of: message)
+        message.requestImageDownload()
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+
+        // then
+        XCTAssertTrue(message.hasDownloadedImage)
+        XCTAssertNil(sut.nextRequest())
+    }
+
+    func testThatItStoresAndDecryptsTheRawDataInTheFileCacheWhenItReceivesAResponse() {
+        // given
+        let plainTextData = Data.secureRandomData(length: 500)
+        let key = Data.randomEncryptionKey()
+        let encryptedData = plainTextData.zmEncryptPrefixingPlainTextIV(key: key)
+        let sha = encryptedData.zmSHA256Digest()
+        let (message, _, _) = createMessage(in: conversation)!
+        let (previewGenericMessage, _) = createPreview(with: message.nonce.transportString(), otr: key, sha: sha)
+
+        message.add(previewGenericMessage)
+        prepareDownload(of: message)
+
+        // when
+        message.requestImageDownload()
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+
+        guard let request = sut.nextRequest() else { return XCTFail("No request generated") }
+        let response = ZMTransportResponse(imageData: encryptedData, httpStatus: 200, transportSessionError: nil, headers: nil)
+
+        request.complete(with: response)
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+
+        // then
+
+        let data = syncMOC.zm_imageAssetCache.assetData(message.nonce, format: .medium, encrypted: false)
+        XCTAssertEqual(data, plainTextData)
+        XCTAssertEqual(message.fileMessageData!.previewData, plainTextData)
+    }
+
+}
