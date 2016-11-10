cd876ffaef8fcc6bb6f6c5b6a06fca375c7e7391
Gerasimenko Mike <github@gerasimenko.me>
2016-11-10 16:00:34 +0100

    Disable CallKit for the release

--- Wire-iOS/Sources/AppController.m
+++ Wire-iOS/Sources/AppController.m
@@ -369,7 +369,7 @@ NSString *const ZMUserSessionDidBecomeAvailableNotification = @"ZMUserSessionDid
     (void)[Settings sharedSettings];
     
     BOOL callKitSupported = ([CXCallObserver class] != nil) && !TARGET_IPHONE_SIMULATOR;
-    BOOL callKitDisabled = [[Settings sharedSettings] disableCallKit];
+    BOOL callKitDisabled = YES;//[[Settings sharedSettings] disableCallKit];
     
     [ZMUserSession setUseCallKit:callKitSupported && !callKitDisabled];
     
