47625ce625c38326b233a896761ade3ee7615fda
Mike Gerasimenko <github@gerasimenko.me>
2016-11-18 19:43:55 +0100

    Open contact support in Safari (#410)
    * Open contact support in Safari
    
    * Use user-defined browser

--- Wire-iOS/Sources/UserInterface/Settings/CellDescriptors/SettingsCellDescriptorFactory.swift
+++ Wire-iOS/Sources/UserInterface/Settings/CellDescriptors/SettingsCellDescriptorFactory.swift
@@ -298,7 +298,8 @@ import Foundation
         }, previewGenerator: .none)
         
         let contactButton = SettingsExternalScreenCellDescriptor(title: "self.help_center.contact_support".localized, isDestructive: false, presentationStyle: .modal, presentationAction: { _ in
-            return BrowserViewController(url: NSURL.wr_askSupport().wr_URLByAppendingLocaleParameter() as URL!)
+            NSURL.wr_askSupport().wr_URLByAppendingLocaleParameter().open()
+            return .none
         }, previewGenerator: .none)
         
         let helpSection = SettingsSectionDescriptor(cellDescriptors: [supportButton, contactButton])
