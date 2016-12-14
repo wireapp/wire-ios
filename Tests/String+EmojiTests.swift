4b7faaa6258bc795b9c4471f1488da701beecf7f
Silvan Dähn <silvandaehn@me.com>
2016-12-14 17:37:48 +0100

    Add extension on String to detect Emoji

---
+++ Tests/String+EmojiTests.swift
@@ -0,0 +1,45 @@
+//
+// Wire
+// Copyright (C) 2016 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+
+import XCTest
+
+
+class String_EmojiTests: XCTestCase {
+
+    func testThatItDetectsAnEmoji() {
+        XCTAssertTrue("🍔".containsEmoji)
+    }
+
+    func testThatItDetectsMultipleEmoji() {
+        XCTAssertTrue("🍔😜🌮🎉🍕".containsEmoji)
+    }
+
+    func testThatItDetectsAnEmojiIfItIsContainedInText() {
+        XCTAssertTrue("abcdefghijklmnopqrstuv🎉wxyz_1234567890-=+'\\/`~".containsEmoji)
+    }
+
+    func testThatItDoesNotDetectAnEmojiIfThereIsNone() {
+        XCTAssertFalse(" abcdefghijklmnopqrstuvwxyz_1234567890-=+'\\/`~".containsEmoji)
+    }
+
+    func testThatItDoesNotDetectAnEmojiIfThereIsNone_EmptyString() {
+        XCTAssertFalse("".containsEmoji)
+    }
+
+}
