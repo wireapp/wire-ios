25139214b0f61cd9941384fb55881116f3878f92
Marco <marcoconti83@gmail.com>
2016-12-15 18:35:27 +0100

    Feature/categorization in core data (#97)
    * WIP
    
    * Store categories in Core Data
    
    * Test fixture for CD migration
    
    * Add database migration test for 2.25
    
    * Pass a list of categories to fetch
    
    * Update message category cache every time the message is updated from an event

--- Source/Model/Message/ZMMessage+Categorization.swift
+++ Source/Model/Message/ZMMessage+Categorization.swift
@@ -19,37 +19,6 @@
 import Foundation
 
 
-/// Type of content in a message
-public struct MessageCategory : OptionSet {
-    
-    public let rawValue: UInt64
-    
-    public static let none = MessageCategory(rawValue: 0)
-    public static let undefined = MessageCategory(rawValue: 1 << 0)
-    public static let text = MessageCategory(rawValue: 1 << 1)
-    public static let link = MessageCategory(rawValue: 1 << 2)
-    public static let image = MessageCategory(rawValue: 1 << 3)
-    public static let GIF = MessageCategory(rawValue: 1 << 4)
-    public static let file = MessageCategory(rawValue: 1 << 5)
-    public static let audio = MessageCategory(rawValue: 1 << 6)
-    public static let video = MessageCategory(rawValue: 1 << 7)
-    public static let location = MessageCategory(rawValue: 1 << 8)
-    public static let liked = MessageCategory(rawValue: 1 << 9)
-    public static let knock = MessageCategory(rawValue: 1 << 10)
-    public static let systemMessage = MessageCategory(rawValue: 1 << 11)
-    
-    public init(rawValue: UInt64) {
-        self.rawValue = rawValue
-    }
-}
-
-extension MessageCategory : Hashable {
-    
-    public var hashValue : Int {
-        return self.rawValue.hashValue
-    }
-}
-
 extension ZMMessage {
     
     /// Type of content present in the message
@@ -63,8 +32,61 @@ extension ZMMessage {
         return category.union(self.likedCategory)
     }
     
+    /// Obj-c compatible function
+    @objc public func updateCategoryCache() {
+        _ = self.storeCategoryCache()
+    }
+    
+    /// A cached version of the cateogry. The getter will recalculate the category if not set already
+    public var cachedCategory : MessageCategory {
+        
+        get {
+            self.willAccessValue(forKey: ZMMessageCachedCategoryKey)
+            let value = self.primitiveValue(forKey: ZMMessageCachedCategoryKey) as! NSNumber
+            self.didAccessValue(forKey: ZMMessageCachedCategoryKey)
+            
+            var category = MessageCategory(rawValue: value.int32Value)
+            if category == .none {
+                category = self.storeCategoryCache()
+            }
+            return category
+        }
+        
+        set {
+            self.willChangeValue(forKey: ZMMessageCachedCategoryKey)
+            self.setPrimitiveValue(NSNumber(value: newValue.rawValue), forKey: ZMMessageCachedCategoryKey)
+            self.didChangeValue(forKey: ZMMessageCachedCategoryKey)
+        }
+    }
+    
+    /// Calculate and save category in the category cache field. If no category
+    /// is passed, it will compute it before storing it.
+    /// - returns: the category that was stored
+    public func storeCategoryCache(category: MessageCategory? = nil) -> MessageCategory {
+        let categoryToStore = category ?? self.category
+        self.cachedCategory = categoryToStore
+        return categoryToStore
+    }
+    
+    /// Sorted fetch request by category. It will match a Core Data object if the intersection of the Core Data value and ANY of the passed
+    /// in categories is matching that category (in other words, the Core Data value can have more bits set that a certain category and it will
+    /// still match).
+    public static func fetchRequestMatching(categories: Set<MessageCategory>) -> NSFetchRequest<NSFetchRequestResult> {
+        let predicates = categories.map {
+            return NSPredicate(format: "(%K & %d) = %d", ZMMessageCachedCategoryKey, $0.rawValue, $0.rawValue)
+        }
+        return self.sortedFetchRequest(with: NSCompoundPredicate(orPredicateWithSubpredicates: predicates))!
+    }
+    
+}
+
+// MARK: - Categories from specific content
+let linkParser = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+
+extension ZMMessage {
+
     /// Category according only to content (excluding likes)
-    private var categoryFromContent : MessageCategory {
+    fileprivate var categoryFromContent : MessageCategory {
         
         let category = [self.textCategory,
                         self.imageCategory,
@@ -79,12 +101,7 @@ extension ZMMessage {
         }
         return category
     }
-}
 
-// MARK: - Individual categories
-let linkParser = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
-
-extension ZMMessage {
     
     fileprivate var imageCategory : MessageCategory {
         guard let imageData = self.imageMessageData else {
@@ -159,5 +176,37 @@ extension ZMMessage {
             return .none
         }
         return .systemMessage
+    }
+}
+
+// MARK: - Categories
+/// Type of content in a message
+public struct MessageCategory : OptionSet {
+    
+    public let rawValue: Int32
+    
+    public static let none = MessageCategory(rawValue: 0)
+    public static let undefined = MessageCategory(rawValue: 1 << 0)
+    public static let text = MessageCategory(rawValue: 1 << 1)
+    public static let link = MessageCategory(rawValue: 1 << 2)
+    public static let image = MessageCategory(rawValue: 1 << 3)
+    public static let GIF = MessageCategory(rawValue: 1 << 4)
+    public static let file = MessageCategory(rawValue: 1 << 5)
+    public static let audio = MessageCategory(rawValue: 1 << 6)
+    public static let video = MessageCategory(rawValue: 1 << 7)
+    public static let location = MessageCategory(rawValue: 1 << 8)
+    public static let liked = MessageCategory(rawValue: 1 << 9)
+    public static let knock = MessageCategory(rawValue: 1 << 10)
+    public static let systemMessage = MessageCategory(rawValue: 1 << 11)
+    
+    public init(rawValue: Int32) {
+        self.rawValue = rawValue
+    }
+}
+
+extension MessageCategory : Hashable {
+    
+    public var hashValue : Int {
+        return self.rawValue.hashValue
     }
 }
