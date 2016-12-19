73876d53c62769f521176b7d6b3d55db7c3788de
Florian Morel <florian.morel@wire.com>
2016-12-19 12:32:24 +0100

    Fix file header

--- wire-ios-share-engineTests/OperationLoopTests.swift
+++ wire-ios-share-engineTests/OperationLoopTests.swift
@@ -1,9 +1,19 @@
 //
-//  OperationLoopTests.swift
-//  wire-ios-share-engine
+// Wire
+// Copyright (C) 2016 Wire Swiss GmbH
 //
-//  Created by Florian Morel on 12/8/16.
-//  Copyright © 2016 com.wire. All rights reserved.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
 import Foundation
