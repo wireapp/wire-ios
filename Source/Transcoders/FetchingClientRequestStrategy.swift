b3961f0b89b552be044dc754a6f55081d181fd8f
Florian Morel <florian.morel@wire.com>
2016-12-19 12:15:35 +0100

    Fetching client strategy

---
+++ Source/Transcoders/FetchingClientRequestStrategy.swift
@@ -0,0 +1,201 @@
+// Wire
+// Copyright (C) 2016 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+import ZMCSystem
+import ZMTransport
+import ZMUtilities
+import Cryptobox
+import ZMCDataModel
+import WireRequestStrategy
+
+private let zmLog = ZMSLog(tag: "fetchClientRS")
+
+
+public let ZMNeedsToUpdateUserClientsNotificationName = "ZMNeedsToUpdateUserClientsNotification"
+public let ZMNeedsToUpdateUserClientsNotificationUserObjectIDKey = "userObjectID"
+
+public extension ZMUser {
+    
+    func fetchUserClients() {
+        NotificationCenter.default.post(
+            name: Notification.Name(rawValue: ZMNeedsToUpdateUserClientsNotificationName),
+            object: nil,
+            userInfo: [ZMNeedsToUpdateUserClientsNotificationUserObjectIDKey: objectID])
+    }
+}
+
+@objc
+public final class FetchingClientRequestStrategy : ZMObjectSyncStrategy, ZMObjectStrategy, ZMUpstreamTranscoder, ZMSingleRequestTranscoder {
+
+    fileprivate(set) var fetchAllClientsSync: ZMSingleRequestSync! = nil
+
+    weak var clientRegistrationStatus: ClientRegistrationDelegate?
+    
+    fileprivate(set) var userClientsObserverToken: NSObjectProtocol!
+    fileprivate(set) var userClientsSync: ZMRemoteIdentifierObjectSync!
+    
+    fileprivate var insertSyncFilter: NSPredicate {
+        return NSPredicate { [unowned self] object, _ -> Bool in
+            guard let client = object as? UserClient else { return false }
+            return client.user == ZMUser.selfUser(in: self.managedObjectContext)
+        }
+    }
+    
+    public init(clientRegistrationStatus:ClientRegistrationDelegate,
+                context: NSManagedObjectContext)
+    {
+        self.clientRegistrationStatus = clientRegistrationStatus
+        
+        super.init(managedObjectContext: context)
+        
+        self.userClientsSync = ZMRemoteIdentifierObjectSync(transcoder: self, managedObjectContext: self.managedObjectContext)
+        
+        self.userClientsObserverToken = NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: ZMNeedsToUpdateUserClientsNotificationName), object: nil, queue: .main) { [unowned self] note in
+            
+            let objectID = note.userInfo?[ZMNeedsToUpdateUserClientsNotificationUserObjectIDKey] as? NSManagedObjectID
+            self.managedObjectContext.performGroupedBlock {
+                guard let optionalUser = try? objectID.flatMap(self.managedObjectContext.existingObject(with:)), let user = optionalUser as? ZMUser  else { return }
+                self.userClientsSync.setRemoteIdentifiersAsNeedingDownload(Set(arrayLiteral: user.remoteIdentifier!))
+                RequestAvailableNotification.notifyNewRequestsAvailable(self)
+            }
+        }
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self.userClientsObserverToken)
+    }
+    
+    public func nextRequest() -> ZMTransportRequest? {
+        guard let clientRegistrationStatus = self.clientRegistrationStatus else {
+                return nil
+        }
+        
+        if !clientRegistrationStatus.clientIsReadyForRequests {
+            return nil
+        }
+        
+        return userClientsSync.nextRequest()
+    }
+    
+    //we don;t use this method but it's required by ZMObjectStrategy protocol
+    public var requestGenerators: [ZMRequestGenerator] {
+        return []
+    }
+    
+    public var contextChangeTrackers: [ZMContextChangeTracker] {
+        return []
+    }
+    
+    public func shouldProcessUpdatesBeforeInserts() -> Bool {
+        return false
+    }
+    
+    public func request(for sync: ZMSingleRequestSync!) -> ZMTransportRequest! {
+        return nil
+    }
+    
+    public func request(forUpdating managedObject: ZMManagedObject, forKeys keys: Set<String>) -> ZMUpstreamRequest? {
+        return nil
+    }
+    
+    public func request(forInserting managedObject: ZMManagedObject, forKeys keys: Set<String>?) -> ZMUpstreamRequest? {
+        return nil
+    }
+    
+    public func shouldRetryToSyncAfterFailed(toUpdate managedObject: ZMManagedObject, request upstreamRequest: ZMUpstreamRequest, response: ZMTransportResponse, keysToParse: Set<String>) -> Bool {
+        return false
+    }
+    
+    public func didReceive(_ response: ZMTransportResponse!, forSingleRequest sync: ZMSingleRequestSync!) {
+        //no-op
+    }
+    
+    public func updateInsertedObject(_ managedObject: ZMManagedObject, request upstreamRequest: ZMUpstreamRequest, response: ZMTransportResponse) {
+        //no-op
+    }
+    
+    /// Returns whether synchronization of this object needs additional requests
+    public func updateUpdatedObject(_ managedObject: ZMManagedObject, requestUserInfo: [AnyHashable: Any]?, response: ZMTransportResponse, keysToParse: Set<String>) -> Bool {
+        return false
+    }
+
+    // Should return the objects that need to be refetched from the BE in case of upload error
+    public func objectToRefetchForFailedUpdate(of managedObject: ZMManagedObject) -> ZMManagedObject? {
+        return nil
+    }
+    
+    public var isSlowSyncDone: Bool {
+        return true
+    }
+    
+    public func setNeedsSlowSync() {
+        //no op
+    }
+    
+    public func processEvents(_ events: [ZMUpdateEvent], liveEvents: Bool, prefetchResult: ZMFetchRequestBatchResult?) {
+        //no-op
+    }
+}
+
+// Used to fetch clients of particluar user when ui asks for them
+extension FetchingClientRequestStrategy: ZMRemoteIdentifierObjectTranscoder {
+    
+    public func maximumRemoteIdentifiersPerRequest(for sync: ZMRemoteIdentifierObjectSync!) -> UInt {
+        return 1
+    }
+    
+    public func request(for sync: ZMRemoteIdentifierObjectSync!, remoteIdentifiers identifiers: Set<UUID>!) -> ZMTransportRequest! {
+        
+        guard let userId = (identifiers.first as NSUUID?)?.transportString() else { return nil }
+        
+        //GET /users/<user-id>/clients
+        let path = NSString.path(withComponents: ["users", "\(userId)", "clients"])
+        return ZMTransportRequest(path: path, method: .methodGET, payload: nil)
+    }
+    
+    public func didReceive(_ response: ZMTransportResponse!, remoteIdentifierObjectSync sync: ZMRemoteIdentifierObjectSync!, forRemoteIdentifiers remoteIdentifiers: Set<UUID>!) {
+        
+        guard let identifier = remoteIdentifiers.first,
+            let user = ZMUser(remoteID: identifier, createIfNeeded: true, in: managedObjectContext),
+            let selfClient = ZMUser.selfUser(in: managedObjectContext).selfClient()
+            else { return }
+        
+        // Create clients from the response
+        guard let arrayPayload = response.payload?.asArray() else { return }
+        let clients: [UserClient] = arrayPayload.flatMap {
+            guard let dict = $0 as? [String: AnyObject], let identifier = dict["id"] as? String else { return nil }
+            let client = UserClient.fetchUserClient(withRemoteId: identifier, forUser:user, createIfNeeded: true)
+            client?.deviceClass = dict["class"] as? String
+            return client
+        }
+        
+        // Remove clients that have not been included in the response
+        let deletedClients = Set(user.clients).subtracting(Set(clients))
+        deletedClients.forEach {
+            $0.deleteClientAndEndSession()
+        }
+        
+        // Add clients without a session to missed clients
+        let newClients = clients.filter { !$0.hasSessionWithSelfClient }
+        guard newClients.count > 0 else { return }
+        selfClient.missesClients(Set(newClients))
+        
+        // add missing clients to ignored clients
+        selfClient.addNewClientsToIgnored(Set(newClients))
+    }
+}
