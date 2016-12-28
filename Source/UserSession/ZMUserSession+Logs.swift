85a377fe121c3023a79d29770168be819c04b5e5
Marco Conti <marcoconti83@gmail.com>
2016-12-28 19:08:42 +0100

    Add function to register save failure handler

--- Source/UserSession/ZMUserSession+Logs.swift
+++ Source/UserSession/ZMUserSession+Logs.swift
@@ -18,6 +18,7 @@
 
 
 import Foundation
+import ZMCDataModel
 
 extension ZMUserSession {
 
@@ -26,6 +27,55 @@ extension ZMUserSession {
         if let tags = ProcessInfo.processInfo.environment["ZMLOG_TAGS"] {
             for tag in (tags.characters.split { $0 == "," }.map { String($0) }) {
                 ZMSLog.set(level: .debug, tag: tag)
+            }
+        }
+    }
+}
+
+// MARK: - Error on context save debugging
+
+public enum ContextType {
+    case UI
+    case Sync
+    case Search
+    case Other
+}
+
+extension NSManagedObjectContext {
+    
+    var type : ContextType {
+        if self.zm_isSyncContext {
+            return .Sync
+        }
+        if self.zm_isUserInterfaceContext {
+            return .UI
+        }
+        if self.zm_isSearchContext {
+            return .Search
+        }
+        return .Other
+    }
+}
+
+extension ZMUserSession {
+    
+    public typealias SaveFailureCallback = (_ metadata: [String: Any], _ type: ContextType, _ error: NSError, _ userInfo: [String: Any]) -> ()
+    
+    /// Register a handle for monitoring when one of the manage object contexts fails
+    /// to save and is rolled back
+    func registerForSaveFailure(handler: @escaping SaveFailureCallback) {
+        self.managedObjectContext.errorOnSaveCallback = { (context, error) in
+            let metadata : [String: Any] = context.persistentStoreCoordinator!.persistentStores[0].metadata as [String: Any]
+            let type = context.type
+            let userInfo : [String: Any] = context.userInfo.asDictionary() as! [String: Any]
+            handler(metadata, type, error, userInfo)
+        }
+        self.syncManagedObjectContext.errorOnSaveCallback = { (context, error) in
+            let metadata : [String: Any] = context.persistentStoreCoordinator!.persistentStores[0].metadata as [String: Any]
+            let type = context.type
+            let userInfo : [String: Any] = context.userInfo.asDictionary() as! [String: Any]
+            self.managedObjectContext.perform {
+                handler(metadata, type, error, userInfo)
             }
         }
     }
