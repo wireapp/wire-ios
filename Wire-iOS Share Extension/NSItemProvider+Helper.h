59d872e2f46ade48e1dc4b094a1227e1fae0973b
Silvan Dähn <silvandaehn@me.com>
2017-01-17 10:06:25 +0100

    Add missing copyright header

--- Wire-iOS Share Extension/NSItemProvider+Helper.h
+++ Wire-iOS Share Extension/NSItemProvider+Helper.h
@@ -1,9 +1,19 @@
 //
-//  NSItemProvider+Helper.h
-//  Wire-iOS
+// Wire
+// Copyright (C) 2016 Wire Swiss GmbH
 //
-//  Created by Jacob on 11/11/16.
-//  Copyright © 2016 Zeta Project Germany GmbH. All rights reserved.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
 #import <Foundation/Foundation.h>
