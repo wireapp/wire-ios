a0aa8107cab1f6e5bacaaf2db00f2c2b5bc62df7
Sabine Geithner <sabine@wire.com>
2017-02-17 15:50:58 +0100

    Various fixes notification center (#185)
    * snapshot primitive values
    
    * Make sure we don’t end up in a loop when a notification causes a save

--- Source/Notifications/SnapshotCenter.swift
+++ Source/Notifications/SnapshotCenter.swift
@@ -61,11 +61,12 @@ public class SnapshotCenter {
         let attributes = Array(object.entity.attributesByName.keys)
         let relationShips = object.entity.relationshipsByName
         
-        let attributesDict = attributes.mapToDictionaryWithOptionalValue{object.value(forKey: $0) as? NSObject}
+        let attributesDict = attributes.mapToDictionaryWithOptionalValue{object.primitiveValue(forKey: $0) as? NSObject}
         let relationshipsDict : [String : Int] = relationShips.mapKeysAndValues(keysMapping: {$0}, valueMapping: { (key, relationShipDescription) in
             guard relationShipDescription.isToMany else { return nil}
-            return (object.value(forKey: key) as? Countable)?.count
+            return (object.primitiveValue(forKey: key) as? Countable)?.count
         })
+        
         return Snapshot(attributes : attributesDict, toManyRelationships : relationshipsDict)
     }
     
