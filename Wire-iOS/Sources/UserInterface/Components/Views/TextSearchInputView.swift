9edb1c500280591b2554e987aba2258c05e9def5
Silvan Daehn <silvandaehn@me.com>
2017-02-20 14:43:24 +0100

    Fix search spinner design (#726)
    * Add new search loading spinner design
    
    * Remove artificial debug delay
    
    * Rerecord snapshots
    
    * Revert "Rerecord snapshots"
    
    This reverts commit ebb2f898229efbe0dd101f0ced9a7f30e6751c44.
    
    * Fix placeholder alignment
    
    * Adjust padding
    
    * Fix constraints

--- Wire-iOS/Sources/UserInterface/Components/Views/TextSearchInputView.swift
+++ Wire-iOS/Sources/UserInterface/Components/Views/TextSearchInputView.swift
@@ -31,6 +31,8 @@ public final class TextSearchInputView: UIView {
     public let searchInput = UITextField()
     public let placeholderLabel = UILabel()
     public let cancelButton = IconButton.iconButtonDefault()
+
+    private let spinner = ProgressSpinner()
     
     public weak var delegate: TextSearchInputViewDelegate?
     public var query: String = "" {
@@ -43,6 +45,12 @@ public final class TextSearchInputView: UIView {
     public var placeholderString: String = "" {
         didSet {
             self.placeholderLabel.text = placeholderString
+        }
+    }
+
+    var isLoading: Bool = false {
+        didSet {
+            spinner.isAnimating = isLoading
         }
     }
     
@@ -67,14 +75,16 @@ public final class TextSearchInputView: UIView {
         cancelButton.addTarget(self, action: #selector(TextSearchInputView.onCancelButtonTouchUpInside(_:)), for: .touchUpInside)
         cancelButton.isHidden = true
         cancelButton.accessibilityIdentifier = "cancel search"
-        [iconView, searchInput, cancelButton, placeholderLabel].forEach(self.addSubview)
-        
+
+        spinner.color = ColorScheme.default().color(withName: ColorSchemeColorTextDimmed, variant: .light)
+        spinner.iconSize = .tiny
+        [iconView, searchInput, cancelButton, placeholderLabel, spinner].forEach(self.addSubview)
+
         self.createConstraints()
     }
     
     private func createConstraints() {
-        
-        constrain(self, iconView, searchInput, cancelButton, placeholderLabel) { selfView, iconView, searchInput, cancelButton, placeholderLabel in
+        constrain(self, iconView, searchInput, placeholderLabel, cancelButton) { selfView, iconView, searchInput, placeholderLabel, cancelButton in
             iconView.leading == selfView.leading
             iconView.width == 48
             iconView.height >= 48
@@ -87,14 +97,24 @@ public final class TextSearchInputView: UIView {
             searchInput.leading == iconView.trailing
             searchInput.top == selfView.top
             searchInput.bottom == selfView.bottom
-            searchInput.trailing == cancelButton.leading
-            
-            placeholderLabel.edges == searchInput.edges
-            
-            cancelButton.centerY == selfView.centerY
-            cancelButton.trailing == selfView.trailing
+
+            placeholderLabel.leading == searchInput.leading
+            placeholderLabel.top == searchInput.top
+            placeholderLabel.bottom == searchInput.bottom
+            placeholderLabel.trailing == cancelButton.leading
+        }
+
+        constrain(self, searchInput, cancelButton, spinner) { view, searchInput, cancelButton, spinner in
+            cancelButton.centerY == view.centerY
+            cancelButton.trailing == view.trailing
             cancelButton.width == 48
             cancelButton.height == 48
+
+            spinner.trailing == cancelButton.leading - 6
+            spinner.centerY == cancelButton.centerY
+            spinner.width == CGFloat(ZetaIconSize.tiny.rawValue)
+
+            searchInput.trailing == spinner.leading - 6
         }
     }
     
