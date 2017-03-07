1a48d66d162b92f94dc698bd2c9e71ce483570a8
Jacob Persson <jacob.persson@gmail.com>
2017-03-07 16:42:35 +0100

    We should not answer degraded incoming calls (#303)

--- Tests/Source/Calling/VoiceChannelV3Tests.swift
+++ Tests/Source/Calling/VoiceChannelV3Tests.swift
@@ -72,7 +72,7 @@ class VoiceChannelV3Tests : MessagingTest {
         XCTAssertTrue(wireCallCenterMock!.didCallAnswerCall)
     }
     
-    func testThatItAnswers_whenTheresAnIncomingDegradedCall() {
+    func testThatItDoesntAnswer_whenTheresAnIncomingDegradedCall() {
         // given
         wireCallCenterMock?.callState = .incoming(video: false)
         conversation?.setValue(NSNumber.init(value: ZMConversationSecurityLevel.secureWithIgnored.rawValue), forKey: "securityLevel")
@@ -81,7 +81,7 @@ class VoiceChannelV3Tests : MessagingTest {
         _ = sut.join(video: false)
         
         // then
-        XCTAssertTrue(wireCallCenterMock!.didCallAnswerCall)
+        XCTAssertFalse(wireCallCenterMock!.didCallAnswerCall)
     }
     
     func testMappingFromCallStateToVoiceChannelV2State() {
