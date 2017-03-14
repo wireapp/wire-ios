921f794b5fce03841a424e8b52425f67bd4a1ce5
Gerasimenko Mike <github@gerasimenko.me>
2017-03-14 20:18:42 +0100

    Bumped dynamic type font scalers

--- WireExtensionComponents/Utilities/UIFont+DynamicType.swift
+++ WireExtensionComponents/Utilities/UIFont+DynamicType.swift
@@ -21,18 +21,18 @@ import Foundation
 extension UIFont {
     @objc internal static func wr_preferredContentSizeMultiplier(for contentSizeCategory: UIContentSizeCategory) -> CGFloat {
         switch contentSizeCategory {
-            case UIContentSizeCategory.accessibilityExtraExtraExtraLarge: return 23.0 / 16.0
-            case UIContentSizeCategory.accessibilityExtraExtraLarge:      return 22.0 / 16.0
-            case UIContentSizeCategory.accessibilityExtraLarge:           return 21.0 / 16.0
-            case UIContentSizeCategory.accessibilityLarge:                return 20.0 / 16.0
-            case UIContentSizeCategory.accessibilityMedium:               return 19.0 / 16.0
-            case UIContentSizeCategory.extraExtraExtraLarge:              return 19.0 / 16.0
-            case UIContentSizeCategory.extraExtraLarge:                   return 18.0 / 16.0
-            case UIContentSizeCategory.extraLarge:                        return 17.0 / 16.0
-            case UIContentSizeCategory.large:                             return 1.0
-            case UIContentSizeCategory.medium:                            return 15.0 / 16.0
-            case UIContentSizeCategory.small:                             return 14.0 / 16.0
-            case UIContentSizeCategory.extraSmall:                        return 13.0 / 16.0
+        case UIContentSizeCategory.accessibilityExtraExtraExtraLarge: return 26.0 / 16.0
+        case UIContentSizeCategory.accessibilityExtraExtraLarge:      return 25.0 / 16.0
+        case UIContentSizeCategory.accessibilityExtraLarge:           return 24.0 / 16.0
+        case UIContentSizeCategory.accessibilityLarge:                return 23.0 / 16.0
+        case UIContentSizeCategory.accessibilityMedium:               return 22.0 / 16.0
+        case UIContentSizeCategory.extraExtraExtraLarge:              return 22.0 / 16.0
+        case UIContentSizeCategory.extraExtraLarge:                   return 20.0 / 16.0
+        case UIContentSizeCategory.extraLarge:                        return 18.0 / 16.0
+        case UIContentSizeCategory.large:                             return 1.0
+        case UIContentSizeCategory.medium:                            return 15.0 / 16.0
+        case UIContentSizeCategory.small:                             return 14.0 / 16.0
+        case UIContentSizeCategory.extraSmall:                        return 13.0 / 16.0
         default:
             return 1.0
         }
