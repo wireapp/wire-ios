b4b61d55b6b62478437b2ee44fdb26e488a77f36
Gerasimenko Mike <github@gerasimenko.me>
2017-03-14 20:18:42 +0100

    Bumped dynamic type font scalers

--- Wire-iOS Tests/FontSchemeTests.swift
+++ Wire-iOS Tests/FontSchemeTests.swift
@@ -61,23 +61,24 @@ class FontSchemeTests: XCTestCase {
     func testThatItConvertsFontToClassyNameIfFontAdjustedExtraExtraExtraLarge() {
         var fixedFontNames: [FontSpec: String] = [:]
         
-        fixedFontNames[FontSpec(.large, .none)]     = "System-Light 29.0"
-        fixedFontNames[FontSpec(.large, .medium)]   = "System-Medium 29.0"
-        fixedFontNames[FontSpec(.large, .light)]    = "System-Light 29.0"
-        fixedFontNames[FontSpec(.large, .thin)]     = "System-Thin 29.0"
+        fixedFontNames[FontSpec(.large, .none)]     = "System-Light 33.0"
+        fixedFontNames[FontSpec(.large, .medium)]   = "System-Medium 33.0"
+        fixedFontNames[FontSpec(.large, .light)]    = "System-Light 33.0"
+        fixedFontNames[FontSpec(.large, .thin)]     = "System-Thin 33.0"
         
-        fixedFontNames[FontSpec(.normal, .none)]    = "System-Light 19.0"
-        fixedFontNames[FontSpec(.normal, .light)]   = "System-Light 19.0"
-        fixedFontNames[FontSpec(.normal, .thin)]    = "System-Thin 19.0"
-        fixedFontNames[FontSpec(.normal, .medium)]  = "System-Medium 19.0"
+        fixedFontNames[FontSpec(.normal, .none)]    = "System-Light 22.0"
+        fixedFontNames[FontSpec(.normal, .light)]   = "System-Light 22.0"
+        fixedFontNames[FontSpec(.normal, .thin)]    = "System-Thin 22.0"
+        fixedFontNames[FontSpec(.normal, .medium)]  = "System-Medium 22.0"
         
-        fixedFontNames[FontSpec(.medium, .none)]    = "System-Light 14.0"
-        fixedFontNames[FontSpec(.medium, .medium)]  = "System-Medium 14.0"
-        fixedFontNames[FontSpec(.medium, .regular)] = "System-Regular 14.0"
+        fixedFontNames[FontSpec(.medium, .none)]    = "System-Light 17.0"
+        fixedFontNames[FontSpec(.medium, .medium)]  = "System-Medium 17.0"
+        fixedFontNames[FontSpec(.medium, .regular)] = "System-Regular 17.0"
         
-        fixedFontNames[FontSpec(.small, .none)]     = "System-Light 13.0"
-        fixedFontNames[FontSpec(.small, .medium)]   = "System-Medium 13.0"
-        fixedFontNames[FontSpec(.small, .light)]    = "System-Light 13.0"
+        fixedFontNames[FontSpec(.small, .none)]     = "System-Light 15.0"
+        fixedFontNames[FontSpec(.small, .medium)]   = "System-Medium 15.0"
+        fixedFontNames[FontSpec(.small, .light)]    = "System-Light 15.0"
+        
         
         // WHEN
         var fontNames: [FontSpec: String] = [:]
