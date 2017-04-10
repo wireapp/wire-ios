4dfa758ac0b2276ee7b8ffbe392ddceec05608c9
Jacob Persson <jacob.persson@gmail.com>
2017-04-10 18:39:26 +0200

    Fix tests hanging due to a observer deadlock (#339)

--- Tests/Source/UserSession/BackgroundAPNSPingBackStatusTests.swift
+++ Tests/Source/UserSession/BackgroundAPNSPingBackStatusTests.swift
@@ -26,18 +26,22 @@ import WireMockTransport
 
 class OperationLoopNewRequestObserver {
     
+    var token : NSObjectProtocol?
     var notifications = [Notification]()
     fileprivate var notificationCenter = NotificationCenter.default
     fileprivate var newRequestNotification = "RequestAvailableNotification"
     
     init() {
-        notificationCenter.addObserver(forName: Notification.Name(rawValue: newRequestNotification), object: nil, queue: .main) { [weak self] note in
+        token = notificationCenter.addObserver(forName: Notification.Name(rawValue: newRequestNotification), object: nil, queue: .main) { [weak self] note in
             self?.notifications.append(note)
         }
     }
     
     deinit {
-        notificationCenter.removeObserver(self)
+        notifications.removeAll()
+        if let token = token {
+            notificationCenter.removeObserver(token)
+        }
     }
 }
 
