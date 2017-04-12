24417d5fbe101cedb50fdf202d2d9f3cdfba97f4
Silvan Daehn <silvandaehn@me.com>
2017-04-12 16:02:08 +0200

    Update for new data-model API (#29)
    * Update for new data-model API
    
    * Bump components

--- Sources/SharingTarget.swift
+++ Sources/SharingTarget.swift
@@ -26,10 +26,10 @@ public protocol SharingTarget {
     func appendTextMessage(_ message: String, fetchLinkPreview: Bool) -> Sendable?
     
     /// Appends an image in the conversation
-    func appendImage(_ data: Data, v3: Bool) -> Sendable?
+    func appendImage(_ data: Data) -> Sendable?
     
     /// Appends a file in the conversation
-    func appendFile(_ metaData: ZMFileMetadata, v3: Bool) -> Sendable?
+    func appendFile(_ metaData: ZMFileMetadata) -> Sendable?
     
     /// Append a location in the conversation
     func appendLocation(_ location: LocationData) -> Sendable?
