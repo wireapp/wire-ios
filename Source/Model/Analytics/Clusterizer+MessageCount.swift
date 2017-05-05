ae4b66b1fa51c746705c1d10bb5acfc970ed17b1
Silvan Daehn <silvandaehn@me.com>
2017-05-05 11:23:18 +0200

    Add more buckets to message count clusterizer (#249)

--- Source/Model/Analytics/Clusterizer+MessageCount.swift
+++ Source/Model/Analytics/Clusterizer+MessageCount.swift
@@ -22,18 +22,28 @@ extension IntegerClusterizer {
         ClusterRange(0, 100),
         ClusterRange(100, 250),
         ClusterRange(250, 500),
-        ClusterRange(500, 1000),
-        ClusterRange(1000, 2000),
-        ClusterRange(2000, 3000),
-        ClusterRange(3000, 4000),
-        ClusterRange(4000, 5000),
-        ClusterRange(5000, 7500),
-        ClusterRange(7500, 10000),
-        ClusterRange(10000, 15000),
-        ClusterRange(15000, 20000),
-        ClusterRange(20000, 30000),
-        ClusterRange(30000, 40000),
-        ClusterRange(40000, 75000)
+        ClusterRange(500, 1_000),
+        ClusterRange(1_000, 2_000),
+        ClusterRange(2_000, 3_000),
+        ClusterRange(3_000, 4_000),
+        ClusterRange(4_000, 5_000),
+        ClusterRange(5_000, 7_500),
+        ClusterRange(7_500, 10_000),
+        ClusterRange(10_000, 15_000),
+        ClusterRange(15_000, 20_000),
+        ClusterRange(20_000, 30_000),
+        ClusterRange(30_000, 40_000),
+        ClusterRange(40_000, 75_000),
+        ClusterRange(75_000, 150_000),
+        ClusterRange(150_000, 300_000),
+        ClusterRange(300_000, 500_000),
+        ClusterRange(500_000, 1_000_000),
+        ClusterRange(1_000_000, 2_000_000),
+        ClusterRange(2_000_000, 5_000_000),
+        ClusterRange(5_000_000, 10_000_000),
+        ClusterRange(10_000_000, 50_000_000),
+        ClusterRange(50_000_000, 100_000_000),
+        ClusterRange(100_000_000, 1_000_000_000)
     ])
 
     static let databaseSize = IntegerClusterizer(ranges: [
@@ -44,14 +54,14 @@ extension IntegerClusterizer {
         ClusterRange(50, 100),
         ClusterRange(100, 200),
         ClusterRange(200, 500),
-        ClusterRange(500, 1000),
-        ClusterRange(1000, 2000),
-        ClusterRange(2000, 3000),
-        ClusterRange(3000, 4000),
-        ClusterRange(4000, 5000),
-        ClusterRange(5000, 10000),
-        ClusterRange(10000, 15000),
-        ClusterRange(15000, 20000)
+        ClusterRange(500, 1_000),
+        ClusterRange(1_000, 2_000),
+        ClusterRange(2_000, 3_000),
+        ClusterRange(3_000, 4_000),
+        ClusterRange(4_000, 5_000),
+        ClusterRange(5_000, 10_000),
+        ClusterRange(10_000, 15_000),
+        ClusterRange(15_000, 20_000)
         ])
 
 }
