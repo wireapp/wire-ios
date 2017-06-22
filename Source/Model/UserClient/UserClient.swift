9da7db8b38328e31ce0746b5d536f85ede16db06
Mike Gerasimenko <github@gerasimenko.me>
2017-06-22 16:52:21 +0200

    Object grouping by key (#293)
    * Make public UserClient properties for Sync Engine
    
    * Object grouping by key
    
    * Optimized grouping
    
    * Added comments
    
    * Revert fetch assert
    
    * Remove moc from the memory
    
    * Ignore nil values
    
    * Bump wire-ios-transport
    
    * Bump wire-ios-transport to 22.1.0

--- Source/Model/UserClient/UserClient.swift
+++ Source/Model/UserClient/UserClient.swift
@@ -65,9 +65,9 @@ public class UserClient: ZMManagedObject, UserClientType {
     @NSManaged public var remoteIdentifier: String?
     @NSManaged public var user: ZMUser?
     @NSManaged public var missingClients: Set<UserClient>?
-    @NSManaged fileprivate var missedByClient: UserClient?
-    @NSManaged fileprivate var addedOrRemovedInSystemMessages: Set<ZMSystemMessage>?
-    @NSManaged public var messagesMissingRecipient: Set<ZMMessage>?
+    @NSManaged public var missedByClient: UserClient?
+    @NSManaged public var addedOrRemovedInSystemMessages: Set<ZMSystemMessage>
+    @NSManaged public var messagesMissingRecipient: Set<ZMMessage>
     @NSManaged public var numberOfKeysRemaining: Int32
     @NSManaged public var activationAddress: String?
     @NSManaged public var activationDate: Date?
