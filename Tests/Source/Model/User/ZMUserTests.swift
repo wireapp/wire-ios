48a38fbfb47c7ad0a936c9566471c10deb432c23
Silvan Daehn <silvandaehn@me.com>
2017-07-04 11:10:06 +0200

    Add test checking handle normalisation when searching (#306)

--- Tests/Source/Model/User/ZMUserTests.swift
+++ Tests/Source/Model/User/ZMUserTests.swift
@@ -390,6 +390,21 @@ extension ZMUserTests {
         XCTAssertEqual(users, [user1])
     }
 
+    func testThatThePredicateUsesTheNormalizedQueryToMatchHandlesWhenSearchingWithLeadingAtSymbol() {
+        // Given
+        let user1 = ZMUser.insert(in: uiMOC, name: "Teapot", handle: "vanessa", connected: true)
+        let user2 = ZMUser.insert(in: uiMOC, name: "Norman", handle: "joao", connected: true)
+        let users = [user1, user2] as NSArray
+
+        // When
+        let predicate = ZMUser.predicateForConnectedUsers(withSearch: "@João")
+        let result = users.filtered(using: predicate) as! [ZMUser]
+
+        // Then
+        XCTAssertEqual(result.count, 1)
+        XCTAssertEqual(result, [user2])
+    }
+
     func testPredicateFilteringConnectedUsersByHandlePrefix() {
         // Given
         let user1 = ZMUser.insert(in: self.uiMOC, name: "Some body", handle: "alonghandle", connected: true)
