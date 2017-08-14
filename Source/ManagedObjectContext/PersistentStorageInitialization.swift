63c71a84d28c06d6b4fa3b5f0c851c17ba162659
John Nguyen <johnxuannguyen@gmail.com>
2017-08-14 12:22:32 +0200

    remove debug hook

--- Source/ManagedObjectContext/PersistentStorageInitialization.swift
+++ Source/ManagedObjectContext/PersistentStorageInitialization.swift
@@ -53,11 +53,6 @@ extension NSPersistentStoreCoordinator {
             let model = NSManagedObjectModel.loadModel()
             UserClientKeysStore.migrateIfNeeded(accountDirectory: accountDirectory, applicationContainer: applicationContainer)
             
-            if PersistentStorageInitialization.debug_returnAfterMigratingClientKeyStore {
-                PersistentStorageInitialization.debug_returnAfterMigratingClientKeyStore = false
-                return
-            }
-            
             completionHandler(NSPersistentStoreCoordinator(
                 storeFile: storeFile,
                 applicationContainer: applicationContainer,
@@ -76,10 +71,6 @@ class PersistentStorageInitialization {
     static var debug_returnAfterMigratingClientKeyStore = false
     
     fileprivate init() {}
-    
-    /// Observer token for application becoming available
-    fileprivate var applicationProtectedDataDidBecomeAvailableObserver: Any! = nil
-    
     
     fileprivate static func executeWhenFileIsAccessible(_ file: URL, usingBlock block: @escaping (Void) -> Void) {
         // We need to handle the case when the database file is encrypted by iOS and user never entered the passcode
