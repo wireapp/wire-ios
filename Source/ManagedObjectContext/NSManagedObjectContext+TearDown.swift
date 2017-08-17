4a6dba014052d0a18e98de354e7b8eb062a345e0
Marco Conti <marcoconti83@gmail.com>
2017-08-17 16:45:07 +0200

    Remove all keys from user info when tearing down
    If some tests will fail, it means that there is some wrong behaviour (using the context after it has been torn down), so the test is correctly identifying an issue by failing.

--- Source/ManagedObjectContext/NSManagedObjectContext+TearDown.swift
+++ Source/ManagedObjectContext/NSManagedObjectContext+TearDown.swift
@@ -36,8 +36,7 @@ extension NSManagedObjectContext {
         // We need to keep the context type information until all other values have been removed,
         // otherwise we risk running into assertions based on the context type.
         guard let allKeys = userInfo.allKeys as? [String] else { return }
-        var keys = Set(allKeys)
-        keys.subtract([IsEventContextKey, IsSyncContextKey, IsUserInterfaceContextKey, IsSearchContextKey])
+        let keys = Set(allKeys)
         userInfo.removeObjects(forKeys: Array(keys))
     }
 }
