3914086274de4c4df49406b020684a545b2d78be
Mike Gerasimenko <github@gerasimenko.me>
2017-08-25 11:11:44 +0200

    Share cryptobox button (#1201)

--- Wire-iOS/Sources/UserInterface/Settings/CellDescriptors/SettingsShareDatabaseCellDescriptor.swift
+++ Wire-iOS/Sources/UserInterface/Settings/CellDescriptors/SettingsShareDatabaseCellDescriptor.swift
@@ -50,3 +50,26 @@ class SettingsShareDatabaseCellDescriptor : SettingsButtonCellDescriptor {
     }
     
 }
+
+class SettingsShareCryptoboxCellDescriptor : SettingsButtonCellDescriptor {
+    
+    let documentDelegate : DocumentDelegate
+    
+    init() {
+        let documentDelegate = DocumentDelegate()
+        self.documentDelegate = documentDelegate
+        
+        super.init(title: "Share Cryptobox", isDestructive: false) { _ in
+            let fileURL = ZMUserSession.shared()!.managedObjectContext.zm_storeURL!.deletingLastPathComponent().deletingLastPathComponent().appendingPathComponent("otr")
+            let archiveURL = fileURL.appendingPathExtension("zip")
+            
+            SSZipArchive.createZipFile(atPath: archiveURL.path, withContentsOfDirectory: fileURL.path)
+            
+            let shareDatabaseDocumentController = UIDocumentInteractionController(url: archiveURL)
+            shareDatabaseDocumentController.delegate = documentDelegate
+            shareDatabaseDocumentController.presentPreview(animated: true)
+        }
+        
+    }
+    
+}
