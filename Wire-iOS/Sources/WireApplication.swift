9457c9b63ae1dad496aa748dec2d0cdd354872a5
Vytis <vytis@proto.lt>
2017-09-05 14:56:22 +0200

    Added a way to avoid asking notification permissions when testing with automation (#1234)

--- Wire-iOS/Sources/WireApplication.swift
+++ Wire-iOS/Sources/WireApplication.swift
@@ -1,35 +1,33 @@
-// 
+//
 // Wire
-// Copyright (C) 2016 Wire Swiss GmbH
-// 
+// Copyright (C) 2017 Wire Swiss GmbH
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see http://www.gnu.org/licenses/.
-// 
+//
 
+import Foundation
+import UIKit
 
-#import <UIKit/UIKit.h>
-
-#import "AppDelegate.h"
-#import "StopWatch.h"
-#import "Wire-Swift.h"
-
-
-int main(int argc, char *argv[])
-{
-    @autoreleasepool {
-        StopWatch *stopWatch = [StopWatch stopWatch];
-        [stopWatch startEvent:@"AppStart"];
-        
-        return UIApplicationMain(argc, argv, NSStringFromClass([WireApplication class]), NSStringFromClass([AppDelegate class]));
+@objc public class WireApplication: UIApplication {
+    
+    public override func registerUserNotificationSettings(_ notificationSettings: UIUserNotificationSettings) {
+        if AutomationHelper.sharedHelper.skipFirstLoginAlerts { return }
+        super.registerUserNotificationSettings(notificationSettings)
+    }
+    
+    override public func registerForRemoteNotifications() {
+        if AutomationHelper.sharedHelper.skipFirstLoginAlerts { return }
+        super.registerForRemoteNotifications()
     }
 }
