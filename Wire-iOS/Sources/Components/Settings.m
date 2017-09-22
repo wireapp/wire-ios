00298fbb7bcf5c0e87b127517fc23a57efbbbf2d
Mike Gerasimenko <github@gerasimenko.me>
2017-09-22 19:06:12 +0200

    Update for the multi call center API (#1279)
    * Update for the multi call center API
    
    * Bump wire-ios-sync-engine to 113.0.0
    
    * Fixed old API

--- Wire-iOS/Sources/Components/Settings.m
+++ Wire-iOS/Sources/Components/Settings.m
@@ -503,7 +503,7 @@ NSString * const UserDefaultDisableLinkPreviews = @"DisableLinkPreviews";
 
 - (void)updateAVSCallingConstantBitRateValue
 {
-    WireCallCenterV3 *callCenter = [WireCallCenterV3 activeInstance];
+    WireCallCenterV3 *callCenter = [[[SessionManager shared] userSession] callCenter];
     if (nil != callCenter) {
         callCenter.useAudioConstantBitRate = self.callingConstantBitRate;
     }
