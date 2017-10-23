a53b9ae4abd0974905af6b90a088f4fb29f4d4e0
Bill Chan <billypchan@gmail.com>
2017-10-23 17:38:12 +0200

    create more tests for String+Filename

--- Tests/String+FilenameTests.swift
+++ Tests/String+FilenameTests.swift
@@ -24,7 +24,6 @@ class String_FilenameTests: XCTestCase {
     func testShortUserNameToFileName() {
         // GIVEN
         let username = "John Smith"
-        
         let filename = username.normalizedFilename.trimmedFilename(numReservedChar: 0)
         
         // WHEN & THEN
@@ -34,21 +33,33 @@ class String_FilenameTests: XCTestCase {
     func testEmojiUserNameToLongFileName() {
         // GIVEN
         let username = "🇭🇰🇭🇰🇭🇰🇭🇰🇭🇰🇭🇰🇭🇰"
-        
         let filename = username.normalizedFilename.trimmedFilename(numReservedChar: 0)
         
+        let uuidString = UUID.create().uuidString
+        
+        let exampleFileName = uuidString + "_" + filename + ".mp4"
+        
         // WHEN & THEN
-        XCTAssertEqual(filename.count, 255 - 4 - 37)
+        XCTAssertEqual(exampleFileName.count, 255)
     }
 
     func testChineseUserNameToLatinFileName() {
         // GIVEN
         let username = "使用者"
-        
         let filename = username.normalizedFilename.trimmedFilename(numReservedChar: 0)
         
         // WHEN & THEN
         XCTAssertEqual(filename, "shi-yong-zhe")
     }
-    
+
+    func testArabicToFileName() {
+        // GIVEN
+        let username = "الأشخاص المفضلين"
+
+        let filename = username.normalizedFilename.trimmedFilename(numReservedChar: 0)
+        
+        // WHEN & THEN
+        XCTAssertEqual(filename, "alashkhas-almfdlyn")
+    }
+
 }
