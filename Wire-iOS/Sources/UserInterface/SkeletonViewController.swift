f83414013a5dd88238aebfdefeb17c127e94ca4d
Vytis <vytis@proto.lt>
2017-10-25 15:18:22 +0200

    Show archive icon in skeleton view (#1375)
    We have been showing wrong icon during the account switch in the bottom right of the skeleton view

--- Wire-iOS/Sources/UserInterface/SkeletonViewController.swift
+++ Wire-iOS/Sources/UserInterface/SkeletonViewController.swift
@@ -174,7 +174,7 @@ class ListSkeletonView  : UIView {
         titleLabel.font = FontSpec(.medium, .semibold).font
         titleLabel.text = "list.title".localized.uppercased()
         
-        buttonRowView = UIStackView(arrangedSubviews: disabledButtons(with: [.person, .compose]))
+        buttonRowView = UIStackView(arrangedSubviews: disabledButtons(with: [.person, .archive]))
         buttonRowView.distribution = .equalCentering
 
         [accountView, titleLabel, listContentView, buttonRowView].forEach(addSubview)
