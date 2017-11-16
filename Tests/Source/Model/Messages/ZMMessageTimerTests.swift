d73e1b81051c9ce07bf6910fa4d1d7a2efbd5ced
John Nguyen <johnxuannguyen@gmail.com>
2017-11-16 14:06:32 +0100

    Wrap timer inside background activity (#395)
    * wrap timer inside background activity
    
    this allows the timer to run when the app in the background, particularly useful for the MessageExpirationTimer in WireMessageStrategy
    
    * add tests
    
    * add method to access internal timer
    
    * fix header

---
+++ Tests/Source/Model/Messages/ZMMessageTimerTests.swift
@@ -0,0 +1,81 @@
+//
+// Wire
+// Copyright (C) 2017 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import XCTest
+@testable import WireDataModel
+import WireTransport
+
+class ZMMessageTimerTests: BaseZMMessageTests {
+    
+    let application = UIApplication.shared
+    let groupQueue = DispatchGroupQueue(queue: .main)
+    var sut: ZMMessageTimer!
+    
+    override func setUp() {
+        super.setUp()
+        BackgroundActivityFactory.sharedInstance().application = application
+        BackgroundActivityFactory.sharedInstance().mainGroupQueue = groupQueue
+        sut = ZMMessageTimer(managedObjectContext: uiMOC)!
+    }
+    
+    override func tearDown() {
+        sut.tearDown()
+        sut = nil
+        super.tearDown()
+    }
+    
+    func testThatItCreatesTheBackgroundActivityWhenTimerStarted() {
+        // given
+        let message = createClientTextMessage("hello", encrypted: false)
+        
+        // when
+        sut.start(forMessageIfNeeded: message, fire: Date(timeIntervalSinceNow: 1.0), userInfo: [:])
+        
+        // then
+        let timer = sut.timer(for: message)
+        XCTAssertNotNil(timer)
+        let bgActivity = timer!.userInfo["bgActivity"] as? ZMBackgroundActivity
+        XCTAssertNotNil(bgActivity)
+    }
+    
+    func testThatItRemovesTheInternalTimerAfterTimerFired() {
+        // given
+        let message = createClientTextMessage("hello", encrypted: false)
+        let expectation = self.expectation(description: "timer fired")
+        sut.timerCompletionBlock = { _, _ in expectation.fulfill() }
+        
+        // when
+        sut.start(forMessageIfNeeded: message, fire: Date(), userInfo: [:])
+        _ = waitForCustomExpectations(withTimeout: 0.5)
+        
+        // then
+        XCTAssertNil(sut.timer(for: message))
+    }
+
+    func testThatItRemovesTheInternalTimerWhenTimerStopped() {
+        // given
+        let message = createClientTextMessage("hello", encrypted: false)
+        sut.start(forMessageIfNeeded: message, fire: Date(), userInfo: [:])
+        
+        // when
+        sut.stop(for: message)
+        
+        // then
+        XCTAssertNil(sut.timer(for: message))
+    }
+}
