8c874a01c45a28aa6df49a7b67a32da35f05a1ac
Mike Gerasimenko <github@gerasimenko.me>
2017-11-23 20:45:30 +0100

    Digit code input field (#1444)
    * Digit code input field
    
    * Fixes for PR review
    
    * Fixed typo

---
+++ Wire-iOS Tests/CharacterInputFieldTests.swift
@@ -0,0 +1,228 @@
+//
+// Wire
+// Copyright (C) 2017 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+import XCTest
+import Cartography
+@testable import Wire
+
+class TestCharacterInputFieldDelegate: NSObject, CharacterInputFieldDelegate {
+    var didChangeText: [String] = []
+    func didChangeText(_ inputField: CharacterInputField, to: String) {
+        didChangeText.append(to)
+    }
+    var didFillInput: Int = 0
+    func didFillInput(inputField: CharacterInputField) {
+        didFillInput = didFillInput + 1
+    }
+}
+
+
+final class CharacterInputFieldTests: XCTestCase {
+    var sut: CharacterInputField! = nil
+    var delegate: TestCharacterInputFieldDelegate! = nil
+    
+    override func setUp() {
+        super.setUp()
+        sut = CharacterInputField(maxLength: 8, characterSet: CharacterSet.decimalDigits)
+        delegate = TestCharacterInputFieldDelegate()
+        sut.delegate = delegate
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        sut.removeFromSuperview()
+        sut = nil
+        delegate = nil
+    }
+    
+    func testThatItCanBecomeFirstResponder() {
+        // when
+        UIApplication.shared.keyWindow?.rootViewController?.view.addSubview(sut)
+        // then
+        XCTAssertTrue(sut.canBecomeFocused)
+        XCTAssertTrue(sut.becomeFirstResponder())
+        XCTAssertTrue(sut.isFirstResponder)
+    }
+    
+    func testThatItSupportsPaste() {
+        XCTAssertTrue(sut.canPerformAction(#selector(UIControl.paste(_:)), withSender: nil))
+    }
+    
+    func testThatItDoesNotCallDelegateForSettingTextDirectly() {
+        // when
+        sut.text = "1234"
+        // then
+        XCTAssertEqual(delegate.didChangeText, [])
+        XCTAssertEqual(sut.text, "1234")
+        XCTAssertFalse(sut.isFilled)
+        XCTAssertEqual(delegate.didFillInput, 0)
+    }
+    
+    func testThatItAppendsOneSymbolAndCallsDelegate() {
+        // when
+        sut.insertText("1")
+        // then
+        XCTAssertEqual(delegate.didChangeText, ["1"])
+        XCTAssertEqual(sut.text, "1")
+        XCTAssertFalse(sut.isFilled)
+        XCTAssertEqual(delegate.didFillInput, 0)
+    }
+    
+    func testThatItDeletesSymbolAndCallsDelegate() {
+        // given
+        sut.text = "1234"
+        // when
+        sut.deleteBackward()
+        // then
+        XCTAssertEqual(delegate.didChangeText, ["123"])
+        XCTAssertEqual(sut.text, "123")
+        XCTAssertFalse(sut.isFilled)
+        XCTAssertEqual(delegate.didFillInput, 0)
+    }
+    
+    func testThatItAllowsToPasteAndCallsDelegate() {
+        // given
+        sut.text = "1234"
+        UIPasteboard.general.string = "567"
+        // when
+        sut.paste(nil)
+        // then
+        XCTAssertEqual(delegate.didChangeText, ["567"])
+        XCTAssertEqual(sut.text, "567")
+        XCTAssertFalse(sut.isFilled)
+        XCTAssertEqual(delegate.didFillInput, 0)
+    }
+    
+    func testThatItForbidsIncompatibleCharacters() {
+        sut.text = "1234"
+        // when
+        sut.insertText("V")
+        // then
+        XCTAssertEqual(delegate.didChangeText, [])
+        XCTAssertEqual(sut.text, "1234")
+        XCTAssertFalse(sut.isFilled)
+        XCTAssertEqual(delegate.didFillInput, 0)
+    }
+    
+    func testThatItAllowsEnteringCharactersUpToMax() {
+        // when
+        sut.insertText("123456789")
+        // then
+        XCTAssertEqual(delegate.didChangeText, ["12345678"])
+        XCTAssertEqual(sut.text, "12345678")
+        XCTAssertTrue(sut.isFilled)
+        XCTAssertEqual(delegate.didFillInput, 1)
+    }
+    
+    func testThatItWorksWithOtherSymbols() {
+        // given
+        let sut = CharacterInputField(maxLength: 100, characterSet: CharacterSet.uppercaseLetters)
+        sut.delegate = delegate
+        // when
+        sut.insertText("123456789")
+        // then
+        XCTAssertEqual(delegate.didChangeText, [])
+        XCTAssertEqual(sut.text, "")
+
+        // when
+        sut.insertText("HELLOWORLD")
+        
+        // then
+        XCTAssertEqual(delegate.didChangeText, ["HELLOWORLD"])
+        XCTAssertEqual(sut.text, "HELLOWORLD")
+        XCTAssertEqual(delegate.didFillInput, 0)
+    }
+}
+
+final class CharacterInputFieldScreenshotTests: ZMSnapshotTestCase {
+    var sut: CharacterInputField! = nil
+    
+    override func setUp() {
+        super.setUp()
+        sut = CharacterInputField(maxLength: 8, characterSet: CharacterSet.decimalDigits)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        sut.removeFromSuperview()
+        sut = nil
+    }
+    
+    func testDefaultState() {
+        // then
+        verify(view: sut.snapshotView())
+    }
+    
+    func testFocusedState() {
+        // given
+        UIApplication.shared.keyWindow?.rootViewController?.view.addSubview(sut)
+
+        // when
+        sut.becomeFirstResponder()
+        
+        // then
+        verify(view: sut.snapshotView())
+    }
+    
+    func testFocusedDeFocusedState() {
+        // given
+        UIApplication.shared.keyWindow?.rootViewController?.view.addSubview(sut)
+        
+        // when
+        sut.becomeFirstResponder()
+        sut.resignFirstResponder()
+        
+        // then
+        verify(view: sut.snapshotView())
+    }
+    
+    func testOneCharacterState() {
+        // when
+        sut.insertText("1")
+        
+        // then
+        verify(view: sut.snapshotView())
+    }
+    
+    func testAllCharactersEnteredState() {
+        // when
+        sut.insertText("12345678")
+        
+        // then
+        verify(view: sut.snapshotView())
+    }
+}
+
+fileprivate extension UIView {
+    func snapshotView() -> UIView {
+        let topView = UIApplication.shared.keyWindow!.rootViewController!.view!
+            
+        topView.addSubview(self)
+
+        constrain(self, topView) { selfView, topView in
+            selfView.center == topView.center
+        }
+        
+        self.layer.speed = 0
+        self.setNeedsLayout()
+        self.layoutIfNeeded()
+        return self
+    }
+}
+
---
+++ Wire-iOS/Sources/UserInterface/Components/CharacterInputField.swift
@@ -0,0 +1,297 @@
+//
+// Wire
+// Copyright (C) 2017 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+import Cartography
+
+public protocol CharacterInputFieldDelegate: NSObjectProtocol {
+    func didChangeText(_ inputField: CharacterInputField, to: String)
+    func didFillInput(inputField: CharacterInputField)
+}
+
+/// Custom input field implementation. Allows entering the characters from @c characterSet up to @c maxLength characters
+/// Allows pasting the text.
+public class CharacterInputField: UIControl, UITextInputTraits {
+    fileprivate var storage = String() {
+        didSet {
+            if storage.characters.count > maxLength {
+                storage = String(storage.prefix(maxLength))
+            }
+            
+            self.updateCharacterViews(isFirstResponder: self.isFirstResponder)
+        }
+    }
+    
+    public let maxLength: Int
+    public let characterSet: CharacterSet
+    public weak var delegate: CharacterInputFieldDelegate? = .none
+    private let characterViews: [CharacterView]
+    private let stackView = UIStackView()
+    
+    fileprivate func prepare(string: String) -> String {
+        var result = string.filter { element -> Bool in
+            guard element.unicodeScalars.count == 1, let firstScalar = element.unicodeScalars.first else {
+                return false
+            }
+            return characterSet.contains(firstScalar)
+        }
+        
+        if result.count > maxLength {
+            result = Array(result.prefix(maxLength))
+        }
+        
+        return String(result)
+    }
+    
+    private func updateCharacterViews(isFirstResponder: Bool) {
+        for index in 0...(maxLength - 1) {
+            let characterView = characterViews[index]
+            
+            if let character = storage.characters.count > index ? storage[storage.index(storage.startIndex, offsetBy: index)] : nil {
+                characterView.character = .char(character)
+            }
+            else if storage.characters.count == index && isFirstResponder {
+                characterView.character = .cursor
+            }
+            else {
+                characterView.character = .none
+            }
+        }
+    }
+    
+    fileprivate func notifyingDelegate(_ action: ()->()) {
+        let wasFilled = self.isFilled
+        let previousText = self.storage
+        
+        action()
+        
+        if previousText != storage {
+            self.delegate?.didChangeText(self, to: storage)
+        }
+        
+        if !wasFilled && self.isFilled {
+            self.delegate?.didFillInput(inputField: self)
+        }
+    }
+    
+    class CharacterView: UIView {
+        private let label = UILabel()
+        private let cursorView = UIView()
+        
+        enum CharacterInView {
+            case char(Character)
+            case cursor
+            case none
+        }
+        
+        var character: CharacterInView = .none {
+            didSet {
+                switch character {
+                case .char(let character):
+                    label.text = String(character)
+                    label.isHidden = false
+                    cursorView.isHidden = true
+                case .cursor:
+                    label.isHidden = true
+                    cursorView.isHidden = false
+                    self.startCursorAnimationIfNeeded()
+                case .none:
+                    label.isHidden = true
+                    cursorView.isHidden = true
+                }
+            }
+        }
+        
+        init() {
+            super.init(frame: .zero)
+            self.layer.cornerRadius = 4
+            self.backgroundColor = .white
+            
+            label.font = UIFont.systemFont(ofSize: 32)
+            self.addSubview(label)
+            cursorView.backgroundColor = .accent()
+            self.addSubview(cursorView)
+            cursorView.isHidden = true
+            
+            constrain(self, label, cursorView) { selfView, label, cursorView in
+                label.center == selfView.center
+                cursorView.width == 2
+                cursorView.height == 36
+                cursorView.center == selfView.center
+            }
+            
+            NotificationCenter.default.addObserver(self, selector: #selector(applicationDidBecomeActive), name: NSNotification.Name.UIApplicationDidBecomeActive, object: nil)
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        override var intrinsicContentSize: CGSize {
+            return CGSize(width: 50, height: 56)
+        }
+        
+        override func didMoveToWindow() {
+            super.didMoveToWindow()
+            self.startCursorAnimationIfNeeded()
+        }
+        
+        private func startCursorAnimationIfNeeded() {
+            if !cursorView.isHidden && cursorView.layer.animation(forKey: "blinkAnimation") == nil {
+                let animation = CAKeyframeAnimation(keyPath: "opacity")
+                animation.values = [1, 1, 0, 0]
+                animation.keyTimes = [0, 0.4, 0.7, 0.9]
+                animation.duration = 0.64
+                animation.autoreverses = true
+                animation.repeatCount = .greatestFiniteMagnitude
+                cursorView.layer.add(animation, forKey: "blinkAnimation")
+            }
+        }
+        
+        @objc fileprivate func applicationDidBecomeActive(_ sender: Any?) {
+            self.startCursorAnimationIfNeeded()
+        }
+    }
+    
+    // MARK: - Overrides
+    
+    init(maxLength: Int, characterSet: CharacterSet) {
+        self.maxLength = maxLength
+        self.characterSet = characterSet
+        characterViews = (0..<maxLength).map { _ in CharacterView() }
+
+        super.init(frame: .zero)
+        stackView.spacing = 8
+        stackView.axis = .horizontal
+        
+        characterViews.forEach(self.stackView.addArrangedSubview)
+        
+        self.addSubview(stackView)
+        
+        constrain(self, stackView) { selfView, stackView in
+            stackView.edges == selfView.edges
+        }
+        
+        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(onLongPress(_:)))
+        self.addGestureRecognizer(longPressGestureRecognizer)
+        
+        self.storage = String()
+    }
+    
+    public required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    public override var canBecomeFocused: Bool {
+        return true
+    }
+    
+    @discardableResult public override func becomeFirstResponder() -> Bool {
+        let result = super.becomeFirstResponder()
+        updateCharacterViews(isFirstResponder: true)
+        return result
+    }
+    
+    @discardableResult public override func resignFirstResponder() -> Bool {
+        let result = super.resignFirstResponder()
+        updateCharacterViews(isFirstResponder: false)
+        return result
+    }
+    
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.becomeFirstResponder()
+    }
+    
+    // MARK: - Paste support
+    
+    @objc fileprivate func onLongPress(_ sender: Any?) {
+        let menuController = UIMenuController.shared
+        menuController.setTargetRect(bounds, in: self)
+        menuController.setMenuVisible(true, animated: true)
+    }
+    
+    public override func paste(_ sender: Any?) {
+        guard let valueToPaste = UIPasteboard.general.string else {
+            return
+        }
+        
+        notifyingDelegate {
+            self.text = valueToPaste
+        }
+    }
+    
+    public override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        switch action {
+        case #selector(paste(_:)):
+            return UIPasteboard.general.string != nil
+        default:
+            return false
+        }
+    }
+    
+    // MARK: - Public API
+    
+    public var isFilled: Bool {
+        return storage.count >= maxLength
+    }
+    
+    public var text: String {
+        set {
+            storage = prepare(string: newValue)
+        }
+        get {
+            return storage
+        }
+    }
+    
+    // MARK: - UITextInputTraits
+    public var keyboardType: UIKeyboardType = .default
+}
+
+extension CharacterInputField: UIKeyInput {
+    public func insertText(_ text: String) {
+        if let _ = text.rangeOfCharacter(from: CharacterSet.newlines) {
+            self.resignFirstResponder()
+            return
+        }
+        
+        let allowedChars = prepare(string: text)
+        guard !allowedChars.isEmpty else {
+            return
+        }
+        
+        notifyingDelegate {
+            self.storage.append(String(allowedChars))
+        }
+    }
+    
+    public func deleteBackward() {
+        notifyingDelegate {
+            self.storage.removeLast()
+        }
+    }
+    
+    public var hasText: Bool {
+        return !storage.isEmpty
+    }
+}
