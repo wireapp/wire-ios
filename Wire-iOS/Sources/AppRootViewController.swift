078afb476877df4588f908c4f66c9b5aaeb640f5
Bill Chan <billypchan@gmail.com>
2017-12-07 12:13:44 +0100

    ZIOS-9273 update overlayWindow.frame when applicationWillEnterForeground instea… (#1438)
    * update overlayWindow.frame when applicationWillEnterForeground instead of applicationDidBecomeActive
    assign UIScreen.main.bounds to overlayWindow.frame instead of size from viewWillTransition
    
    * update overlayWindow frame size with UIApplication.shared.keyWindow?.frame
    
    * clean up

--- Wire-iOS/Sources/AppRootViewController.swift
+++ Wire-iOS/Sources/AppRootViewController.swift
@@ -21,30 +21,30 @@ import UIKit
 import Classy
 
 @objc
-class AppRootViewController : UIViewController {
-    
-    public let mainWindow : UIWindow
-    public let overlayWindow : UIWindow
-    public fileprivate(set) var sessionManager : SessionManager?
-    
+class AppRootViewController: UIViewController {
+
+    public let mainWindow: UIWindow
+    public let overlayWindow: UIWindow
+    public fileprivate(set) var sessionManager: SessionManager?
+
     fileprivate var sessionManagerCreatedSessionObserverToken: Any?
     fileprivate var sessionManagerDestroyedSessionObserverToken: Any?
     fileprivate var soundEventListeners = [UUID : SoundEventListener]()
-    
-    public fileprivate(set) var visibleViewController : UIViewController?
-    fileprivate let appStateController : AppStateController
-    fileprivate lazy var classyCache : ClassyCache = {
+
+    public fileprivate(set) var visibleViewController: UIViewController?
+    fileprivate let appStateController: AppStateController
+    fileprivate lazy var classyCache: ClassyCache = {
         return ClassyCache()
     }()
-    fileprivate let fileBackupExcluder : FileBackupExcluder
-    fileprivate let avsLogObserver : AVSLogObserver
+    fileprivate let fileBackupExcluder: FileBackupExcluder
+    fileprivate let avsLogObserver: AVSLogObserver
     fileprivate var authenticatedBlocks : [() -> Void] = []
-    fileprivate let transitionQueue : DispatchQueue = DispatchQueue(label: "transitionQueue")
+    fileprivate let transitionQueue: DispatchQueue = DispatchQueue(label: "transitionQueue")
     fileprivate var isClassyInitialized = false
     fileprivate let mediaManagerLoader = MediaManagerLoader()
 
     var flowController: TeamCreationFlowController!
-    
+
     fileprivate weak var requestToOpenViewDelegate: ZMRequestsToOpenViewsDelegate? {
         didSet {
             if let delegate = requestToOpenViewDelegate {
@@ -53,12 +53,19 @@ class AppRootViewController : UIViewController {
             }
         }
     }
-    
+
     fileprivate var performWhenRequestsToOpenViewsDelegateAvailable: ((ZMRequestsToOpenViewsDelegate)->())?
+
+    func updateOverlayWindowFrame() {
+        self.overlayWindow.frame = UIApplication.shared.keyWindow?.frame ?? UIScreen.main.bounds
+    }
 
     override public func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         mainWindow.frame.size = size
-        overlayWindow.frame.size = size
+
+        coordinator.animate(alongsideTransition: nil, completion: { _ in
+            self.updateOverlayWindowFrame()
+        })
     }
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
@@ -69,59 +76,60 @@ class AppRootViewController : UIViewController {
 
         mainWindow = UIWindow(frame: UIScreen.main.bounds)
         mainWindow.accessibilityIdentifier = "ZClientMainWindow"
-        
+
         overlayWindow = PassthroughWindow(frame: UIScreen.main.bounds)
         overlayWindow.backgroundColor = .clear
         overlayWindow.windowLevel = UIWindowLevelStatusBar + 1
         overlayWindow.accessibilityIdentifier = "ZClientNotificationWindow"
         overlayWindow.rootViewController = NotificationWindowRootViewController()
-        
+
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
 
         AutomationHelper.sharedHelper.installDebugDataIfNeeded()
 
         appStateController.delegate = self
-        
+
         // Notification window has to be on top, so must be made visible last.  Changing the window level is
         // not possible because it has to be below the status bar.
         mainWindow.rootViewController = self
         mainWindow.makeKeyAndVisible()
         overlayWindow.makeKeyAndVisible()
         mainWindow.makeKey()
-        
+
         configureMediaManager()
-        
+
         if let appGroupIdentifier = Bundle.main.appGroupIdentifier {
             let sharedContainerURL = FileManager.sharedContainerDirectory(for: appGroupIdentifier)
             fileBackupExcluder.excludeLibraryFolderInSharedContainer(sharedContainerURL: sharedContainerURL)
         }
-        
+
         NotificationCenter.default.addObserver(self, selector: #selector(onContentSizeCategoryChange), name: Notification.Name.UIContentSizeCategoryDidChange, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(applicationWillEnterForeground), name: Notification.Name.UIApplicationWillEnterForeground, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(applicationDidEnterBackground), name: Notification.Name.UIApplicationDidEnterBackground, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(applicationDidBecomeActive), name: Notification.Name.UIApplicationDidBecomeActive, object: nil)
+         NotificationCenter.default.addObserver(self, selector: #selector(applicationDidBecomeActive), name: Notification.Name.UIApplicationDidBecomeActive, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(onUserGrantedAudioPermissions), name: Notification.Name.UserGrantedAudioPermissions, object: nil)
-        
+
         transition(to: .headless)
-        
+
         enqueueTransition(to: appStateController.appState)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     public override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         view.frame = mainWindow.bounds
     }
-    
-    public func launch(with launchOptions : LaunchOptions) {
+
+    public func launch(with launchOptions: LaunchOptions) {
         let bundle = Bundle.main
         let appVersion = bundle.infoDictionary?[kCFBundleVersionKey as String] as? String
         let mediaManager = AVSMediaManager.sharedInstance()
         let analytics = Analytics.shared()
-        
+
         SessionManager.create(
             appVersion: appVersion!,
             mediaManager: mediaManager!,
@@ -129,8 +137,7 @@ class AppRootViewController : UIViewController {
             delegate: appStateController,
             application: UIApplication.shared,
             launchOptions: launchOptions,
-            blacklistDownloadInterval: Settings.shared().blacklistDownloadInterval)
-        { sessionManager in
+            blacklistDownloadInterval: Settings.shared().blacklistDownloadInterval) { sessionManager in
             self.sessionManager = sessionManager
             self.sessionManagerCreatedSessionObserverToken = sessionManager.addSessionManagerCreatedSessionObserver(self)
             self.sessionManagerDestroyedSessionObserverToken = sessionManager.addSessionManagerDestroyedSessionObserver(self)
@@ -140,44 +147,43 @@ class AppRootViewController : UIViewController {
             sessionManager.useConstantBitRateAudio = Settings.shared().callingConstantBitRate
         }
     }
-    
+
     func enqueueTransition(to appState: AppState, completion: (() -> Void)? = nil) {
-        
+
         transitionQueue.async {
-            
+
             let transitionGroup = DispatchGroup()
             transitionGroup.enter()
-            
+
             DispatchQueue.main.async {
                 self.prepare(for: appState, completionHandler: {
                     transitionGroup.leave()
                 })
             }
-            
+
             transitionGroup.wait()
         }
-        
-        
+
         transitionQueue.async {
-            
+
             let transitionGroup = DispatchGroup()
             transitionGroup.enter()
-            
+
             DispatchQueue.main.async {
                 self.transition(to: appState, completionHandler: {
                     transitionGroup.leave()
                     completion?()
                 })
             }
-            
+
             transitionGroup.wait()
         }
     }
-    
+
     func transition(to appState: AppState, completionHandler: (() -> Void)? = nil) {
-        var viewController : UIViewController? = nil
+        var viewController: UIViewController? = nil
         requestToOpenViewDelegate = nil
-        
+
         switch appState {
         case .blacklisted:
             viewController = BlacklistViewController()
@@ -231,16 +237,16 @@ class AppRootViewController : UIViewController {
             executeAuthenticatedBlocks()
             let clientViewController = ZClientViewController()
             clientViewController.isComingFromRegistration = completedRegistration
-            
+
             Analytics.shared().team = ZMUser.selfUser().team
-            
+
             viewController = clientViewController
         case .headless:
             viewController = LaunchImageViewController()
         case .loading(account: let toAccount, from: let fromAccount):
             viewController = SkeletonViewController(from: fromAccount, to: toAccount)
         }
-        
+
         if let viewController = viewController {
             transition(to: viewController, animated: true) {
                 self.requestToOpenViewDelegate = viewController as? ZMRequestsToOpenViewsDelegate
@@ -250,8 +256,7 @@ class AppRootViewController : UIViewController {
             completionHandler?()
         }
     }
-    
-    
+
     private func dismissModalsFromAllChildren(of viewController: UIViewController?) {
         guard let viewController = viewController else { return }
         for child in viewController.childViewControllers {
@@ -261,24 +266,23 @@ class AppRootViewController : UIViewController {
             dismissModalsFromAllChildren(of: child)
         }
     }
-    
-    func transition(to viewController : UIViewController, animated : Bool = true, completionHandler: (() -> Void)? = nil) {
-        
+
+    func transition(to viewController: UIViewController, animated: Bool = true, completionHandler: (() -> Void)? = nil) {
+
         // If we have some modal view controllers presented in any of the (grand)children
         // of this controller they stay in memory and leak on iOS 10.
         dismissModalsFromAllChildren(of: visibleViewController)
         visibleViewController?.willMove(toParentViewController: nil)
-        
+
         if let previousViewController = visibleViewController, animated {
-        
+
             addChildViewController(viewController)
             transition(from: previousViewController,
                        to: viewController,
                        duration: 0.5,
                        options: .transitionCrossDissolve,
                        animations: nil,
-                       completion:
-                { (finished) in
+                       completion: { (finished) in
                     viewController.didMove(toParentViewController: self)
                     previousViewController.removeFromParentViewController()
                     self.visibleViewController = viewController
@@ -299,18 +303,18 @@ class AppRootViewController : UIViewController {
             completionHandler?()
         }
     }
-    
+
     func prepare(for appState: AppState, completionHandler: @escaping () -> Void) {
-        
+
         if appState == .authenticated(completedRegistration: false) {
             (overlayWindow.rootViewController as? NotificationWindowRootViewController)?.transitionToLoggedInSession()
         } else {
             overlayWindow.rootViewController = NotificationWindowRootViewController()
         }
-        
+
         if !isClassyInitialized && isClassyRequired(for: appState) {
             isClassyInitialized = true
-            
+
             let windows = [mainWindow, overlayWindow]
             DispatchQueue.main.async {
                 self.setupClassy(with: windows)
@@ -320,7 +324,7 @@ class AppRootViewController : UIViewController {
             completionHandler()
         }
     }
-    
+
     func isClassyRequired(for appState: AppState) -> Bool {
         switch appState {
         case .authenticated, .unauthenticated, .loading:
@@ -329,24 +333,24 @@ class AppRootViewController : UIViewController {
             return false
         }
     }
-    
+
     func configureMediaManager() {
         self.mediaManagerLoader.send(message: .appStart)
     }
-    
+
     func setupClassy(with windows: [UIWindow]) {
-        
+
         let colorScheme = ColorScheme.default()
         colorScheme.accentColor = UIColor.accent()
         colorScheme.variant = ColorSchemeVariant(rawValue: Settings.shared().colorScheme.rawValue) ?? .light
-        
+
         let fontScheme = FontScheme(contentSizeCategory: UIApplication.shared.preferredContentSizeCategory)
         CASStyler.default().cache = classyCache
         CASStyler.bootstrapClassy(withTargetWindows: windows)
         CASStyler.default().apply(colorScheme)
         CASStyler.default().apply(fontScheme: fontScheme)
     }
-    
+
     func onContentSizeCategoryChange() {
         Message.invalidateMarkdownStyle()
         UIFont.wr_flushFontCache()
@@ -355,7 +359,7 @@ class AppRootViewController : UIViewController {
         let fontScheme = FontScheme(contentSizeCategory: UIApplication.shared.preferredContentSizeCategory)
         CASStyler.default().apply(fontScheme: fontScheme)
     }
-    
+
     public func performWhenAuthenticated(_ block : @escaping () -> Void) {
         if appStateController.appState == .authenticated(completedRegistration: false) {
             block()
@@ -363,72 +367,72 @@ class AppRootViewController : UIViewController {
             authenticatedBlocks.append(block)
         }
     }
-    
+
     func executeAuthenticatedBlocks() {
         while !authenticatedBlocks.isEmpty {
             authenticatedBlocks.removeFirst()()
         }
     }
-    
+
     func reload() {
         enqueueTransition(to: .headless)
         enqueueTransition(to: self.appStateController.appState)
     }
-    
+
 }
 
 // MARK: - Status Bar / Supported Orientations
 
 extension AppRootViewController {
-    
+
     override var shouldAutorotate: Bool {
         return true
     }
-    
+
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return UIViewController.wr_supportedInterfaceOrientations()
     }
-    
+
     override var prefersStatusBarHidden: Bool {
         return visibleViewController?.prefersStatusBarHidden ?? false
     }
-    
+
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return visibleViewController?.preferredStatusBarStyle ?? .default
     }
-    
+
 }
 
-extension AppRootViewController : AppStateControllerDelegate {
-    
+extension AppRootViewController: AppStateControllerDelegate {
+
     func appStateController(transitionedTo appState: AppState, transitionCompleted: @escaping () -> Void) {
         enqueueTransition(to: appState, completion: transitionCompleted)
     }
-        
+
 }
 
 // MARK: - RequestToOpenViewsDelegate
 
-extension AppRootViewController : ZMRequestsToOpenViewsDelegate {
-    
+extension AppRootViewController: ZMRequestsToOpenViewsDelegate {
+
     public func showConversationList(for userSession: ZMUserSession!) {
         whenRequestsToOpenViewsDelegateAvailable(do: { delegate in
             delegate.showConversationList(for: userSession)
         })
     }
-    
+
     public func userSession(_ userSession: ZMUserSession!, show conversation: ZMConversation!) {
         whenRequestsToOpenViewsDelegateAvailable(do: { delegate in
             delegate.userSession(userSession, show: conversation)
         })
     }
-    
+
     public func userSession(_ userSession: ZMUserSession!, show message: ZMMessage!, in conversation: ZMConversation!) {
         whenRequestsToOpenViewsDelegateAvailable(do: { delegate in
             delegate.userSession(userSession, show: message, in: conversation)
         })
     }
-    
+
     internal func whenRequestsToOpenViewsDelegateAvailable(do closure: @escaping (ZMRequestsToOpenViewsDelegate)->()) {
         if let delegate = self.requestToOpenViewDelegate {
             closure(delegate)
@@ -441,27 +445,30 @@ extension AppRootViewController : ZMRequestsToOpenViewsDelegate {
 
 // MARK: - Application Icon Badge Number
 
-extension AppRootViewController : LocalNotificationResponder {
+extension AppRootViewController: LocalNotificationResponder {
 
     func processLocal(_ notification: ZMLocalNotification, forSession session: ZMUserSession) {
         (self.overlayWindow.rootViewController as! NotificationWindowRootViewController).show(notification)
     }
-    
+
+    @objc fileprivate func applicationWillEnterForeground() {
+        updateOverlayWindowFrame()
+    }
+
     @objc fileprivate func applicationDidEnterBackground() {
         let unreadConversations = sessionManager?.accountManager.totalUnreadCount ?? 0
         UIApplication.shared.applicationIconBadgeNumber = unreadConversations
     }
 
     @objc fileprivate func applicationDidBecomeActive() {
-        overlayWindow.frame.size = UIScreen.main.bounds.size
+        updateOverlayWindowFrame()
     }
 }
 
-
 // MARK: - Session Manager Observer
 
-extension AppRootViewController : SessionManagerCreatedSessionObserver, SessionManagerDestroyedSessionObserver {
-    
+extension AppRootViewController: SessionManagerCreatedSessionObserver, SessionManagerDestroyedSessionObserver {
+
     func sessionManagerCreated(userSession: ZMUserSession) {
         for (accountId, session) in sessionManager?.backgroundUserSessions ?? [:] {
             if session == userSession {
@@ -469,17 +476,16 @@ extension AppRootViewController : SessionManagerCreatedSessionObserver, SessionM
             }
         }
     }
-    
+
     func sessionManagerDestroyedUserSession(for accountId: UUID) {
         soundEventListeners[accountId] = nil
     }
 }
 
-  
 // MARK: - Audio Permissions granted
 
-extension AppRootViewController  {
-    
+extension AppRootViewController {
+
     func onUserGrantedAudioPermissions() {
         sessionManager?.updateCallNotificationStyleFromSettings()
     }
@@ -487,7 +493,7 @@ extension AppRootViewController  {
 
 // MARK: - Transition form LandingViewController to RegistrationViewController
 
-extension AppRootViewController : LandingViewControllerDelegate {
+extension AppRootViewController: LandingViewControllerDelegate {
     func landingViewControllerDidChooseCreateTeam() {
         flowController.startFlow()
     }
