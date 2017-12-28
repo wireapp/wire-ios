e2e977743cad4ebcc754feada2a82132f46ebe1b
Bill Chan <billypchan@gmail.com>
2017-12-28 13:33:19 +0100

    Fix/voice channel view controller timer retain  (#1564)
    * proved VoiceChannelViewController's timer retains itself
    
    * fix merge conflict
    
    * rewrite Timer.scheduledTimer with .allVersionCompatibleScheduledTimer

---
+++ Wire-iOS Tests/VoiceChannelViewControllerTests.swift
@@ -0,0 +1,56 @@
+//
+// Wire
+// Copyright (C) 2017 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import XCTest
+@testable import Wire
+
+final class VoiceChannelViewControllerTests: XCTestCase {
+    
+    weak var sut: VoiceChannelViewController!
+    var mockConversation: MockConversation!
+
+    override func setUp() {
+        super.setUp()
+        mockConversation = MockConversation()
+        mockConversation.conversationType = .oneOnOne
+        mockConversation.displayName = "John Doe"
+        mockConversation.connectedUser = MockUser.mockUsers().last!
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+
+    func testVoiceChannelViewControllerIsNotRetainedAfterTimerIsScheduled(){
+        autoreleasepool{
+            // GIVEN
+
+            var voiceChannelViewController: VoiceChannelViewController! = VoiceChannelViewController(conversation: (mockConversation as Any) as! ZMConversation)
+            sut = voiceChannelViewController
+
+            voiceChannelViewController.startCallDurationTimer()
+            // WHEN
+            voiceChannelViewController = nil
+        }
+
+        // THEN
+        XCTAssertNil(sut)
+
+    }
+}
