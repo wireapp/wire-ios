8d8b8c7a9bb7669d588df5838354277111f2f801
Vytis Šibonis <vytis@wire.com>
2018-02-23 19:12:42 +0100

    Fix tests

--- Tests/Source/Utility/ZMUpdateEvent+Testing.swift
+++ Tests/Source/Utility/ZMUpdateEvent+Testing.swift
@@ -23,4 +23,8 @@ extension ZMUpdateEvent {
     public convenience override init() {
         self.init(uuid: nil, payload: ["type": "conversation.create"], transient: false, decrypted: false, source: .download)!
     }
+
+    @objc static func dummy() -> ZMUpdateEvent {
+        return ZMUpdateEvent()
+    }
 }
