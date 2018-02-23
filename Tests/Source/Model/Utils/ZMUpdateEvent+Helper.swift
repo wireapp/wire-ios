338a8599887e44776685331bb21f82d4213c729e
Vytis Šibonis <vytis@wire.com>
2018-02-23 18:23:59 +0100

    Fix tests

--- Tests/Source/Model/Utils/ZMUpdateEvent+Helper.swift
+++ Tests/Source/Model/Utils/ZMUpdateEvent+Helper.swift
@@ -21,6 +21,6 @@ import WireTransport
 
 extension ZMUpdateEvent {
     public convenience override init() {
-        self.init(uuid: nil, payload: [:], transient: false, decrypted: false, source: .download)!
+        self.init(uuid: nil, payload: ["type": "conversation.create"], transient: false, decrypted: false, source: .download)!
     }
 }
