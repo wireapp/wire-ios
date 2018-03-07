7d7737ad70f0d1db19023f9a317f49f01f36b621
Mike Gerasimenko <github@gerasimenko.me>
2018-03-07 10:45:05 +0100

    Do not show the options in non-team conversation (#1829)

--- Wire-iOS/Sources/UserInterface/GroupDetails/GroupDetailsViewController.swift
+++ Wire-iOS/Sources/UserInterface/GroupDetails/GroupDetailsViewController.swift
@@ -148,7 +148,7 @@ class GroupDetailsViewController: UIViewController, ZMConversationObserver, Grou
         sections.append(renameGroupSectionController)
         self.renameGroupSectionController = renameGroupSectionController
         
-        if !ZMUser.selfUser().isGuest(in: conversation) && ZMUser.selfUser().isTeamMember {
+        if conversation.team != nil && !ZMUser.selfUser().isGuest(in: conversation) && ZMUser.selfUser().isTeamMember {
             let guestOptionsSectionController = GuestOptionsSectionController(conversation: conversation, delegate: self, syncCompleted: didCompleteInitialSync)
             sections.append(guestOptionsSectionController)
         }
