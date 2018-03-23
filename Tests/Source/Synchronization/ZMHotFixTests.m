a771bc33c5bd602ed6d5391cebcd159ead2ed6ff
Silvan Daehn <silvandaehn@me.com>
2018-03-23 11:58:33 +0100

    Remove ZMVersion (moved to data-model) (#716)
    * Remove ZMVersion (moved to data-model)
    
    * Fix potentially comparing nil versions
    
    * Bump components

--- Tests/Source/Synchronization/ZMHotFixTests.m
+++ Tests/Source/Synchronization/ZMHotFixTests.m
@@ -25,66 +25,6 @@
 #import "ZMHotFixDirectory.h"
 #import <WireSyncEngine/WireSyncEngine-Swift.h>
 
-@interface VersionNumberTests : MessagingTest
-@end
-
-
-@implementation VersionNumberTests
-
-- (void)setUp {
-    [super setUp];
-}
-
-- (void)tearDown
-{
-    [super tearDown];
-}
-
-- (void)testThatItComparesCorrectly
-{
-    // given
-    NSString *version1String = @"0.1";
-    NSString *version2String = @"1.0";
-    NSString *version3String = @"1.0";
-    NSString *version4String = @"1.0.1";
-    NSString *version5String = @"1.1";
-    
-    ZMVersion *version1 = [[ZMVersion alloc] initWithVersionString:version1String];
-    ZMVersion *version2 = [[ZMVersion alloc] initWithVersionString:version2String];
-    ZMVersion *version3 = [[ZMVersion alloc] initWithVersionString:version3String];
-    ZMVersion *version4 = [[ZMVersion alloc] initWithVersionString:version4String];
-    ZMVersion *version5 = [[ZMVersion alloc] initWithVersionString:version5String];
-
-    // then
-    XCTAssertEqual([version1 compareWithVersion:version2], NSOrderedAscending);
-    XCTAssertEqual([version1 compareWithVersion:version3], NSOrderedAscending);
-    XCTAssertEqual([version1 compareWithVersion:version4], NSOrderedAscending);
-    XCTAssertEqual([version1 compareWithVersion:version5], NSOrderedAscending);
-
-    XCTAssertEqual([version2 compareWithVersion:version1], NSOrderedDescending);
-    XCTAssertEqual([version2 compareWithVersion:version3], NSOrderedSame);
-    XCTAssertEqual([version2 compareWithVersion:version4], NSOrderedAscending);
-    XCTAssertEqual([version2 compareWithVersion:version5], NSOrderedAscending);
-
-    XCTAssertEqual([version3 compareWithVersion:version1], NSOrderedDescending);
-    XCTAssertEqual([version3 compareWithVersion:version2], NSOrderedSame);
-    XCTAssertEqual([version3 compareWithVersion:version4], NSOrderedAscending);
-    XCTAssertEqual([version3 compareWithVersion:version5], NSOrderedAscending);
-
-    XCTAssertEqual([version4 compareWithVersion:version1], NSOrderedDescending);
-    XCTAssertEqual([version4 compareWithVersion:version2], NSOrderedDescending);
-    XCTAssertEqual([version4 compareWithVersion:version3], NSOrderedDescending);
-    XCTAssertEqual([version4 compareWithVersion:version5], NSOrderedAscending);
-    
-    XCTAssertEqual([version5 compareWithVersion:version1], NSOrderedDescending);
-    XCTAssertEqual([version5 compareWithVersion:version2], NSOrderedDescending);
-    XCTAssertEqual([version5 compareWithVersion:version3], NSOrderedDescending);
-    XCTAssertEqual([version5 compareWithVersion:version4], NSOrderedDescending);
-}
-
-@end
-
-
 
 
 @interface FakeHotFixDirectory : ZMHotFixDirectory
