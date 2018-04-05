fabe88cf2e16c8fc187b1391624ec59d75a568c4
Gerasimenko Mike <mike@gerasimenko.me>
2018-04-05 10:53:05 +0200

    Updated the operator name

--- Source/Composition.swift
+++ Source/Composition.swift
@@ -19,7 +19,7 @@
 import Foundation
 
 /// Function composition operator: (f1 ∘ f2)(x) = f1(f2(x))
-infix operator => : AdditionPrecedence
-public func => <A, B, C>(f: @escaping (B) -> C, g: @escaping (A) -> B) -> (A) -> C {
+infix operator >>> : AdditionPrecedence
+public func >>> <A, B, C>(f: @escaping (B) -> C, g: @escaping (A) -> B) -> (A) -> C {
     return { x in f(g(x)) }
 }
