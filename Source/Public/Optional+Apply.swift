59824a7a7ee8734bfa249ed7f682746cfdcb5995
Silvan Daehn <silvandaehn@me.com>
2018-04-11 16:40:31 +0200

    Fix infinite loop when performing a throwing grouped sync block (#45)
    * Fix infinite loop when performing a throwing grouped sync block
    
    * Fix added method not working without groups

--- Source/Public/Optional+Apply.swift
+++ Source/Public/Optional+Apply.swift
@@ -22,7 +22,7 @@ extension Optional {
     /// Like map, but intended to be used to perform side effects.
     /// Basically what `forEach` on `Collection` is compared to `map`, but for `Optional`.
     /// - parameter block: The closure to be executed in case self holds a value.
-    public func apply(_ block: ((Wrapped) -> Void)) {
+    public func apply(_ block: (Wrapped) -> Void) {
         if case .some(let unwrapped) = self {
             block(unwrapped)
         }
