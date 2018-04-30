0210a9789a55c2ffc7f55bd426fa696ba5ee939c
Vytis <vytis@users.noreply.github.com>
2018-04-30 14:18:38 +0200

    Using sync context for clearing import metadata (#482)

--- Source/ManagedObjectContext/NSManagedObjectContext+BackupImport.swift
+++ Source/ManagedObjectContext/NSManagedObjectContext+BackupImport.swift
@@ -22,6 +22,7 @@ extension NSManagedObjectContext {
     
     /// Prepare a backed up database for being imported, deleting self client, push token etc.
     func prepareToImportBackup() {
+        require(self.zm_isSyncContext, "Needs to be run on Sync Context to avoid race conditions")
         setPersistentStoreMetadata(nil as Data?, key: ZMPersistedClientIdKey)
         setPersistentStoreMetadata(nil as Data?, key: PersistentMetadataKey.importedFromBackup.rawValue)
         setPersistentStoreMetadata(nil as Data?, key: PersistentMetadataKey.pushToken.rawValue)
