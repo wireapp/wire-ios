94b297eb76fd5e4237ad8dc9f0164d4e6b22f837
Bill Chan <billypchan@gmail.com>
2018-05-08 11:13:40 +0200

    Fix/zios 9897 render transparent background images (png) correctly (fix for UIPasteboard) (#2067)
    * WIP PNG type for clip board content
    
    * encode mediaAsset as PNG
    
    * extension UIImage: MediaAsset
    
    * refeactering
    
    * clean up
    
    * rewrite UIPasteboard(MediaAsset) in Swift
    
    * fix unit test
    
    * update snapshot
    
    * refactoring
    
    * border color for dark variant
    
    * clean up
    
    * clean up
    
    * update snapshot

---
+++ Wire-iOS/Sources/UserInterface/Components/UIPasteboard+MediaAsset.swift
@@ -0,0 +1,55 @@
+//
+// Wire
+// Copyright (C) 2018 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+import MobileCoreServices
+
+extension UIPasteboard {
+
+    func pasteboardType(forMediaAsset mediaAsset: MediaAsset) -> String {
+        if mediaAsset.isGIF() {
+            return kUTTypeGIF as String
+        } else if mediaAsset.isTransparent() {
+            return kUTTypePNG as String
+        } else {
+            return kUTTypeJPEG as String
+        }
+    }
+
+    @objc public func mediaAsset() -> MediaAsset? {
+        if contains(pasteboardTypes: [kUTTypeGIF as String]) {
+            let data: Data? = self.data(forPasteboardType: kUTTypeGIF as String)
+            return FLAnimatedImage(animatedGIFData: data)
+        } else if contains(pasteboardTypes: [kUTTypePNG as String]) {
+            let data: Data? = self.data(forPasteboardType: kUTTypePNG as String)
+            if let aData = data {
+                return UIImage(data: aData)
+            }
+            return nil
+        } else if wr_hasImages() {
+            return image
+        }
+        return nil
+    }
+
+    func setMediaAsset(_ image: MediaAsset?) {
+        guard let image = image else { return }
+
+        UIPasteboard.general.setData(image.data(), forPasteboardType: pasteboardType(forMediaAsset: image))
+    }
+}
--- WireExtensionComponents/Views/TextView.m
+++ WireExtensionComponents/Views/TextView.m
@@ -26,6 +26,7 @@
 #import "UILabel+TextTransform.h"
 #import "UIPasteboard+Compatibility.h"
 #import <WireExtensionComponents/WireExtensionComponents-Swift.h>
+#import "Wire-Swift.h"
 @import Classy;
 
 static NSString* ZMLogTag ZM_UNUSED = @"UI";
