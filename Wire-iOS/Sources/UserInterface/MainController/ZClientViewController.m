ff22d33e82fcf01b0e59eab85b4342a42462da20
Bill Chan <billypchan@gmail.com>
2018-05-16 17:17:14 +0200

    Feature/zios 9990 as a person registering to wire account i want to be asked whether i want to send usage and crash reports to wire when i create any type of account (#2128)
    * show DataUsagePermissionDialog after registration
    When the user taps skip in the dialog, set disableCrashAndAnalyticsSharing to true
    
    * clean up
    
    * Update ConversationListViewController+DataUsagePermissions.swift
    
    * remove MUser.selfUser().handle != nil check

--- Wire-iOS/Sources/UserInterface/MainController/ZClientViewController.m
+++ Wire-iOS/Sources/UserInterface/MainController/ZClientViewController.m
@@ -130,6 +130,8 @@
         [[GroupConversationCell appearanceWhenContainedInInstancesOfClasses:@[StartUIView.class]] setColorSchemeVariant:ColorSchemeVariantDark];
         [[GroupConversationCell appearanceWhenContainedInInstancesOfClasses:@[StartUIView.class]] setContentBackgroundColor:UIColor.clearColor];
         [[UIView appearanceWhenContainedInInstancesOfClasses:@[UIAlertController.class]] setTintColor:[ColorScheme.defaultColorScheme colorWithName:ColorSchemeColorTextForeground variant:ColorSchemeVariantLight]];
+
+        [self setupConversationListViewController];
     }
     return self;
 }
@@ -143,8 +145,8 @@
     
     self.view.backgroundColor = [UIColor blackColor];
 
-    [self setupConversationListViewController];
-    
+    [self.conversationListViewController view];
+
     self.splitViewController = [[SplitViewController alloc] init];
     self.splitViewController.delegate = self;
     [self addChildViewController:self.splitViewController];
@@ -256,7 +258,6 @@
     self.conversationListViewController = [[ConversationListViewController alloc] init];
     self.conversationListViewController.isComingFromRegistration = self.isComingFromRegistration;
     self.conversationListViewController.needToShowDataUsagePermissionDialog = NO;
-    [self.conversationListViewController view];
 }
 
 #pragma mark - ZMUserObserver
