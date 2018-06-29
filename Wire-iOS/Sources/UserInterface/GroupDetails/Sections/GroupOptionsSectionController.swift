b52fc7e64c6b25f8922eb6faf4ff3162068b978d
Silvan Daehn <silvandaehn@me.com>
2018-06-29 13:58:07 +0200

    Add conversation option section title (#2348)
    * Add section title for conversation options menu
    
    * Remove hardcoded section height

--- Wire-iOS/Sources/UserInterface/GroupDetails/Sections/GroupOptionsSectionController.swift
+++ Wire-iOS/Sources/UserInterface/GroupDetails/Sections/GroupOptionsSectionController.swift
@@ -65,6 +65,10 @@ class GroupOptionsSectionController: GroupDetailsSectionController {
     }
 
     // MARK: - Collection View
+    
+    override var sectionTitle: String {
+        return "participants.section.settings".localized.uppercased()
+    }
 
     override func prepareForUse(in collectionView: UICollectionView?) {
         super.prepareForUse(in: collectionView)
@@ -74,10 +78,6 @@ class GroupOptionsSectionController: GroupDetailsSectionController {
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return options.count
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return CGSize(width: collectionView.bounds.size.width, height: 32)
     }
 
     override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
