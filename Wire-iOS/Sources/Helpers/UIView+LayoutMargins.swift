a01bc44b6ff9f2d456914cec9d80afeca8e7a1ae
Silvan Daehn <silvandaehn@me.com>
2018-06-29 12:25:15 +0200

    Delay showing loading spinners in multiple parts of the app (#2346)
    * Delay showing loading spinners in multiple parts of the app
    
    * Remove duplicate code

--- Wire-iOS/Sources/Helpers/UIView+LayoutMargins.swift
+++ Wire-iOS/Sources/Helpers/UIView+LayoutMargins.swift
@@ -1,9 +1,19 @@
 //
-//  UIView+ContentInsets.swift
-//  Wire-iOS
+// Wire
+// Copyright (C) 2018 Wire Swiss GmbH
 //
-//  Created by Jacob Persson on 08.03.18.
-//  Copyright © 2018 Zeta Project Germany GmbH. All rights reserved.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
 import Foundation
