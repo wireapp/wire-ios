0266f03c2d64edde058bf08a49a3bd8824374408
Silvan Daehn <silvandaehn@me.com>
2018-07-10 15:01:16 +0200

    Update participants presentation in group details view controller (#2386)
    * Adjust group details to only collapse when showing more than 7 participants
    
    * Add searchable group participants details list
    
    * Add snapshot test
    
    * Fix copyright header
    
    * Add close button and adjust tests
    
    * Fix copyright header year

---
+++ Wire-iOS/Sources/UserInterface/GroupDetails/GroupParticipantsDetail/GroupParticipantsDetailViewController.swift
@@ -0,0 +1,110 @@
+//
+// Wire
+// Copyright (C) 2018 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import UIKit
+
+final class GroupParticipantsDetailViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    private let collectionView = UICollectionView(forUserList: ())
+    private let searchViewController = SearchHeaderViewController(userSelection: .init(), variant: ColorScheme.default.variant)
+    private let viewModel: GroupParticipantsDetailViewModel
+    
+    weak var delegate: GroupDetailsUserDetailPresenter?
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return wr_supportedInterfaceOrientations
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return ColorScheme.default.statusBarStyle
+    }
+    
+    init(participants: [ZMBareUser], conversation: ZMConversation) {
+        viewModel = GroupParticipantsDetailViewModel(
+            participants: participants,
+            conversation: conversation
+        )
+
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        createConstraints()
+    }
+    
+    private func setupViews() {
+        addToSelf(searchViewController)
+        searchViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        searchViewController.delegate = viewModel
+        viewModel.participantsDidChange = collectionView.reloadData
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UserCell.self, forCellWithReuseIdentifier: UserCell.reuseIdentifier)
+        (collectionView.collectionViewLayout as! UICollectionViewFlowLayout).itemSize = .init(width: view.bounds.size.width, height: 56)
+        title = "participants.all.title".localized.uppercased()
+        view.backgroundColor = UIColor(scheme: .contentBackground)
+        navigationItem.rightBarButtonItem = navigationController?.closeItem()
+    }
+    
+    private func createConstraints() {
+        NSLayoutConstraint.activate([
+            searchViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchViewController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            searchViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: searchViewController.view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    // MARK: - UICollectionViewDelegateFlowLayout & UICollectionViewDataSource
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.participants.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: UserCell.reuseIdentifier, for: indexPath) as! UserCell
+        cell.configure(
+            with: .user(viewModel.participants[indexPath.row]),
+            conversation: viewModel.conversation,
+            showSeparator: viewModel.participants.count - 1 != indexPath.row
+        )
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let user = viewModel.participants[indexPath.row] as? ZMUser else { return }
+        delegate?.presentDetails(for: user)
+    }
+
+}
--- Wire-iOS/Sources/UserInterface/GroupDetails/Sections/ParticipantsSectionController.swift
+++ Wire-iOS/Sources/UserInterface/GroupDetails/Sections/ParticipantsSectionController.swift
@@ -18,59 +18,109 @@
 
 import Foundation
 
+protocol ParticipantsCellConfigurable: Reusable {
+    func configure(with rowType: ParticipantsRowType, conversation: ZMConversation, showSeparator: Bool)
+}
+
+enum ParticipantsRowType {
+    case user(ZMBareUser)
+    case showAll(Int)
+    
+    init(_ user: ZMBareUser) {
+        self = .user(user)
+    }
+    
+    var cellType: ParticipantsCellConfigurable.Type {
+        switch self {
+        case .user: return UserCell.self
+        case .showAll: return ShowAllParticipantsCell.self
+        }
+    }
+}
+
+private struct ParticipantsSectionViewModel {
+    static private let maxParticipants = 7
+    let rows: [ParticipantsRowType]
+    let participants: [ZMBareUser]
+    
+    var sectionAccesibilityIdentifier = "label.groupdetails.participants"
+    
+    var sectionTitle: String {
+        return "participants.section.participants".localized(args: participants.count).uppercased()
+    }
+
+    init(participants: [ZMBareUser]) {
+        self.participants = participants
+        rows = ParticipantsSectionViewModel.computeRows(participants)
+    }
+    
+    static func computeRows(_ participants: [ZMBareUser]) -> [ParticipantsRowType] {
+        guard participants.count > maxParticipants else { return participants.map(ParticipantsRowType.init) }
+        return participants[0..<5].map(ParticipantsRowType.init) + [.showAll(participants.count)]
+    }
+}
+
+extension UserCell: ParticipantsCellConfigurable {
+    func configure(with rowType: ParticipantsRowType, conversation: ZMConversation, showSeparator: Bool) {
+        guard case let .user(user) = rowType else { preconditionFailure() }
+        configure(with: user, conversation: conversation)
+        accessoryIconView.isHidden = false
+        accessibilityIdentifier = "participants.section.participants.cell"
+        self.showSeparator = showSeparator
+    }
+}
 
 class ParticipantsSectionController: GroupDetailsSectionController {
     
     fileprivate weak var collectionView: UICollectionView?
     private weak var delegate: GroupDetailsSectionControllerDelegate?
-    private let participants: [ZMBareUser]
+    private let viewModel: ParticipantsSectionViewModel
     private let conversation: ZMConversation
     private var token: AnyObject?
     
     init(participants: [ZMBareUser], conversation: ZMConversation, delegate: GroupDetailsSectionControllerDelegate) {
-        self.participants = participants
+        viewModel = .init(participants: participants)
         self.conversation = conversation
         self.delegate = delegate
-        
         super.init()
-        
         token = UserChangeInfo.add(observer: self, for: nil, userSession: ZMUserSession.shared()!)
     }
     
     override func prepareForUse(in collectionView : UICollectionView?) {
         super.prepareForUse(in: collectionView)
-        
-        collectionView?.register(UserCell.self, forCellWithReuseIdentifier: UserCell.zm_reuseIdentifier)
-        
+        collectionView?.register(UserCell.self, forCellWithReuseIdentifier: UserCell.reuseIdentifier)
+        collectionView?.register(ShowAllParticipantsCell.self, forCellWithReuseIdentifier: ShowAllParticipantsCell.reuseIdentifier)
         self.collectionView = collectionView
     }
     
     override var sectionTitle: String {
-        return "participants.section.participants".localized(args: participants.count).uppercased()
+        return viewModel.sectionTitle
     }
     
     override var sectionAccessibilityIdentifier: String {
-        return "label.groupdetails.participants"
+        return viewModel.sectionAccesibilityIdentifier
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return participants.count
+        return viewModel.rows.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let user = participants[indexPath.row]
-        let cell = collectionView.dequeueReusableCell(ofType: UserCell.self, for: indexPath)
-        
-        cell.configure(with: user, conversation: conversation)
-        cell.showSeparator = (participants.count - 1) != indexPath.row
-        cell.accessoryIconView.isHidden = false
-        cell.accessibilityIdentifier = "participants.section.participants.cell"
+        let configuration = viewModel.rows[indexPath.row]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: configuration.cellType.reuseIdentifier, for: indexPath) as! ParticipantsCellConfigurable & UICollectionViewCell
+        let showSeparator = (viewModel.rows.count - 1) != indexPath.row
+        cell.configure(with: configuration, conversation: conversation, showSeparator: showSeparator)
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        guard let user = participants[indexPath.row] as? ZMUser else { return }
-        delegate?.presentDetails(for: user)
+        switch viewModel.rows[indexPath.row] {
+        case .user(let bareUser):
+            guard let user = bareUser as? ZMUser else { return }
+            delegate?.presentDetails(for: user)
+        case .showAll:
+            delegate?.presentFullParticipantsList(for: viewModel.participants, in: conversation)
+        }
     }
     
 }
@@ -79,7 +129,6 @@ extension ParticipantsSectionController: ZMUserObserver {
     
     func userDidChange(_ changeInfo: UserChangeInfo) {
         guard changeInfo.connectionStateChanged || changeInfo.nameChanged else { return }
-        
         collectionView?.reloadData()
     }
     
