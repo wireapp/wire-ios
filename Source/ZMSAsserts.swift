4a338ed29a8b601b5a784855b5ac1b1370b4ea02
Silvan Daehn <silvandaehn@me.com>
2018-07-17 14:09:09 +0200

    Add requireInternalFailure function not needing a condition (#29)

--- Source/ZMSAsserts.swift
+++ Source/ZMSAsserts.swift
@@ -63,3 +63,9 @@ public func requireInternal(_ condition: Bool, _ message: @autoclosure () -> Str
     guard !Environment.current.isAppStore, !condition else { return }
     fatal(message(), file: file, line: line)
 }
+
+/// Termiantes the application if the current build is not an AppsStore build
+public func requireInternalFailure(_ message: @autoclosure () -> String, file: String = #file, line: Int = #line) {
+    guard !Environment.current.isAppStore else { return }
+    fatal(message(), file: file, line: line)
+}
