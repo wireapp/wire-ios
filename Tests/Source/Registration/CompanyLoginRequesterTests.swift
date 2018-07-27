67d46dca738ad37416860d61ceb081592d78b636
Silvan Daehn <silvandaehn@me.com>
2018-07-27 14:26:46 +0200

    Add first implementation of company token validation logic (#819)
    * Add first implementation of company token validation logic
    
    * Use URLSession instead of transport session
    
    * Adjust API and add error parsing
    
    * Include callback urls in validation check url, add tests

--- Tests/Source/Registration/CompanyLoginRequesterTests.swift
+++ Tests/Source/Registration/CompanyLoginRequesterTests.swift
@@ -51,18 +51,168 @@ class CompanyLoginRequesterTests: XCTestCase {
 
         // THEN
         guard let validationURL = url else {
-            XCTFail("The requester did not call the delegate.")
-            return
+           return XCTFail("The requester did not call the delegate.")
         }
 
         guard let components = URLComponents(url: validationURL, resolvingAgainstBaseURL: false) else {
-            XCTFail("The requester did not request to open a valid URL.")
-            return
+            return XCTFail("The requester did not request to open a valid URL.")
         }
 
         XCTAssertEqual(components.query(for: "success_redirect"), "wire://login/success?cookie=$cookie&userid=$userid&validation_token=\(validationIdentifier)")
         XCTAssertEqual(components.query(for: "error_redirect"), "wire://login/failure?label=$label&validation_token=\(validationIdentifier)")
         XCTAssertEqual(validationURL.absoluteString.removingPercentEncoding, expectedURL.absoluteString)
     }
+    
+    func testThatItReturnsNilWhenVerifyingTokenNoError() {
+        // Given
+        let session = MockSession { url in
+            let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+            return (nil, response, nil)
+        }
 
+        let sut = CompanyLoginRequester(
+            backendHost: "localhost",
+            callbackScheme: "wire",
+            defaults: UserDefaults(suiteName: name)!,
+            session: session
+        )
+        
+        let callbackExpectation = expectation(description: "The completion closure is called")
+        
+        // When
+        sut.validate(token: .create()) { error in
+            XCTAssertNil(error)
+            callbackExpectation.fulfill()
+        }
+        
+        // Then
+        waitForExpectations(timeout: 0.5, handler: nil)
+    }
+    
+    func testThatItReturnsInvalidCodeErrorFor404Response() {
+        // Given
+        let session = MockSession { url in
+            let response = HTTPURLResponse(url: url, statusCode: 404, httpVersion: nil, headerFields: nil)
+            return (nil, response, nil)
+        }
+        
+        let sut = CompanyLoginRequester(
+            backendHost: "localhost",
+            callbackScheme: "wire",
+            defaults: UserDefaults(suiteName: name)!,
+            session: session
+        )
+        
+        let callbackExpectation = expectation(description: "The completion closure is called")
+        
+        // When
+        sut.validate(token: .create()) { error in
+            XCTAssertEqual(error, .invalidCode)
+            callbackExpectation.fulfill()
+        }
+        
+        // Then
+        waitForExpectations(timeout: 0.5, handler: nil)
+    }
+    
+    func testThatItReturnsUnknownErrorForServerError() {
+        // Given
+        let session = MockSession { url in
+            let response = HTTPURLResponse(url: url, statusCode: 500, httpVersion: nil, headerFields: nil)
+            return (nil, response, nil)
+        }
+        
+        let sut = CompanyLoginRequester(
+            backendHost: "localhost",
+            callbackScheme: "wire",
+            defaults: UserDefaults(suiteName: name)!,
+            session: session
+        )
+        
+        let callbackExpectation = expectation(description: "The completion closure is called")
+        
+        // When
+        sut.validate(token: .create()) { error in
+            XCTAssertEqual(error, .unknown)
+            callbackExpectation.fulfill()
+        }
+        
+        // Then
+        waitForExpectations(timeout: 0.5, handler: nil)
+    }
+    
+    func testThatItReturnsUnknownErrorForTransportError() {
+        // Given
+        let session = MockSession { url in
+            let error = NSError(domain: "", code: NSURLErrorTimedOut, userInfo: nil)
+            return (nil, nil, error)
+        }
+        
+        let sut = CompanyLoginRequester(
+            backendHost: "localhost",
+            callbackScheme: "wire",
+            defaults: UserDefaults(suiteName: name)!,
+            session: session
+        )
+        
+        let callbackExpectation = expectation(description: "The completion closure is called")
+        
+        // When
+        sut.validate(token: .create()) { error in
+            XCTAssertEqual(error, .unknown)
+            callbackExpectation.fulfill()
+        }
+        
+        // Then
+        waitForExpectations(timeout: 0.5, handler: nil)
+    }
+    
+    func testThatItReturnsUnknownErrorForInvalidResponse() {
+        // Given
+        let session = MockSession { _ in (nil, nil, nil) }
+
+        let sut = CompanyLoginRequester(
+            backendHost: "localhost",
+            callbackScheme: "wire",
+            defaults: UserDefaults(suiteName: name)!,
+            session: session
+        )
+        
+        let callbackExpectation = expectation(description: "The completion closure is called")
+        
+        // When
+        sut.validate(token: .create()) { error in
+            XCTAssertEqual(error, .unknown)
+            callbackExpectation.fulfill()
+        }
+        
+        // Then
+        waitForExpectations(timeout: 0.5, handler: nil)
+    }
+
+}
+
+// MARK: - Helper
+
+fileprivate class MockSession: NSObject, URLSessionProtocol {
+
+    class MockURLSessionDataTask: URLSessionDataTask {
+        override func resume() {
+            // no-op
+        }
+    }
+
+    typealias RequestHandler = (URL) -> (Data?, URLResponse?, Error?)
+    let handler: RequestHandler
+    
+    init(handler: @escaping RequestHandler) {
+        self.handler = handler
+        super.init()
+    }
+    
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        let (data, response, error) = handler(url)
+        completionHandler(data, response, error)
+        return MockURLSessionDataTask()
+    }
 }
