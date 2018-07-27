2cdf2e1ed7fdaf83d42eea6f1a59fedd66bb7a18
Jacob Persson <jacob.persson@gmail.com>
2018-07-27 10:23:26 +0200

    Change test to reflect updated behaviour for ignored calls [skip ci]

--- Tests/Source/Integration/CallingV3Tests.swift
+++ Tests/Source/Integration/CallingV3Tests.swift
@@ -513,12 +513,8 @@ class CallingV3Tests : IntegrationTest {
         // and when
         closeCall(user: user, reason: .canceled)
         
-        // then
-        XCTAssertEqual(convObserver!.notifications.count, 2)
-        if let change = convObserver!.notifications.lastObject as? ConversationChangeInfo {
-            XCTAssertTrue(change.conversationListIndicatorChanged)
-        }
-        XCTAssertEqual(conversationUnderTest.conversationListIndicator, .missedCall)
+        // then (We don't show a missed call indicator for calls which the self user previously ignored)
+        XCTAssertEqual(conversationUnderTest.conversationListIndicator, .none)
     }
 
     func testThatItFiresAConversationChangeNotificationWhenAGroupCallIsJoined() {
