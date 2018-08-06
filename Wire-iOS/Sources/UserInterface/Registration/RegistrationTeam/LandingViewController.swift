b067e236b55e8ed2513ab7483cd6f916f6decd5d
Silvan Daehn <silvandaehn@me.com>
2018-08-06 10:48:39 +0200

    Show status bar in landing view controller (#2498)
    * Show status bar in landing view controller
    
    * Hide status bar in tests
    
    * Fix status bar in tests

--- Wire-iOS/Sources/UserInterface/Registration/RegistrationTeam/LandingViewController.swift
+++ Wire-iOS/Sources/UserInterface/Registration/RegistrationTeam/LandingViewController.swift
@@ -182,6 +182,7 @@ final class LandingViewController: UIViewController, CompanyLoginControllerDeleg
         companyLoginController?.isAutoDetectionEnabled = true
         companyLoginController?.detectLoginCode()
         token = PreLoginAuthenticationNotification.register(self, for: SessionManager.shared?.unauthenticatedSession)
+        UIApplication.shared.wr_updateStatusBarForCurrentControllerAnimated(animated)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -362,7 +363,17 @@ final class LandingViewController: UIViewController, CompanyLoginControllerDeleg
     }
 
     override var prefersStatusBarHidden: Bool {
-        return true
+        // FIXME: We have to hide the status bar when running tests as we are not using a test host.
+        // Some view controllers we test using snapshots requets a status bar appearance update when
+        // they appear. If that happens we will query the topmost view controller which will be the
+        // LandingViewController in tests as we are using no test host application to run tests.
+        // Unfortunately this hack is needed until we either change how we manage the status bar or
+        // start using a test host to run tests.
+        return ProcessInfo.processInfo.isRunningTests
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .default
     }
     
     // MARK: - CompanyLoginControllerDelegate
