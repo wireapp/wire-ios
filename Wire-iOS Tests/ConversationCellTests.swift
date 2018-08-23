aca225b8ad3a16c65bafd7376a29c304c64a879a
Bill Chan <billypchan@gmail.com>
2018-08-23 13:47:31 +0200

    Remove Classy styling for ConversationCellBurstTimestampView (#2567)
    * remove burst time stamp style
    
    * update snapshots for character spacing
    
    * burstTimestampView prepareForReuse method
    
    * set the color in one place
    
    * remove prepareForReuse and update tests

--- Wire-iOS Tests/ConversationCellTests.swift
+++ Wire-iOS Tests/ConversationCellTests.swift
@@ -31,6 +31,24 @@ final class ConversationCellTests: XCTestCase {
     override func tearDown() {
         sut = nil
         super.tearDown()
+        ColorScheme.default.variant = .light
+    }
+
+    func testThatBurstTimestampViewColorIsCorrectInLightTheme() {
+        // GIVEN & WHEN
+        let sut = ConversationCell(style: .default, reuseIdentifier: nil)
+
+        // THEN
+        XCTAssertEqual(sut.burstTimestampView.label.textColor, UIColor(scheme: .textForeground, variant:.light))
+    }
+
+    func testThatBurstTimestampViewColorIsCorrectInDarkTheme() {
+        // GIVEN & WHEN
+        ColorScheme.default.variant = .dark
+        let sut = ConversationCell(style: .default, reuseIdentifier: nil)
+
+        // THEN
+        XCTAssertEqual(sut.burstTimestampView.label.textColor, UIColor(scheme: .textForeground, variant:.dark))
     }
 
     func testConversationCellIsNotRetainedAfterTimerIsScheduled() {
