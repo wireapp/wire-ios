2910df5be8c1038bdeeabf7d0bc1c133c9976dba
Bill Chan <billypchan@gmail.com>
2018-08-27 11:16:51 +0200

    Fix/translate remain tests (#2583)
    * translate MockMessageFactory into Swift
    
    * clean up
    
    * fix for objc callers
    
    * fix dead loop
    
    * fix range of the user array
    
    * translated ParticipantDeviceCellTests
    
    * translated IconSystemCellTests
    
    * translated ConversationListBottomBarControllerTests
    
    * move snapshot ref images
    
    * translated ProfilePictureStepViewControllerTests
    
    * undo ConversationListBottomBarControllerTests
    
    * undo snapshot location
    
    * translated ConversationTitleViewTests
    
    * ArchivedNavigationBarTests
    
    * DeleteMessageTests
    
    * ConversationListBottomBarControllerTests
    
    * move snapshots
    
    * clean up
    
    * clean up
    
    * clean up
    
    * fix compiler error

---
+++ Wire-iOS Tests/ConversationListBottomBarControllerTests.swift
@@ -0,0 +1,146 @@
+//
+// Wire
+// Copyright (C) 2018 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import XCTest
+@testable import Wire
+
+class MockConversationListBottomBarDelegate: NSObject, ConversationListBottomBarControllerDelegate {
+    func conversationListBottomBar(_ bar: ConversationListBottomBarController, didTapButtonWithType buttonType: ConversationListButtonType) {
+        switch (buttonType) {
+        case .archive:
+            self.archiveButtonTapCount += 1
+        case .compose:
+            self.composeButtonCallCount += 1
+        case .camera:
+            self.cameraButtonTapCount += 1
+        case .startUI:
+            self.startUIButtonCallCount += 1
+        }
+    }
+
+    var startUIButtonCallCount: Int = 0
+    var archiveButtonTapCount: Int = 0
+    var cameraButtonTapCount: Int = 0
+    var composeButtonCallCount: Int = 0
+}
+
+final class ConversationListBottomBarControllerTests: ZMSnapshotTestCase {
+    
+    var sut: ConversationListBottomBarController!
+    var mockDelegate: MockConversationListBottomBarDelegate!
+
+    override func setUp() {
+        super.setUp()
+
+        snapshotBackgroundColor = UIColor(white: 0.2, alpha: 1) // In order to make the separator more visible
+        accentColor = .brightYellow
+        mockDelegate = MockConversationListBottomBarDelegate()
+        UIView.performWithoutAnimation({
+            self.sut = ConversationListBottomBarController(delegate: self.mockDelegate)
+        })
+    }
+    
+    override func tearDown() {
+        sut = nil
+        mockDelegate = nil
+        
+        super.tearDown()
+    }
+
+    func testThatItRendersTheBottomBarCorrectlyInInitialState() {
+        // when
+        XCTAssertFalse(sut.showSeparator)
+
+        // then
+        verifyInAllPhoneWidths(view: sut.view)
+    }
+
+    func testThatTheSeparatorIsNotHiddenWhen_ShowSeparator_IsSetToYes() {
+        // when
+        sut.showSeparator = true
+
+        // then
+        XCTAssertFalse(sut.separator.isHidden)
+        verifyInAllPhoneWidths(view: sut.view)
+    }
+
+    func testThatItHidesTheContactsTitleAndShowsArchivedButtonWhen_ShowArchived_IsSetToYes() {
+        // when
+        sut.showArchived = true
+
+        // then
+        verifyInAllPhoneWidths(view: sut.view)
+    }
+
+    func testThatItShowsTheContactsTitleAndHidesTheArchivedButtonWhen_ShowArchived_WasSetToYesAndIsSetToNo() {
+        // given
+        accentColor = .strongBlue // To make the snapshot distinguishable from the inital state
+        sut.showArchived = true
+
+        // when
+        sut.showArchived = false
+
+        // then
+        verifyInAllPhoneWidths(view: sut.view)
+    }
+
+    func testThatItCallsTheDelegateWhenTheContactsButtonIsTapped() {
+        // when
+        sut.startUIButton.sendActions(for: .touchUpInside)
+
+        // then
+        XCTAssertEqual(mockDelegate.startUIButtonCallCount, 1)
+        XCTAssertEqual(mockDelegate.archiveButtonTapCount, 0)
+        XCTAssertEqual(mockDelegate.cameraButtonTapCount, 0)
+        XCTAssertEqual(mockDelegate.composeButtonCallCount, 0)
+    }
+
+    func testThatItCallsTheDelegateWhenTheArchivedButtonIsTapped() {
+        // when
+        sut.archivedButton.sendActions(for: .touchUpInside)
+
+        // then
+        XCTAssertEqual(mockDelegate.startUIButtonCallCount, 0)
+        XCTAssertEqual(mockDelegate.archiveButtonTapCount, 1)
+        XCTAssertEqual(mockDelegate.cameraButtonTapCount, 0)
+        XCTAssertEqual(mockDelegate.composeButtonCallCount, 0)
+    }
+
+    func testThatItCallsTheDelegateWhenTheCameraButtonIsTapped() {
+        // when
+        sut.cameraButton.sendActions(for: .touchUpInside)
+
+        // then
+        XCTAssertEqual(mockDelegate.startUIButtonCallCount, 0)
+        XCTAssertEqual(mockDelegate.archiveButtonTapCount, 0)
+        XCTAssertEqual(mockDelegate.cameraButtonTapCount, 1)
+        XCTAssertEqual(mockDelegate.composeButtonCallCount, 0)
+    }
+
+    func testThatItCallsTheDelegateWhenTheComposeButtonIsTapped() {
+        // when
+        sut.composeButton.sendActions(for: .touchUpInside)
+
+        // then
+        XCTAssertEqual(mockDelegate.startUIButtonCallCount, 0)
+        XCTAssertEqual(mockDelegate.archiveButtonTapCount, 0)
+        XCTAssertEqual(mockDelegate.cameraButtonTapCount, 0)
+        XCTAssertEqual(mockDelegate.composeButtonCallCount, 1)
+    }
+
+}
---
+++ Wire-iOS Tests/ConversationTitleViewTests.swift
@@ -0,0 +1,80 @@
+//
+// Wire
+// Copyright (C) 2018 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import XCTest
+@testable import Wire
+
+final class ConversationTitleViewTests: ZMSnapshotTestCase {
+    
+    var sut: ConversationTitleView!
+    var conversation: MockConversation!
+
+    override func setUp() {
+        super.setUp()
+        conversation = MockConversation()
+        conversation.relatedConnectionState = .accepted
+        conversation.displayName = "Alan Turing"
+        sut = ConversationTitleView(conversation: conversation as Any as! ZMConversation, interactive: true)
+        snapshotBackgroundColor = UIColor.white
+    }
+    
+    override func tearDown() {
+        sut = nil
+        conversation = nil
+
+        super.tearDown()
+    }
+
+    func testThatItRendersTheConversationDisplayNameCorrectly() {
+        verify(view: sut)
+    }
+
+    func testThatItUpdatesTheTitleViewAndRendersTheVerifiedShieldCorrectly() {
+        // when
+        conversation.securityLevel = .secure
+        sut = ConversationTitleView(conversation: conversation as Any as! ZMConversation, interactive: true)
+
+        // then
+        verify(view: sut)
+    }
+
+    func testThatItDoesNotRenderTheDownArrowForOutgoingConnections() {
+        // when
+        conversation.relatedConnectionState = .sent
+        sut = ConversationTitleView(conversation: conversation as Any as! ZMConversation, interactive: true)
+
+        // then
+        verify(view: sut)
+    }
+
+    func testThatItExecutesTheTapHandlerOnTitleTap() {
+        // given
+        var callCount: Int = 0
+        sut.tapHandler = { button in
+            callCount += 1
+        }
+
+        XCTAssertEqual(callCount, 0)
+
+        // when
+        sut.titleButton.sendActions(for: .touchUpInside)
+
+        // then
+        XCTAssertEqual(callCount, 1)
+    }
+}
