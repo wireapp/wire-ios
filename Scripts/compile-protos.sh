1aae7bc5eb812b56587865b3becd5b06cb4e8a5a
Jacob Persson <jacob.persson@gmail.com>
2018-09-12 11:39:24 +0200

    Update and  re-compile protobufs (#20)
    * Bump protobuf deps
    
    * Don't assume location of protoc
    
    * Compile protobuf classes

--- Scripts/compile-protos.sh
+++ Scripts/compile-protos.sh
@@ -9,7 +9,7 @@ cd ${CARTHAGE}/Checkouts/generic-message-proto/ios/
 cp ./../proto/messages.proto ./
 mkdir -p ./tmp
 cat ios.proto messages.proto > ./tmp/messages.proto
-/usr/local/bin/protoc ./tmp/messages.proto --objc_out="${BASE_FOLDER}/Protos/" --plugin=/usr/local/bin/protoc-gen-objc
+protoc ./tmp/messages.proto --objc_out="${BASE_FOLDER}/Protos/" --plugin=/usr/local/bin/protoc-gen-objc
 rm -rf ./tmp
 rm messages.proto
 
@@ -18,7 +18,7 @@ cd ${CARTHAGE}/Checkouts/backend-api-protobuf/ios/
 cp ./../proto/otr.proto ./
 mkdir -p ./tmp
 cat ios.proto otr.proto > ./tmp/otr.proto
-/usr/local/bin/protoc ./tmp/otr.proto --objc_out="${BASE_FOLDER}/Protos/" --plugin=/usr/local/bin/protoc-gen-objc
+protoc ./tmp/otr.proto --objc_out="${BASE_FOLDER}/Protos/" --plugin=/usr/local/bin/protoc-gen-objc
 rm -rf ./tmp
 rm otr.proto
 
