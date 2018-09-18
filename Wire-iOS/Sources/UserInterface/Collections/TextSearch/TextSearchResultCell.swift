103eacc4b70c3b3d0d34e1f6c25fbc0dab0056a9
John Nguyen <johnxuannguyen@gmail.com>
2018-09-18 14:49:37 +0200

    Fix ambiguous contraints (#2674)

--- Wire-iOS/Sources/UserInterface/Collections/TextSearch/TextSearchResultCell.swift
+++ Wire-iOS/Sources/UserInterface/Collections/TextSearch/TextSearchResultCell.swift
@@ -133,6 +133,7 @@ internal class TextSearchResultCell: UITableViewCell {
         
         resultCountView.isHidden = totalMatches <= 1
         resultCountView.textLabel.text = "\(totalMatches)"
+        resultCountView.updateCollapseConstraints(isCollapsed: false)
     }
     
     public func configure(with newMessage: ZMConversationMessage, queries newQueries: [String]) {
