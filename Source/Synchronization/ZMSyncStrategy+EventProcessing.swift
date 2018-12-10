ed7f4ff8e58f8b408396d633cb6e75093ae88359
Jacob Persson <jacob.persson@gmail.com>
2018-12-10 15:57:21 +0100

    Process each event completely before processing the next one (#923)
    * Process each event completely before processing the next one
    
    * Remove autorelease pool

--- Source/Synchronization/ZMSyncStrategy+EventProcessing.swift
+++ Source/Synchronization/ZMSyncStrategy+EventProcessing.swift
@@ -40,10 +40,10 @@ extension ZMSyncStrategy: ZMUpdateEventConsumer {
             let prefetchResult = syncMOC.executeFetchRequestBatchOrAssert(fetchRequest)
             
             Logging.eventProcessing.info("Consuming: [\n\(decryptedUpdateEvents.map({ "\tevent: \(ZMUpdateEvent.eventTypeString(for: $0.type) ?? "Unknown")" }).joined(separator: "\n"))\n]")
-            
-            for eventConsumer in self.eventConsumers {
-                autoreleasepool {
-                    eventConsumer.processEvents(decryptedUpdateEvents, liveEvents: true, prefetchResult: prefetchResult)
+        
+            for event in decryptedUpdateEvents {
+                for eventConsumer in self.eventConsumers {
+                    eventConsumer.processEvents([event], liveEvents: true, prefetchResult: prefetchResult)
                 }
             }
             
