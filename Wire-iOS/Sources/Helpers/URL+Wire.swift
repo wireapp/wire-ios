f609f49e403852171abd33d75e4e4fe0b6d4f1bb
Bill Chan <billypchan@gmail.com>
2018-12-20 01:50:32 -0800

    Store URLs in a JSON file (#3051)
    * create WireUrl for reading json file
    
    create tests
    
    * move all URLs to json filw
    
    * test for NSURL static func
    
    * remove snapshot test
    
    * clean up
    
    * refactor with Codable protocol
    
    * replace dictionary with URL properties
    
    * move url.json to config

--- Wire-iOS/Sources/Helpers/URL+Wire.swift
+++ Wire-iOS/Sources/Helpers/URL+Wire.swift
@@ -16,7 +16,9 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-import UIKit
+import Foundation
+
+private let zmLog = ZMSLog(tag: "URL")
 
 @objc enum TeamSource: Int {
     case onboarding, settings
@@ -29,15 +31,80 @@ import UIKit
     }
 }
 
+struct WireUrl: Codable {
+    let wireAppOnItunes: URL
+
+    let emailAlreadyInUseLearnMore: URL
+    let usernameLearnMore: URL
+    let fingerprintLearnMore: URL
+    let fingerprintHowToVerify: URL
+    let privacyPolicy: URL
+    let licenseInformation: URL
+    let website: URL
+    let passwordReset: URL
+    let support: URL
+    let askSupport: URL
+    let reportAbuse: URL
+    let cannotDecryptHelp: URL
+    let cannotDecryptNewRemoteIDHelp: URL
+    let createTeam: URL
+    let createTeamFeatures: URL
+    let manageTeam: URL
+    let emailInUseLearnMore: URL
+    let randomProfilePictureSource: URL
+
+    let termsOfServicesTeams: URL
+    let termsOfServicesPersonal: URL
+
+    let manageTeamBase: URL
+
+    static var shared: WireUrl! = {
+        guard let filePath = Bundle.main.url(forResource: "url", withExtension: "json") else {
+            zmLog.error("Failed to get URL from bundle")
+            return nil
+        }
+
+        return WireUrl(filePath: filePath)
+    }()
+
+    private init?(filePath: URL) {
+
+        let data: Data
+        do {
+            data = try Data(contentsOf: filePath)
+        } catch {
+            zmLog.error("Failed to load URL at path: \(filePath), error: \(error)")
+            return nil
+        }
+
+        let decoder = JSONDecoder()
+
+        do {
+            self = try decoder.decode(WireUrl.self, from: data)
+        } catch {
+            zmLog.error("Failed to parse JSON at path: \(filePath), error: \(error)")
+            return nil
+        }
+    }
+}
+
 extension URL {
-    
+
     var appendingLocaleParameter: URL {
         return (self as NSURL).wr_URLByAppendingLocaleParameter() as URL
     }
-    
+
     static func manageTeam(source: TeamSource) -> URL {
-        let query = "utm_source=\(source.parameterValue)&utm_term=ios"
-        return URL(string: "https://teams.wire.com/login?\(query)")!.appendingLocaleParameter
+        let baseURL = WireUrl.shared.manageTeamBase
+
+        let queryItems = [URLQueryItem(name: "utm_source", value: source.parameterValue),
+                          URLQueryItem(name: "utm_term", value: "ios")]
+
+        var components = URLComponents(url: baseURL, resolvingAgainstBaseURL: false)
+
+        components?.queryItems = queryItems
+
+        return components!.url!.appendingLocaleParameter
     }
 }
 
@@ -46,86 +113,86 @@ extension URL {
 extension URL {
 
     static var wr_wireAppOnItunes: URL {
-        return URL(string: "https://geo.itunes.apple.com/us/app/wire/id930944768?mt=8")!
+        return WireUrl.shared.wireAppOnItunes
     }
 
     static var wr_emailAlreadyInUseLearnMore: URL {
-        return URL(string: "https://support.wire.com/hc/en-us/articles/115004082129-My-email-address-is-already-in-use-and-I-cannot-create-an-account-What-can-I-do-")!
+        return WireUrl.shared.emailAlreadyInUseLearnMore
     }
 
     static var wr_usernameLearnMore: URL {
-        return URL(string: "https://wire.com/support/username/")!
+        return WireUrl.shared.usernameLearnMore
     }
 
     static var wr_fingerprintLearnMore: URL {
-        return URL(string: "https://wire.com/privacy/why")!
+        return WireUrl.shared.fingerprintLearnMore
     }
 
     static var wr_fingerprintHowToVerify: URL {
-        return URL(string: "https://wire.com/privacy/how")!
+        return WireUrl.shared.fingerprintHowToVerify
     }
 
     static var wr_privacyPolicy: URL {
-        return URL(string: "https://wire.com/legal/privacy/embed/")!
+        return WireUrl.shared.privacyPolicy
     }
 
     static var wr_licenseInformation: URL {
-        return URL(string: "https://wire.com/legal/licenses/embed/")!
+        return WireUrl.shared.licenseInformation
     }
 
     static var wr_website: URL {
-        return URL(string: "https://wire.com")!
+        return WireUrl.shared.website
     }
 
     static var wr_passwordReset: URL {
-        return URL(string: "https://account.wire.com/forgot/")!
+        return WireUrl.shared.passwordReset
     }
 
     static var wr_support: URL {
-        return URL(string: "https://support.wire.com")!
+        return WireUrl.shared.support
     }
 
     static var wr_askSupport: URL {
-        return URL(string: "https://support.wire.com/hc/requests/new")!
+        return WireUrl.shared.askSupport
     }
 
     static var wr_reportAbuse: URL {
-        return URL(string: "https://wire.com/support/misuse/")!
+        return WireUrl.shared.reportAbuse
     }
 
     static var wr_cannotDecryptHelp: URL {
-        return URL(string: "https://wire.com/privacy/error-1")!
+        return WireUrl.shared.cannotDecryptHelp
     }
 
     static var wr_cannotDecryptNewRemoteIDHelp: URL {
-        return URL(string: "https://wire.com/privacy/error-2")!
+        return WireUrl.shared.cannotDecryptNewRemoteIDHelp
     }
 
     static var wr_createTeam: URL {
-        return URL(string: "https://wire.com/create-team?pk_campaign=client&pk_kwd=ios")!
+        return WireUrl.shared.createTeam
     }
 
     static var wr_createTeamFeatures: URL {
-        return URL(string: "https://wire.com/teams/learnmore/")!
+        return WireUrl.shared.createTeamFeatures
     }
 
     static var wr_manageTeam: URL {
-        return URL(string: "https://teams.wire.com/login?pk_campaign=client&pk_kwd=ios")!
+        return WireUrl.shared.manageTeam
     }
 
     static var wr_emailInUseLearnMore: URL {
-        return URL(string: "https://wire.com/support/email-in-use")!
+        return WireUrl.shared.emailInUseLearnMore
     }
 
     static var wr_randomProfilePictureSource: URL {
-        return URL(string: "https://source.unsplash.com/800x800/?landscape")!
+        return WireUrl.shared.randomProfilePictureSource
     }
 
     static func wr_termsOfServicesURL(forTeamAccount isTeamAccount: Bool) -> URL {
         if isTeamAccount {
-            return URL(string: "https://wire.com/legal/terms/teams")!
+            return WireUrl.shared.termsOfServicesTeams
         } else {
-            return URL(string: "https://wire.com/legal/terms/personal")!
+            return WireUrl.shared.termsOfServicesPersonal
         }
     }
 
