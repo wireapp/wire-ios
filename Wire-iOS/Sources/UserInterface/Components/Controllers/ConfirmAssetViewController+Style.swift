fc155f69a2332bcf1dfd2e0f91d22c93b02bc02d
Alexis Aubry <me@alexaubry.fr>
2019-04-18 09:07:28 +0200

    Rewrite constraints without helpers + update UI (#3368)

--- Wire-iOS/Sources/UserInterface/Components/Controllers/ConfirmAssetViewController+Style.swift
+++ Wire-iOS/Sources/UserInterface/Components/Controllers/ConfirmAssetViewController+Style.swift
@@ -26,14 +26,37 @@ extension ConfirmAssetViewController {
     }
 
     @objc func setupStyle() {
+        applyColorScheme(ColorScheme.default.variant)
+
+        titleLabel.font = UIFont.mediumSemiboldFont
+        titleLabel.numberOfLines = 0
+        titleLabel.textAlignment = .center
+
+        acceptImageButton.layer.cornerRadius = 8
+        acceptImageButton.titleLabel?.font = .smallSemiboldFont
+
+        rejectImageButton.layer.cornerRadius = 8
+        rejectImageButton.titleLabel?.font = .smallSemiboldFont
+    }
+
+    func applyColorScheme(_ colorSchemeVariant: ColorSchemeVariant) {
         view.backgroundColor = UIColor.from(scheme: .background)
         imageToolbarSeparatorView?.backgroundColor = UIColor.from(scheme: .separator)
         topPanel.backgroundColor = UIColor.from(scheme: .background)
-        
-        titleLabel.font = UIFont.mediumSemiboldFont
+
         titleLabel.textColor = UIColor.from(scheme: .textForeground)
-        
+
+        acceptImageButton.setTitleColor(.white, for: .normal)
+        acceptImageButton.setTitleColor(.whiteAlpha40, for: .highlighted)
+        acceptImageButton.setBackgroundImageColor(UIColor.accent(), for: .normal)
+        acceptImageButton.setBackgroundImageColor(UIColor.accentDarken, for: .highlighted)
+
+        rejectImageButton.setTitleColor(UIColor.from(scheme: .textForeground, variant: colorSchemeVariant), for: .normal)
+        rejectImageButton.setTitleColor(UIColor.from(scheme: .textDimmed, variant: colorSchemeVariant), for: .highlighted)
+        rejectImageButton.setBackgroundImageColor(UIColor.from(scheme: .secondaryAction, variant: colorSchemeVariant), for: .normal)
+        rejectImageButton.setBackgroundImageColor(UIColor.from(scheme: .secondaryActionDimmed, variant: colorSchemeVariant), for: .highlighted)
     }
+
     
     @objc func createVideoPanel() {
         playerViewController = AVPlayerViewController()
@@ -48,4 +71,5 @@ extension ConfirmAssetViewController {
         
         view.addSubview(playerViewController.view)
     }
+    
 }
--- Wire-iOS/Sources/UserInterface/Components/Controllers/ConfirmAssetViewController.h
+++ Wire-iOS/Sources/UserInterface/Components/Controllers/ConfirmAssetViewController.h
@@ -16,8 +16,9 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 // 
 
+@import UIKit;
 
-#import <UIKit/UIKit.h>
+#import "MediaAsset.h"
 
 @protocol MediaAsset;
 
