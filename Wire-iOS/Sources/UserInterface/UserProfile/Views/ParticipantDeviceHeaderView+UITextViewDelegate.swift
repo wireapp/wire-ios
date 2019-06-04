0a9d0ed21308124ee30963d44672b067b4c75bab
Bill Chan <billypchan@gmail.com>
2019-06-04 16:01:01 +0200

    UserClientListViewController as ParticipantDeviceHeaderViewDelegate (#3450)

--- Wire-iOS/Sources/UserInterface/UserProfile/Views/ParticipantDeviceHeaderView+UITextViewDelegate.swift
+++ Wire-iOS/Sources/UserInterface/UserProfile/Views/ParticipantDeviceHeaderView+UITextViewDelegate.swift
@@ -1,39 +1,28 @@
-// 
+
 // Wire
-// Copyright (C) 2016 Wire Swiss GmbH
-// 
+// Copyright (C) 2019 Wire Swiss GmbH
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see http://www.gnu.org/licenses/.
-// 
+//
 
+import Foundation
 
-#import <UIKit/UIKit.h>
+extension ParticipantDeviceHeaderView: UITextViewDelegate {
 
-@protocol ParticipantDeviceHeaderViewDelegate;
+    public func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        delegate.participantsDeviceHeaderViewDidTapLearnMore(self)
 
-@interface ParticipantDeviceHeaderView : UIView
-
-- (instancetype)initWithUserName:(NSString *)userName;
-
-@property (weak, nonatomic) id <ParticipantDeviceHeaderViewDelegate> delegate;
-@property (nonatomic, readonly) NSString *userName;
-@property (nonatomic) BOOL showUnencryptedLabel;
-
-@end
-
-
-@protocol ParticipantDeviceHeaderViewDelegate <NSObject>
-
-- (void)participantsDeviceHeaderViewDidTapLearnMore:(ParticipantDeviceHeaderView *)headerView;
-
-@end
+        return false
+    }
+}
