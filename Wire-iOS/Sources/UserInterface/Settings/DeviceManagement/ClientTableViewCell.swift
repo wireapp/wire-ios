9aef097d893a15803884b03dceeffe7ad8f4e5c6
Bill Chan <billypchan@gmail.com>
2019-06-27 17:35:05 +0200

    Fix: Add a way to see the verification status of a device in the device list (#3500)
    * make verifiedLabel accessible
    
    * make the icon accessible and added label
    remove not used unverifiedIconView
    
    * Update Wire-iOS/Sources/UserInterface/UserProfile/Devices/UserClientCell.swift
    
    Co-Authored-By: Alexis Aubry <me@alexaubry.fr>
    
    * Update Wire-iOS/Sources/UserInterface/UserProfile/Devices/UserClientCell.swift
    
    Co-Authored-By: Alexis Aubry <me@alexaubry.fr>
    
    * Ref to other element in this cell, `accessibilityIdentifier` should be constant.
    
    * undo

--- Wire-iOS/Sources/UserInterface/Settings/DeviceManagement/ClientTableViewCell.swift
+++ Wire-iOS/Sources/UserInterface/Settings/DeviceManagement/ClientTableViewCell.swift
@@ -29,7 +29,7 @@ class ClientTableViewCell: UITableViewCell {
     let activationLabel = UILabel(frame: CGRect.zero)
     let fingerprintLabel = UILabel(frame: CGRect.zero)
     let verifiedLabel = UILabel(frame: CGRect.zero)
-    
+
     private let activationLabelFont = UIFont.smallLightFont
     private let activationLabelDateFont = UIFont.smallSemiboldFont
     
@@ -116,7 +116,8 @@ class ClientTableViewCell: UITableViewCell {
         activationLabel.accessibilityIdentifier = "device activation date"
         fingerprintLabel.accessibilityIdentifier = "device fingerprint"
         verifiedLabel.accessibilityIdentifier = "device verification status"
-        
+        verifiedLabel.isAccessibilityElement = true
+
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         [self.nameLabel, self.labelLabel, self.activationLabel, self.fingerprintLabel, self.verifiedLabel].forEach(self.contentView.addSubview)
