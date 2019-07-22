0ed29a57e7a08107e788d5bcab1dea2a8e7d0f61
Bill Chan <billypchan@gmail.com>
2019-07-22 17:11:21 +0200

    Fix: SettingsTechnicalReportViewController status bar update after back from email composer (#3542)
    * fix status bar
    
    * snapshot test
    
    * WIP remove Cartography
    
    * fix constraints
    
    * clean up
    
    * lint

--- Wire-iOS Tests/SwiftSnapshotTesting/XCTestCase+SnapshotTesting.swift
+++ Wire-iOS Tests/SwiftSnapshotTesting/XCTestCase+SnapshotTesting.swift
@@ -38,7 +38,7 @@ extension ViewImageConfig: Hashable {
 /// MARK: - snapshoting all iPhone sizes
 extension XCTestCase {
     /// snapshot file name suffixs
-    static let phoneConfigNames: [SnapshotTesting.ViewImageConfig:String] = [
+    static let phoneConfigNames: [SnapshotTesting.ViewImageConfig: String] = [
         .iPhoneSe: "iPhone-4_0_Inch",
         .iPhone8: "iPhone-4_7_Inch",
         .iPhone8Plus: "iPhone-5_5_Inch",
@@ -82,6 +82,7 @@ extension XCTestCase {
 
     func verify(matching value: UIViewController,
                 named name: String? = nil,
+                record recording: Bool = false,
                 file: StaticString = #file,
                 testName: String = #function,
                 line: UInt = #line) {
@@ -89,6 +90,7 @@ extension XCTestCase {
         let failure = verifySnapshot(matching: value,
                                      as: .image,
                                      named: name,
+                                     record: recording,
                                      snapshotDirectory: snapshotDirectory(file: file),
                                      file: file, testName: testName, line: line)
 
@@ -107,7 +109,6 @@ extension XCTestCase {
 
         XCTAssertNil(failure, file: file, line: line)
     }
-
 
     func verify<Value, Format>(matching value: Value,
                                as snapshotting: Snapshotting<Value, Format>,
