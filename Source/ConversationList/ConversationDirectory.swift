04f0e72e6979642367c1fbb5c50c914d053d7420
Bill Chan <billypchan@gmail.com>
2019-09-27 11:20:39 +0200

    Feature/folders (#787)
    * Add ConversationDirectoryType protocol (#782)
    
    * Add ConversationDirectoryType protocol
    
    * Add new lists to allConversationLists
    
    * Add more list types
    
    * Include outgoing connections in oneToOne conversation list
    
    * Update ZMConverationListDirectoryTests
    
    * Remove pre-defined folders
    
    * Fix broken tests
    
    * Fix incorrect predicateForTeamOneToOneConversation
    
    * Update Source/ConversationList/ConversationDirectory.swift
    
    Co-Authored-By: Bill Chan <billypchan@gmail.com>
    
    * Expose which lists were updated
    
    * Fix conflict
    
    * Fix addObserver method
    
    * Fix conversation directory did change not firing (#783)
    
    * fix conversationDirectoryDidChange not fired issue
    
    * observers all lists
    
    * expose init for UI unit tests (#784)

---
+++ Source/ConversationList/ConversationDirectory.swift
@@ -0,0 +1,129 @@
+//
+// Wire
+// Copyright (C) 2019 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+
+public enum ConversationListType {
+    case archived, unarchived, pending, contacts, groups
+}
+
+public struct ConversationDirectoryChangeInfo {
+    
+    public var reloaded: Bool
+    public var updatedLists: [ConversationListType]
+
+    public init(reloaded: Bool, updatedLists: [ConversationListType]) {
+        self.reloaded = reloaded
+        self.updatedLists = updatedLists
+    }
+    
+}
+
+public protocol ConversationDirectoryObserver {
+    
+    func conversationDirectoryDidChange(_ changeInfo: ConversationDirectoryChangeInfo)
+    
+}
+
+public protocol FolderType {
+    var name: String { get }
+    var conversations: [ZMConversation] { get }
+}
+
+public protocol ConversationDirectoryType {
+    
+    var folders: [FolderType] { get }
+    
+    func conversations(by: ConversationListType) -> [ZMConversation]
+    
+    func addObserver(_ observer: ConversationDirectoryObserver) -> Any
+    
+}
+
+extension ZMConversationListDirectory: ConversationDirectoryType {
+    
+    public func conversations(by type: ConversationListType) -> [ZMConversation] {
+        switch type {
+        case .archived:
+            return archivedConversations as! [ZMConversation]
+        case .unarchived:
+            return unarchivedConversations as! [ZMConversation]
+        case .pending:
+            return pendingConnectionConversations as! [ZMConversation]
+        case .contacts:
+            return oneToOneConversations as! [ZMConversation]
+        case .groups:
+            return groupConversations as! [ZMConversation]
+        }
+    }
+    
+    public var folders: [FolderType] {
+        return []
+    }
+    
+    public func addObserver(_ observer: ConversationDirectoryObserver) -> Any {
+        let observerProxy = ConversationListObserverProxy(observer: observer, directory: self)
+
+        var tokens:[Any] = []
+        [archivedConversations, unarchivedConversations, pendingConnectionConversations, oneToOneConversations, groupConversations].forEach() {
+            let token = ConversationListChangeInfo.add(observer: observerProxy, for: $0, managedObjectContext: managedObjectContext)
+            tokens.append(token)
+        }
+
+        let token = ConversationListChangeInfo.add(observer: observerProxy, managedObjectContext: managedObjectContext)
+        return tokens + [token, observerProxy]
+    }
+    
+}
+
+fileprivate class ConversationListObserverProxy: NSObject, ZMConversationListObserver, ZMConversationListReloadObserver  {
+    
+    var observer: ConversationDirectoryObserver
+    var directory: ZMConversationListDirectory
+    
+    init(observer: ConversationDirectoryObserver, directory: ZMConversationListDirectory) {
+        self.observer = observer
+        self.directory = directory
+    }
+    
+    func conversationListsDidReload() {
+        observer.conversationDirectoryDidChange(ConversationDirectoryChangeInfo(reloaded: true, updatedLists: []))
+    }
+    
+    func conversationListDidChange(_ changeInfo: ConversationListChangeInfo) {
+        let updatedLists: [ConversationListType]
+        
+        switch changeInfo.conversationList {
+        case directory.oneToOneConversations:
+            updatedLists = [.contacts]
+        case directory.groupConversations:
+            updatedLists = [.groups]
+        case directory.archivedConversations:
+            updatedLists = [.archived]
+        case directory.pendingConnectionConversations:
+            updatedLists = [.pending]
+        case directory.unarchivedConversations:
+            updatedLists = [.unarchived]
+        default:
+            updatedLists = []
+        }
+        
+        observer.conversationDirectoryDidChange(ConversationDirectoryChangeInfo(reloaded: false, updatedLists: updatedLists))
+    }
+    
+}
--- Tests/Source/Model/ConversationList/ZMConversationListDirectoryTests.m
+++ Tests/Source/Model/ConversationList/ZMConversationListDirectoryTests.m
@@ -31,7 +31,8 @@
 
 @property (nonatomic) ZMConversation *archivedGroupConversation;
 @property (nonatomic) ZMConversation *archivedOneToOneConversation;
-@property (nonatomic) ZMConversation *pendingConnectionConversation;
+@property (nonatomic) ZMConversation *incomingPendingConnectionConversation;
+@property (nonatomic) ZMConversation *outgoingPendingConnectionConversation;
 @property (nonatomic) ZMConversation *invalidConversation;
 @property (nonatomic) ZMConversation *groupConversation;
 @property (nonatomic) ZMConversation *oneToOneConversation;
@@ -71,11 +72,17 @@
     self.archivedOneToOneConversation.connection.status = ZMConnectionStatusAccepted;
     self.archivedOneToOneConversation.userDefinedName = @"archivedOneToOneConversation";
     
-    self.pendingConnectionConversation = [self createConversation];
-    self.pendingConnectionConversation.conversationType = ZMConversationTypeConnection;
-    self.pendingConnectionConversation.connection = [ZMConnection insertNewObjectInManagedObjectContext:self.uiMOC];
-    self.pendingConnectionConversation.connection.status = ZMConnectionStatusPending;
-    self.pendingConnectionConversation.userDefinedName = @"pendingConnectionConversation";
+    self.incomingPendingConnectionConversation = [self createConversation];
+    self.incomingPendingConnectionConversation.conversationType = ZMConversationTypeConnection;
+    self.incomingPendingConnectionConversation.connection = [ZMConnection insertNewObjectInManagedObjectContext:self.uiMOC];
+    self.incomingPendingConnectionConversation.connection.status = ZMConnectionStatusPending;
+    self.incomingPendingConnectionConversation.userDefinedName = @"incomingPendingConnectionConversation";
+    
+    self.outgoingPendingConnectionConversation = [self createConversation];
+    self.outgoingPendingConnectionConversation.conversationType = ZMConversationTypeConnection;
+    self.outgoingPendingConnectionConversation.connection = [ZMConnection insertNewObjectInManagedObjectContext:self.uiMOC];
+    self.outgoingPendingConnectionConversation.connection.status = ZMConnectionStatusSent;
+    self.outgoingPendingConnectionConversation.userDefinedName = @"outgoingConnectionConversation";
     
     self.groupConversation = [self createConversation];
     self.groupConversation.conversationType = ZMConversationTypeGroup;
@@ -108,7 +115,8 @@
     
     self.invalidConversation = nil;
     self.groupConversation = nil;
-    self.pendingConnectionConversation = nil;
+    self.incomingPendingConnectionConversation = nil;
+    self.outgoingPendingConnectionConversation = nil;
     self.archivedOneToOneConversation = nil;
     self.archivedGroupConversation = nil;
     self.oneToOneConversation = nil;
@@ -122,40 +130,40 @@
 {
     // when
     ZMConversationList *list = self.uiMOC.conversationListDirectory.conversationsIncludingArchived;
-    NSSet *exepected = [NSSet setWithArray:@[self.archivedGroupConversation, self.archivedOneToOneConversation, self.groupConversation, self.oneToOneConversation]];
+    NSSet *expected = [NSSet setWithArray:@[self.archivedGroupConversation, self.archivedOneToOneConversation, self.groupConversation, self.oneToOneConversation, self.outgoingPendingConnectionConversation]];
     // then
     
-    XCTAssertEqualObjects([NSSet setWithArray:list], exepected);
+    XCTAssertEqualObjects([NSSet setWithArray:list], expected);
 }
 
 - (void)testThatItReturnsUnarchivedConversations;
 {
     // when
     ZMConversationList *list = self.uiMOC.conversationListDirectory.unarchivedConversations;
-    NSSet *exepected = [NSSet setWithArray:@[self.groupConversation, self.oneToOneConversation]];
+    NSSet *expected = [NSSet setWithArray:@[self.groupConversation, self.oneToOneConversation, self.outgoingPendingConnectionConversation]];
     
     // then
-    XCTAssertEqualObjects([NSSet setWithArray:list], exepected);
+    XCTAssertEqualObjects([NSSet setWithArray:list], expected);
 }
 
 - (void)testThatItReturnsArchivedConversations;
 {
     // when
     ZMConversationList *list = self.uiMOC.conversationListDirectory.archivedConversations;
-    NSSet *exepected = [NSSet setWithArray:@[self.archivedGroupConversation, self.archivedOneToOneConversation]];
+    NSSet *expected = [NSSet setWithArray:@[self.archivedGroupConversation, self.archivedOneToOneConversation]];
     
     // then
-    XCTAssertEqualObjects([NSSet setWithArray:list], exepected);
+    XCTAssertEqualObjects([NSSet setWithArray:list], expected);
 }
 
 - (void)testThatItReturnsPendingConversations;
 {
     // when
     ZMConversationList *list = self.uiMOC.conversationListDirectory.pendingConnectionConversations;
-    NSSet *exepected = [NSSet setWithArray:@[self.pendingConnectionConversation]];
+    NSSet *expected = [NSSet setWithArray:@[self.incomingPendingConnectionConversation]];
     
     // then
-    XCTAssertEqualObjects([NSSet setWithArray:list], exepected);
+    XCTAssertEqualObjects([NSSet setWithArray:list], expected);
 }
 
 - (void)testThatItKeepsReturningTheSameObject
@@ -172,21 +180,41 @@
 {
     // when
     ZMConversationList *list = self.uiMOC.conversationListDirectory.clearedConversations;
-    NSSet *exepected = [NSSet setWithArray:@[self.clearedConversation]];
+    NSSet *expected = [NSSet setWithArray:@[self.clearedConversation]];
     
     // then
-    XCTAssertEqualObjects([NSSet setWithArray:list], exepected);
+    XCTAssertEqualObjects([NSSet setWithArray:list], expected);
 }
 
 - (void)testThatItNotReturnsClearedConversationsIn_ConversationsIncludingArchived
 {
     // when
     ZMConversationList *list = self.uiMOC.conversationListDirectory.conversationsIncludingArchived;
-    NSSet *exepected = [NSSet setWithArray:@[self.clearedConversation]];
+    NSSet *expected = [NSSet setWithArray:@[self.clearedConversation]];
     
     // then
     // cleared conversations should not be included in conversationsIncludingArchived
-    XCTAssertFalse([[NSSet setWithArray:list] intersectsSet:exepected]);
+    XCTAssertFalse([[NSSet setWithArray:list] intersectsSet:expected]);
+}
+
+- (void)testThatItsReturnsGroupConversations
+{
+    // when
+    ZMConversationList *list = self.uiMOC.conversationListDirectory.groupConversations;
+    NSSet *expected = [NSSet setWithArray:@[self.groupConversation]];
+    
+    // then
+    XCTAssertEqualObjects([NSSet setWithArray:list], expected);
+}
+
+- (void)testThatItsReturnsOneToOneConversations
+{
+    // when
+    ZMConversationList *list = self.uiMOC.conversationListDirectory.oneToOneConversations;
+    NSSet *expected = [NSSet setWithArray:@[self.oneToOneConversation, self.outgoingPendingConnectionConversation]];
+    
+    // then
+    XCTAssertEqualObjects([NSSet setWithArray:list], expected);
 }
 
 - (void)testThatAllListsAreIncluded
@@ -198,6 +226,8 @@
     XCTAssertTrue([directory.allConversationLists containsObject:directory.archivedConversations]);
     XCTAssertTrue([directory.allConversationLists containsObject:directory.pendingConnectionConversations]);
     XCTAssertTrue([directory.allConversationLists containsObject:directory.clearedConversations]);
+    XCTAssertTrue([directory.allConversationLists containsObject:directory.oneToOneConversations]);
+    XCTAssertTrue([directory.allConversationLists containsObject:directory.groupConversations]);
 }
 
 @end
