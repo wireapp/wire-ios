86bb473d22ac6027002f845f07447fef5b81daea
Bill Chan <billypchan@gmail.com>
2019-10-29 14:51:38 +0100

    unwrap sharingSession (#3759)

--- Wire-iOS Share Extension/ShareExtensionViewController.swift
+++ Wire-iOS Share Extension/ShareExtensionViewController.swift
@@ -237,10 +237,13 @@ final class ShareExtensionViewController: SLComposeServiceViewController {
     }
 
     /// Invoked when the user wants to post.
-    @objc func appendPostTapped() {
+    @objc
+    private func appendPostTapped() {
+        guard let sharingSession = sharingSession else { return }
+
         navigationController?.navigationBar.items?.first?.rightBarButtonItem?.isEnabled = false
         
-        postContent?.send(text: contentText, sharingSession: sharingSession!) { [weak self] progress in
+        postContent?.send(text: contentText, sharingSession: sharingSession) { [weak self] progress in
             guard let `self` = self, let postContent = self.postContent else { return }
 
             switch progress {
