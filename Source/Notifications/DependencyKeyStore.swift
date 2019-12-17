42ee24d7936701aeafdbb2c7e17f2fb85a9eff31
Bill Chan <billypchan@gmail.com>
2019-12-17 17:52:22 +0100

    fix: role change not trigger conversaton change (#841)
    * tested that role change affects conversation
    
    * change keyPathsForValuesAffectingLocalParticipantRoles only
    
    * test
    
    * Update ConversationObserverTests.swift

--- Source/Notifications/DependencyKeyStore.swift
+++ Source/Notifications/DependencyKeyStore.swift
@@ -188,7 +188,12 @@ class DependencyKeyStore {
     }
     
     /// Returns the inverse of keyPathsForValuesAffectingValueForKey, all observable keys that are affected by `key`
-    func observableKeysAffectedByValue(_ classIdentifier: String, key: String) -> Set<String>{
+    ///
+    /// - Parameters:
+    ///   - classIdentifier: the class's id, e.g. "Conversation"
+    ///   - key: the key, e.g. "participantRoles.role"
+    /// - Returns: the inverse of keyPathsForValuesAffectingValueForKey, all observable keys that are affected by `key`
+    func observableKeysAffectedByValue(_ classIdentifier: String, key: String) -> Set<String> {
         var keys = effectedKeys[classIdentifier]?[key] ?? Set()
         if let otherKeys = observableKeys[classIdentifier], otherKeys.contains(key) {
             keys.insert(key)
