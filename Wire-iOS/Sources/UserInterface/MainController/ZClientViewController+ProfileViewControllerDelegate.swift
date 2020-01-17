a95e265ea359946d86b68d54e5af9f8a1e41c6d0
Bill Chan <billypchan@gmail.com>
2020-01-17 15:50:34 +0100

    do not use argument with no name (#3950)
    * do not use argument with no name
    
    * fix test

--- Wire-iOS/Sources/UserInterface/MainController/ZClientViewController+ProfileViewControllerDelegate.swift
+++ Wire-iOS/Sources/UserInterface/MainController/ZClientViewController+ProfileViewControllerDelegate.swift
@@ -21,7 +21,7 @@ import Foundation
 extension ZClientViewController: ProfileViewControllerDelegate {
     
     func profileViewController(_ controller: ProfileViewController?, wantsToNavigateTo conversation: ZMConversation) {
-        select(conversation, focusOnView: true, animated: true)
+        select(conversation: conversation, focusOnView: true, animated: true)
     }
 
     func profileViewController(_ controller: ProfileViewController?, wantsToCreateConversationWithName name: String?, users: Set<ZMUser>) {
