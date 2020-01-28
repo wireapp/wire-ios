952cb14e1653d11d213627cb038e4a116d1e44b1
KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
2020-01-28 19:06:31 +0200

    [Chore] Bump Data model (#1158)
    * Bump components
    
    * Fix test data model

--- Tests/Source/Data Model/ServiceUserTests.swift
+++ Tests/Source/Data Model/ServiceUserTests.swift
@@ -51,6 +51,8 @@ final class DummyServiceUser: NSObject, ServiceUser {
     
     var readReceiptsEnabled: Bool = true
     
+    var isVerified: Bool = false
+    
     var richProfile: [UserRichProfileField] = []
     
     /// Whether the user can create conversations.
