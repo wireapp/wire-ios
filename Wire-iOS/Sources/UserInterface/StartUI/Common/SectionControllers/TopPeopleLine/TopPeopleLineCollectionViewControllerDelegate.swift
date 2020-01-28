71e8aca23ba8afd3818934ccce7e45f9c06ffbdc
John Nguyen <polyxo@protonmail.com>
2020-01-28 16:34:05 +0100

    [Refactor] Convert TopPeopleCell to Swift (#3995)
    * Convert top people classes to Swift
    
    * Remove dead code
    
    * Clean up
    
    * Remove dead code
    
    * Fix constraints error

--- Wire-iOS/Sources/UserInterface/StartUI/Common/SectionControllers/TopPeopleLine/TopPeopleLineCollectionViewControllerDelegate.swift
+++ Wire-iOS/Sources/UserInterface/StartUI/Common/SectionControllers/TopPeopleLine/TopPeopleLineCollectionViewControllerDelegate.swift
@@ -1,32 +1,24 @@
-// 
+//
 // Wire
-// Copyright (C) 2016 Wire Swiss GmbH
-// 
+// Copyright (C) 2020 Wire Swiss GmbH
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see http://www.gnu.org/licenses/.
-// 
+//
 
+import Foundation
 
-#import <UIKit/UIKit.h>
+protocol TopPeopleLineCollectionViewControllerDelegate: class {
 
-@protocol UserType;
-@class ZMConversation;
-
-@interface TopPeopleCell : UICollectionViewCell
-@property (nonatomic, strong) id<UserType> user;
-@property (nonatomic, strong) ZMConversation *conversation;
-
-@property (nonatomic, copy)   NSString *displayName;
-
-@property (nonatomic, copy)   void (^doubleTapAction)(TopPeopleCell *);
-@end
+    func topPeopleLineCollectionViewControllerDidSelect(_ conversation: ZMConversation)
+}
