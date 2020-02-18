7049fe63eddb3fecc40e8b320b5b18ccfc206073
Bill Chan <billypchan@gmail.com>
2020-02-18 12:30:03 +0100

    Convert ZoomTransition (#4087)
    * converted ZoomTransition
    
    * refactoring
    
    * undo
    
    * lint

---
+++ Wire-iOS/Sources/UserInterface/Components/Transitions/ZoomTransition.swift
@@ -0,0 +1,97 @@
+// 
+// Wire
+// Copyright (C) 2020 Wire Swiss GmbH
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+final class ZoomTransition: NSObject, UIViewControllerAnimatedTransitioning {
+    private var interactionPoint = CGPoint.zero
+    private var reversed = false
+
+    init(interactionPoint: CGPoint, reversed: Bool) {
+        super.init()
+
+        self.interactionPoint = interactionPoint
+        self.reversed = reversed
+    }
+
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.65
+    }
+
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        guard let fromView = transitionContext.fromView,
+              let toView = transitionContext.toView else { return }
+        let containerView = transitionContext.containerView
+
+        containerView.addSubview(toView)
+
+        if !transitionContext.isAnimated {
+            transitionContext.completeTransition(true)
+            return
+        }
+
+        toView.layoutIfNeeded()
+
+        fromView.alpha = 1
+        fromView.layer.needsDisplayOnBoundsChange = false
+
+        if reversed {
+
+            UIView.wr_animate(easing: .easeInExpo, duration: 0.35, animations: {
+                fromView.alpha = 0
+                fromView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+            }) { finished in
+                fromView.transform = .identity
+            }
+
+            toView.alpha = 0
+            toView.transform = CGAffineTransform(scaleX: 2, y: 2)
+
+            UIView.wr_animate(easing: .easeOutExpo, duration: 0.35, animations: {
+                toView.alpha = 1
+                toView.transform = .identity
+            }) { finished in
+                transitionContext.completeTransition(true)
+            }
+        } else {
+
+            var frame = fromView.frame
+            fromView.layer.anchorPoint = interactionPoint
+            fromView.frame = frame
+
+            UIView.wr_animate(easing: .easeInExpo, duration: 0.35, animations: {
+                fromView.alpha = 0
+                fromView.transform = CGAffineTransform(scaleX: 2, y: 2)
+            }) { finished in
+                fromView.transform = .identity
+            }
+
+            frame = toView.frame
+            toView.layer.anchorPoint = interactionPoint
+            toView.frame = frame
+
+            toView.alpha = 0
+            toView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+
+            UIView.wr_animate(easing: .easeOutExpo, duration: 0.35, delay: 0.3, animations: {
+                toView.alpha = 1
+                toView.transform = .identity
+            }) { finished in
+                transitionContext.completeTransition(true)
+            }
+        }
+    }
+}
