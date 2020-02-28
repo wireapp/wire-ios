77795b3452b1e6a2a543f2fb1e6bb10672589e4a
KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
2020-02-28 10:17:58 +0100

    [Fix] Display the full name in it's entirety on the self profile page (#4134)
    * Display the full name in it's entirety on the self profile page, spanning over a maximum of three lines
    
    * Fix snapshots
    
    * setContentCompressionResistancePriority for imageView
    
    * Add width constraint

--- Wire-iOS/Sources/UserInterface/SelfProfile/ProfileHeaderViewController.swift
+++ Wire-iOS/Sources/UserInterface/SelfProfile/ProfileHeaderViewController.swift
@@ -94,6 +94,9 @@ final class ProfileHeaderViewController: UIViewController, Themeable {
         label.font = FontSpec(.large, .light).font!
         label.accessibilityTraits.insert(.header)
         label.lineBreakMode = .byTruncatingTail
+        label.numberOfLines = 3
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
         
         return label
     }()
@@ -138,10 +141,14 @@ final class ProfileHeaderViewController: UIViewController, Themeable {
         imageView.isAccessibilityElement = true
         imageView.accessibilityElementsHidden = false
         imageView.accessibilityIdentifier = "user image"
+        imageView.setImageConstraint(resistance: 249, hugging: 750)
+        imageView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        imageView.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
+
         imageView.initialsFont = UIFont.systemFont(ofSize: 55, weight: .semibold).monospaced()
         imageView.userSession = session
         imageView.user = user
-                
+        
         if let session = session {
             tokens.append(UserChangeInfo.add(observer: self, for: user, in: session))
         }
@@ -219,18 +226,10 @@ final class ProfileHeaderViewController: UIViewController, Themeable {
         let trailingSpaceConstraint = stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
         let bottomSpaceConstraint = stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20)
         
-        leadingSpaceConstraint.priority = .defaultLow
-        topSpaceConstraint.priority = .defaultLow
-        trailingSpaceConstraint.priority = .defaultLow
-        bottomSpaceConstraint.priority = .defaultLow
-        
+        let widthImageConstraint = imageView.widthAnchor.constraint(lessThanOrEqualToConstant: 164)
         NSLayoutConstraint.activate([
-            // imageView
-            imageView.widthAnchor.constraint(equalToConstant: 164),
-            imageView.heightAnchor.constraint(equalToConstant: 164),
-            
             // stackView
-            leadingSpaceConstraint, topSpaceConstraint, trailingSpaceConstraint, bottomSpaceConstraint
+            widthImageConstraint, leadingSpaceConstraint, topSpaceConstraint, trailingSpaceConstraint, bottomSpaceConstraint
             ])
     }
     
