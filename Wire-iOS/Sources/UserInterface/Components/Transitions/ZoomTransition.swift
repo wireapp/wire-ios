7a50a229dea29b8fe21191bbbef61dfe3d0d814d
Jacob Persson <jacob.persson@gmail.com>
2020-02-28 15:20:18 +0100

    Fix ZoomTransition not working on iPad where toView and fromView can be nil (#4141)

--- Wire-iOS/Sources/UserInterface/Components/Transitions/ZoomTransition.swift
+++ Wire-iOS/Sources/UserInterface/Components/Transitions/ZoomTransition.swift
@@ -32,11 +32,14 @@ final class ZoomTransition: NSObject, UIViewControllerAnimatedTransitioning {
     }
 
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-        guard let fromView = transitionContext.fromView,
-              let toView = transitionContext.toView else { return }
+        
+        let toView = transitionContext.toView
+        let fromView = transitionContext.fromView
         let containerView = transitionContext.containerView
 
-        containerView.addSubview(toView)
+        if let toView = toView {
+            containerView.addSubview(toView)
+        }
 
         if !transitionContext.isAnimated {
             transitionContext.completeTransition(true)
@@ -45,50 +48,52 @@ final class ZoomTransition: NSObject, UIViewControllerAnimatedTransitioning {
 
         containerView.layoutIfNeeded()
 
-        fromView.alpha = 1
-        fromView.layer.needsDisplayOnBoundsChange = false
+        fromView?.alpha = 1
+        fromView?.layer.needsDisplayOnBoundsChange = false
 
         if reversed {
 
             UIView.animate(easing: .easeInExpo, duration: 0.35, animations: {
-                fromView.alpha = 0
-                fromView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+                fromView?.alpha = 0
+                fromView?.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
             }) { finished in
-                fromView.transform = .identity
+                fromView?.transform = .identity
             }
 
-            toView.alpha = 0
-            toView.transform = CGAffineTransform(scaleX: 2, y: 2)
+            toView?.alpha = 0
+            toView?.transform = CGAffineTransform(scaleX: 2, y: 2)
 
             UIView.animate(easing: .easeOutExpo, duration: 0.35, animations: {
-                toView.alpha = 1
-                toView.transform = .identity
+                toView?.alpha = 1
+                toView?.transform = .identity
             }) { finished in
                 transitionContext.completeTransition(true)
             }
         } else {
 
-            var frame = fromView.frame
-            fromView.layer.anchorPoint = interactionPoint
-            fromView.frame = frame
-
-            UIView.animate(easing: .easeInExpo, duration: 0.35, animations: {
-                fromView.alpha = 0
-                fromView.transform = CGAffineTransform(scaleX: 2, y: 2)
-            }) { finished in
-                fromView.transform = .identity
+            if let frame = fromView?.frame {
+                fromView?.layer.anchorPoint = interactionPoint
+                fromView?.frame = frame
             }
 
-            frame = toView.frame
-            toView.layer.anchorPoint = interactionPoint
-            toView.frame = frame
+            UIView.animate(easing: .easeInExpo, duration: 0.35, animations: {
+                fromView?.alpha = 0
+                fromView?.transform = CGAffineTransform(scaleX: 2, y: 2)
+            }) { finished in
+                fromView?.transform = .identity
+            }
 
-            toView.alpha = 0
-            toView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+            if let frame = toView?.frame  {
+                toView?.layer.anchorPoint = interactionPoint
+                toView?.frame = frame
+            }
+
+            toView?.alpha = 0
+            toView?.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
 
             UIView.animate(easing: .easeOutExpo, duration: 0.35, delayTime: 0.3, animations: {
-                toView.alpha = 1
-                toView.transform = .identity
+                toView?.alpha = 1
+                toView?.transform = .identity
             }) { finished in
                 transitionContext.completeTransition(true)
             }
