3ad8577b1287f1bbc32eeb4241f1b62371768a41
Jacob Persson <jacob.persson@gmail.com>
2020-03-06 10:21:23 +0100

    [Fix] missing cryptobox logs on public builds (#4162)
    * Add support for corrupting a session
    
    * Enable default logs tags also on public builds

--- Wire-iOS/Sources/Components/Settings+Logging.swift
+++ Wire-iOS/Sources/Components/Settings+Logging.swift
@@ -43,8 +43,6 @@ extension Settings {
     
     /// Loads from user default the list of logs that are enabled
     @objc public func loadEnabledLogs() {
-        guard Bundle.developerModeEnabled else { return }
-        
         var tagsToEnable: Set<String> = ["AVS", "Network", "SessionManager", "Conversations", "calling", "link previews", "event-processing", "SyncStatus", "OperationStatus", "Push", "Crypto", "cryptobox"]
 
         if let savedTags = UserDefaults.shared().object(forKey: enabledLogsKey) as? Array<String> {
