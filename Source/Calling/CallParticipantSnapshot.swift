f7d4ad085d13285e8550af66e95166ea0141ae2e
John Nguyen <polyxo@protonmail.com>
2020-03-13 11:18:45 +0100

    [Refactor] Integrate AVS 5.6 (#1183)
    * Update callback signatures
    
    * Add new network quality case
    
    * Add new participant call state
    
    * Add AudioState
    
    * Make VideoState Codable
    
    * Refactor AVSParticipantsChange using Codable
    
    * Clean up
    
    * Add todos
    
    * Bump AVS to 5.6
    
    * Store AudioState in AVSCAllMember
    
    * Pass clientId down to network quality handler
    
    * Pass the clientId to the network handler
    
    * Add temporary clientId to handleCallState
    
    * Propagate clientId for data established handler
    
    * Propagate clientId for established call handler
    
    * Propagate clientId for call end handler
    
    * Clean up
    
    * Use clientId in call established handler
    
    * Refactor finding members
    
    * Clean up
    
    * Fix tests
    
    * Bump AVS version
    
    * Fix regression
    
    * Refactor updating members
    
    * Clean up
    
    * Add tests
    
    * Clean up tests
    
    * Delete uncalled function
    
    * Propagate network quality changes to participant snapshot
    
    * Clean up
    
    * Clean up
    
    * Update callback signatures
    
    * Add new network quality case
    
    * Add new participant call state
    
    * Add AudioState
    
    * Make VideoState Codable
    
    * Refactor AVSParticipantsChange using Codable
    
    * Clean up
    
    * Add todos
    
    * Bump AVS to 5.6
    
    * Store AudioState in AVSCAllMember
    
    * Pass clientId down to network quality handler
    
    * Pass the clientId to the network handler
    
    * Add temporary clientId to handleCallState
    
    * Propagate clientId for data established handler
    
    * Propagate clientId for established call handler
    
    * Propagate clientId for call end handler
    
    * Clean up
    
    * Use clientId in call established handler
    
    * Refactor finding members
    
    * Clean up
    
    * Fix tests
    
    * Bump AVS version
    
    * Fix regression
    
    * Refactor updating members
    
    * Clean up
    
    * Add tests
    
    * Clean up tests
    
    * Delete uncalled function
    
    * Propagate network quality changes to participant snapshot
    
    * Clean up
    
    * Clean up
    
    * Add clientId to remaining handlers
    
    * Make clientId non nil
    
    * Fix typo
    
    * Remove old fixme
    
    * Fix comments
    
    * Make the userId for handleCallEnd non optional
    
    * Delete dead code
    
    * Pull up handling of certain callstates
    
    * Clean up
    
    * Propagate the clientId to the network changed notification
    
    * Fix test

--- Source/Calling/CallParticipantSnapshot.swift
+++ Source/Calling/CallParticipantSnapshot.swift
@@ -20,83 +20,101 @@ import Foundation
 import WireUtilities
 
 class CallParticipantsSnapshot {
-    
-    public private(set) var members : OrderedSetState<AVSCallMember>
 
-    // We take the worst quality of all the legs
-    public var networkQuality: NetworkQuality {
-        return members.array.map(\.networkQuality)
-            .sorted() { $0.rawValue < $1.rawValue }
+    // MARK: - Properties
+
+    private unowned var callCenter: WireCallCenterV3
+    private let conversationId: UUID
+    private(set) var members: OrderedSetState<AVSCallMember>
+
+    /// Worst network quality of all the participants.
+
+    var networkQuality: NetworkQuality {
+        return members.array
+            .map(\.networkQuality)
+            .sorted { $0.rawValue < $1.rawValue }
             .last ?? .normal
     }
-    
-    fileprivate unowned var callCenter : WireCallCenterV3
-    fileprivate let conversationId : UUID
-    
+
+    // MARK: - Life Cycle
+
     init(conversationId: UUID, members: [AVSCallMember], callCenter: WireCallCenterV3) {
         self.callCenter = callCenter
         self.conversationId = conversationId
         self.members = type(of: self).removeDuplicateMembers(members)
     }
-    
-    // Remove duplicates see: https://wearezeta.atlassian.net/browse/ZIOS-8610
-    static func removeDuplicateMembers(_ members: [AVSCallMember]) -> OrderedSetState<AVSCallMember> {
-        let callMembers = members.reduce([AVSCallMember]()){ (filtered, member) in
-            filtered + (filtered.contains(member) ? [] : [member])
-        }
-        
-        return callMembers.toOrderedSetState()
-    }
-    
+
+    // MARK: - Updates
+
     func callParticipantsChanged(participants: [AVSCallMember]) {
         members = type(of:self).removeDuplicateMembers(participants)
         notifyChange()
     }
 
-    func callParticpantVideoStateChanged(userId: UUID, clientId: String, videoState: VideoState) {
-        guard let callMember = findMember(userId: userId, clientId: clientId) else { return }
-
-        update(updatedMember: AVSCallMember(userId: userId, clientId: clientId, audioEstablished: callMember.audioEstablished, videoState: videoState))
+    func callParticipantVideoStateChanged(userId: UUID, clientId: String, videoState: VideoState) {
+        updateMember(userId: userId, clientId: clientId, videoState: videoState)
     }
 
-    func callParticpantAudioEstablished(userId: UUID) {
-        guard let callMember = members.array.first(where: { $0.remoteId == userId }) else { return }
-
-        update(updatedMember: AVSCallMember(userId: userId, clientId: callMember.clientId, audioEstablished: true, videoState: callMember.videoState))
+    func callParticipantAudioEstablished(userId: UUID, clientId: String) {
+        updateMember(userId: userId, clientId: clientId, audioState: .established)
     }
 
-    func callParticpantNetworkQualityChanged(userId: UUID, networkQuality: NetworkQuality) {
-        guard let callMember = members.array.first(where: { $0.remoteId == userId }) else { return }
-
-        update(updatedMember: AVSCallMember(userId: userId, clientId: callMember.clientId, audioEstablished: callMember.audioEstablished, videoState: callMember.videoState, networkQuality: networkQuality))
+    func callParticipantNetworkQualityChanged(userId: UUID, clientId: String, networkQuality: NetworkQuality) {
+        updateMember(userId: userId, clientId: clientId, networkQuality: networkQuality)
     }
-    
-    func update(updatedMember: AVSCallMember) {
-        guard let targetMember = findMember(userId: updatedMember.remoteId, clientId: updatedMember.clientId) else { return }
+
+    // MARK: - Helpers
+
+    /// Updates the locally stored member for the given userId and clientId with the given non nil properties.
+
+    private func updateMember(userId: UUID,
+                              clientId: String,
+                              audioState: AudioState? = nil,
+                              videoState: VideoState? = nil,
+                              networkQuality: NetworkQuality? = nil) {
+
+        guard let localMember = findMember(with: userId, clientId: clientId) else { return }
+
+        let updatedMember = AVSCallMember(userId: userId,
+                                          clientId: clientId,
+                                          audioState: audioState ?? localMember.audioState,
+                                          videoState: videoState ?? localMember.videoState,
+                                          networkQuality: networkQuality ?? localMember.networkQuality)
 
         members = OrderedSetState(array: members.array.map({ member in
-            member == targetMember ? updatedMember : member
+            member == localMember ? updatedMember : member
         }))
     }
 
-    func notifyChange() {
+    /// Returns the member matching the given userId and clientId.
+
+    private func findMember(with userId: UUID, clientId: String) -> AVSCallMember? {
+        return members.array.first { $0.remoteId == userId && $0.clientId == clientId }
+    }
+
+    /// Notifies observers of a potential change in the participants set.
+
+    private func notifyChange() {
         guard let context = callCenter.uiMOC else { return }
         
-        let participants = members.map { CallParticipant(member: $0, context: context) }.compactMap(\.self)
-        WireCallCenterCallParticipantNotification(conversationId: conversationId, participants: participants).post(in: context.notificationContext)
+        let participants = members
+            .map { CallParticipant(member: $0, context: context) }
+            .compactMap(\.self)
+
+        WireCallCenterCallParticipantNotification(conversationId: conversationId, participants: participants)
+            .post(in: context.notificationContext)
     }
 
-    public func callParticipantState(forUser userId: UUID) -> CallParticipantState {
-        guard let callMember = members.array.first(where: { $0.remoteId == userId }) else { return .unconnected }
-        
-        return callMember.callParticipantState
-    }
+}
 
-    /// Tries to find the call member with the matching userId and clientId, otherwise the first member
-    /// with the matching userId.
-    ///
-    private func findMember(userId: UUID, clientId: String?) -> AVSCallMember? {
-        let participantsByUser = members.array.filter { $0.remoteId == userId }
-        return participantsByUser.first { $0.clientId == clientId } ?? participantsByUser.first
+extension CallParticipantsSnapshot {
+
+    // Remove duplicates see: https://wearezeta.atlassian.net/browse/ZIOS-8610
+    private static func removeDuplicateMembers(_ members: [AVSCallMember]) -> OrderedSetState<AVSCallMember> {
+        let callMembers = members.reduce([AVSCallMember]()) { (filtered, member) in
+            filtered + (filtered.contains(member) ? [] : [member])
+        }
+
+        return callMembers.toOrderedSetState()
     }
 }
