3a6af40dd65adb8a9a4e6ec299e84f9831b18505
Bill Chan <billypchan@gmail.com>
2020-03-18 10:43:17 +0100

    convert TextTransform (#4193)
    * convert TextTransform and caller
    
    * add file
    
    * clean up

--- Wire-iOS/Sources/Helpers/TextTransform.swift
+++ Wire-iOS/Sources/Helpers/TextTransform.swift
@@ -1,6 +1,6 @@
 //
 // Wire
-// Copyright (C) 2018 Wire Swiss GmbH
+// Copyright (C) 2020 Wire Swiss GmbH
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -16,10 +16,11 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-typedef NS_CLOSED_ENUM(NSUInteger, TextTransform) {
-    TextTransformNone = 0,
-    TextTransformUpper,
-    TextTransformLower,
-    TextTransformCapitalize
-};
+import Foundation
 
+enum TextTransform {
+    case none
+    case upper
+    case lower
+    case capitalize
+}
