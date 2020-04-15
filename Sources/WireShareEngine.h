a9e6a0cdc994f18c2b8ec2c44de8eb50d37d8621
Bill Chan <billypchan@gmail.com>
2020-04-15 18:37:32 +0200

    undo (#159)

---
+++ Sources/WireShareEngine.h
@@ -0,0 +1,29 @@
+//
+// Wire
+// Copyright (C) 2016 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for wire-ios-share-engine.
+FOUNDATION_EXPORT double wire_ios_share_engineVersionNumber;
+
+//! Project version string for wire-ios-share-engine.
+FOUNDATION_EXPORT const unsigned char wire_ios_share_engineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <wire_ios_share_engine/PublicHeader.h>
+
+
