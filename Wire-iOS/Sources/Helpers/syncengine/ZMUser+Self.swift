c8378167e64cb2c63465cda6fcf772c3de52e9ef
Bill Chan <billypchan@gmail.com>
2020-04-27 10:54:52 +0200

    do not force cast MockUser to ZMUser (#4283)

--- Wire-iOS/Sources/Helpers/syncengine/ZMUser+Self.swift
+++ Wire-iOS/Sources/Helpers/syncengine/ZMUser+Self.swift
@@ -1,6 +1,6 @@
 //
 // Wire
-// Copyright (C) 2019 Wire Swiss GmbH
+// Copyright (C) 2020 Wire Swiss GmbH
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -17,9 +17,9 @@
 //
 
 import Foundation
-import WireDataModel
 import WireSyncEngine
 
+#if targetEnvironment(simulator)
 typealias EditableUser = ZMUser & ZMEditableUser
 
 protocol SelfUserProviderUI {
@@ -31,7 +31,6 @@ extension ZMUser {
     /// Return self's User object
     ///
     /// - Returns: a ZMUser<ZMEditableUser> object for app target, or a MockUser object for test.
-    @objc
     static func selfUser() -> EditableUser! {
 
         if let mockUserClass = NSClassFromString("MockUser") as? SelfUserProviderUI.Type {
@@ -43,3 +42,16 @@ extension ZMUser {
         }
     }
 }
+#else
+extension ZMUser {
+
+    /// Return self's User object
+    ///
+    /// - Returns: a ZMUser object for app target
+    static func selfUser() -> ZMUser! {
+        guard let session = ZMUserSession.shared() else { return nil }
+
+        return ZMUser.selfUser(inUserSession: session)
+    }
+}
+#endif
