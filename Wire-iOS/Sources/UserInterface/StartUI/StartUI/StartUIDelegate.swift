f7110a02facb9d4d8030850c24b85b18c535e94d
Jacob Persson <jacob.persson@gmail.com>
2020-04-27 18:22:16 +0200

    [Feature] Removing unboxing of search users in start UI. (#4302)
    * [Feature] Trigger user data & membership fetch on profile screen (#4232)
    
    * Trigger fetch when profile screen is opened
    
    * Fix UserType conformance
    
    * Refresh user data and membership when opening a profile
    
    * Pin datamodel
    
    * Pin components to feature branches
    
    * Update Wire-iOS Tests/DeviceManagement/ProfileViewControllerTests.swift
    
    Co-Authored-By: Jacob Persson <jacob.persson@gmail.com>
    
    Co-authored-by: Jacob Persson <jacob.persson@gmail.com>
    
    * [Feature] Request team metadata refresh on account screen (#4235)
    
    * Refresh team metadata when account screen opened
    
    * Update snapshots
    
    * Update snapshots again
    
    * [Feature] Fetch team metadata every 24 hours (#4237)
    
    * Rename Contacts section to Recents for teams (#4242)
    
    * Pin request strategy
    
    * [Fix] Always show availability status of other users (#4249)
    
    * Update for API changes
    
    * Update tests
    
    * Adapt to search results returning ZMSearchUser
    
    * Bump components
    
    * Change StartUIDelegate to reflect the fact that only one user can be selected
    
    * Revert string change
    
    Co-authored-by: John Nguyen <polyxo@protonmail.com>

--- Wire-iOS/Sources/UserInterface/StartUI/StartUI/StartUIDelegate.swift
+++ Wire-iOS/Sources/UserInterface/StartUI/StartUI/StartUIDelegate.swift
@@ -20,7 +20,7 @@ import Foundation
 import WireDataModel
 
 protocol StartUIDelegate: class {
-    func startUI(_ startUI: StartUIViewController, didSelect users: UserSet)
+    func startUI(_ startUI: StartUIViewController, didSelect user: UserType)
     func startUI(_ startUI: StartUIViewController, didSelect conversation: ZMConversation)
     func startUI(_ startUI: StartUIViewController,
                  createConversationWith users: UserSet,
