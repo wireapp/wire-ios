6f43e6699cfb98156d59475d2c17f89d08ebb09f
Bill Chan <billypchan@gmail.com>
2020-05-05 10:32:02 +0200

    removed unused parameter (#1227)

--- Source/SessionManager/SessionManager+UserActivity.swift
+++ Source/SessionManager/SessionManager+UserActivity.swift
@@ -21,8 +21,7 @@ import Foundation
 extension SessionManager {
     
     /// Forwards the Handoff/CallKit activity that user would like to continue in the app
-    @objc(continueUserActivity:restorationHandler:)
-    public func continueUserActivity(_ userActivity : NSUserActivity, restorationHandler: ([AnyObject]?) -> Void) -> Bool {
+    public func continueUserActivity(_ userActivity : NSUserActivity) -> Bool {
         if #available(iOS 10.0, *) {
             return callKitManager?.continueUserActivity(userActivity) ?? false
         } else {
