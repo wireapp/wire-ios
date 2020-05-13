b6949e923f479fd4eb726f933c8705dc2bf7b56b
Bill Chan <billypchan@gmail.com>
2020-05-13 14:24:14 +0200

    fix warnings (#4338)

--- Wire-iOS/Sources/UserInterface/Collections/CollectionsSectionSet.swift
+++ Wire-iOS/Sources/UserInterface/Collections/CollectionsSectionSet.swift
@@ -21,27 +21,27 @@ import WireDataModel
 
 
 /// This option set represents the collection sections.
-public struct CollectionsSectionSet: OptionSet, Hashable {
-    public let rawValue: UInt
+struct CollectionsSectionSet: OptionSet, Hashable {
+    let rawValue: UInt
     
-    public init(rawValue: UInt) {
+    init(rawValue: UInt) {
         self.rawValue = rawValue
     }
     
-    public init?(index: UInt) {
+    init?(index: UInt) {
         self = type(of: self).visible[Int(index)]
     }
     
-    public static let none = CollectionsSectionSet(rawValue: 0)
-    public static let images = CollectionsSectionSet(rawValue: 1)
-    public static let filesAndAudio = CollectionsSectionSet(rawValue: 1 << 1)
-    public static let videos = CollectionsSectionSet(rawValue: 1 << 2)
-    public static let links = CollectionsSectionSet(rawValue: 1 << 3)
-    public static let loading = CollectionsSectionSet(rawValue: 1 << 4) // special section that shows the loading view
+    static let none = CollectionsSectionSet([])
+    static let images = CollectionsSectionSet(rawValue: 1)
+    static let filesAndAudio = CollectionsSectionSet(rawValue: 1 << 1)
+    static let videos = CollectionsSectionSet(rawValue: 1 << 2)
+    static let links = CollectionsSectionSet(rawValue: 1 << 3)
+    static let loading = CollectionsSectionSet(rawValue: 1 << 4) // special section that shows the loading view
     
     /// Returns all possible section types
-    public static let all: CollectionsSectionSet = [.images, .filesAndAudio, .videos, .links, .loading]
+    static let all: CollectionsSectionSet = [.images, .filesAndAudio, .videos, .links, .loading]
     
     /// Returns visible sections in the display order
-    public static let visible: [CollectionsSectionSet] = [images, videos, links, filesAndAudio, loading]
+    static let visible: [CollectionsSectionSet] = [images, videos, links, filesAndAudio, loading]
 }
