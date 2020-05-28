460a32cf8ae5704dd371586a78d0b05d0b7ad580
KaterinaWire <katerina@wire.com>
2020-05-28 15:05:53 +0200

    Initial

---
+++ Resources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wire-ios-notification-engine
+//
+//  Created by Katerina on 28.05.20.
+//  Copyright © 2020 Wire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
