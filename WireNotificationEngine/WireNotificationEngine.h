92a81e845c70e170597201a1f94bddbd64601c92
KaterinaWire <katerina@wire.com>
2020-06-03 12:13:18 +0200

    Add framework and test targets

---
+++ WireNotificationEngine/WireNotificationEngine.h
@@ -0,0 +1,19 @@
+//
+//  WireNotificationEngine.h
+//  WireNotificationEngine
+//
+//  Created by Katerina on 03.06.20.
+//  Copyright © 2020 Wire. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WireNotificationEngine.
+FOUNDATION_EXPORT double WireNotificationEngineVersionNumber;
+
+//! Project version string for WireNotificationEngine.
+FOUNDATION_EXPORT const unsigned char WireNotificationEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WireNotificationEngine/PublicHeader.h>
+
+
