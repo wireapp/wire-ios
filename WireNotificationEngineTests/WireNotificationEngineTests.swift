1b001e7e0c6f21f95530d393341a173d1e331b4c
KaterinaWire <katerina@wire.com>
2020-06-04 16:31:29 +0200

    Add test example

--- WireNotificationEngineTests/WireNotificationEngineTests.swift
+++ WireNotificationEngineTests/WireNotificationEngineTests.swift
@@ -17,27 +17,22 @@
 //
 
 import XCTest
+import WireDataModel
+import WireTesting
+@testable import WireNotificationEngine
 
-class WireNotificationEngineTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class WireNotificationEngineTests: ZMTBaseTest {
+    
+    override func setUp() {
+        super.setUp()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testExample() {
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+        XCTAssertTrue(true)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
