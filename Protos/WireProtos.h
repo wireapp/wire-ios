a9dee2c29565cfbbc417186707b6e3a532a503b8
David-Henner <david.henner@wire.com>
2020-06-04 09:48:12 +0200

    [Refactor] Remove Messages.pb  (#40)
    * remove objc messages protos
    
    * Fix project not building
    
    * remove objc protos
    
    * remove import
    
    * remove ProtocolBuffers framework reference

--- Protos/WireProtos.h
+++ Protos/WireProtos.h
@@ -27,6 +27,3 @@ FOUNDATION_EXPORT const unsigned char ProtosVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Protos/PublicHeader.h>
 
-#import <WireProtos/Messages.pb.h>
-#import <WireProtos/Otr.pb.h>
-
