711b154d55745138be64dee333898502f20aa41a
KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
2020-06-09 17:46:54 +0200

    [Fix] Internal debug message for the GenericMessage (#994)
    * Add debugDescription for the GenericMessage extension
    
    * Modify the debugDescription
    
    * Clean up
    
    * Changed the debugDescription property name
    
    * Changed the description property declaration

--- Source/Model/Message/ZMOTRMessage+UpdateEvent.swift
+++ Source/Model/Message/ZMOTRMessage+UpdateEvent.swift
@@ -45,7 +45,7 @@ extension ZMOTRMessage {
                 appendInvalidSystemMessage(forUpdateEvent: updateEvent, toConversation: conversation, inContext: moc)
                 return nil
         }
-        zmLog.debug("Processing:\n\(message.debugDescription)")
+        zmLog.debug("Processing:\n\(message)")
         
         // Update the legal hold state in the conversation
         conversation.updateSecurityLevelIfNeededAfterReceiving(message: message, timestamp: updateEvent.timeStamp() ?? Date())
