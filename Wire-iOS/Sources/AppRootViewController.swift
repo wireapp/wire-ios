e8dcdc634317fe6140c14823aebdbeb6012174e4
David-Henner <david.henner@wire.com>
2020-07-28 13:39:15 +0200

    [Feature] limit number of video participants (#4486)
    * first draft
    
    * update video configuration
    
    * remove VideoCallType
    
    * update video call button display
    
    * one-line ternary
    
    * fix tests
    
    * bump SE
    
    * use `isConferenceCall`
    
    * Fix test compilation
    
    Co-authored-by: John Nguyen <polyxo@protonmail.com>

--- Wire-iOS/Sources/AppRootViewController.swift
+++ Wire-iOS/Sources/AppRootViewController.swift
@@ -200,8 +200,6 @@ final class AppRootViewController: UIViewController, SpinnerCapable {
             self.quickActionsManager = QuickActionsManager(sessionManager: sessionManager,
                                                            application: UIApplication.shared)
         }
-
-        ZMConversation.callCenterConfiguration = configuration.callCenterConfiguration
     }
 
     func enqueueTransition(to appState: AppState, completion: (() -> Void)? = nil) {
