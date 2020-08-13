e381fddd5be669dd60d647098e8f59eef2f2a1cd
marcomaddy1982 <30315526+marcomaddy1982@users.noreply.github.com>
2020-08-13 14:02:30 +0200

    [Feature]: Feature Flag CBR Enabled (#4526)

--- Wire-iOS/Sources/UserInterface/Settings/CellDescriptors/SettingsCellDescriptorFactory+Options.swift
+++ Wire-iOS/Sources/UserInterface/Settings/CellDescriptors/SettingsCellDescriptorFactory+Options.swift
@@ -33,7 +33,7 @@ extension SettingsCellDescriptorFactory {
             chatHeadsSection,
             soundAlertSection,
             callKitSection,
-            VBRSection,
+            SecurityFlags.forceConstantBitRateCalls.isEnabled ? nil : VBRSection,
             conferenceCallingSection,
             soundsSection,
             externalAppsSection,
