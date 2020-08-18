567ece26f8c5fd27f0d05b0fbbe93d4ea846cf20
Jacob Persson <jacob.persson@gmail.com>
2020-08-18 11:16:10 +0200

    [Feature] Encryption at rest (#174)
    * [Feature] Update DeliveryConfirmationDelegate removal (#172)
    
    * Update after removing DeliveryConfirmationDelegate protocol
    
    * Update Cartfile.resolved
    
    * Update Cartfile.resolved
    
    * Update after changes to to PushMessageHandler protocol (#173)
    
    * Bump components
    
    Co-authored-by: John Nguyen <polyxo@protonmail.com>

--- Sources/SharingSession.swift
+++ Sources/SharingSession.swift
@@ -24,36 +24,8 @@ import WireRequestStrategy
 import WireLinkPreview
 
 class PushMessageHandlerDummy : NSObject, PushMessageHandler {
-
-    func process(_ message: ZMMessage) {
-        // nop
-    }
-
-    public func process(_ event: ZMUpdateEvent) {
-        // nop
-    }
     
     func didFailToSend(_ message: ZMMessage) {
-        // nop
-    }
-    
-}
-
-class DeliveryConfirmationDummy : NSObject, DeliveryConfirmationDelegate {
-    
-    static var sendDeliveryReceipts: Bool {
-        return false
-    }
-    
-    var needsToSyncMessages: Bool {
-        return false
-    }
-    
-    func needsToConfirmMessage(_ messageNonce: UUID) {
-        // nop
-    }
-    
-    func didConfirmMessage(_ messageNonce: UUID) {
         // nop
     }
     
@@ -112,7 +84,6 @@ extension BackendEnvironmentProvider {
 class ApplicationStatusDirectory : ApplicationStatus {
 
     let transportSession : ZMTransportSession
-    let deliveryConfirmationDummy : DeliveryConfirmationDummy
     
     /// The authentication status used to verify a user is authenticated
     public let authenticationStatus: AuthenticationStatusProvider
@@ -126,7 +97,6 @@ class ApplicationStatusDirectory : ApplicationStatus {
         self.transportSession = transportSession
         self.authenticationStatus = authenticationStatus
         self.clientRegistrationStatus = clientRegistrationStatus
-        self.deliveryConfirmationDummy = DeliveryConfirmationDummy()
         self.linkPreviewDetector = linkPreviewDetector
     }
     
@@ -158,11 +128,7 @@ class ApplicationStatusDirectory : ApplicationStatus {
     public var requestCancellation: ZMRequestCancellation {
         return transportSession
     }
-    
-    public var deliveryConfirmation: DeliveryConfirmationDelegate {
-        return deliveryConfirmationDummy
-    }
-    
+        
     func requestSlowSync() {
         // we don't do slow syncing in the share engine
     }
