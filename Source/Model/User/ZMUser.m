d1b91d60a1b0b88b3203ea5945d336c53a0c4001
marcomaddy1982 <30315526+marcomaddy1982@users.noreply.github.com>
2020-10-05 10:02:17 +0200

    [Feature]: Analytics ID Improvements (#1036)
    * - added transportString
    
    * -added tests
    
    * - tests
    
    * - UUID validation with regex
    
    * - address the PR comments for the tests
    
    * - clean up
    
    * - clean up

--- Source/Model/User/ZMUser.m
+++ Source/Model/User/ZMUser.m
@@ -707,7 +707,7 @@ static NSString *const AnalyticsIdentifierKey = @"analyticsIdentifier";
     
     if (selfUser == nil) {
         selfUser = [ZMUser insertNewObjectInManagedObjectContext:moc];
-        selfUser.analyticsIdentifier = [[NSUUID UUID] UUIDString];
+        selfUser.analyticsIdentifier = [[NSUUID UUID] transportString];
         RequireString([moc obtainPermanentIDsForObjects:@[selfUser] error:&error],
                       "Failed to get ID for self user: %lu", (long) error.code);
     }
