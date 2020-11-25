174e8cf2f649676d21eb100fc98ccbad49dfab5b
David-Henner <david.henner@wire.com>
2020-11-25 10:14:16 +0100

    [Feature] maximise / minimise video stream on double tap (#4718)
    * toggle maximized screen on double tap
    
    * use isMaximized
    
    * fit video in view when maximized
    
    * update fill mode logic to support screenshare
    
    * use collectionView to find item
    
    * update tests

--- Wire-iOS/Sources/UserInterface/Calling/CallViewController/CallViewController.swift
+++ Wire-iOS/Sources/UserInterface/Calling/CallViewController/CallViewController.swift
@@ -108,11 +108,9 @@ final class CallViewController: UIViewController {
     }
 
     @objc func handleDoubleTap(_ sender: UITapGestureRecognizer) {
-
         guard !isOverlayVisible else { return }
 
-        let location = sender.location(in: self.view)
-        videoGridViewController.switchFillMode(location: location)
+        videoGridViewController.handleDoubleTap(gesture: sender)
     }
 
     deinit {
