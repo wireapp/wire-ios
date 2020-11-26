c01187a0a39fd2741ebc350660586d31b232a2f3
Bill Chan <billypchan@gmail.com>
2020-11-26 10:23:24 +0100

    do not load level when testing (#4739)

--- Wire-iOS Tests/AudioEffectsPickerViewControllerTests.swift
+++ Wire-iOS Tests/AudioEffectsPickerViewControllerTests.swift
@@ -19,7 +19,7 @@
 import XCTest
 @testable import Wire
 
-class AudioEffectsPickerViewControllerTests: ZMSnapshotTestCase {
+final class AudioEffectsPickerViewControllerTests: ZMSnapshotTestCase {
     var sut: AudioEffectsPickerViewController! = .none
     
     
