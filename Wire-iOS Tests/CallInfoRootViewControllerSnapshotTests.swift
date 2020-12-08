b0125d611d3181cc038432138de6c39dd85256ef
Bill Chan <billypchan@gmail.com>
2020-12-08 22:43:21 +0100

    inject self user in CallInfoViewController (#4761)

--- Wire-iOS Tests/CallInfoRootViewControllerSnapshotTests.swift
+++ Wire-iOS Tests/CallInfoRootViewControllerSnapshotTests.swift
@@ -40,7 +40,7 @@ final class CallInfoViewControllerSnapshotTests: XCTestCase, CoreDataFixtureTest
         let fixture = CallInfoTestFixture(otherUser: otherUser)
 
         // when
-        let sut = CallInfoViewController(configuration: fixture.oneToOneIncomingAudioRinging)
+        let sut = CallInfoViewController(configuration: fixture.oneToOneIncomingAudioRinging, user: coreDataFixture.selfUser)
 
         // then
         verifyAllIPhoneSizes(matching: sut)
