1b57359ddd18fb79e7a4922fd894931827b7f21b
Jacob Persson <jacob.persson@gmail.com>
2020-12-15 09:23:24 +0100

    Revert "Fix: remove unnecessary member variables from background view controller (#4765)"
    This reverts commit 2c2b70036d8e385fac52cf038082487caee3a055.

--- Wire-iOS Tests/BackgroundViewControllerTests.swift
+++ Wire-iOS Tests/BackgroundViewControllerTests.swift
@@ -22,69 +22,52 @@ import XCTest
 final class BackgroundViewControllerTests: XCTestCase {
 
     var selfUser: MockUserType!
-    var sut: BackgroundViewController!
-    var snapshotExpectation: XCTestExpectation!
-    var firstTrigger = true
 
     override func setUp() {
         super.setUp()
         accentColor = .violet
         selfUser = MockUserType.createSelfUser(name: "")
         selfUser.accentColorValue = .violet
-        
-        firstTrigger = true
-   }
+    }
 
     override func tearDown() {
         selfUser = nil
-        sut = nil
-        snapshotExpectation = nil
 
         super.tearDown()
     }
 
     func testThatItShowsUserWithoutImage() {
         // GIVEN
-        snapshotExpectation = expectation(description: "snapshot verified")
+        let sut = BackgroundViewController(user: selfUser, userSession: .none)
+        XCTAssertTrue(waitForGroupsToBeEmpty([sut.dispatchGroup]))
 
-        let userImageLoaded: Completion = {
-            // WHEN & THEN
-            self.verify(matching: self.sut)
-            self.snapshotExpectation.fulfill()
-        }
-
-        sut = BackgroundViewController(user: selfUser, userSession: .none, userImageLoaded: userImageLoaded)
-
-        waitForExpectations(timeout: 5)
+        // WHEN & THEN
+        verify(matching: sut)
     }
 
     func testThatItShowsUserWithImage() {
         // GIVEN
-        snapshotExpectation = expectation(description: "snapshot verified")
-
-        let userImageLoaded: Completion = {
-            // WHEN
-            ///TODO: hacks to make below line passes
-            self.selfUser.accentColorValue = self.selfUser.accentColorValue
-
-            // THEN
-            self.verify(matching: self.sut)
-            self.snapshotExpectation.fulfill()
-        }
-
         selfUser.completeImageData = image(inTestBundleNamed: "unsplash_matterhorn.jpg").pngData()
-        sut = BackgroundViewController(user: selfUser, userSession: .none, userImageLoaded: userImageLoaded)
+        let sut = BackgroundViewController(user: selfUser, userSession: .none)
+        // make sure view is loaded
+        _ = sut.view
+        // WHEN
+        ///TODO: hacks to make below line passes
+        selfUser.accentColorValue = selfUser.accentColorValue
 
-        waitForExpectations(timeout: 5)
+        XCTAssertTrue(waitForGroupsToBeEmpty([sut.dispatchGroup], timeout: 10))
+
+        // WHEN & THEN
+        verify(matching: sut)
     }
 
     func testThatItUpdatesForUserAccentColorUpdate_fromAccentColor() {
         // GIVEN
-        sut = BackgroundViewController(user: selfUser, userSession: .none)
-
+        let sut = BackgroundViewController(user: selfUser, userSession: .none)
+        _ = sut.view
         // WHEN
         selfUser.accentColorValue = .brightOrange
-        sut.updateFor(user: selfUser, imageMediumDataChanged: false, accentColorValueChanged: true)
+        sut.updateFor(imageMediumDataChanged: false, accentColorValueChanged: true)
 
         // THEN
         verify(matching: sut)
@@ -93,74 +76,53 @@ final class BackgroundViewControllerTests: XCTestCase {
     func testThatItUpdatesForUserAccentColorUpdate_fromUserImageRemoved() {
         // GIVEN
         selfUser.completeImageData = image(inTestBundleNamed: "unsplash_matterhorn.jpg").pngData()
-        sut = BackgroundViewController(user: selfUser, userSession: .none)
+        let sut = BackgroundViewController(user: selfUser, userSession: .none)
+        _ = sut.view
         // WHEN
         selfUser.completeImageData = nil
         selfUser.accentColorValue = .brightOrange
-        sut.updateFor(user: selfUser, imageMediumDataChanged: true, accentColorValueChanged: true)
+        sut.updateFor(imageMediumDataChanged: true, accentColorValueChanged: true)
         // THEN
         verify(matching: sut)
     }
 
     func testThatItUpdatesForUserAccentColorUpdate_fromUserImage() {
         // GIVEN
-        snapshotExpectation = expectation(description: "snapshot verified")
-
         selfUser.completeImageData = image(inTestBundleNamed: "unsplash_matterhorn.jpg").pngData()
-        sut = BackgroundViewController(user: selfUser, userSession: .none, userImageLoaded: createIgnoreFirstTriggerVerifyClosure())
-
+        let sut = BackgroundViewController(user: selfUser, userSession: .none)
+        _ = sut.view
         // WHEN
         selfUser.accentColorValue = .brightOrange
+        sut.updateFor(imageMediumDataChanged: true, accentColorValueChanged: true)
+        XCTAssertTrue(waitForGroupsToBeEmpty([sut.dispatchGroup]))
 
-        ///This triggers user image updating again, we skip the first snapshot triggered by sut init
-        sut.updateFor(user: selfUser, imageMediumDataChanged: true, accentColorValueChanged: true)
-
-        waitForExpectations(timeout: 10)
+        // THEN
+        verify(matching: sut)
     }
 
     func testThatItUpdatesForUserImageUpdate_fromAccentColor() {
         // GIVEN
-        snapshotExpectation = expectation(description: "snapshot verified")
-
-
         selfUser.completeImageData = nil
-        sut = BackgroundViewController(user: selfUser, userSession: .none, userImageLoaded: createIgnoreFirstTriggerVerifyClosure())
+        let sut = BackgroundViewController(user: selfUser, userSession: .none)
+        _ = sut.view
         // WHEN
         selfUser.completeImageData = image(inTestBundleNamed: "unsplash_matterhorn.jpg").pngData()
-
-        ///This triggers user image updating again, we skip the first snapshot triggered by sut init
-        sut.updateFor(user: selfUser, imageMediumDataChanged: true, accentColorValueChanged: false)
-
-        waitForExpectations(timeout: 10)
+        sut.updateFor(imageMediumDataChanged: true, accentColorValueChanged: false)
+        XCTAssertTrue(waitForGroupsToBeEmpty([sut.dispatchGroup]))
+        // THEN
+        verify(matching: sut)
     }
 
     func testThatItUpdatesForUserImageUpdate_fromUserImage() {
         // GIVEN
-        snapshotExpectation = expectation(description: "snapshot verified")
-
         selfUser.completeImageData = image(inTestBundleNamed: "unsplash_matterhorn.jpg").pngData()
-        sut = BackgroundViewController(user: selfUser, userSession: .none, userImageLoaded: createIgnoreFirstTriggerVerifyClosure())
+        let sut = BackgroundViewController(user: selfUser, userSession: .none)
+        _ = sut.view
         // WHEN
         selfUser.completeImageData = image(inTestBundleNamed: "unsplash_burger.jpg").pngData()
-        sut.updateFor(user: selfUser, imageMediumDataChanged: true, accentColorValueChanged: false)
-
-        waitForExpectations(timeout: 10)
-    }
-    
-    private func createIgnoreFirstTriggerVerifyClosure(file: StaticString = #file,
-                                                       testName: String = #function,
-                                                       line: UInt = #line) -> Completion {
-        let userImageLoaded: Completion = {
-            if self.firstTrigger {
-                self.firstTrigger = false
-                return
-            }
-            
-            // THEN
-            self.verify(matching: self.sut, file: file, testName: testName, line: line)
-            self.snapshotExpectation.fulfill()
-        }
-        
-        return userImageLoaded
+        sut.updateFor(imageMediumDataChanged: true, accentColorValueChanged: false)
+        XCTAssertTrue(waitForGroupsToBeEmpty([sut.dispatchGroup]))
+        // THEN
+        verify(matching: sut)
     }
 }
--- Wire-iOS/Sources/UserInterface/ConversationList/BackgroundViewController.swift
+++ Wire-iOS/Sources/UserInterface/ConversationList/BackgroundViewController.swift
@@ -1,6 +1,6 @@
 //
 // Wire
-// Copyright (C) 2020 Wire Swiss GmbH
+// Copyright (C) 2017 Wire Swiss GmbH
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -16,73 +16,75 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
+
 import UIKit
 import WireSyncEngine
 
 final class BackgroundViewController: UIViewController {
-
-    /// call back when user image view's image is loaded. It may call multiple times.
-    /// First time occurs after view is loaded if user has image data
-    let userImageLoaded: Completion?
-
+    
+    lazy var dispatchGroup: DispatchGroup = DispatchGroup()
+    
     fileprivate let imageView = UIImageView()
     private let cropView = UIView()
     private let darkenOverlay = UIView()
     private var blurView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
-    private var userObserverToken: NSObjectProtocol!
-
+    private var userObserverToken: NSObjectProtocol! = .none
+    private let user: UserType
+    
     var darkMode: Bool = false {
         didSet {
             darkenOverlay.isHidden = !darkMode
         }
     }
-
+    
     init(user: UserType,
-         userSession: ZMUserSession?,
-         userImageLoaded: Completion? = nil) {
-        self.userImageLoaded = userImageLoaded
-
+         userSession: ZMUserSession?) {
+        self.user = user
         super.init(nibName: .none, bundle: .none)
-
-        configureViews()
-        createConstraints()
-
-        updateForUser(user: user)
-        updateForColorScheme()
-
-        setupObservers(user: user, userSession: userSession)
+        
+        setupObservers(userSession: userSession)
     }
-
-    private func setupObservers(user: UserType, userSession: ZMUserSession?) {
+    
+    private func setupObservers(userSession: ZMUserSession?) {
         guard !ProcessInfo.processInfo.isRunningTests else { return }
 
         if let userSession = userSession {
             userObserverToken = UserChangeInfo.add(observer: self, for: user, in: userSession)
         }
-
+        
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(colorSchemeChanged),
                                                name: .SettingsColorSchemeChanged,
                                                object: nil)
     }
-
+    
     @available(*, unavailable)
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.configureViews()
+        self.createConstraints()
+        
+        self.updateForUser()
+        self.updateForColorScheme()
+    }
+    
     private var child: UIViewController? {
         return children.first
     }
-
+    
     override var childForStatusBarStyle: UIViewController? {
         return child
     }
-
+    
     override var childForStatusBarHidden: UIViewController? {
         return child
     }
-
+    
     private func configureViews() {
         let factor = BackgroundViewController.backgroundScaleFactor
         imageView.contentMode = .scaleAspectFill
@@ -90,17 +92,17 @@ final class BackgroundViewController: UIViewController {
 
         cropView.clipsToBounds = true
         darkenOverlay.backgroundColor = UIColor(white: 0, alpha: 0.16)
-
+        
         [imageView, blurView, darkenOverlay].forEach(self.cropView.addSubview)
-
+        
         self.view.addSubview(self.cropView)
     }
-
+    
     private func createConstraints() {
-        [cropView,
-         blurView,
-         imageView,
-         darkenOverlay].disableAutoresizingMaskTranslation()
+        cropView.translatesAutoresizingMaskIntoConstraints = false
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        darkenOverlay.translatesAutoresizingMaskIntoConstraints = false
 
         let constraints = [
             // Crop view
@@ -125,59 +127,61 @@ final class BackgroundViewController: UIViewController {
             darkenOverlay.leadingAnchor.constraint(equalTo: cropView.leadingAnchor),
             darkenOverlay.topAnchor.constraint(equalTo: cropView.topAnchor),
             darkenOverlay.trailingAnchor.constraint(equalTo: cropView.trailingAnchor),
-            darkenOverlay.bottomAnchor.constraint(equalTo: cropView.bottomAnchor)
+            darkenOverlay.bottomAnchor.constraint(equalTo: cropView.bottomAnchor),
         ]
 
         NSLayoutConstraint.activate(constraints)
     }
 
-    private func updateForUser(user: UserType) {
-        guard isViewLoaded else {
+    private func updateForUser() {
+        guard self.isViewLoaded else {
             return
         }
-
-        updateForUserImage(user: user)
-        updateForAccentColor(user: user)
+        
+        updateForUserImage()
+        updateForAccentColor()
     }
 
-    private func updateForUserImage(user: UserType) {
+    private func updateForUserImage() {
+        dispatchGroup.enter()
         user.imageData(for: .complete, queue: DispatchQueue.global(qos: .background)) { [weak self] (imageData) in
-            var image: UIImage?
+            var image: UIImage? = nil
             if let imageData = imageData {
                 image = BackgroundViewController.blurredAppBackground(with: imageData)
             }
-
+            
             DispatchQueue.main.async {
                 self?.imageView.image = image
-                self?.userImageLoaded?()
+                self?.dispatchGroup.leave()
             }
         }
     }
-
-    private func updateForAccentColor(user: UserType) {
+    
+    private func updateForAccentColor() {
         setBackground(color: UIColor(fromZMAccentColor: user.accentColorValue))
     }
-
+    
     private func updateForColorScheme() {
         darkMode = ColorScheme.default.variant == .dark
     }
+    
+    func updateFor(imageMediumDataChanged: Bool, accentColorValueChanged: Bool) {
 
-    func updateFor(user: UserType, imageMediumDataChanged: Bool, accentColorValueChanged: Bool) {
         if imageMediumDataChanged {
-            updateForUserImage(user: user)
+            updateForUserImage()
         }
-
+        
         if accentColorValueChanged {
-            updateForAccentColor(user: user)
+            updateForAccentColor()
         }
     }
-
+    
     static let backgroundScaleFactor: CGFloat = 1.4
-
+    
     static func blurredAppBackground(with imageData: Data) -> UIImage? {
         return UIImage(from: imageData, withMaxSize: 40)?.desaturatedImage(with: CIContext.shared, saturation: 2)
     }
-
+        
     fileprivate func setBackground(color: UIColor) {
         self.imageView.backgroundColor = color
     }
@@ -190,8 +194,8 @@ final class BackgroundViewController: UIViewController {
 
 extension BackgroundViewController: ZMUserObserver {
     func userDidChange(_ changeInfo: UserChangeInfo) {
-        updateFor(user: changeInfo.user,
-                  imageMediumDataChanged: changeInfo.imageMediumDataChanged,
-                  accentColorValueChanged: changeInfo.accentColorValueChanged)
+        updateFor(imageMediumDataChanged: changeInfo.imageMediumDataChanged,
+                       accentColorValueChanged: changeInfo.accentColorValueChanged)
     }
 }
+
