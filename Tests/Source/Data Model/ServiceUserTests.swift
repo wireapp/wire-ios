cf6bbd66eea4b5c8328aaf413c60cbae39b44c1f
Bill Chan <billypchan@gmail.com>
2020-12-17 12:16:00 +0100

    chore: bump components (#1364)
    * bump components
    
    * add new property
    
    * init isTrust to false

--- Tests/Source/Data Model/ServiceUserTests.swift
+++ Tests/Source/Data Model/ServiceUserTests.swift
@@ -23,6 +23,8 @@ import XCTest
 
 final class DummyServiceUser: NSObject, ServiceUser {
     
+    var isTrusted: Bool = false
+    
     var hasLegalHoldRequest: Bool = false
     
     var needsRichProfileUpdate: Bool = false
