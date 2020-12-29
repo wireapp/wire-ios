62528bd5153e6ec0176ec28a908ec05f32f8fd33
Bill Chan <billypchan@gmail.com>
2020-12-29 13:48:33 +0100

    fix image not loaded issue (#4784)

--- Wire-iOS/Sources/Helpers/NSData+ImageType.swift
+++ Wire-iOS/Sources/Helpers/NSData+ImageType.swift
@@ -16,12 +16,11 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-///TODO: move to utilities
 import Foundation
 
 extension Data {
     var isJPEG: Bool {
-        let array = self.withUnsafeBytes { (unsafeRawBufferPointer: UnsafeRawBufferPointer) in
+        let array = withUnsafeBytes { (unsafeRawBufferPointer: UnsafeRawBufferPointer) in
             [UInt8](UnsafeBufferPointer(start: unsafeRawBufferPointer.bindMemory(to: UInt8.self).baseAddress!, count: 3))
         }
         let JPEGHeader: [UInt8] = [0xFF, 0xD8, 0xFF]
