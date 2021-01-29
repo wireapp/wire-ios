a5438710d8b17ad964702e383c0376fb1ada9d6a
Bill Chan <billypchan@gmail.com>
2021-01-29 17:36:51 +0100

    Fix:  CallInfoViewControllerSnapshotTests & ConnectRequestsCellSnapshotTests without ZMUser (#4874)
    * fix test
    
    * fix tests
    
    * add createDefaultOtherUser
    
    * regen snapshot

--- Wire-iOS Tests/Mocks/MockUserType+Creation.swift
+++ Wire-iOS Tests/Mocks/MockUserType+Creation.swift
@@ -76,5 +76,14 @@ extension MockUserType {
         user.teamRole = teamID != nil ? .member : .none
         return user
     }
-
+    
+    /// Create a connected Mock user with name Bruno and orange accent color
+    /// - Returns: a mock user
+    class func createDefaultOtherUser() -> MockUserType {
+        let user = MockUserType.createUser(name: "Bruno")
+        user.accentColorValue = .brightOrange
+        user.isConnected = true
+        
+        return user
+    }
 }
