fcbb6e4587b9f2cf4ac0cb5d04bf1a50fab51d42
John Nguyen <polyxo@protonmail.com>
2021-02-04 10:33:04 +0100

    [Refactor] App lock config sync (#1389)
    * Create default and request update when quick sync finished
    
    * Pin data model
    
    * Pin components
    
    * Remove redundant perform block
    
    * Clean up
    
    * Bump components

--- Source/UserSession/ZMUserSession.swift
+++ Source/UserSession/ZMUserSession.swift
@@ -504,6 +504,12 @@ extension ZMUserSession: ZMSyncStateDelegate {
         managedObjectContext.performGroupedBlock { [weak self] in
             self?.notifyThirdPartyServices()
         }
+
+        // TODO: [John] This is a tempory solution until we add support for slow syncing
+        // team features and config update events.
+        guard let team = ZMUser.selfUser(in: syncManagedObjectContext).team else { return }
+        Feature.createDefaultInstanceIfNeeded(name: .appLock, team: team, context: syncManagedObjectContext)
+        team.enqueueBackendRefresh(for: .appLock)
     }
     
     func processEvents() {
@@ -538,7 +544,7 @@ extension ZMUserSession: ZMSyncStateDelegate {
             thirdPartyServicesDelegate?.userSessionIsReadyToUploadServicesData(userSession: self)
         }
     }
-    
+
 }
 
 extension ZMUserSession: URLActionProcessor {
