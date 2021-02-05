242ef4ec3e648fb967fd4c7888cf192daec3818c
Bill Chan <billypchan@gmail.com>
2021-02-05 14:48:11 +0100

    remove hack for simulator build (#4888)

--- Wire-iOS/Sources/Authentication/Backup/BackupRestoreController.swift
+++ Wire-iOS/Sources/Authentication/Backup/BackupRestoreController.swift
@@ -54,13 +54,6 @@ final class BackupRestoreController: NSObject {
 
     fileprivate func showFilePicker() {
         // Test code to verify restore
-        #if arch(i386) || arch(x86_64)
-        let testFilePath = "/var/tmp/backup.ios_wbu"
-        if FileManager.default.fileExists(atPath: testFilePath) {
-            self.restore(with: URL(fileURLWithPath: testFilePath))
-            return
-        }
-        #endif
 
         let picker = UIDocumentPickerViewController(
             documentTypes: BackupRestoreController.WireBackupUTIs,
