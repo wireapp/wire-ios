cfe14faa62aaa537d649622e09bc02e02aeba9f5
Bill Chan <billypchan@gmail.com>
2021-06-07 17:34:45 +0200

    convert ZMSyncStrategy.callingRequestStrategy to swift (#1446)
    * covert ZMSyncStrategy.callingRequestStrategy to swfit for remove @objc c of CallingRequestStrategy
    
    * add file to project
    
    * clean up objc code
    
    * Update Tests/Source/Integration/APNSTests.m

--- Source/Synchronization/ZMSyncStrategy.swift
+++ Source/Synchronization/ZMSyncStrategy.swift
@@ -1,45 +1,28 @@
-// 
+//
 // Wire
-// Copyright (C) 2016 Wire Swiss GmbH
-// 
+// Copyright (C) 2021 Wire Swiss GmbH
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see http://www.gnu.org/licenses/.
-// 
+//
 
 
-#import "ZMSyncStrategy.h"
+import Foundation
 
-@interface ZMSyncStrategy (Internal)
-
-@property (atomic, readonly) BOOL tornDown;
-@property (nonatomic, weak, readonly) NSManagedObjectContext *uiMOC;
-@property (nonatomic, readonly) NotificationDispatcher *notificationDispatcher;
-
-@end
-
-
-@interface ZMSyncStrategy (AppBackgroundForeground)
-
-- (void)appDidEnterBackground:(NSNotification *)note;
-- (void)appWillEnterForeground:(NSNotification *)note;
-
-@end
-
-
-@interface ZMSyncStrategy (Testing)
-
-@property (nonatomic) BOOL contextMergingDisabled;
-
-@end
-
-
+extension ZMSyncStrategy {
+    var callingRequestStrategy: CallingRequestStrategy? {
+        return strategyDirectory?.requestStrategies.first(where: { requestStrategy in
+            requestStrategy is CallingRequestStrategy
+        }) as? CallingRequestStrategy
+    }
+}
