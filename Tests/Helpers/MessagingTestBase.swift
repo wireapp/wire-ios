2886c57bfaf39cf7565da2ba35b94b70e663de51
Jacob Persson <jacob.persson@gmail.com>
2021-06-14 17:49:36 +0200

    [Feature] Support federation aware API for fetching prekeys (#307)
    * [Refactor] Simplify MissingClientsRequestStrategy (#305)
    
    * [Refactor] Make payload processing typesafe and simplify MissingClientsRequestStrategy
    
    * Delete duplicated method
    
    * Use dictionary default
    
    * Improve documentation
    
    * [Feature] Support new federation aware endpoint for fetching prekeys (#306)
    
    * [Refactor] Make payload processing typesafe and simplify MissingClientsRequestStrategy
    
    * Delete duplicated method
    
    * [Feature] Support new prekey endpoint with a fallback to old endpoint if unvailable
    
    * [Refactor] Rename payloads to make it more clear we are dealing with qualified userIDs
    
    * Use dictionary default
    
    * Improve documentation
    
    * Simplify using dictionary defaults
    
    * Fix tests which expects the domain to not be set
    
    * Reduce code duplication
    
    * Remove force uwnrap
    
    * Delete file which re-appeared after a merge

--- Tests/Helpers/MessagingTestBase.swift
+++ Tests/Helpers/MessagingTestBase.swift
@@ -230,6 +230,7 @@ extension MessagingTestBase {
     func createUser(alsoCreateClient: Bool = false) -> ZMUser {
         let user = ZMUser.insertNewObject(in: self.syncMOC)
         user.remoteIdentifier = UUID.create()
+        user.domain = "example.com"
         if alsoCreateClient {
             _ = self.createClient(user: user)
         }
@@ -272,6 +273,7 @@ extension MessagingTestBase {
     fileprivate func createSelfClient() -> UserClient {
         let user = ZMUser.selfUser(in: self.syncMOC)
         user.remoteIdentifier = UUID.create()
+        user.domain = "example.com"
         
         let selfClient = UserClient.insertNewObject(in: self.syncMOC)
         selfClient.remoteIdentifier = "baddeed"
