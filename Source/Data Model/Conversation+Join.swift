5837ebd26feb0ceeeda1a301e57d558264e647a9
KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
2021-06-22 15:54:11 +0200

    [Fix] Perform the request from viewContext (#1461)
    * Run join conversation request in viewContext
    
    * Update the doc

--- Source/Data Model/Conversation+Join.swift
+++ Source/Data Model/Conversation+Join.swift
@@ -54,7 +54,7 @@ extension ZMConversation {
     ///   - transportSession: session to handle requests
     ///   - eventProcessor: update event processor
     ///   - contextProvider: context provider
-    ///   - completion: called when the user joines the conversation or when it fails. If the completion is a success, it is run in the main thread
+    ///   - completion: called on the main thread when the user joins the conversation or when it fails. If the completion is a success, it is run in the main thread
     public static func join(key: String,
                             code: String,
                             transportSession: TransportSessionType,
@@ -66,7 +66,7 @@ extension ZMConversation {
         let syncContext = contextProvider.syncContext
         let viewContext = contextProvider.viewContext
 
-        request.add(ZMCompletionHandler(on: syncContext, block: { response in
+        request.add(ZMCompletionHandler(on: viewContext, block: { response in
             switch response.httpStatus {
             case 200:
                 guard let payload = response.payload,
