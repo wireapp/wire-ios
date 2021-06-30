d2530b75cb3026e3378aa71213bcfbf0086f6608
Bill Chan <billypchan@gmail.com>
2021-06-30 11:46:54 +0200

    Fix: designated initializer cannot be declared in an extension (#314)
    * bump
    
    * code update
    
    * Update Cartfile.resolved
    
    * Update LinkPreviewAssetUploadRequestStrategy.swift
    
    * Update Cartfile
    
    * Update Cartfile.resolved

--- Sources/Request Strategies/Assets/Link Preview/LinkPreviewAssetUploadRequestStrategy.swift
+++ Sources/Request Strategies/Assets/Link Preview/LinkPreviewAssetUploadRequestStrategy.swift
@@ -149,7 +149,7 @@ extension LinkPreviewAssetUploadRequestStrategy : ZMUpstreamTranscoder {
 
             let updatedText = Text.with {
                 $0.content = messageText
-                $0.mentions = mentions.compactMap { WireProtos.Mention($0) }
+                $0.mentions = mentions.compactMap { $0.convertToProtosMention() }
                 $0.linkPreview = [linkPreview]
             }
 
