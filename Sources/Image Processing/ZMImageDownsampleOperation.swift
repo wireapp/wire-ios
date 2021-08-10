9506e471e05149a388403409867c62696df89837
Bill Chan <billypchan@gmail.com>
2021-08-10 15:04:44 +0200

    Move convertToMime to Utilities  (#54)
    * mv converter to Utilities
    
    * bump
    
    * Update Cartfile.resolved
    
    * Update Cartfile.resolved

--- Sources/Image Processing/ZMImageDownsampleOperation.swift
+++ Sources/Image Processing/ZMImageDownsampleOperation.swift
@@ -18,54 +18,13 @@
 
 
 import Foundation
-import CoreServices
-import UniformTypeIdentifiers
+import WireUtilities
 
 extension ZMImageDownsampleOperation {
-    private func convertToMime(uti: String) -> String? {
-        
-        let mimeType: String
-        if #available(iOS 14, *) {
-            guard let utType = UTType(uti) else {
-                return nil
-            }
-
-            if let preferredMIMEType = utType.preferredMIMEType {
-                mimeType = preferredMIMEType
-            } else {
-                #if targetEnvironment(simulator)
-                ///HACK: hard code MIME when preferredMIMEType is nil for M1 simulator, we should file a ticket to apple for this issue
-                switch utType {
-                case .jpeg:
-                    mimeType = "image/jpeg"
-                case .png:
-                    mimeType = "image/png"
-                case .gif:
-                    mimeType = "image/gif"
-                default:
-                    return nil
-                }
-                #else
-                return nil
-                #endif
-            }
-
-        } else {
-            let unmanagedMime = UTTypeCopyPreferredTagWithClass(uti as CFString, kUTTagClassMIMEType)
-            
-            guard let retainedValue = unmanagedMime?.takeRetainedValue() else {
-                return nil
-            }
-
-            mimeType = retainedValue as String
-        }
-
-        return mimeType
-    }
     
     @objc
     func createImageProperties(uti: String, imageSize: CGSize) -> ZMIImageProperties? {
-        guard let mimeType = convertToMime(uti: uti) else {
+        guard let mimeType = UTIHelper.convertToMime(uti: uti) else {
             return nil
         }
         
