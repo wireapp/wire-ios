3a1a314e04a9336948e960f6836893d5931437cb
Bill Chan <billypchan@gmail.com>
2021-09-27 13:30:09 +0200

    Fix: always use landscape mode UI layout on macOS (#5267)
    * always run as landscape mode on macOS
    
    * refactoring
    
    * clean up

--- Wire-iOS/Sources/UserInterface/Components/Controllers/SplitViewController.swift
+++ Wire-iOS/Sources/UserInterface/Components/Controllers/SplitViewController.swift
@@ -309,7 +309,7 @@ final class SplitViewController: UIViewController, SplitLayoutObservable {
         }
     }
 
-    // MARK: - updte size
+    // MARK: - update size
 
     /// return true if right view (mostly conversation screen) is fully visible
     var isRightViewControllerRevealed: Bool {
@@ -321,18 +321,27 @@ final class SplitViewController: UIViewController, SplitLayoutObservable {
         }
     }
 
+    private var isiOSAppOnMac: Bool {
+        if #available(iOS 14.0, *) {
+            return ProcessInfo.processInfo.isiOSAppOnMac
+        }
+
+        return false
+    }
+
     /// Update layoutSize for the change of traitCollection and the current orientation
     ///
     /// - Parameters:
     ///   - traitCollection: the new traitCollection
     private func updateLayoutSize(for traitCollection: UITraitCollection) {
-        switch (traitCollection.horizontalSizeClass, UIApplication.shared.statusBarOrientation.isPortrait) {
-        case (.regular, true):
-            self.layoutSize = .regularPortrait
-        case (.regular, false):
-            self.layoutSize = .regularLandscape
+
+        switch (isiOSAppOnMac, traitCollection.horizontalSizeClass, UIApplication.shared.statusBarOrientation.isPortrait) {
+        case (true, _, true), (false, .regular, false):
+            layoutSize = .regularLandscape
+        case (false, .regular, true):
+            layoutSize = .regularPortrait
         default:
-            self.layoutSize = .compact
+            layoutSize = .compact
         }
     }
 
