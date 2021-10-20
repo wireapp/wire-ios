7e779f1d92b62bd19b3184bd96e42026f7c625aa
Jacob Persson <jacob.persson@gmail.com>
2021-10-20 10:41:49 +0200

    [Feature] Include domain in session id (#74)
    * Include the domain in the session identifier
    
    * Make method public
    
    * Make domain optional
    
    * Remove extra )

--- WireCryptoboxTests/TestHelper.swift
+++ WireCryptoboxTests/TestHelper.swift
@@ -21,7 +21,7 @@ import Foundation
 import WireCryptobox
 
 /// sample client ID
-let hardcodedClientId = EncryptionSessionIdentifier(userId: "1e9b4e18", clientId: "7a9eb715")
+let hardcodedClientId = EncryptionSessionIdentifier(domain: "example.com", userId: "1e9b4e18", clientId: "7a9eb715")
 
 /// sample prekey
 let hardcodedPrekey = "pQABAQUCoQBYIEIir0myj5MJTvs19t585RfVi1dtmL2nJsImTaNXszRwA6EAoQBYIGpa1sQFpCugwFJRfD18d9+TNJN2ZL3H0Mfj/0qZw0ruBPY="
