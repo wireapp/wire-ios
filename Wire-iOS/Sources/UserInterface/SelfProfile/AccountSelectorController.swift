8c21c0779429b38678e5812fe207eabd121b668e
Bill Chan <billypchan@gmail.com>
2021-10-22 15:24:00 +0200

    Remove cartography account selector view (#5315)
    * clean up
    
    * replace line view with stack view
    
    * lint
    
    * clean up
    
    * clean up AccountSelectorController
    
    * fix the layout with auto layout mask

--- Wire-iOS/Sources/UserInterface/SelfProfile/AccountSelectorController.swift
+++ Wire-iOS/Sources/UserInterface/SelfProfile/AccountSelectorController.swift
@@ -16,9 +16,7 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-import Foundation
 import UIKit
-import Cartography
 import WireSyncEngine
 
 final class AccountSelectorController: UIViewController {
@@ -29,21 +27,18 @@ final class AccountSelectorController: UIViewController {
         super.init(nibName: nil, bundle: nil)
 
         applicationDidBecomeActiveToken = NotificationCenter.default.addObserver(forName: UIApplication.didBecomeActiveNotification, object: nil, queue: nil, using: { [weak self] _ in
-            guard let `self` = self else {
-                return
-            }
-            self.updateShowAccountsIfNeeded()
+            self?.updateShowAccountsIfNeeded()
         })
 
         accountsView.delegate = self
         view.addSubview(accountsView)
-        constrain(view, accountsView) { selfView, accountsView in
-            accountsView.edges == selfView.edges
-        }
+        accountsView.translatesAutoresizingMaskIntoConstraints = false
+        accountsView.fitIn(view: view)
 
         setShowAccounts(to: SessionManager.shared?.accountManager.accounts.count > 1)
     }
 
+    @available(*, unavailable)
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
