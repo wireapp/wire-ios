006a6cb73dd2556884deb13b0c4880808bf7d97b
Jacob Persson <jacob.persson@gmail.com>
2021-10-26 11:10:53 +0200

    Fix: can't remove service (#395)

--- Sources/Payloads/Payload.swift
+++ Sources/Payloads/Payload.swift
@@ -30,6 +30,14 @@ enum Payload {
     typealias ClientListByQualifiedUserID = [String: ClientListByUserID]
     typealias UserProfiles = [Payload.UserProfile]
 
+    struct EventContainer<T: Codable>: Codable {
+        enum CodingKeys: String, CodingKey {
+            case event
+        }
+
+        let event: T
+    }
+
     struct QualifiedUserIDList: Codable, Hashable {
 
         enum CodingKeys: String, CodingKey {
