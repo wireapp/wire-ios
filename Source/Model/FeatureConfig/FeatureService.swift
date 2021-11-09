9c4b79e439b53f06c1fab6a253ecd2058b357dc3
John Nguyen <polyxo@protonmail.com>
2021-11-09 14:34:52 +0100

    [Feature] Self-deleting message config (#1232)
    * Self-deleting message config (part 0) (#1220)
    
    * Add new feature name
    
    * Define SelfDeletingMessages
    
    * Add methods to fetch and store
    
    * Create default instance
    
    * Calculate needsToNotifyUser
    
    * Add comments
    
    * Self deleting messages (part 1) (#1229)
    
    * Use feature config in usertype protocol
    
    * Override message timeout
    
    * Add tests
    
    * Allow setter
    
    * Refactor ZMConversation+Ephemeral
    
    * Move MessageDestructionTimeoutValue to new file
    
    * Remove MessageDestructionTimeout
    
    * Clean up
    
    * Further refactors
    
    * Fix comment
    
    * Clean up
    
    * Update header
    
    * Remove wait block
    
    * Consider leap years
    
    * Remove folder
    
    * Self-deleting message config (part 2) (#1231)
    
    * Move responsibility to post notification to feature service
    
    * Clean up
    
    * Refactor how to notify of changes
    
    * Fix tests
    
    * Make method internal
    
    * Notify when status changes
    
    * Clean up tests
    
    * Clean up
    
    * Move back appendMessageTimerUpdateMessage method
    
    * Clean up
    
    * [Fix] Setup hasInitialDefault property (#1256)
    
    * Do not change the needsToNotifyUser property for feature if it's an initial status change
    
    * Roll back changes with hasBeenUpdatedFromBackend property
    
    Co-authored-by: KaterinaWire <katerina@wire.com>
    Co-authored-by: KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>

--- Source/Model/FeatureConfig/FeatureService.swift
+++ Source/Model/FeatureConfig/FeatureService.swift
@@ -43,7 +43,6 @@ public class FeatureService {
 
     // MARK: - Accessors
 
-    /// The app lock
     public func fetchAppLock() -> Feature.AppLock {
         var result: Feature.AppLock!
 
@@ -57,12 +56,11 @@ public class FeatureService {
     }
 
     public func storeAppLock(_ appLock: Feature.AppLock) {
-        context.performGroupedAndWait {
-            let config = try! JSONEncoder().encode(appLock.config)
-            Feature.updateOrCreate(havingName: .appLock, in: $0) {
-                $0.status = appLock.status
-                $0.config = config
-            }
+        let config = try! JSONEncoder().encode(appLock.config)
+
+        Feature.updateOrCreate(havingName: .appLock, in: context) {
+            $0.status = appLock.status
+            $0.config = config
         }
     }
 
@@ -78,22 +76,72 @@ public class FeatureService {
     }
 
     public func storeConferenceCalling(_ conferenceCalling: Feature.ConferenceCalling) {
-        context.performGroupedAndWait {
-            Feature.updateOrCreate(havingName: .conferenceCalling, in: $0) {
-                $0.status = conferenceCalling.status
-            }
+        Feature.updateOrCreate(havingName: .conferenceCalling, in: context) {
+            $0.status = conferenceCalling.status
         }
+
+        guard
+            needsToNotifyUser(for: .conferenceCalling),
+            conferenceCalling.status == .enabled
+        else {
+            return
+        }
+
+        notifyChange(.conferenceCallingIsAvailable)
     }
 
-    /// The file sharing
     public func fetchFileSharing() -> Feature.FileSharing {
-        let feature = Feature.fetch(name: .fileSharing, context: context)!
-        return .init(status: feature.status)
+        var result: Feature.FileSharing!
+
+        context.performGroupedAndWait {
+            let feature = Feature.fetch(name: .fileSharing, context: $0)!
+            result = .init(status: feature.status)
+        }
+
+        return result
     }
 
     public func storeFileSharing(_ fileSharing: Feature.FileSharing) {
         Feature.updateOrCreate(havingName: .fileSharing, in: context) {
             $0.status = fileSharing.status
+        }
+
+        guard needsToNotifyUser(for: .fileSharing) else { return }
+
+        switch fileSharing.status {
+        case .disabled:
+            notifyChange(.fileSharingDisabled)
+
+        case .enabled:
+            notifyChange(.fileSharingEnabled)
+        }
+    }
+
+    public func fetchSelfDeletingMesssages() -> Feature.SelfDeletingMessages {
+        let feature = Feature.fetch(name: .selfDeletingMessages, context: context)!
+        let config = try! JSONDecoder().decode(Feature.SelfDeletingMessages.Config.self, from: feature.config!)
+        return .init(status: feature.status, config: config)
+    }
+
+    public func storeSelfDeletingMessages(_ selfDeletingMessages: Feature.SelfDeletingMessages) {
+        let config = try! JSONEncoder().encode(selfDeletingMessages.config)
+
+        Feature.updateOrCreate(havingName: .selfDeletingMessages, in: context) {
+            $0.status = selfDeletingMessages.status
+            $0.config = config
+        }
+
+        guard needsToNotifyUser(for: .selfDeletingMessages) else { return }
+
+        switch (selfDeletingMessages.status, selfDeletingMessages.config.enforcedTimeoutSeconds) {
+        case (.disabled, _):
+            notifyChange(.selfDeletingMessagesIsDisabled)
+
+        case (.enabled, let enforcedTimeout) where enforcedTimeout > 0:
+            notifyChange(.selfDeletingMessagesIsEnabled(enforcedTimeout: enforcedTimeout))
+
+        case (.enabled, _):
+            notifyChange(.selfDeletingMessagesIsEnabled(enforcedTimeout: nil))
         }
     }
 
@@ -110,6 +158,9 @@ public class FeatureService {
                 
             case .fileSharing:
                 storeFileSharing(.init())
+
+            case .selfDeletingMessages:
+                storeSelfDeletingMessages(.init())
             }
         }
     }
@@ -127,7 +178,7 @@ public class FeatureService {
         }
     }
 
-    public func needsToNotifyUser(for featureName: Feature.Name) -> Bool {
+    func needsToNotifyUser(for featureName: Feature.Name) -> Bool {
         var result = false
 
         context.performGroupedAndWait {
@@ -144,5 +195,34 @@ public class FeatureService {
             feature?.needsToNotifyUser = notifyUser
         }
     }
+
+    private func notifyChange(_ change: FeatureChange) {
+        NotificationCenter.default.post(name: .featureDidChangeNotification, object: change)
+    }
+
+}
+
+extension FeatureService {
+
+    /// A type that represents the possible changes to feature configs.
+    ///
+    /// These can be used by the ui layer to determine what kind of alert
+    /// it needs to display to inform the user of changes.
+
+    public enum FeatureChange {
+
+        case conferenceCallingIsAvailable
+        case selfDeletingMessagesIsDisabled
+        case selfDeletingMessagesIsEnabled(enforcedTimeout: UInt?)
+        case fileSharingEnabled
+        case fileSharingDisabled
+
+    }
+
+}
+
+extension Notification.Name {
+
+    public static let featureDidChangeNotification = Notification.Name("FeatureDidChangeNotification")
 
 }
