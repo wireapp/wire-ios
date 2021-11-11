f8eb40fd80999cad8c6c72c05858a95d3ee2d8a6
Bill Chan <billypchan@gmail.com>
2021-11-11 10:24:38 +0100

    Fix: search group selector has no tab bar (#5380)
    * create snapshot with original tabBar code
    
    * prevent test crash
    
    * fixed snapshot
    
    * lint

---
+++ Wire-iOS Tests/SearchGroup/SearchGroupSelectorSnapshotTests.swift
@@ -0,0 +1,62 @@
+//
+// Wire
+// Copyright (C) 2021 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import SnapshotTesting
+import XCTest
+@testable import Wire
+
+final class SearchGroupSelectorSnapshotTests: XCTestCase {
+    var sut: SearchGroupSelector!
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        sut = nil
+
+        super.tearDown()
+    }
+
+    private func createSut() {
+        sut = SearchGroupSelector(style: .light)
+        sut.frame = CGRect(origin: .zero, size: CGSize(width: 320, height: 320))
+    }
+
+    func testForInitState_WhenSelfUserCanNotCreateService() {
+        // GIVEN
+        let mockSelfUser = MockUserType.createSelfUser(name: "selfUser")
+        SelfUser.provider = SelfProvider(selfUser: mockSelfUser)
+        createSut()
+
+        // WHEN & THEN
+        verify(matching: sut)
+    }
+
+    func testThatServiceTabExists_WhenSelfUserCanCreateService() {
+        // GIVEN
+        let mockSelfUser = MockUserType.createSelfUser(name: "selfUser")
+        mockSelfUser.canCreateService = true
+        SelfUser.provider = SelfProvider(selfUser: mockSelfUser)
+
+        createSut()
+
+        // WHEN & THEN
+        verify(matching: sut)
+    }
+}
