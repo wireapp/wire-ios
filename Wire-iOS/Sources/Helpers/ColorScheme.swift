c8034366ef32d7bd7a9d8965dae2f785cc025344
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2021-11-15 10:13:20 +0100

    Fix: iOS: Username color in the conversation messages view show black colour in dark mode  (#5373)
    * Fix: Username color to always show a color from the list
    
    By making these changes we now know that when a color is .underfined it will set to .strongBlue.
    
    * ColorScheme: Duplicate value 0.8 to be the first and second value of the array
    
    Remove 0.0 entirely so it doesn't mess up with how we "generate" a UIColor based o our enum.
    
    * Add UnitTests for testing the nameColor method
    
    We test if we get the expectedColor (.strongBlue) when  the accentColor is set to undefined. In both .light and .dark variants
    
    * ColorSchemeTests: Fix test implementation
    
    I wasn't mixing black and white respectively in both test cases.
    
    * Documentation for the tests
    
    * ColorSchemeTests: Improve the naming of the tests
    
    Apply suggestions from code review
    
    Co-authored-by: Bill Chan <billypchan@gmail.com>
    
    * Improve the naming of the tests
    
    Based on the suggestions Katerina made. From: `testForIsStrongBlue_WhenUndefinedInDarkVariant` to `testForIsStrongBlue_WhenAccentColorIsUndefinedInDarkVariant` etc
    
    Co-authored-by: KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
    
    Co-authored-by: Bill Chan <billypchan@gmail.com>
    Co-authored-by: KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>

--- Wire-iOS/Sources/Helpers/ColorScheme.swift
+++ Wire-iOS/Sources/Helpers/ColorScheme.swift
@@ -304,8 +304,8 @@ extension UIColor {
 
     /// Creates UIColor instance with color corresponding to @p accentColor that can be used to display the name.
     // NB: the order of coefficients must match ZMAccentColor enum ordering
-    private static let accentColorNameColorBlendingCoefficientsDark: [CGFloat] = [0.0, 0.8, 0.72, 1.0, 0.8, 0.8, 0.8, 0.64]
-    private static let accentColorNameColorBlendingCoefficientsLight: [CGFloat] = [0.0, 0.8, 0.72, 1.0, 0.8, 0.8, 0.64, 1.0]
+    private static let accentColorNameColorBlendingCoefficientsDark: [CGFloat] = [0.8, 0.8, 0.72, 1.0, 0.8, 0.8, 0.8, 0.64]
+    private static let accentColorNameColorBlendingCoefficientsLight: [CGFloat] = [0.8, 0.8, 0.72, 1.0, 0.8, 0.8, 0.64, 1.0]
 
     /// Creates UIColor instance with color corresponding to @p accentColor that can be used to display the name.
     class func nameColor(for accentColor: ZMAccentColor, variant: ColorSchemeVariant) -> UIColor {
