87b1d1532170dda160e5dadb3300c5645124f99a
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2021-12-23 10:48:46 +0100

    fix: SwiftLint warnings for multiple rules & add support for Apple Silicon Brew Directory (#45)
    * fix: SwiftLint warnings for multiple rules
    
    - **Opening Brace Spacing Violation**: Opening braces should be preceded by a single space and on the same line as the declaration. `(opening_brace)`
    - **Colon Violation**: Colons should be next to the identifier when specifying a type and next to the key in dictionary literals. `(colon)`
    - **Trailing Whitespace Violation**: Lines should not have trailing whitespace. `(trailing_whitespace)`
    
    * chore: SwiftLint add support for Apple Silicon brew directory
    
    By default, Xcode can't find any packages installed by homebrew on Apple Silicon Macs, since they're now installed to a different location that isn't on the default $PATH (/opt/homebrew/bin/, rather than /usr/local/bin).
    
    Workaround: explicitly add this location to $PATH before attempting to run swiftlint.
    
    * fix: SwiftLint warnings for rule implicit_getter
    
    Implicit Getter Violation: Computed read-only properties should avoid using the get keyword. (implicit_getter)
    
    * fix: SwiftLint warnings for trailing and bracket violations

--- Sources/UIImage+Trimmed.swift
+++ Sources/UIImage+Trimmed.swift
@@ -20,57 +20,57 @@ import Foundation
 import UIKit
 
 public extension UIImage {
-    
-    var imageWithAlphaTrimmed : UIImage {
+
+    var imageWithAlphaTrimmed: UIImage {
         let originalSize = self.size
-        
+
         var minX = Int.max
         var maxX = Int.min
         var minY = Int.max
         var maxY = Int.min
         var nonAlphaBounds = CGRect.zero
-        var trimmedImage : UIImage = self
-        
+        var trimmedImage: UIImage = self
+
         UIGraphicsBeginImageContextWithOptions(originalSize, false, scale)
-        
+
         draw(at: CGPoint.zero)
-        
+
         if let context = UIGraphicsGetCurrentContext(), var pixelData = context.data?.assumingMemoryBound(to: UInt32.self) {
-            
+
             let alignment = (8 - (context.width % 8)) % 8
-            
+
             for y in 0..<context.height * 1 {
                 for x in 0..<context.width * 1 {
                     let alpha = UInt8((pixelData.pointee >> 24) & 255)
-                    
+
                     if alpha > 0 {
                         minX = min(x, minX)
                         maxX = max(x, maxX)
                         minY = min(y, minY)
                         maxY = max(y, maxY)
                     }
-                    
+
                     pixelData = pixelData.successor()
                 }
                 pixelData = pixelData.advanced(by: alignment)
             }
-            
+
             nonAlphaBounds = CGRect(x: CGFloat(minX) / scale, y: CGFloat(minY) / scale, width: CGFloat(maxX - minX) / scale, height: CGFloat(maxY - minY) / scale)
         }
-        
+
         UIGraphicsEndImageContext()
-        
+
         UIGraphicsBeginImageContextWithOptions(nonAlphaBounds.size, false, scale)
-        
+
         draw(at: CGPoint(x: -nonAlphaBounds.origin.x, y: -nonAlphaBounds.origin.y))
-        
+
         if let image = UIGraphicsGetImageFromCurrentImageContext() {
             trimmedImage = image
         }
-        
+
         UIGraphicsEndImageContext()
-        
+
         return trimmedImage
     }
-    
+
 }
--- Tests/TestHelper.swift
+++ Tests/TestHelper.swift
@@ -20,17 +20,17 @@ import Foundation
 import UIKit
 
 extension UIImage {
-    
+
     convenience init?(testImageNamed name: String) {
         func url(forTestResourceNamed name: String) -> URL? {
             let bundle = Bundle(for: CanvasTests.self)
-            return bundle.url(forResource: (name as NSString).deletingPathExtension, withExtension:(name as NSString).pathExtension)
+            return bundle.url(forResource: (name as NSString).deletingPathExtension, withExtension: (name as NSString).pathExtension)
         }
-        
+
         guard let url = url(forTestResourceNamed: name) else { return nil }
         guard let data = try? Data(contentsOf: url) else { return nil }
 
         self.init(data: data, scale: 2)
     }
-    
+
 }
