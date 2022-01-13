93073b08a91ed0ca569e6eb74dfb24f7d8282067
Bill Chan <billypchan@gmail.com>
2022-01-13 16:07:52 +0100

    feat: support DISABLE_CALL_QUALITY_SURVEY flag SQPIT-845 (#5512)
    * DISABLE_CALL_QUALITY_SURVEY flag
    
    * canPresentCallQualitySurvey return false

--- Wire-iOS/Sources/UserInterface/Calling/CallQualityController/CallQualityController.swift
+++ Wire-iOS/Sources/UserInterface/Calling/CallQualityController/CallQualityController.swift
@@ -58,9 +58,13 @@ class CallQualityController: NSObject {
      */
 
     var canPresentCallQualitySurvey: Bool {
+        #if DISABLE_CALL_QUALITY_SURVEY
+        return false
+        #else
         return Bundle.developerModeEnabled
             && !AutomationHelper.sharedHelper.disableCallQualitySurvey
             && AppDelegate.shared.launchType != .unknown
+        #endif
     }
 
     // MARK: - Events
@@ -113,7 +117,9 @@ class CallQualityController: NSObject {
             return
         }
 
+        #if !DISABLE_CALL_QUALITY_SURVEY
         router?.presentCallQualitySurvey(with: callDuration)
+        #endif
     }
 
     /// Presents the debug log prompt after a call failure.
