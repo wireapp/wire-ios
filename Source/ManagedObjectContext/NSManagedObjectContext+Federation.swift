76ac420c388e2256fb617f7b5720d6d7089cc131
David-Henner <david.henner@wire.com>
2022-05-30 11:57:17 +0200

    feat: migrate database after federation is enabled - FS-492 (#1367)
    * feat: migrate database for federation FS-492
    
    * add tests
    
    * Update Source/ManagedObjectContext/NSManagedObjectContext+Federation.swift
    
    Co-authored-by: KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
    
    * chore: Update version.xcconfig for version: '289.1.0' [skip ci]
    
    * trigger
    
    Co-authored-by: KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
    Co-authored-by: zenkins <iosx@wire.com>

---
+++ Source/ManagedObjectContext/NSManagedObjectContext+Federation.swift
@@ -0,0 +1,51 @@
+//
+// Wire
+// Copyright (C) 2022 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+
+private protocol FederationMigratable: ZMManagedObject {
+    static var predicateForObjectsNeedingFederationMigration: NSPredicate? { get }
+    var domain: String? { get set }
+}
+
+extension ZMConversation: FederationMigratable {
+    static let predicateForObjectsNeedingFederationMigration: NSPredicate? = NSPredicate(format: "%K == nil", #keyPath(ZMConversation.domain))
+}
+
+extension ZMUser: FederationMigratable {
+    static let predicateForObjectsNeedingFederationMigration: NSPredicate? = NSPredicate(format: "%K == nil", #keyPath(ZMUser.domain))
+}
+
+extension NSManagedObjectContext {
+
+    public func migrateToFederation() throws {
+        try migrateInstancesTowardsFederation(type: ZMUser.self)
+        try migrateInstancesTowardsFederation(type: ZMConversation.self)
+    }
+
+    private func migrateInstancesTowardsFederation<T: FederationMigratable>(type: T.Type) throws {
+        let fetchRequest = NSFetchRequest<T>(entityName: T.entityName())
+        fetchRequest.returnsObjectsAsFaults = false
+        fetchRequest.fetchBatchSize = 100
+        fetchRequest.predicate = T.predicateForObjectsNeedingFederationMigration
+
+        try fetchRequest.execute().modifyAndSaveInBatches { instance in
+            instance.domain = APIVersion.domain
+        }
+    }
+}
