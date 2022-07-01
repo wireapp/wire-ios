9a9f384e791c231cc896c89ce1996a2e48f27b7f
David-Henner <david.henner@wire.com>
2022-07-01 12:49:59 +0200

    feat(mls): integrate CoreCrypto framework FS-646 (#1759)
    * core crypto setup
    
    * refactor
    
    * add missing protocol
    
    * move logic to user session
    
    * pin dependency
    
    * remove delegate
    
    * save core crypto on the sync context
    
    * update cartfile

---
+++ Source/UserSession/ZMUserSession+CoreCrypto.swift
@@ -0,0 +1,83 @@
+//
+// Wire
+// Copyright (C) 2022 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+import WireDataModel
+
+extension ZMUserSession {
+    var coreCryptoConfiguration: CoreCryptoConfiguration? {
+        let user = ZMUser.selfUser(in: managedObjectContext)
+
+        guard let clientId = user.selfClient()?.remoteIdentifier else { return nil }
+
+        let accountDirectory = CoreDataStack.accountDataFolder(
+            accountIdentifier: user.remoteIdentifier,
+            applicationContainer: sharedContainerURL
+        )
+        FileManager.default.createAndProtectDirectory(at: accountDirectory)
+        let mlsDirectory = accountDirectory.appendingMLSFolder()
+
+        do {
+            let key = try CoreCryptoKeyProvider.coreCryptoKey()
+            return CoreCryptoConfiguration(
+                path: mlsDirectory.path,
+                key: key.base64EncodedString(),
+                clientId: clientId
+            )
+        } catch {
+            // TODO: Error handling
+            fatalError(String(describing: error))
+        }
+    }
+
+    var coreCrypto: CoreCryptoProtocol? {
+        get {
+            var coreCrypto: CoreCryptoProtocol?
+            syncContext.performAndWait {
+                coreCrypto = syncContext.coreCrypto
+            }
+            return coreCrypto
+        }
+        set {
+            syncContext.performAndWait {
+                syncContext.coreCrypto = newValue
+            }
+        }
+    }
+}
+
+extension NSManagedObjectContext {
+    private static let coreCrytpoUserInfoKey = "CoreCryptoKey"
+
+    public var coreCrypto: CoreCryptoProtocol? {
+        get {
+            precondition(zm_isSyncContext, "core crypto should only be accessed on the sync context")
+            return userInfo[Self.coreCrytpoUserInfoKey] as? CoreCryptoProtocol
+        }
+        set {
+            precondition(zm_isSyncContext, "core crypto should only be accessed on the sync context")
+            userInfo[Self.coreCrytpoUserInfoKey] = newValue
+        }
+    }
+}
+
+extension URL {
+    func appendingMLSFolder() -> URL {
+        return appendingPathComponent("mls")
+    }
+}
