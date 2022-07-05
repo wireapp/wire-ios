dfd0fe6bdf52276c80fb802ad6ad270ca2da1498
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2022-07-05 13:50:00 +0200

    feat(mls): add support for mls public keys FS-537 (#1377)
    * feat(mls): add support for mls public keys FS-537
    
    * fix tests
    
    * add public initializer
    
    * introduce MLSController
    
    * lint

---
+++ MLS/MLSController.swift
@@ -0,0 +1,67 @@
+//
+// Wire
+// Copyright (C) 2022 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+
+public final class MLSController {
+
+    // MARK: - Properties
+
+    private weak var context: NSManagedObjectContext?
+    private let coreCrypto: CoreCryptoProtocol
+    private let logger = ZMSLog(tag: "core-crypto")
+
+    // MARK: - Life cycle
+
+    init(
+        context: NSManagedObjectContext,
+        coreCrypto: CoreCryptoProtocol
+    ) {
+        self.context = context
+        self.coreCrypto = coreCrypto
+
+        do {
+            try generatePublicKeysIfNeeded()
+        } catch {
+            logger.error("failed to generate public keys: \(String(describing: error))")
+        }
+    }
+
+    // MARK: - Methods
+
+    private func generatePublicKeysIfNeeded() throws {
+        guard
+            let context = context,
+            let selfClient = ZMUser.selfUser(in: context).selfClient()
+        else {
+            return
+        }
+
+        var keys = selfClient.mlsPublicKeys
+
+        if keys.ed25519 == nil {
+            let keyBytes = try coreCrypto.wire_clientPublicKey()
+            let keyData = Data(keyBytes)
+            keys.ed25519 = keyData.base64EncodedString()
+        }
+
+        selfClient.mlsPublicKeys = keys
+        context.saveOrRollback()
+    }
+
+}
