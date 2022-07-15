50cfe3543fb6ad13cbff0494adbc92d6c2e40338
ghuMustafa <106667609+ghuMustafa@users.noreply.github.com>
2022-07-15 06:55:46 -0700

    feat(mls): upload key packages FS-538 (#1388)
    * feat(mls): added methods to count generate and uploading key packages FS-538
    
    * fixed the core crypto generating and uploading valid key packages
    
    * removed the prints
    
    * fix build error
    
    * added the review suggestions
    
    * added review suggestions
    
    * add review suggestions
    
    Co-authored-by: John Nguyen <28632506+johnxnguyen@users.noreply.github.com>

--- MLS/MLSController.swift
+++ MLS/MLSController.swift
@@ -26,14 +26,20 @@ public final class MLSController {
     private let coreCrypto: CoreCryptoProtocol
     private let logger = ZMSLog(tag: "core-crypto")
 
+    let targetUnclaimedKeyPackageCount = 100
+
+    let actionProvider: MLSActionsProviderProtocol
+
     // MARK: - Life cycle
 
     init(
         context: NSManagedObjectContext,
-        coreCrypto: CoreCryptoProtocol
+        coreCrypto: CoreCryptoProtocol,
+        actionProvider: MLSActionsProviderProtocol = MLSActionsProvider()
     ) {
         self.context = context
         self.coreCrypto = coreCrypto
+        self.actionProvider = actionProvider
 
         do {
             try generatePublicKeysIfNeeded()
@@ -42,7 +48,7 @@ public final class MLSController {
         }
     }
 
-    // MARK: - Methods
+    // MARK: - Public keys
 
     private func generatePublicKeysIfNeeded() throws {
         guard
@@ -64,4 +70,121 @@ public final class MLSController {
         context.saveOrRollback()
     }
 
+    // MARK: - Key packages
+
+    enum MLSKeyPackagesError: Error {
+
+        case failedToGenerateKeyPackages
+
+    }
+
+    /// Uploads new key packages if needed.
+    ///
+    /// Checks how many key packages are available on the backend and
+    /// generates new ones if there are less than 50% of the target unclaimed key package count..
+
+    public func uploadKeyPackagesIfNeeded() {
+        guard let context = context else { return }
+        let user = ZMUser.selfUser(in: context)
+        guard let clientID = user.selfClient()?.remoteIdentifier else { return }
+
+        // TODO: Here goes the logic to determine how check to remaining key packages and re filling the new key packages after calculating number of welcome messages it receives by the client.
+
+        /// For now temporarily we generate and upload at most 100 new key packages
+
+         countUnclaimedKeyPackages(clientID: clientID, context: context.notificationContext) { unclaimedKeyPackageCount in
+            guard unclaimedKeyPackageCount <= self.targetUnclaimedKeyPackageCount / 2 else { return }
+
+            do {
+                let amount = UInt32(self.targetUnclaimedKeyPackageCount - unclaimedKeyPackageCount)
+                let keyPackages = try self.generateKeyPackages(amountRequested: amount)
+
+                self.uploadKeyPackages(
+                    clientID: clientID,
+                    keyPackages: keyPackages,
+                    context: context.notificationContext
+                )
+
+            } catch {
+                self.logger.error("failed to generate new key packages: \(String(describing: error))")
+            }
+        }
+    }
+
+    private func countUnclaimedKeyPackages(clientID: String, context: NotificationContext, completion: @escaping (Int) -> Void) {
+        actionProvider.countUnclaimedKeyPackages(clientID: clientID, context: context) { result in
+            switch result {
+            case .success(let count):
+                completion(count)
+
+            case .failure(let error):
+                self.logger.error("failed to fetch MLS key packages count with error: \(String(describing: error))")
+            }
+        }
+    }
+
+    private func generateKeyPackages(amountRequested: UInt32) throws -> [String] {
+
+        var keyPackages = [[UInt8]]()
+
+        do {
+
+            keyPackages = try coreCrypto.wire_clientKeypackages(amountRequested: amountRequested)
+
+        } catch let error {
+            logger.error("failed to generate new key packages: \(String(describing: error))")
+            throw MLSKeyPackagesError.failedToGenerateKeyPackages
+        }
+
+        if keyPackages.isEmpty {
+            logger.error("CoreCrypto generated empty key packages array")
+            throw MLSKeyPackagesError.failedToGenerateKeyPackages
+        }
+
+        return keyPackages.map {
+            Data($0).base64EncodedString()
+        }
+    }
+
+    private func uploadKeyPackages(clientID: String, keyPackages: [String], context: NotificationContext) {
+        actionProvider.uploadKeyPackages(clientID: clientID, keyPackages: keyPackages, context: context) { result in
+            switch result {
+            case .success:
+                break
+
+            case .failure(let error):
+                self.logger.error("failed to upload key packages: \(String(describing: error))")
+            }
+        }
+    }
+}
+
+protocol MLSActionsProviderProtocol {
+
+    func countUnclaimedKeyPackages(
+        clientID: String,
+        context: NotificationContext,
+        resultHandler: @escaping CountSelfMLSKeyPackagesAction.ResultHandler
+    )
+
+    func uploadKeyPackages(
+        clientID: String,
+        keyPackages: [String],
+        context: NotificationContext,
+        resultHandler: @escaping UploadSelfMLSKeyPackagesAction.ResultHandler
+    )
+
+}
+
+private class MLSActionsProvider: MLSActionsProviderProtocol {
+
+    func countUnclaimedKeyPackages(clientID: String, context: NotificationContext, resultHandler: @escaping CountSelfMLSKeyPackagesAction.ResultHandler) {
+        let action = CountSelfMLSKeyPackagesAction(clientID: clientID, resultHandler: resultHandler)
+        action.send(in: context)
+    }
+
+    func uploadKeyPackages(clientID: String, keyPackages: [String], context: NotificationContext, resultHandler: @escaping UploadSelfMLSKeyPackagesAction.ResultHandler) {
+        let action = UploadSelfMLSKeyPackagesAction(clientID: clientID, keyPackages: keyPackages, resultHandler: resultHandler)
+        action.send(in: context)
+    }
 }
