d4ece00333db55e3b1723d0f3b11cfe9e168504f
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2022-09-01 15:50:08 +0300

    fix: the color of the email field in the profile view (#5820)
    * fix: the color of the email field in the profile view
    
    * chore: address PR comments from @KaterinaWire
    
    * chore: cleanup

--- Wire-iOS/Sources/UserInterface/UserProfile/Details/Cells/UserPropertyCell.swift
+++ Wire-iOS/Sources/UserInterface/UserProfile/Details/Cells/UserPropertyCell.swift
@@ -70,6 +70,16 @@ final class UserPropertyCell: SeparatorTableViewCell {
         }
     }
 
+    // MARK: - Configuration
+
+    override var isHighlighted: Bool {
+        didSet {
+            backgroundColor = isHighlighted
+            ? UIColor(white: 0, alpha: 0.08)
+            : SemanticColors.View.backgroundUserCell
+        }
+    }
+
     // MARK: - Initialization
 
     override func setUp() {
@@ -80,7 +90,8 @@ final class UserPropertyCell: SeparatorTableViewCell {
 
     private func configureSubviews() {
         backgroundColor = SemanticColors.View.backgroundUserCell
-
+        propertyNameLabel.textColor = SemanticColors.Label.textUserPropertyCellName
+        propertyValueLabel.textColor = SemanticColors.Label.textDefault
         addBorder(for: .bottom)
 
         separator.isHidden = true
@@ -95,10 +106,7 @@ final class UserPropertyCell: SeparatorTableViewCell {
         contentStack.alignment = .leading
         contentView.addSubview(contentStack)
 
-        applyColorScheme(colorSchemeVariant)
         shouldGroupAccessibilityChildren = true
-        backgroundColor = SemanticColors.View.backgroundUserCell
-        separator.isHidden = true
     }
 
     private func configureConstraints() {
@@ -110,14 +118,6 @@ final class UserPropertyCell: SeparatorTableViewCell {
             contentStack.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
             contentStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
         ])
-    }
-
-    // MARK: - Configuration
-
-    override func applyColorScheme(_ variant: ColorSchemeVariant) {
-        super.applyColorScheme(variant)
-        propertyNameLabel.textColor = SemanticColors.Label.textUserPropertyCellName
-        propertyValueLabel.textColor = SemanticColors.Label.textDefault
     }
 
 }
