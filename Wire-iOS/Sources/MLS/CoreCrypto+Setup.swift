a7f542fc46a0aad06fb333ae4980692ecb4d241e
David-Henner <david.henner@wire.com>
2022-09-06 15:57:42 +0200

    refactor(mls): core crypto setup flow FS-541 (#5809)
    * refactor(mls): MLS controller setup
    
    * dependencies
    
    * post merge fix
    
    * update dependencies

---
+++ Wire-iOS/Sources/MLS/CoreCrypto+Setup.swift
@@ -0,0 +1,32 @@
+//
+// Wire
+// Copyright (C) 2022 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+import WireCoreCrypto
+import WireSyncEngine
+
+extension CoreCrypto {
+    static func setup(with configuration: CoreCryptoConfiguration) throws -> CoreCrypto {
+        return try .init(
+            path: configuration.path,
+            key: configuration.key,
+            clientId: configuration.clientId,
+            entropySeed: nil
+        )
+    }
+}
