669aff5b1c5a5acb0f27b5f281fdc066c7dd2ab0
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2022-10-24 10:49:58 +0200

    fix(mls): can't be added to groups when EAR enabled FS-1093 (#1424)

--- MLS/MLSController.swift
+++ MLS/MLSController.swift
@@ -159,6 +159,7 @@ public final class MLSController: MLSControllerProtocol {
         }
 
         generateClientPublicKeysIfNeeded()
+        uploadKeyPackagesIfNeeded()
         fetchBackendPublicKeys()
         updateKeyMaterialForAllStaleGroupsIfNeeded()
         schedulePeriodicKeyMaterialUpdateCheck()
