985f5882750bae3238cbdd9ccd63da21d4b94a45
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2023-01-12 10:56:44 +0100

    chore: setup ci test script
    * initial test action
    
    * dummy change
    
    * Update run-tests.yml
    
    * fix step conditions
    
    * fix workspace name
    
    * potential script fix
    
    * run workspace setup
    
    * build utilities
    
    * build data model
    
    * build sync engine
    
    * build ziphy
    
    * build WireCanvas
    
    * build WireTesting
    
    * Revert "dummy change"
    
    This reverts commit 7400232af6a6d09d79be121291246cc80d69540e.
    
    * dummy change in WireSystem
    
    * Revert "dummy change in WireSystem"
    
    This reverts commit 3eb275d5562b95738ba4a37966398e8f80c5628c.
    
    * dummy change in WireTesting
    
    * remove pipes
    
    * Revert "dummy change in WireTesting"
    
    This reverts commit e8667252eddb2f40e9f69861ede16c3732cd0eeb.
    
    * dummy change in WireUtilities
    
    * build WireTesting
    
    * add WireTesting as test dependency to WireUtilities
    
    * don't explicitly build WireTesting for WireUtilities
    
    * fix dependencies for WireImages
    
    * move Cartfiles to root directory
    
    * update setup script
    
    * fix dependencies
    
    * update action

---
+++ wire-ios-canvas/WireCanvasTestHost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WireCanvasTestHost
+//
+//  Created by John Nguyen on 11/01/23.
+//  Copyright © 2023 Wire GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
