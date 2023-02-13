e083706f84a0c37a5b6f4803636f32debf6fb30b
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2023-02-13 13:43:24 +0100

    fix sync engine tests

--- wire-ios-sync-engine/Tests/Source/Mocks/MockMLSController.swift
+++ wire-ios-sync-engine/Tests/Source/Mocks/MockMLSController.swift
@@ -22,7 +22,7 @@ import WireDataModel
 // Temporary Mock until we find a way to use a single mocked `MLSControllerProcotol` accross frameworks
 class MockMLSController: MLSControllerProtocol {
     func commitPendingProposals(in groupID: WireDataModel.MLSGroupID) async throws {
-        fatalError("not implemented")
+
     }
 
     func uploadKeyPackagesIfNeeded() {
