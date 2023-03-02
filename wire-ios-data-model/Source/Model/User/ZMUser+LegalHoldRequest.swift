f7989ee433e9f2b4a5a1b36feb15c8d9774f0146
KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
2023-03-02 14:31:34 +0100

    Update wire-ios-data-model/Source/Model/User/ZMUser+LegalHoldRequest.swift
    Co-authored-by: David-Henner <david.henner@wire.com>

--- wire-ios-data-model/Source/Model/User/ZMUser+LegalHoldRequest.swift
+++ wire-ios-data-model/Source/Model/User/ZMUser+LegalHoldRequest.swift
@@ -306,10 +306,10 @@ extension ZMUser: SelfLegalHoldSubject {
     }
 
     private func fetchFingerprint(through keystore: UserClientKeysStore) -> String? {
-        guard let preKey = legalHoldRequest?.lastPrekey,
-              let fingerprintData = EncryptionSessionsDirectory.fingerprint(fromPrekey: preKey.key) else {
-                  return nil
-              }
+        guard 
+            let preKey = legalHoldRequest?.lastPrekey,
+            let fingerprintData = EncryptionSessionsDirectory.fingerprint(fromPrekey: preKey.key) 
+        else { return nil }
         return String(data: fingerprintData, encoding: .utf8)
     }
 
