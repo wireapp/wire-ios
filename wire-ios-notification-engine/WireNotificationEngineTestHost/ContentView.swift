4e3a20f65ef3b14203fc99cba344721c7e1e70b5
David Henner <david.henner@wire.com>
2023-03-02 12:01:54 +0100

    add test host for notification engine

---
+++ wire-ios-notification-engine/WireNotificationEngineTestHost/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WireNotificationEngineTestHost
+//
+//  Created by David Henner on 02.03.23.
+//  Copyright © 2023 Wire. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
