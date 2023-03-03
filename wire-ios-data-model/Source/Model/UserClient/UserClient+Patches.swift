7e54e343d684f898c4c5bbe2d3ed17bf8e8682ec
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2023-03-03 15:57:14 +0100

    create temporary keystore if needed

--- wire-ios-data-model/Source/Model/UserClient/UserClient+Patches.swift
+++ wire-ios-data-model/Source/Model/UserClient/UserClient+Patches.swift
@@ -32,19 +32,41 @@ extension UserClient {
             return
         }
 
-        moc.proteusProvider.perform(
-            withProteusService: { proteusService in
-                // TODO: migrate v1 to v2
-            },
-            withKeyStore: { keyStore in
-                keyStore.encryptionContext.perform { session in
-                    for client in allClients {
-                        client.migrateSessionIdentifierFromV1IfNeeded(sessionDirectory: session)
-                        client.needsSessionMigration = false
-                    }
-                }
+        // Important:
+        // If we're migrating from Cryptobox to Core Crypto, it's important that we
+        // migrate the session ids before moving the session files into Core Crypto.
+        //
+        // However, we would only have an instance of `ProteusService` (backed by Core
+        // Crypto) and not an instance of the `UserClientKeyStore` (backed by Cryptobox).
+        //
+        // Migration of the session ids relies on the keystore, so if we don't have an
+        // instance if the keystore already, we create one temporarily for this migration
+        // assuming that we'll perform the migration from Cryptobox to Core Crypto afterwards.
+
+        let keyStore: UserClientKeysStore
+
+        if let existingKeyStore = moc.zm_cryptKeyStore {
+            keyStore = existingKeyStore
+        } else {
+            guard
+                let accountDirectory = moc.accountDirectoryURL,
+                let applicationContainer = moc.applicationContainerURL
+            else {
+                fatalError("Can not migration proteus session ids")
             }
-        )
+
+            keyStore = UserClientKeysStore(
+                accountDirectory: accountDirectory,
+                applicationContainer: applicationContainer
+            )
+        }
+
+        keyStore.encryptionContext.perform { session in
+            for client in allClients {
+                client.migrateSessionIdentifierFromV1IfNeeded(sessionDirectory: session)
+                client.needsSessionMigration = false
+            }
+        }
     }
 
     static func migrateAllSessionsClientIdentifiersV3(in moc: NSManagedObjectContext) {
@@ -55,18 +77,41 @@ extension UserClient {
             return
         }
 
-        moc.proteusProvider.perform(
-            withProteusService: { proteusService in
-                // TODO: migrate v2 to v3
-            },
-            withKeyStore: { keyStore in
-                keyStore.encryptionContext.perform { session in
-                    for client in allClients {
-                        client.migrateSessionIdentifierFromV2IfNeeded(sessionDirectory: session)
-                        client.needsSessionMigration = false
-                    }
-                }
+        // Important:
+        // If we're migrating from Cryptobox to Core Crypto, it's important that we
+        // migrate the session ids before moving the session files into Core Crypto.
+        //
+        // However, we would only have an instance of `ProteusService` (backed by Core
+        // Crypto) and not an instance of the `UserClientKeyStore` (backed by Cryptobox).
+        //
+        // Migration of the session ids relies on the keystore, so if we don't have an
+        // instance if the keystore already, we create one temporarily for this migration
+        // assuming that we'll perform the migration from Cryptobox to Core Crypto afterwards.
+
+        let keyStore: UserClientKeysStore
+
+        if let existingKeyStore = moc.zm_cryptKeyStore {
+            keyStore = existingKeyStore
+        } else {
+            guard
+                let accountDirectory = moc.accountDirectoryURL,
+                let applicationContainer = moc.applicationContainerURL
+            else {
+                fatalError("Can not migration proteus session ids")
             }
-        )
+
+            keyStore = UserClientKeysStore(
+                accountDirectory: accountDirectory,
+                applicationContainer: applicationContainer
+            )
+        }
+
+        keyStore.encryptionContext.perform { session in
+            for client in allClients {
+                client.migrateSessionIdentifierFromV2IfNeeded(sessionDirectory: session)
+                client.needsSessionMigration = false
+            }
+        }
     }
+
 }
