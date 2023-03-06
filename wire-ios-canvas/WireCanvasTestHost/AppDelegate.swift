38cd702d66b172ab855cf79188bb635d1fcc27b8
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2023-03-06 11:08:44 +0100

    chore: test commit

--- wire-ios-canvas/WireCanvasTestHost/AppDelegate.swift
+++ wire-ios-canvas/WireCanvasTestHost/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   }
 
   // MARK: UISceneSession Lifecycle
-
   func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
     // Called when a new scene session is being created.
     // Use this method to select a configuration to create the new scene with.
@@ -29,6 +28,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
   }
-
 
 }
