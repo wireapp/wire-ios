cff45755315fe2484f4fb0ef6665108c8d8bdebe
KaterinaWire <katerina@wire.com>
2023-03-14 15:14:18 +0100

    Fix tests

--- wire-ios-data-model/Source/Model/Message/ZMMessage.m
+++ wire-ios-data-model/Source/Model/Message/ZMMessage.m
@@ -129,6 +129,7 @@ NSString * const ZMMessageDecryptionErrorCodeKey = @"decryptionErrorCode";
 @implementation ZMMessage
 
 @dynamic missingRecipients;
+@dynamic isExpired;
 @dynamic expirationReasonCode;
 @dynamic expirationDate;
 @dynamic destructionDate;
@@ -138,8 +139,6 @@ NSString * const ZMMessageDecryptionErrorCodeKey = @"decryptionErrorCode";
 @dynamic isObfuscated;
 @dynamic normalizedText;
 @dynamic delivered;
-
-@synthesize isExpired = _isExpired;
 
 - (instancetype)initWithNonce:(NSUUID *)nonce managedObjectContext:(NSManagedObjectContext *)managedObjectContext
 {
@@ -228,18 +227,16 @@ NSString * const ZMMessageDecryptionErrorCodeKey = @"decryptionErrorCode";
     self.expirationDate = nil;
 }
 
-- (BOOL)isExpired;
-{
-    return _isExpired;
-}
-
 - (void)setIsExpired:(BOOL)isExpired;
 {
-    /// When isExpired is false, then expirationReasonCode should not contain any value.
+    [self willChangeValueForKey:ZMMessageIsExpiredKey];
+    [self setPrimitiveValue:@(isExpired) forKey:ZMMessageIsExpiredKey];
+    [self didChangeValueForKey:ZMMessageIsExpiredKey];
+
+    // When isExpired is false, then expirationReasonCode should not contain any value.
     if (isExpired == NO && self.expirationReasonCode != nil) {
         self.expirationReasonCode = nil;
     }
-    _isExpired = isExpired;
 }
 
 - (void)markAsSent
