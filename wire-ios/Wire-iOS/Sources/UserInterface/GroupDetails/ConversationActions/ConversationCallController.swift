fc0a726eb1fda0db42987caafd71e878e50e7ded
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2023-03-23 10:39:57 +0100

    fix: Show alert for initiating a large call for video call

--- wire-ios/Wire-iOS/Sources/UserInterface/GroupDetails/ConversationActions/ConversationCallController.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/GroupDetails/ConversationActions/ConversationCallController.swift
@@ -52,10 +52,24 @@ final class ConversationCallController: NSObject {
         }
     }
 
-    func startVideoCall(started: (() -> Void)?) {
-        conversation.confirmJoiningCallIfNeeded(alertPresenter: target) { [conversation] in
-            started?()
-            conversation.startVideoCall()
+    func startVideoCall(started: Completion?) {
+        let startCall = { [weak self] in
+            guard let `self` = self else { return }
+            self.conversation.confirmJoiningCallIfNeeded(alertPresenter: self.target) {
+                started?()
+                self.conversation.startVideoCall()
+            }
+        }
+
+        if conversation.localParticipants.count <= 4 {
+            startCall()
+        } else {
+            confirmGroupCall {[weak self] accepted in
+                self?.target.setNeedsStatusBarAppearanceUpdate()
+
+                guard accepted else { return }
+                startCall()
+            }
         }
     }
 
