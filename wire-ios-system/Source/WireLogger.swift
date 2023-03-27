412f5b52b47f3ffad83561a96c69db682e7a5f23
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2023-03-27 10:14:05 +0200

    chore: add logs for notification flow IC-129 (#130)

--- wire-ios-system/Source/WireLogger.swift
+++ wire-ios-system/Source/WireLogger.swift
@@ -142,5 +142,7 @@ public extension WireLogger {
 
     static let proteus = WireLogger(tag: "proteus")
     static let shareExtension = WireLogger(tag: "share-extension")
+    static let notifications = WireLogger(tag: "notifications")
+    static let calling = WireLogger(tag: "calling")
 
 }
