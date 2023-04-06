95dc09a8dd45326d0a4db00ea91d7ff9d049162a
KaterinaWire <katerina@wire.com>
2023-04-06 12:47:29 +0200

    revert changes

--- wire-ios/Wire-iOS Tests/Mocks/MockMessage.swift
+++ wire-ios/Wire-iOS Tests/Mocks/MockMessage.swift
@@ -338,7 +338,7 @@ class MockMessage: NSObject, ZMConversationMessage, ConversationCompositeMessage
     var conversationLike: ConversationLike? = .none
 
     var deliveryState: ZMDeliveryState = .delivered
-    var failedToSendReason: FailedToSendReason = .unknown
+    var failedToSendReason: MessageSendFailure = .unknown
     var failedToSendUsers: [UserType]? = nil
 
     var imageMessageData: ZMImageMessageData? = .none
