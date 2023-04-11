66b4f457a9171e1df5860b87ebe0b2dbfcb84617
François Benaiteau <francois.benaiteau@gmail.com>
2023-04-11 18:46:00 +0200

    Revert "name constants"
    a869a8f5677c4f271c2333d17cdd7d4678f9fe38

--- wire-ios-sync-engine/Source/UserSession/ZMUserSession+CoreCrypto.swift
+++ wire-ios-sync-engine/Source/UserSession/ZMUserSession+CoreCrypto.swift
@@ -86,13 +86,10 @@ extension ZMUserSession {
     private func updateKeychainItemAccess() {
         WireLogger.coreCrypto.info("updating keychain item access")
 
-        let serviceName = "wire.com"
-        let accountPrefix = "keystore_salt"
-
         for account in accountsForAllItemsNeedingUpdates() {
             let query = [
                 kSecClass: kSecClassGenericPassword,
-                kSecAttrService: serviceName,
+                kSecAttrService: "wire.com",
                 kSecAttrAccount: account
             ] as CFDictionary
 
@@ -123,7 +120,7 @@ extension ZMUserSession {
             item[kSecAttrAccount as String] as? String
         }.filter { account in
             // Core Crypto says that the items are all prefixed with this.
-            account.hasPrefix(accountPrefix)
+            account.hasPrefix("keystore_salt")
         }
     }
 
