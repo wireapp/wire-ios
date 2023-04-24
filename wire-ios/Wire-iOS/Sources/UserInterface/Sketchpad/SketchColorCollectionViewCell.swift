d52c938862dc787a12676c710bf71c44daa086d7
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2023-04-24 14:33:47 +0200

    chore: Cleanup SketchColorCollectionViewCell

--- wire-ios/Wire-iOS/Sources/UserInterface/Sketchpad/SketchColorCollectionViewCell.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/Sketchpad/SketchColorCollectionViewCell.swift
@@ -1,17 +1,17 @@
 //
 // Wire
 // Copyright (C) 2016 Wire Swiss GmbH
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see http://www.gnu.org/licenses/.
 // 
@@ -213,14 +213,17 @@ final class SketchColorCollectionViewCell: UICollectionViewCell {
             return
         }
 
-        contentStackView.translatesAutoresizingMaskIntoConstraints = false
-        knobView.translatesAutoresizingMaskIntoConstraints = false
-        contentStackView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-        contentStackView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
-        contentStackView.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        contentStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 5).isActive = true
-        knobView.widthAnchor.constraint(equalToConstant: 25).isActive = true
-        knobView.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        [contentStackView, knobView].prepareForLayout()
+
+        NSLayoutConstraint.activate([
+            contentStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            contentStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            contentStackView.topAnchor.constraint(equalTo: topAnchor),
+            contentStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 5),
+            knobView.widthAnchor.constraint(equalToConstant: 25),
+            knobView.heightAnchor.constraint(equalToConstant: 25)
+        ])
+
         initialContraintsCreated = true
     }
 }
