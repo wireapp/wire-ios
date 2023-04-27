945d10e987dca3a32c0411512c1c8987e1745a98
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2023-04-27 11:19:53 +0200

    chore: Update Snapshots for Xcode 14.2 and using iOS 16.2 simulator (#185)
    Co-authored-by: François Benaiteau <netbe@users.noreply.github.com>

--- wire-ios-data-model/sourcery/generated/AutoMockable.generated.swift
+++ wire-ios-data-model/sourcery/generated/AutoMockable.generated.swift
@@ -1,4 +1,4 @@
-// Generated using Sourcery 1.8.1 — https://github.com/krzysztofzablocki/Sourcery
+// Generated using Sourcery 2.0.1 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
 // swiftlint:disable line_length
 // swiftlint:disable variable_name
