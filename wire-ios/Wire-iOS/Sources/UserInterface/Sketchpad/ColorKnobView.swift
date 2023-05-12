64617da818bb042086bdf611b8ace4b06d9192b1
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2023-05-12 10:33:30 +0200

    chore: cleanup

--- wire-ios/Wire-iOS/Sources/UserInterface/Sketchpad/ColorKnobView.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/Sketchpad/ColorKnobView.swift
@@ -1,20 +1,20 @@
-// 
+//
 // Wire
 // Copyright (C) 2020 Wire Swiss GmbH
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see http://www.gnu.org/licenses/.
-// 
+//
 
 import UIKit
 
@@ -83,8 +83,7 @@ final class ColorKnobView: UIView {
 
     // MARK: - Helpers
     var knobBorderColor: UIColor? {
-        if knobColor == SemanticColors.DrawingColors.black ||
-           knobColor == SemanticColors.DrawingColors.white {
+        if knobColor == SemanticColors.DrawingColors.white {
             return .black
         }
         return knobColor
