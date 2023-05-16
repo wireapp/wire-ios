b53f601e41149e1813efab68409f073215f49f09
François Benaiteau <francois.benaiteau@gmail.com>
2023-05-16 13:12:47 +0200

    fix: failing test on LegacyPersistedDataPatchesTests

--- wire-ios-data-model/Tests/Source/Utils/LegacyPersistedDataPatchesTests.swift
+++ wire-ios-data-model/Tests/Source/Utils/LegacyPersistedDataPatchesTests.swift
@@ -212,7 +212,7 @@ class LegacyPersistedDataPatchesTests: ZMBaseManagedObjectTest {
 
             let sessionsURL = otrURL.appendingPathComponent("sessions")
             let oldSession = sessionsURL.appendingPathComponent(newClient.remoteIdentifier!)
-            let newSession = sessionsURL.appendingPathComponent(newClient.sessionIdentifier!.rawValue)
+            var newSession = sessionsURL.appendingPathComponent(newClient.sessionIdentifier!.rawValue)
 
             XCTAssertTrue(FileManager.default.fileExists(atPath: newSession.path))
             let previousData = try! Data(contentsOf: newSession)
@@ -226,6 +226,7 @@ class LegacyPersistedDataPatchesTests: ZMBaseManagedObjectTest {
             LegacyPersistedDataPatch.applyAll(in: self.syncMOC, fromVersion: "0.0.0")
 
             // THEN
+            newSession = sessionsURL.appendingPathComponent(newClient.sessionIdentifier!.rawValue)
             let readData = try? Data(contentsOf: newSession)
             XCTAssertNotNil(readData)
             XCTAssertEqual(readData, previousData)
