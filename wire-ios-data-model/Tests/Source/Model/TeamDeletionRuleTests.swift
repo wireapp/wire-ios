a9d0197d1bc9304caf7683a13cadef66c687fa6b
François Benaiteau <francois.benaiteau@gmail.com>
2023-05-16 13:12:22 +0200

    make sure that developer flag is set safely

--- wire-ios-data-model/Tests/Source/Model/TeamDeletionRuleTests.swift
+++ wire-ios-data-model/Tests/Source/Model/TeamDeletionRuleTests.swift
@@ -23,10 +23,16 @@ class TeamDeletionRuleTests: BaseZMClientMessageTests {
 
     override class func setUp() {
         super.setUp()
+        DeveloperFlag.storage = UserDefaults(suiteName: UUID().uuidString)!
         var flag = DeveloperFlag.proteusViaCoreCrypto
         flag.isOn = false
     }
 
+    override class func tearDown() {
+        super.tearDown()
+        DeveloperFlag.storage = UserDefaults.standard
+    }
+    
     func testThatItDoesntDeleteConversationsWhichArePartOfATeamWhenTeamGetsDeleted() {
         // given
         let team = Team.insertNewObject(in: uiMOC)
