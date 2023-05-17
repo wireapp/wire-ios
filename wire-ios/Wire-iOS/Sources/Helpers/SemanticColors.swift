c1aa91aa5c1f525936cd653c9f331aacea74d1e2
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2023-05-17 09:36:03 +0200

    chore: Get rid of strongBlue from legaxyColors

--- wire-ios/Wire-iOS/Sources/Helpers/SemanticColors.swift
+++ wire-ios/Wire-iOS/Sources/Helpers/SemanticColors.swift
@@ -30,7 +30,6 @@ public enum SemanticColors {
 
     public enum LegacyColors {
         // Legacy accent colors
-        static let strongBlue = UIColor(red: 0.141, green: 0.552, blue: 0.827, alpha: 1)
         static let strongLimeGreen = UIColor(red: 0, green: 0.784, blue: 0, alpha: 1)
         static let vividRed = UIColor(red: 1, green: 0.152, blue: 0, alpha: 1)
     }
