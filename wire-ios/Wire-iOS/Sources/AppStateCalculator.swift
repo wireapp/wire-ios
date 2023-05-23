fbc2a85462ad55a1fcde7abde339501fb7c95803
François Benaiteau <francois.benaiteau@gmail.com>
2023-05-23 09:39:35 +0200

    WIP: add logs

--- wire-ios/Wire-iOS/Sources/AppStateCalculator.swift
+++ wire-ios/Wire-iOS/Sources/AppStateCalculator.swift
@@ -115,6 +115,7 @@ class AppStateCalculator {
 
         self.appState = appState
         self.pendingAppState = nil
+        WireLogger(tag: "AppState").debug("transitioning to app state: \(appState)")
         ZMSLog(tag: "AppState").debug("transitioning to app state: \(appState)")
         delegate?.appStateCalculator(self, didCalculate: appState, completion: {
             completion?()
@@ -198,6 +199,7 @@ extension AppStateCalculator: SessionManagerDelegate {
     }
 
     func sessionManagerDidReportLockChange(forSession session: UserSessionAppLockInterface) {
+        WireLogger.ear.info("sessionManagerDidReportLockChange")
         if session.isLocked {
             transition(to: .locked)
         } else {
@@ -221,6 +223,7 @@ extension AppStateCalculator: SessionManagerDelegate {
 extension AppStateCalculator: AuthenticationCoordinatorDelegate {
     func userAuthenticationDidComplete(addedAccount: Bool) {
         // TODO: [John] Avoid singleton.
+        WireLogger.ear.info("userAuthenticationDidComplete")
         if ZMUserSession.shared()?.isLocked == true {
             transition(to: .locked)
         } else {
