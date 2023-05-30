93ae497d954b2df076d91de1db348059297daa75
KaterinaWire <katerina@wire.com>
2023-05-30 14:08:26 +0200

    Move refreshUsersMissingMetadata and  refreshConversationsMissingMetadata to the separate file

---
+++ wire-ios-sync-engine/Source/UserSession/ZMUserSession+RecurringAction.swift
@@ -0,0 +1,53 @@
+//
+// Wire
+// Copyright (C) 2023 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+
+extension ZMUserSession {
+    
+    func refreshUsersMissingMetadata() -> RecurringAction? {
+        /// Performing actions every 3 hours.
+        let interval: TimeInterval = 3 * 60 * 60
+        
+        let fetchRequest = ZMUser.sortedFetchRequest(with: ZMUser.predicateForUsersArePendingToRefreshMetadata())
+        guard let users = managedObjectContext.fetchOrAssert(request: fetchRequest) as? [ZMUser] else {
+            return nil
+        }
+        
+        return RecurringAction(id: UUID().uuidString,
+                               perform:  users.forEach { $0.refreshData() },
+                               interval: interval)
+    }
+    
+    func refreshConversationsMissingMetadata() -> RecurringAction? {
+        /// Performing actions every 3 hours.
+        let interval: TimeInterval = 3 * 60 * 60
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: ZMConversation.entityName())
+        fetchRequest.predicate = ZMConversation.predicateForConversationsArePendingToRefreshMetadata()
+        
+        guard let conversations = managedObjectContext.executeFetchRequestOrAssert(fetchRequest) as? [ZMConversation] else {
+            return nil
+        }
+        
+        return RecurringAction(id: UUID().uuidString,
+                               perform: conversations.forEach { $0.needsToBeUpdatedFromBackend = true },
+                               interval: interval)
+    }
+    
+}
