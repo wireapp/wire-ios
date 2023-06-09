155394d165c5cf051f43150342191c93ea1390d2
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2023-06-09 14:10:15 +0200

    fix: Add some SnapshotTests for Reactions

---
+++ wire-ios/Wire-iOS Tests/ConversationMessageCell/ConversationReactionMessageTests.swift
@@ -0,0 +1,81 @@
+//
+// Wire
+// Copyright (C) 2023 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import XCTest
+@testable import Wire
+import SnapshotTesting
+
+final class ConversationReactionMessageTests: CoreDataSnapshotTestCase {
+
+    // MARK: - Properties
+
+    var message: MockMessage!
+    var sut: MessageReactionsCell!
+
+    // MARK: - setUp
+
+    override func setUp() {
+        super.setUp()
+        SelfUser.setupMockSelfUser()
+
+        message = MockMessageFactory.textMessage(withText: "Hello, it's me!")
+        message.deliveryState = .sent
+        message.conversation = otherUserConversation
+
+        sut = MessageReactionsCell()
+        sut.frame = CGRect(x: 0, y: 0, width: 375, height: 70)
+    }
+
+    // MARK: - tearDown
+
+    override func tearDown() {
+        message = nil
+        sut = nil
+        super.tearDown()
+    }
+
+    // MARK: - Snapshot Tests
+
+    func testThatItConfiguresWithSelfReaction() {
+        recordMode = true
+        // GIVEN
+        let configuration = MessageReactionsCell.Configuration(message: message)
+        message.backingUsersReaction = [MessageReaction.like.unicodeValue: [selfUser]]
+
+        sut.configure(with: configuration, animated: false)
+
+        // THEN
+        verify(view: sut)
+    }
+
+    func testThatItConfiguresWithOtherReactions() {
+        // GIVEN
+        recordMode = true
+        let usersWhoReactionWithBeamingFace = MockUser.mockUsers().filter { !$0.isSelfUser }
+        let configuration = MessageReactionsCell.Configuration(message: message)
+
+        message.backingUsersReaction = [MessageReaction.beamingFace.unicodeValue: usersWhoReactionWithBeamingFace]
+
+        // WHEN
+        sut.configure(with: configuration, animated: false)
+
+        // THEN
+        verify(view: sut)
+    }
+
+}
