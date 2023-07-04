9ed1b94ed99a7cb16af34a7809cea58e9a9ace1e
François Benaiteau <francois.benaiteau@gmail.com>
2023-07-04 17:17:18 +0200

    rerecord all snapshots on CI

--- wire-ios/Wire-iOS Tests/Utils/ZMSnapshotTestCase.swift
+++ wire-ios/Wire-iOS Tests/Utils/ZMSnapshotTestCase.swift
@@ -101,7 +101,7 @@ class ZMSnapshotTestCase: FBSnapshotTestCase {
         snapshotBackgroundColor = UIColor.clear
 
         // Enable when the design of the view has changed in order to update the reference snapshots
-        recordMode = strcmp(getenv("RECORDING_SNAPSHOTS"), "YES") == 0
+        recordMode = true //strcmp(getenv("RECORDING_SNAPSHOTS"), "YES") == 0
 
         usesDrawViewHierarchyInRect = true
 
