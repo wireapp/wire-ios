c5ac7576dbf7dfd852ffbd39ca75cc7fc9f93bc6
François Benaiteau <francois.benaiteau@gmail.com>
2023-07-05 16:36:54 +0200

    Revert "increase precision"
    82af25e6b3cf6ce48cdbbacd643a6eee136092a6

--- wire-ios/Wire-iOS Tests/SwiftSnapshotTesting/XCTestCase+SnapshotTesting.swift
+++ wire-ios/Wire-iOS Tests/SwiftSnapshotTesting/XCTestCase+SnapshotTesting.swift
@@ -22,7 +22,7 @@ import SnapshotTesting
 import UIKit
 
 // Precision of matching snapshots. Lower this value to fix issue with difference with Intel and Apple Silicon
-private let precision: Float  = 0.98
+private let precision: Float  = 0.95
 private let perceptualPrecision: Float = 1
 
 extension ViewImageConfig: Hashable {
