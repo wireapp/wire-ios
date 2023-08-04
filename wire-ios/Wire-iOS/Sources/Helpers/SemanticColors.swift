6243ea0d89544d273cbc4f9a5d0b0766b4c82b98
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2023-08-04 15:54:44 +0200

    chore: add new colors in semantic colors

--- wire-ios/Wire-iOS/Sources/Helpers/SemanticColors.swift
+++ wire-ios/Wire-iOS/Sources/Helpers/SemanticColors.swift
@@ -28,11 +28,6 @@ import WireCommonComponents
 /// The last part is optional
 public enum SemanticColors {
 
-    public enum LegacyColors {
-        // Legacy accent colors
-        static let vividRed = UIColor(red: 1, green: 0.152, blue: 0, alpha: 1)
-    }
-
     public enum Switch {
         static let backgroundOnStateEnabled = UIColor(light: Asset.Colors.green600Light, dark: Asset.Colors.green700Dark)
         static let backgroundOffStateEnabled = UIColor(light: Asset.Colors.gray70, dark: Asset.Colors.gray70)
@@ -102,6 +97,9 @@ public enum SemanticColors {
         // System Message Icon Colors
         static let foregroundExclamationMarkInSystemMessage = UIColor(light: Asset.Colors.red500Light, dark: Asset.Colors.red500Dark)
         static let foregroundCheckMarkInSystemMessage = UIColor(light: Asset.Colors.green500Light, dark: Asset.Colors.green500Dark)
+
+        static let backgroundLegalHold = UIColor(light: Asset.Colors.red500Light, dark: Asset.Colors.red500Dark)
+        static let foregroundLegalhold = UIColor(light: Asset.Colors.red500Light, dark: Asset.Colors.red500Dark)
     }
 
     public enum View {
