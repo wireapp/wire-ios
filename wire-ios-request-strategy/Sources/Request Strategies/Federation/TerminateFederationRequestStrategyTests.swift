827d91b52d4e7665d3679131c58abca30557f555
KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
2023-08-14 23:41:54 +0200

    feat: Parse 2 new events: "federation.delete" and "federation.connectionRemoved" WPB-2548 (#386)
    Co-authored-by: MarcinR <mratajczakmail@gmail.com>
    Co-authored-by: John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
    Co-authored-by: David-Henner <david.henner@wire.com>

---
+++ wire-ios-request-strategy/Sources/Request Strategies/Federation/TerminateFederationRequestStrategyTests.swift
@@ -0,0 +1,109 @@
+////
+// Wire
+// Copyright (C) 2023 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import XCTest
+@testable import WireRequestStrategy
+
+class TerminateFederationRequestStrategyTests: MessagingTestBase {
+
+    // MARK: - Properties
+
+    var sut: TerminateFederationRequestStrategy!
+    var mockApplicationStatus: MockApplicationStatus!
+    var manager: MockFederationTerminationManager!
+
+    // MARK: - Life cycle
+
+    override func setUp() {
+        super.setUp()
+        mockApplicationStatus = MockApplicationStatus()
+        mockApplicationStatus.mockSynchronizationState = .slowSyncing
+
+        sut = TerminateFederationRequestStrategy(
+            withManagedObjectContext: syncMOC,
+            applicationStatus: mockApplicationStatus
+        )
+        manager = MockFederationTerminationManager()
+        sut.federationTerminationManager = manager
+    }
+
+    override func tearDown() {
+        mockApplicationStatus = nil
+        manager = nil
+        sut = nil
+        super.tearDown()
+    }
+
+    // MARK: - Processing events
+
+    func testThatItProcessesEvent_FederationDelete() {
+        syncMOC.performGroupedAndWait { _ in
+            // GIVEN
+            let payload: NSDictionary = [
+                "type": "federation.delete",
+                "domain": "example.com"
+            ]
+
+            let event = ZMUpdateEvent(fromEventStreamPayload: payload, uuid: nil)!
+
+            // WHEN
+            self.sut.processEvents([event], liveEvents: false, prefetchResult: nil)
+        }
+
+        XCTAssertTrue(self.waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+
+        // THEN
+        XCTAssertTrue(manager.didCallHandleFederationTerminationWith)
+    }
+
+    func testThatItProcessesEvent_federationConnectionRemoved() {
+        syncMOC.performGroupedAndWait { _ in
+            // GIVEN
+            let payload: NSDictionary = [
+                "type": "federation.connectionRemoved",
+                "domains": ["anta.wire.link", "foma.wire.link"]
+            ]
+
+            let event = ZMUpdateEvent(fromEventStreamPayload: payload, uuid: nil)!
+
+            // WHEN
+            self.sut.processEvents([event], liveEvents: false, prefetchResult: nil)
+        }
+
+        XCTAssertTrue(self.waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+
+        // THEN
+        XCTAssertTrue(manager.didCallhandleFederationTerminationBetween)
+    }
+
+}
+
+class MockFederationTerminationManager: FederationTerminationManagerInterface {
+
+    var didCallHandleFederationTerminationWith: Bool = false
+    var didCallhandleFederationTerminationBetween: Bool = false
+
+    func handleFederationTerminationWith(_ domain: String) {
+        didCallHandleFederationTerminationWith = true
+    }
+
+    func handleFederationTerminationBetween(_ domain: String, otherDomain: String) {
+        didCallhandleFederationTerminationBetween = true
+    }
+
+}
