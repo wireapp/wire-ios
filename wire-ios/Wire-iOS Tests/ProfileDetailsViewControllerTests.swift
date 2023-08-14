e9329715a8e88ca6883b0244a156109c1f82525a
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2023-08-14 11:34:33 +0200

    chore: Replace XCTestCase with BaseSnapshotTestCase and update snapshot

--- wire-ios/Wire-iOS Tests/ProfileDetailsViewControllerTests.swift
+++ wire-ios/Wire-iOS Tests/ProfileDetailsViewControllerTests.swift
@@ -20,7 +20,7 @@ import SnapshotTesting
 import XCTest
 @testable import Wire
 
-final class ProfileDetailsViewControllerTests: XCTestCase {
+final class ProfileDetailsViewControllerTests: BaseSnapshotTestCase {
 
     var selfUserTeam: UUID!
     var selfUser: MockUserType!
