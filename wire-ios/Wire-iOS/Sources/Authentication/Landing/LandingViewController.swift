0fbb73e0cd4c96f274fdd63dcbea5da9a2ab5ee9
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2023-08-29 10:22:54 +0200

    chore: add diya and elna to backend switcher (#432)

--- wire-ios/Wire-iOS/Sources/Authentication/Landing/LandingViewController.swift
+++ wire-ios/Wire-iOS/Sources/Authentication/Landing/LandingViewController.swift
@@ -415,7 +415,7 @@ final class LandingViewController: AuthenticationStepViewController {
 
     var isCustomBackend: Bool {
         switch backendEnvironment.environmentType.value {
-        case .production, .staging, .qaDemo, .qaDemo2, .anta, .bella, .chala, .foma:
+        case .production, .staging, .qaDemo, .qaDemo2, .anta, .bella, .chala, .diya, .elna, .foma:
             return false
         case .custom:
             return true
