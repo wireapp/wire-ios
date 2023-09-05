0b6c5f9b96e360a4bafe208411ff9bfcfb1be91f
KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
2023-09-05 18:33:54 +0200

    Update wire-ios-request-strategy/Sources/Object Syncs/Helpers/MessageSyncTests.swift
    Co-authored-by: François Benaiteau <netbe@users.noreply.github.com>

--- wire-ios-request-strategy/Sources/Object Syncs/Helpers/MessageSyncTests.swift
+++ wire-ios-request-strategy/Sources/Object Syncs/Helpers/MessageSyncTests.swift
@@ -105,7 +105,7 @@ final class MessageSyncTests: MessagingTestBase {
         }
     }
 
-    func test_ItDoesntGenerateARequest(apiVersion: APIVersion) {
+    private func internalTest_ItDoesntGenerateARequest(apiVersion: APIVersion) {
         syncMOC.performGroupedBlockAndWait {
             // Given
             self.groupConversation.messageProtocol = .mls
