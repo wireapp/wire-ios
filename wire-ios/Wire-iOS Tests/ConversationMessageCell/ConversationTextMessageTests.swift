0073bf2bffbfb3d1b5deec2a30051d7a914c14d7
François Benaiteau <francois.benaiteau@gmail.com>
2023-09-21 21:58:56 +0200

    fix: remove superflous rule

--- wire-ios/Wire-iOS Tests/ConversationMessageCell/ConversationTextMessageTests.swift
+++ wire-ios/Wire-iOS Tests/ConversationMessageCell/ConversationTextMessageTests.swift
@@ -207,7 +207,6 @@ final class ConversationTextMessageTests: BaseSnapshotTestCase {
 
     // MARK: - Helper Methods
 
-    // swiftlint:disable:next line_length
     func createMessage(withText: String = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
                        userName: String = "Bruno") -> MockMessage {
         let message = MockMessageFactory.textMessage(withText: withText)
