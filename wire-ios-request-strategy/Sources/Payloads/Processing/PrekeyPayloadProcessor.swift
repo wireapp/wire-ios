fa63a3c26133fec0ffd91ef83b0c1410c461a6dd
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2023-10-04 10:09:55 +0200

    process prekeys

--- wire-ios-request-strategy/Sources/Payloads/Processing/PrekeyPayloadProcessor.swift
+++ wire-ios-request-strategy/Sources/Payloads/Processing/PrekeyPayloadProcessor.swift
@@ -1,5 +1,6 @@
+//
 // Wire
-// Copyright (C) 2021 Wire Swiss GmbH
+// Copyright (C) 2023 Wire Swiss GmbH
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -17,35 +18,71 @@
 
 import Foundation
 
-// MARK: - Prekeys
-
-extension Payload.PrekeyByUserID {
+final class PrekeyPayloadProcessor {
 
     /// Establish new sessions using the prekeys retreived for each client.
     ///
-    /// - parameter selfClient: The self user's client
-    /// - parameter context: The `NSManagedObjectContext` on which the operation should be performed
-    /// - parameter domain: originating domain of the clients.
+    /// - Parameters:
+    ///   - payload: The payload containing the prekeys
+    ///   - selfClient: The self user's client
+    ///   - context: The `NSManagedObjectContext` on which the operation should be performed
     ///
     /// - returns `True` if there's more sessions which needs to be established.
-    func establishSessions(with selfClient: UserClient,
-                           context: NSManagedObjectContext,
-                           domain: String? = nil) -> Bool {
-        for (userID, prekeyByClientID) in self {
+
+    func establishSessions(
+        from payload: Payload.PrekeyByQualifiedUserID,
+        with selfClient: UserClient,
+        context: NSManagedObjectContext
+    ) -> Bool {
+        for (domain, prekeyByUserID) in payload {
+            establishSessions(
+                from: prekeyByUserID,
+                with: selfClient,
+                context: context,
+                domain: domain
+            )
+        }
+
+        let hasMoreMissingClients = selfClient.missingClients?.isEmpty == false
+        return hasMoreMissingClients
+    }
+
+    /// Establish new sessions using the prekeys retreived for each client.
+    ///
+    /// - Parameters:
+    ///   - payload: The payload containing the prekeys
+    ///   - selfClient: The self user's client
+    ///   - context: The `NSManagedObjectContext` on which the operation should be performed
+    ///   - domain: originating domain of the clients.
+    ///
+    /// - returns `True` if there's more sessions which needs to be established.
+
+    @discardableResult
+    func establishSessions(
+        from payload: Payload.PrekeyByUserID,
+        with selfClient: UserClient,
+        context: NSManagedObjectContext,
+        domain: String? = nil
+    ) -> Bool {
+        for (userID, prekeyByClientID) in payload {
             for (clientID, prekey) in prekeyByClientID {
                 guard
                     let userID = UUID(uuidString: userID),
                     let user = ZMUser.fetch(with: userID, domain: domain, in: context),
-                    let missingClient = UserClient.fetchUserClient(withRemoteId: clientID,
-                                                                   forUser: user,
-                                                                   createIfNeeded: true)
+                    let missingClient = UserClient.fetchUserClient(
+                        withRemoteId: clientID,
+                        forUser: user,
+                        createIfNeeded: true
+                    )
                 else {
                     continue
                 }
 
                 if let prekey = prekey {
-                    missingClient.establishSessionAndUpdateMissingClients(prekey: prekey,
-                                                                          selfClient: selfClient)
+                    missingClient.establishSessionAndUpdateMissingClients(
+                        prekey: prekey,
+                        selfClient: selfClient
+                    )
                 } else {
                     missingClient.markClientAsInvalidAfterFailingToRetrievePrekey(selfClient: selfClient)
                 }
@@ -54,42 +91,23 @@ extension Payload.PrekeyByUserID {
         }
 
         let hasMoreMissingClients = (selfClient.missingClients?.count ?? 0) > 0
-
         return hasMoreMissingClients
     }
 
 }
 
-extension Payload.PrekeyByQualifiedUserID {
-
-    /// Establish new sessions using the prekeys retreived for each client.
-    ///
-    /// - parameter selfClient: The self user's client
-    /// - parameter context: The `NSManagedObjectContext` on which the operation should be performed
-    ///
-    /// - returns `True` if there's more sessions which needs to be established.
-    func establishSessions(with selfClient: UserClient, context: NSManagedObjectContext) -> Bool {
-        for (domain, prekeyByUserID) in self {
-            _ = prekeyByUserID.establishSessions(with: selfClient, context: context, domain: domain)
-        }
-
-        let hasMoreMissingClients = selfClient.missingClients?.isEmpty == false
-
-        return hasMoreMissingClients
-    }
-
-}
-
-// MARK: - UserClient
-
-extension UserClient {
+private extension UserClient {
 
     /// Creates session and update missing clients and messages that depend on those clients
-    fileprivate func establishSessionAndUpdateMissingClients(prekey: Payload.Prekey,
-                                                             selfClient: UserClient) {
 
-        let sessionCreated = selfClient.establishSessionWithClient(self,
-                                                                   usingPreKey: prekey.key)
+    func establishSessionAndUpdateMissingClients(
+        prekey: Payload.Prekey,
+        selfClient: UserClient
+    ) {
+        let sessionCreated = selfClient.establishSessionWithClient(
+            self,
+            usingPreKey: prekey.key
+        )
 
        // If the session creation failed, the client probably has corrupted prekeys,
        // we mark the client in order to send him a bogus message and not block all requests
@@ -98,13 +116,13 @@ extension UserClient {
        selfClient.removeMissingClient(self)
    }
 
-    fileprivate func markClientAsInvalidAfterFailingToRetrievePrekey(selfClient: UserClient) {
+    func markClientAsInvalidAfterFailingToRetrievePrekey(selfClient: UserClient) {
         failedToEstablishSession = true
         clearMessagesMissingRecipient()
         selfClient.removeMissingClient(self)
     }
 
-    fileprivate func clearMessagesMissingRecipient() {
+    func clearMessagesMissingRecipient() {
         messagesMissingRecipient.forEach {
             if let message = $0 as? ZMOTRMessage {
                 message.doesNotMissRecipient(self)
@@ -185,3 +203,4 @@ extension Payload.ClientListByQualifiedUserID {
     }
 
 }
+
