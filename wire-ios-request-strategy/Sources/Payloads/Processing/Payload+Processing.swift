d22798b477497ef9bee7230e0ad0b990456e8411
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2023-10-04 09:44:38 +0200

    process user client updates

--- wire-ios-request-strategy/Sources/Payloads/Processing/Payload+Processing.swift
+++ wire-ios-request-strategy/Sources/Payloads/Processing/Payload+Processing.swift
@@ -17,49 +17,6 @@
 
 import Foundation
 
-extension Payload.UserClient {
-
-    func update(_ client: WireDataModel.UserClient) {
-        client.needsToBeUpdatedFromBackend = false
-
-        guard
-            client.user?.isSelfUser == false,
-            let deviceClass = deviceClass
-        else { return }
-
-        client.deviceClass = DeviceClass(rawValue: deviceClass)
-    }
-
-    func createOrUpdateClient(for user: ZMUser) -> WireDataModel.UserClient {
-        let client = WireDataModel.UserClient.fetchUserClient(withRemoteId: id, forUser: user, createIfNeeded: true)!
-
-        update(client)
-
-        return client
-    }
-
-}
-
-extension Array where Array.Element == Payload.UserClient {
-
-    func updateClients(for user: ZMUser, selfClient: UserClient) {
-        let clients: [UserClient] = map { $0.createOrUpdateClient(for: user) }
-
-        // Remove clients that have not been included in the response
-        let deletedClients = user.clients.subtracting(clients)
-        deletedClients.forEach {
-            $0.deleteClientAndEndSession()
-        }
-
-        // Mark new clients as missed and ignore them
-        let newClients = Set(clients.filter({ $0.isInserted || !$0.hasSessionWithSelfClient }))
-        selfClient.missesClients(newClients)
-        selfClient.addNewClientsToIgnored(newClients)
-        selfClient.updateSecurityLevelAfterDiscovering(newClients)
-    }
-
-}
-
 // MARK: - Prekeys
 
 extension Payload.PrekeyByUserID {
