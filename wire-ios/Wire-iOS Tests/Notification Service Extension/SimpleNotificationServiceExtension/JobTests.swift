976f2fed9daf1215e7588dd435dcce2452a6bde5
KaterinaWire <katerina@wire.com>
2023-10-05 17:49:53 +0200

    revert test commit

--- wire-ios/Wire-iOS Tests/Notification Service Extension/SimpleNotificationServiceExtension/JobTests.swift
+++ wire-ios/Wire-iOS Tests/Notification Service Extension/SimpleNotificationServiceExtension/JobTests.swift
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
- 
+
 import XCTest
 @testable import Wire_Notification_Service_Extension
 
