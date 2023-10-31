2d8b21b48620a01765c69ee621c847fe68371df9
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-10-31 14:42:15 +0100

    chore: configure sourcery for request strategy

--- wire-ios-request-strategy/sourcery/generated/AutoMockable.generated.swift
+++ wire-ios-request-strategy/sourcery/generated/AutoMockable.generated.swift
@@ -10,26 +10,30 @@ import UIKit
 import AppKit
 #endif
 
-
 @testable import WireRequestStrategy
 
+public class MockMessageSenderInterface: MessageSenderInterface {
 
+    // MARK: - Life cycle
 
+    public init() {}
 
+    // MARK: - sendMessage
 
+    public var sendMessageMessage_Invocations: [any SendableMessage] = []
+    public var sendMessageMessage_MockMethod: ((any SendableMessage) async -> Swift.Result<Void, MessageSendError>)?
+    public var sendMessageMessage_MockValue: Swift.Result<Void, MessageSendError>?
 
+    public func sendMessage(message: any SendableMessage) async -> Swift.Result<Void, MessageSendError> {
+        sendMessageMessage_Invocations.append(message)
 
+        if let mock = sendMessageMessage_MockMethod {
+            return await mock(message)
+        } else if let mock = sendMessageMessage_MockValue {
+            return mock
+        } else {
+            fatalError("no mock for `sendMessageMessage`")
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
