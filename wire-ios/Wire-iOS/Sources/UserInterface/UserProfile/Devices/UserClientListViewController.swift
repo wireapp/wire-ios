0aac409fab13c860564215d68a5e23a394546b7f
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2023-11-03 14:07:22 +0100

    fix: errors

--- wire-ios/Wire-iOS/Sources/UserInterface/UserProfile/Devices/UserClientListViewController.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/UserProfile/Devices/UserClientListViewController.swift
@@ -20,7 +20,6 @@ import Foundation
 import UIKit
 import WireSyncEngine
 
-final class UserClientListViewController: UIViewController, 
 final class UserClientListViewController: UIViewController,
                                           UICollectionViewDelegateFlowLayout,
                                           UICollectionViewDataSource {
