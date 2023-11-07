3a5a6a3a4e392161df8216d5ee235b2a2d15e097
Christoph Aldrian <christoph.aldrian@wire.com>
2023-11-07 18:27:26 +0100

    chore: exclude linux targets

--- scripts/Package.swift
+++ scripts/Package.swift
@@ -6,15 +6,23 @@ import PackageDescription
 let package = Package(
     name: "Scripts",
     products: [
-        .library(name: "Dummy", targets: ["Dummy"])
+        .plugin(name: "SwiftLintPlugin", targets: ["SwiftLintPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/realm/SwiftLint.git", exact: "0.53.0"),
+        //.package(url: "https://github.com/realm/SwiftLint.git", exact: "0.53.0"),
     ],
     targets: [
-        .target(
-            name: "Dummy",
-            plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]
+        .plugin(
+            name: "SwiftLintPlugin",
+            capability: .buildTool(),
+            dependencies: [
+                .target(name: "SwiftLintBinary", condition: .when(platforms: [.macOS]))
+            ]
+        ),
+        .binaryTarget(
+            name: "SwiftLintBinary",
+            url: "https://github.com/realm/SwiftLint/releases/download/0.53.0/SwiftLintBinary-macos.artifactbundle.zip",
+            checksum: "03416a4f75f023e10f9a76945806ddfe70ca06129b895455cc773c5c7d86b73e"
         )
     ]
 )
---
+++ scripts/Plugins/SwiftLintPlugin/SwiftLintPlugin.swift
@@ -0,0 +1,83 @@
+import Foundation
+import PackagePlugin
+
+@main
+struct SwiftLintPlugin: BuildToolPlugin {
+    func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        guard let sourceTarget = target as? SourceModuleTarget else {
+            return []
+        }
+        return createBuildCommands(
+            inputFiles: sourceTarget.sourceFiles(withSuffix: "swift").map(\.path),
+            packageDirectory: context.package.directory,
+            workingDirectory: context.pluginWorkDirectory,
+            tool: try context.tool(named: "swiftlint")
+        )
+    }
+
+    private func createBuildCommands(
+        inputFiles: [Path],
+        packageDirectory: Path,
+        workingDirectory: Path,
+        tool: PluginContext.Tool
+    ) -> [Command] {
+        if inputFiles.isEmpty {
+            // Don't lint anything if there are no Swift source files in this target
+            return []
+        }
+
+        var arguments = [
+            "lint",
+            "--quiet",
+            // We always pass all of the Swift source files in the target to the tool,
+            // so we need to ensure that any exclusion rules in the configuration are
+            // respected.
+            "--force-exclude"
+        ]
+
+        // Determine whether we need to enable cache or not (for Xcode Cloud we don't)
+        if ProcessInfo.processInfo.environment["CI_XCODE_CLOUD"] == "TRUE" {
+            arguments.append("--no-cache")
+        } else {
+            arguments.append("--cache-path")
+            arguments.append("\(workingDirectory)")
+        }
+
+        // Manually look for configuration files, to avoid issues when the plugin does not execute our tool from the
+        // package source directory.
+        if let configuration = packageDirectory.firstConfigurationFileInParentDirectories() {
+            arguments.append(contentsOf: ["--config", "\(configuration.string)"])
+        }
+        arguments += inputFiles.map(\.string)
+
+        // We are not producing output files and this is needed only to not include cache files into bundle
+        let outputFilesDirectory = workingDirectory.appending("Output")
+
+        return [
+            .prebuildCommand(
+                displayName: "SwiftLint",
+                executable: tool.path,
+                arguments: arguments,
+                outputFilesDirectory: outputFilesDirectory
+            )
+        ]
+    }
+}
+
+#if canImport(XcodeProjectPlugin)
+import XcodeProjectPlugin
+
+extension SwiftLintPlugin: XcodeBuildToolPlugin {
+    func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
+        let inputFilePaths = target.inputFiles
+            .filter { $0.type == .source && $0.path.extension == "swift" }
+            .map(\.path)
+        return createBuildCommands(
+            inputFiles: inputFilePaths,
+            packageDirectory: context.xcodeProject.directory,
+            workingDirectory: context.pluginWorkDirectory,
+            tool: try context.tool(named: "swiftlint")
+        )
+    }
+}
+#endif
