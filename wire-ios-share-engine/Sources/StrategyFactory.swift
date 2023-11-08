f22805a21b2cb7feb29f634d8225552238b20baf
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-11-09 00:58:40 +0100

    chore: remove MessageSync from ClientMessageRequestStrategy

--- wire-ios-share-engine/Sources/StrategyFactory.swift
+++ wire-ios-share-engine/Sources/StrategyFactory.swift
@@ -26,14 +26,31 @@ class StrategyFactory {
     unowned let syncContext: NSManagedObjectContext
     let applicationStatus: ApplicationStatus
     let linkPreviewPreprocessor: LinkPreviewPreprocessor
+    let messageSender: MessageSenderInterface
     private(set) var strategies = [AnyObject]()
 
     private var tornDown = false
 
-    init(syncContext: NSManagedObjectContext, applicationStatus: ApplicationStatus, linkPreviewPreprocessor: LinkPreviewPreprocessor) {
+    init(syncContext: NSManagedObjectContext,
+         applicationStatus: ApplicationStatus,
+         linkPreviewPreprocessor: LinkPreviewPreprocessor,
+         transportSession: TransportSessionType
+    ) {
+        let httpClient = HttpClientImpl(transportSession: transportSession, queue: syncContext)
+        let apiProvider = APIProvider(httpClient: httpClient)
+        let sessionEstablisher = SessionEstablisher(context: syncContext, apiProvider: apiProvider)
+        let messageDependencyResolver = MessageDependencyResolver(context: syncContext)
+        let quickSyncObserver = QuickSyncObserver(context: syncContext, applicationStatus: applicationStatus, notificationContext: syncContext.notificationContext)
         self.linkPreviewPreprocessor = linkPreviewPreprocessor
         self.syncContext = syncContext
         self.applicationStatus = applicationStatus
+        self.messageSender = MessageSender(
+            apiProvider: apiProvider,
+            clientRegistrationDelegate: applicationStatus.clientRegistrationDelegate,
+            sessionEstablisher: sessionEstablisher,
+            messageDependencyResolver: messageDependencyResolver,
+            quickSyncObserver: quickSyncObserver,
+            context: syncContext)
         self.strategies = createStrategies(linkPreviewPreprocessor: linkPreviewPreprocessor)
     }
 
@@ -84,10 +101,10 @@ class StrategyFactory {
 
     private func createClientMessageRequestStrategy() -> ClientMessageRequestStrategy {
         return ClientMessageRequestStrategy(
-            withManagedObjectContext: syncContext,
+            context: syncContext,
             localNotificationDispatcher: PushMessageHandlerDummy(),
             applicationStatus: applicationStatus,
-            messageSender: nil
+            messageSender: messageSender
         )
     }
 
@@ -104,7 +121,7 @@ class StrategyFactory {
     }
 
     private func createLinkPreviewUpdateRequestStrategy() -> LinkPreviewUpdateRequestStrategy {
-        return LinkPreviewUpdateRequestStrategy(withManagedObjectContext: syncContext, applicationStatus: applicationStatus)
+        return LinkPreviewUpdateRequestStrategy(managedObjectContext: syncContext, messageSender: messageSender)
     }
 
     // MARK: - Asset V3
@@ -130,6 +147,6 @@ class StrategyFactory {
     }
 
     private func createAssetClientMessageRequestStrategy() -> AssetClientMessageRequestStrategy {
-        return AssetClientMessageRequestStrategy(withManagedObjectContext: syncContext, applicationStatus: applicationStatus)
+        return AssetClientMessageRequestStrategy(managedObjectContext: syncContext, messageSender: messageSender)
     }
 }
