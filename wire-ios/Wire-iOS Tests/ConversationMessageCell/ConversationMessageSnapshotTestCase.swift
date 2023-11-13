3717ff5acf452e3fd5562304da9975ab415296f1
Christoph Aldrian <caldrian@users.noreply.github.com>
2023-11-13 12:57:18 +0100

    fix: remove misleading function XCTAssertArrayEqual [WPB-4920] (#672)

--- wire-ios/Wire-iOS Tests/ConversationMessageCell/ConversationMessageSnapshotTestCase.swift
+++ wire-ios/Wire-iOS Tests/ConversationMessageCell/ConversationMessageSnapshotTestCase.swift
@@ -33,12 +33,6 @@ extension ConversationMessageContext {
                                                                        spacing: 0)
 }
 
-func XCTAssertArrayEqual(_ descriptions: [Any], _ expectedDescriptions: [Any], file: StaticString = #file, line: UInt = #line) {
-    let classes = descriptions.map { String(describing: $0) }
-    let expectedClasses = expectedDescriptions.map { String(describing: $0) }
-    XCTAssertEqual(classes, expectedClasses, file: file, line: line)
-}
-
 class ConversationMessageSnapshotTestCase: ZMSnapshotTestCase {
 
     var userSession: UserSessionMock!
