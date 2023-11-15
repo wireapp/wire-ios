0e71bb20bac32d2eb9a35ccd748ef17d29b08b60
François Benaiteau <francois.benaiteau@gmail.com>
2023-11-15 16:05:08 +0100

    replace assert by XCTFail

--- wire-ios-testing/Source/Public/ZMTBaseTest.swift
+++ wire-ios-testing/Source/Public/ZMTBaseTest.swift
@@ -23,7 +23,7 @@ extension ZMTBaseTest {
     public static func checkForMemoryLeaksAfterTestClassCompletes() {
         if MemoryReferenceDebugger.aliveObjects.count > 0 {
             print("Leaked: \(MemoryReferenceDebugger.aliveObjectsDescription)")
-            assert(false)
+            XCTFail("Leaked: \(MemoryReferenceDebugger.aliveObjectsDescription)")
         }
     }
 
