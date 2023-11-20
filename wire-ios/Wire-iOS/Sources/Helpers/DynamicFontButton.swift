ae79af4494e55a66dfc131a5c870756e50c11284
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2023-11-20 14:09:53 +0100

    Update wire-ios/Wire-iOS/Sources/Helpers/DynamicFontButton.swift
    Co-authored-by: Markus Faßbender <markus.fassbender@wire.com>

--- wire-ios/Wire-iOS/Sources/Helpers/DynamicFontButton.swift
+++ wire-ios/Wire-iOS/Sources/Helpers/DynamicFontButton.swift
@@ -37,7 +37,7 @@ class DynamicFontButton: StylableButton, DynamicTypeCapable {
 
     @available(*, deprecated, message: "Use `init(style:)` instead")
     init(fontSpec: FontSpec = .normalRegularFont) {
-        self.onRedrawFont = { return fontSpec.font }
+        self.onRedrawFont = { fontSpec.font }
         super.init(frame: .zero)
 
         titleLabel?.font = fontSpec.font
