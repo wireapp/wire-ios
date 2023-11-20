9b0a45d8969d6ecca75effb0e0254614d2d65ca2
David-Henner <david.henner@wire.com>
2023-11-20 09:33:25 +0100

    chore: set up sourcery for WireRequestStrategy (#691)

---
+++ wire-ios-request-strategy/sourcery/generated/AutoMockable.generated.swift
@@ -0,0 +1,35 @@
+// Generated using Sourcery 2.1.2 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+// swiftlint:disable line_length
+// swiftlint:disable variable_name
+
+import Foundation
+#if os(iOS) || os(tvOS) || os(watchOS)
+import UIKit
+#elseif os(OSX)
+import AppKit
+#endif
+
+
+@testable import WireRequestStrategy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
