6751e6771de3a830454ba19fe606d5764f87250e
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2023-11-29 17:42:25 +0100

    chore: groom

--- wire-ios-request-strategy/Sources/Request Strategies/User/UserProfileRequestStrategy.swift
+++ wire-ios-request-strategy/Sources/Request Strategies/User/UserProfileRequestStrategy.swift
@@ -243,10 +243,7 @@ class UserProfileByIDTranscoder: IdentifierObjectSyncTranscoder {
     }
 
     func request(for identifiers: Set<UUID>, apiVersion: APIVersion) -> ZMTransportRequest? {
-        if apiVersion.rawValue > 0 {
-            return nil
-        }
-
+        guard apiVersion == .v0 else { return nil }
         // GET /users?ids=?
         let userIDs = identifiers.map({ $0.transportString() }).joined(separator: ",")
         return ZMTransportRequest(getFromPath: "/users?ids=\(userIDs)", apiVersion: apiVersion.rawValue)
