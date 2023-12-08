b802b4ce775a4f683d90d8d2a69cb0b9f78da79c
Arun <arunbalakrishnan89@gmail.com>
2023-12-08 10:34:52 +0100

    Added snapshot tests

---
+++ wire-ios/Wire-iOS Tests/DeviceManagement/E2eIdentityCertificateViewTests.swift
@@ -0,0 +1,92 @@
+//
+// Wire
+// Copyright (C) 2023 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+
+import XCTest
+import SwiftUI
+@testable import Wire
+
+final class E2eIdentityCertificateViewTests: ZMSnapshotTestCase {
+
+    var sut: UIHostingController<E2EIdentityCertificateDetailsView>!
+    var kCertificate: String {
+        return "BEGIN CERTIFICATE\n---------\n"
+        + String(repeating: "abcedefghijklmnop", count: 100)
+        + "\n-------------\nEND Certificate"
+    }
+
+    func setupSut(
+        certificateDetails: String,
+        mode: UIUserInterfaceStyle,
+        isDownloadAndCopyEnabled: Bool,
+        isMenuPresented: Bool
+    ) {
+        let certificateView = E2EIdentityCertificateDetailsView(
+            certificateDetails: kCertificate,
+            isDownloadAndCopyEnabled: isDownloadAndCopyEnabled,
+            isMenuPresented: isMenuPresented
+        )
+        sut = UIHostingController(rootView: certificateView)
+        sut.view.frame = UIScreen.main.bounds
+        sut.overrideUserInterfaceStyle = mode
+    }
+
+    func testGivenCopyIsDisabledWhenCertificateIsAvailableThenRightViewIsShown() {
+        setupSut(
+            certificateDetails: kCertificate,
+            mode: .light,
+            isDownloadAndCopyEnabled: false,
+            isMenuPresented: false
+        )
+        verify(matching: sut)
+    }
+
+    func testGivenCopyIsEnabledAndCertificateIsAvailableWhenMenuIsPresentedThenRightViewIsShown() {
+        setupSut(
+            certificateDetails: kCertificate,
+            mode: .light,
+            isDownloadAndCopyEnabled: true,
+            isMenuPresented: true
+        )
+        verify(matching: sut)
+    }
+
+    // MARK: Dark Mode
+
+    func testGivenCopyIsDisabledWhenCertificateIsAvailableThenRightViewIsShownInDarkMode() {
+        setupSut(
+            certificateDetails: kCertificate,
+            mode: .dark,
+            isDownloadAndCopyEnabled: false,
+            isMenuPresented: false
+        )
+        verify(matching: sut)
+    }
+
+    func testGivenCopyIsEnabledAndCertificateIsAvailableWhenMenuIsPresentedThenRightViewIsShownInDarkMode() {
+        setupSut(
+            certificateDetails: kCertificate,
+            mode: .dark,
+            isDownloadAndCopyEnabled: true,
+            isMenuPresented: true
+        )
+        verify(matching: sut)
+    }
+
+}
