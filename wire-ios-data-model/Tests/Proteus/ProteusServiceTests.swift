1aa395a2270715a24cf938ee2ce9030344e1eb7a
François Benaiteau <francois.benaiteau@gmail.com>
2023-12-18 22:59:37 +0100

    chore: move MockCoreCrypto to DataModelSupport

--- wire-ios-data-model/Tests/Proteus/ProteusServiceTests.swift
+++ wire-ios-data-model/Tests/Proteus/ProteusServiceTests.swift
@@ -19,8 +19,9 @@
 import Foundation
 import XCTest
 import WireCoreCrypto
-import WireDataModelSupport
+
 @testable import WireDataModel
+@testable import WireDataModelSupport
 
 class ProteusServiceTests: XCTestCase {
 
