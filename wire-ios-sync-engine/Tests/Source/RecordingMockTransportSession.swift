9735e2a0d398d8f61ca215a6bd03cfd97c2fbc88
Christoph Aldrian <christoph.aldrian@wire.com>
2023-12-19 11:58:24 +0100

    fix: build error in sync engine

--- wire-ios-sync-engine/Tests/Source/RecordingMockTransportSession.swift
+++ wire-ios-sync-engine/Tests/Source/RecordingMockTransportSession.swift
@@ -64,10 +64,6 @@ class RecordingMockTransportSession: NSObject, TransportSessionType {
         lastEnqueuedRequest = request
     }
 
-    func enqueue(_ request: ZMTransportRequest, queue: ZMSGroupQueue) async -> ZMTransportResponse {
-        fatalError("not implemented")
-    }
-
     func attemptToEnqueueSyncRequest(generator: () -> ZMTransportRequest?) -> ZMTransportEnqueueResult {
         guard let request = generator() else {
             return ZMTransportEnqueueResult(didHaveLessRequestsThanMax: true, didGenerateNonNullRequest: false)
