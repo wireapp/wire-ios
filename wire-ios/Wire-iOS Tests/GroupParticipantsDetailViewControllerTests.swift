911b7060132eed7bbf1db0c348b2a81a62a02489
KaterinaWire <katerina@wire.com>
2023-12-21 15:26:49 +0100

    add tests to GroupDetailsViewControllerSnapshotTests

--- wire-ios/Wire-iOS Tests/GroupParticipantsDetailViewControllerTests.swift
+++ wire-ios/Wire-iOS Tests/GroupParticipantsDetailViewControllerTests.swift
@@ -33,6 +33,8 @@ private final class MockConversation: MockStableRandomParticipantsConversation, 
     var messageProtocol: MessageProtocol = .proteus
 
     var mlsGroupID: WireDataModel.MLSGroupID?
+
+    var mlsVerificationStatus: WireDataModel.MLSVerificationStatus?
 }
 
 final class GroupParticipantsDetailViewControllerTests: ZMSnapshotTestCase {
