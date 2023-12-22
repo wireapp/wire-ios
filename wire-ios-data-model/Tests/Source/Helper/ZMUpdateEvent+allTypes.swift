2baeeeb4e153666a9f4b70b2d60125b7424d8160
Christoph Aldrian <christoph.aldrian@wire.com>
2023-12-22 18:37:00 +0100

    fix: file headers

--- wire-ios-data-model/Tests/Source/Helper/ZMUpdateEvent+allTypes.swift
+++ wire-ios-data-model/Tests/Source/Helper/ZMUpdateEvent+allTypes.swift
@@ -1,6 +1,6 @@
 //
 // Wire
-// Copyright (C) 2023 Wire Swiss GmbH
+// Copyright (C) 2024 Wire Swiss GmbH
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -18,5 +18,7 @@
 
 extension ZMUpdateEvent {
     @objc
-    static let allTypes = ZMUpdateEventType.allCases.map { $0.rawValue as NSNumber }
+    static let allTypes = ZMUpdateEventType
+        .allCases
+        .map { $0.rawValue as NSNumber }
 }
