2c107314dd7cf126ffee55d4ff83da2065e895be
Arun <arunbalakrishnan89@gmail.com>
2024-01-15 19:04:07 +0100

    fixed sanpshot

--- wire-ios/Wire-iOS/Sources/UserInterface/DeviceView/Views/Components/DeviceDetailsE2EIdentityCertificateView.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/DeviceView/Views/Components/DeviceDetailsE2EIdentityCertificateView.swift
@@ -63,7 +63,7 @@ struct DeviceDetailsE2EIdentityCertificateView: View {
             }
             Spacer()
         }
-        if let certificate = viewModel.e2eIdentityCertificate, certificate.isValid {
+        if let certificate = viewModel.e2eIdentityCertificate {
             Text(L10n.Localizable.Device.Details.Section.E2ei.serialNumber)
                 .font(FontSpec.smallSemiboldFont.swiftUIFont)
                 .foregroundColor(SemanticColors.Label.textSectionHeader.swiftUIColor)
