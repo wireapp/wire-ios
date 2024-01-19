ed959d68e0f07db56514ee26031d966bea7a101b
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2024-01-19 17:48:29 +0100

    chore: Separate classes into their own files

---
+++ wire-ios/Wire-iOS/Sources/UserInterface/Settings/ColorPickerController.swift
@@ -0,0 +1,110 @@
+//
+// Wire
+// Copyright (C) 2024 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import UIKit
+import WireSyncEngine
+import WireCommonComponents
+
+protocol ColorPickerControllerDelegate: AnyObject {
+    func colorPicker(_ colorPicker: ColorPickerController, didSelectColor color: AccentColor)
+    func colorPickerWantsToDismiss(_ colotPicker: ColorPickerController)
+}
+
+class ColorPickerController: UIViewController {
+    let tableView = UITableView()
+
+    static private let rowHeight: CGFloat = 56
+
+    let colors: [AccentColor]
+    var selectedColor: AccentColor?
+    weak var delegate: ColorPickerControllerDelegate?
+
+    init(colors: [AccentColor]) {
+        self.colors = colors
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        createConstraints()
+    }
+
+    private func setupViews() {
+        view.backgroundColor = SemanticColors.View.backgroundDefault
+        view.addSubview(tableView)
+
+        tableView.register(PickerCell.self, forCellReuseIdentifier: PickerCell.reuseIdentifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        self.navigationItem.rightBarButtonItem = navigationController?.closeItem()
+    }
+
+    private func createConstraints() {
+        [tableView].prepareForLayout()
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.heightAnchor.constraint(equalToConstant: Self.rowHeight * CGFloat(colors.count)),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+
+    @objc
+    private func didPressDismiss(_ sender: AnyObject?) {
+        delegate?.colorPickerWantsToDismiss(self)
+    }
+}
+
+extension ColorPickerController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return type(of: self).rowHeight
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PickerCell.reuseIdentifier) as? PickerCell else {
+            fatal("Cannot create cell")
+        }
+
+        cell.color = colors[indexPath.row]
+        let colorName = colors[indexPath.row].name
+        cell.setColorName(colorName)
+
+        cell.isSelected = cell.color == selectedColor
+        if cell.isSelected {
+            tableView.selectRow(at: indexPath, animated: false, scrollPosition: .none)
+        }
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        delegate?.colorPicker(self, didSelectColor: colors[indexPath.row])
+        selectedColor = colors[indexPath.row]
+    }
+}
