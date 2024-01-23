072e5283393b714f232abaeb24d53c23e1427408
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2024-01-23 17:39:25 +0100

    feat: Add support for system message for read-only proteus conversations - WPB-5805 (#858)
    Co-authored-by: Markus Faßbender <markus.fassbender@wire.com>
    Co-authored-by: Christoph Aldrian <caldrian@users.noreply.github.com>
    Co-authored-by: David-Henner <david.henner@wire.com>
    Co-authored-by: Christoph Aldrian <christoph.aldrian@wire.com>
    Co-authored-by: François Benaiteau <netbe@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
    Co-authored-by: KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
    Co-authored-by: zenkins <iosx@wire.com>
    Co-authored-by: François Benaiteau <francois.benaiteau@gmail.com>

---
+++ wire-ios/Tests/Mocks/SwiftMockConversation+ConversationCreation.swift
@@ -0,0 +1,44 @@
+//
+// Wire
+// Copyright (C) 2024 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+extension SwiftMockConversation {
+
+    static func oneOnOneConversation(
+        otherUser: UserType = MockUser.mockUsers().first!
+    ) -> SwiftMockConversation {
+        let selfUser = (MockUser.mockSelf() as Any) as! ZMUser
+        let mockConversation = SwiftMockConversation()
+        mockConversation.conversationType = .oneOnOne
+        mockConversation.displayName = otherUser.name
+        mockConversation.connectedUserType = otherUser
+
+        return mockConversation
+    }
+
+    static func groupConversation(
+        selfUser: UserType = MockUserType.createSelfUser(name: "Alice"),
+        otherUser: UserType = SwiftMockLoader.mockUsers().first!
+    ) -> SwiftMockConversation {
+        let mockConversation = SwiftMockConversation()
+        mockConversation.conversationType = .group
+        mockConversation.displayName = otherUser.name
+
+        return mockConversation
+    }
+
+}
