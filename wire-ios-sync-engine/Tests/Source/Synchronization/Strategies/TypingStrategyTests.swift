19e0669437b05049a058a2ab2882567affe2849b
François Benaiteau <francois.benaiteau@gmail.com>
2024-01-23 22:40:12 +0100

    fix TypingStrategyTests

--- wire-ios-sync-engine/Tests/Source/Synchronization/Strategies/TypingStrategyTests.swift
+++ wire-ios-sync-engine/Tests/Source/Synchronization/Strategies/TypingStrategyTests.swift
@@ -200,7 +200,7 @@ final class TypingStrategyTests: MessagingTest {
         TypingStrategy.notifyTranscoderThatUser(isTyping: true, in: conversation)
         XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
 
-        let request = sut.nextRequest(for: version)
+        let request = syncMOC.performAndWait { sut.nextRequest(for: version) }
 
         // then
         XCTAssertNotNil(request)
@@ -208,27 +208,31 @@ final class TypingStrategyTests: MessagingTest {
     }
 
     func testThatItForwardsAStartedTypingEvent() {
-        // given
-        let event = typingEvent(isTyping: true)
+        syncMOC.performAndWait {
+            // given
+            let event = typingEvent(isTyping: true)
 
-        // when
-        sut.processEvents([event], liveEvents: true, prefetchResult: nil)
+            // when
+            sut.processEvents([event], liveEvents: true, prefetchResult: nil)
 
-        // then
-        XCTAssertTrue(typing.isUserTyping(user: userA, in: conversationA))
+            // then
+            XCTAssertTrue(typing.isUserTyping(user: userA, in: conversationA))
+        }
     }
 
     func testThatItForwardsAStoppedTypingEvent() {
-        // given
-        let event = typingEvent(isTyping: false)
-        simulateTyping()
+        syncMOC.performAndWait {
+            // given
+            let event = typingEvent(isTyping: false)
+            simulateTyping()
 
-        // when
-        sut.processEvents([event], liveEvents: true, prefetchResult: nil)
+            // when
+            sut.processEvents([event], liveEvents: true, prefetchResult: nil)
 
-        // then
-        XCTAssertTrue(typing.didSetTypingUsers)
-        XCTAssertFalse(typing.isUserTyping(user: userA, in: conversationA))
+            // then
+            XCTAssertTrue(typing.didSetTypingUsers)
+            XCTAssertFalse(typing.isUserTyping(user: userA, in: conversationA))
+        }
     }
 
     func testThatItDoesNotForwardsAnUnknownTypingEvent() {
@@ -255,14 +259,15 @@ final class TypingStrategyTests: MessagingTest {
 
         // edit message is an allowed type that can fire a otr-message-add notification
         let message = GenericMessage(content: MessageEdit(replacingMessageID: .create(), text: Text(content: "demo")))
-        let payload = payloadForOTRMessageAdd(with: message) as ZMTransportData
+        let payload = syncMOC.performAndWait { self.payloadForOTRMessageAdd(with: message) as ZMTransportData }
         let event = ZMUpdateEvent(fromEventStreamPayload: payload as ZMTransportData, uuid: nil)!
 
-        simulateTyping()
+        syncMOC.performAndWait {
+            simulateTyping()
 
-        // when
-        sut.processEvents([event], liveEvents: true, prefetchResult: nil)
-
+            // when
+            sut.processEvents([event], liveEvents: true, prefetchResult: nil)
+        }
         // then
         XCTAssertTrue(typing.didSetTypingUsers)
         XCTAssertFalse(typing.isUserTyping(user: userA, in: conversationA))
@@ -287,14 +292,14 @@ final class TypingStrategyTests: MessagingTest {
     }
 
     func tryToForwardOTRMessageWithoutReply(with message: GenericMessage) {
-        let payload = payloadForOTRMessageAdd(with: message) as ZMTransportData
+        let payload = self.payloadForOTRMessageAdd(with: message) as ZMTransportData
         let event = ZMUpdateEvent(fromEventStreamPayload: payload, uuid: nil)!
+        syncMOC.performAndWait {
+            simulateTyping()
 
-        simulateTyping()
-
-        // when
-        sut.processEvents([event], liveEvents: true, prefetchResult: nil)
-
+            // when
+            sut.processEvents([event], liveEvents: true, prefetchResult: nil)
+        }
         // then
         XCTAssertFalse(typing.didSetTypingUsers)
         XCTAssertTrue(typing.isUserTyping(user: userA, in: conversationA)) // user is still typing
@@ -337,7 +342,7 @@ final class TypingStrategyTests: MessagingTest {
         TypingStrategy.notifyTranscoderThatUser(isTyping: true, in: conversation)
         XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
 
-        let request = sut.nextRequest(for: .v0)
+        let request = syncMOC.performAndWait { sut.nextRequest(for: .v0) }
 
         // then
         XCTAssertNotNil(request)
@@ -353,7 +358,7 @@ final class TypingStrategyTests: MessagingTest {
         TypingStrategy.notifyTranscoderThatUser(isTyping: true, in: conversation)
         XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
 
-        let request = sut.nextRequest(for: .v0)
+        let request = syncMOC.performAndWait { sut.nextRequest(for: .v0) }
 
         // then
         XCTAssertNotNil(request)
@@ -368,14 +373,14 @@ final class TypingStrategyTests: MessagingTest {
         TypingStrategy.notifyTranscoderThatUser(isTyping: true, in: conversation1)
         XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
 
-        let request = sut.nextRequest(for: .v0)
+        let request = syncMOC.performAndWait { sut.nextRequest(for: .v0) }
         XCTAssertTrue(isExpected(request: request, for: conversation1, isTyping: true))
 
         // when
         TypingStrategy.notifyTranscoderThatUser(isTyping: true, in: conversation2)
         XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
-        let request1 = sut.nextRequest(for: .v0)
-        let request2 = sut.nextRequest(for: .v0)
+        let request1 = syncMOC.performAndWait { sut.nextRequest(for: .v0) }
+        let request2 = syncMOC.performAndWait { sut.nextRequest(for: .v0) }
 
         // then
         let request1IsforConv1 = isExpected(request: request1, for: conversation1, isTyping: false)
@@ -395,7 +400,9 @@ final class TypingStrategyTests: MessagingTest {
         TypingStrategy.notifyTranscoderThatUser(isTyping: true, in: conversation1)
         XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
 
-        let request1 = sut.nextRequest(for: .v0)
+        let request1 = syncMOC.performAndWait {
+            sut.nextRequest(for: .v0)
+        }
 
         // then
         XCTAssertNotNil(request1)
@@ -405,7 +412,9 @@ final class TypingStrategyTests: MessagingTest {
         TypingStrategy.notifyTranscoderThatUser(isTyping: false, in: conversation2)
         XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
 
-        let request2 = sut.nextRequest(for: .v0)
+        let request2 = syncMOC.performAndWait {
+            sut.nextRequest(for: .v0)
+        }
 
         // then
         // and then returns the startTypingEvent of the new conversation
@@ -413,7 +422,9 @@ final class TypingStrategyTests: MessagingTest {
         XCTAssertTrue(isExpected(request: request2, for: conversation2, isTyping: false))
 
         // finally
-        XCTAssertNil(sut.nextRequest(for: .v0))
+        syncMOC.performAndWait {
+            XCTAssertNil(sut.nextRequest(for: .v0))
+        }
     }
 
     func testThatItReturnsTheNextValidRequest() {
@@ -423,7 +434,9 @@ final class TypingStrategyTests: MessagingTest {
         TypingStrategy.notifyTranscoderThatUser(isTyping: true, in: conversation)
         XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
 
-        let request = sut.nextRequest(for: .v0)
+        let request = syncMOC.performAndWait {
+            sut.nextRequest(for: .v0)
+        }
         XCTAssertTrue(isExpected(request: request, for: conversation, isTyping: true))
 
         // when
@@ -433,14 +446,18 @@ final class TypingStrategyTests: MessagingTest {
         TypingStrategy.notifyTranscoderThatUser(isTyping: false, in: conversation)
         XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
 
-        let request1 = sut.nextRequest(for: .v0)
+        let request1 = syncMOC.performAndWait {
+            sut.nextRequest(for: .v0)
+        }
 
         // then
         XCTAssertNotNil(request1)
         XCTAssertTrue(isExpected(request: request1, for: conversation, isTyping: false))
 
         // finally
-        XCTAssertNil(sut.nextRequest(for: .v0))
+        syncMOC.performAndWait {
+            XCTAssertNil(sut.nextRequest(for: .v0))
+        }
     }
 
     func testThatItReturns_OnlyOne_RequestsWhenReceiving_One_TypingNotification() {
@@ -450,11 +467,15 @@ final class TypingStrategyTests: MessagingTest {
         TypingStrategy.notifyTranscoderThatUser(isTyping: true, in: conversation)
         XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
 
-        let request1 = sut.nextRequest(for: .v0)
+        let request1 = syncMOC.performAndWait {
+            sut.nextRequest(for: .v0)
+        }
         XCTAssertNotNil(request1)
 
         // when
-        let request2 = sut.nextRequest(for: .v0)
+        let request2 = syncMOC.performAndWait {
+            sut.nextRequest(for: .v0)
+        }
 
         // then
         XCTAssertNil(request2)
@@ -472,8 +493,9 @@ final class TypingStrategyTests: MessagingTest {
         XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
 
         // when
-        let request1 = sut.nextRequest(for: .v0)
-        let request2 = sut.nextRequest(for: .v0)
+        let (request1, request2) = syncMOC.performAndWait {
+            (sut.nextRequest(for: .v0), sut.nextRequest(for: .v0))
+        }
 
         // then
         // Note: the first typing is ended because we assume that typing can only happen in one conversation at a time
@@ -499,8 +521,9 @@ final class TypingStrategyTests: MessagingTest {
         XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
 
         // when
-        let request1 = sut.nextRequest(for: .v0)
-        let request2 = sut.nextRequest(for: .v0)
+        let (request1, request2) = syncMOC.performAndWait {
+            (sut.nextRequest(for: .v0), sut.nextRequest(for: .v0))
+        }
 
         // then
         // Note: the first typing is ended because we assume that typing can only happen in one conversation at a time
@@ -522,11 +545,13 @@ final class TypingStrategyTests: MessagingTest {
         TypingStrategy.notifyTranscoderThatUser(isTyping: true, in: conversation)
         XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
 
-        // when
-        let request = self.sut.nextRequest(for: .v0)
+        syncMOC.performAndWait {
+            // when
+            let request = self.sut.nextRequest(for: .v0)
 
-        // then
-        XCTAssertNil(request)
+            // then
+            XCTAssertNil(request)
+        }
     }
 
     func testThatItDoesNotReturnARequestsWhenTheClientRegistrationDelegateIsNotReady() {
@@ -539,13 +564,16 @@ final class TypingStrategyTests: MessagingTest {
 
         // when
         mockApplicationStatus.mockSynchronizationState = .unauthenticated
-        let request = self.sut.nextRequest(for: .v0)
 
-        // then
-        XCTAssertNil(request)
+        syncMOC.performAndWait {
+            let request = self.sut.nextRequest(for: .v0)
+
+            // then
+            XCTAssertNil(request)
+        }
     }
 
-    func testThatAppendingAMessageClearsTheIsTypingState() {
+    func testThatAppendingAMessageClearsTheIsTypingState() throws {
         // given
         let conversation = insertUIConversation()
 
@@ -559,7 +587,7 @@ final class TypingStrategyTests: MessagingTest {
         })
 
         // when
-        try! conversation.appendText(content: "foo")
+        try conversation.appendText(content: "foo")
 
         // then
         withExtendedLifetime(token) {
@@ -568,16 +596,19 @@ final class TypingStrategyTests: MessagingTest {
     }
 
     func testThatRemovingMemberClearsTypingState() {
-        // given
-        simulateTyping()
-        let event = memberLeaveEvent()
-        XCTAssertTrue(typing.isUserTyping(user: userA, in: conversationA)) // user is still typing
+        syncMOC.performAndWait {
+            // given
+            simulateTyping()
+            let event = memberLeaveEvent()
+            XCTAssertTrue(typing.isUserTyping(user: userA, in: conversationA)) // user is still typing
 
-        // when
-        sut.processEvents([event], liveEvents: true, prefetchResult: nil)
+            // when
+            sut.processEvents([event], liveEvents: true, prefetchResult: nil)
 
-        // then
-        XCTAssertFalse(typing.isUserTyping(user: userA, in: conversationA)) // user is not typing
+            // then
+            XCTAssertFalse(typing.isUserTyping(user: userA, in: conversationA)) // user is not typing
+
+        }
     }
 
 }
@@ -585,11 +616,11 @@ final class TypingStrategyTests: MessagingTest {
 // MARK: - Sending multiple requests
 extension TypingStrategyTests {
 
-    func requestsForSendingNotifications(isTyping: [Bool], delay: TestTyping) -> (ZMConversation, [ZMTransportRequest?]) {
+    func requestsForSendingNotifications(isTyping: [Bool], delay: TestTyping) throws -> (ZMConversation, [ZMTransportRequest?]) {
         var result = [ZMTransportRequest?]()
         let conversation = insertUIConversation()
 
-        isTyping.forEach {
+        try isTyping.forEach {
             TypingStrategy.notifyTranscoderThatUser(isTyping: $0, in: conversation)
             XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
             if delay == .delay {
@@ -597,78 +628,67 @@ extension TypingStrategyTests {
                 Thread.sleep(forTimeInterval: interval)
             }
 
-            let request = self.sut.nextRequest(for: .v0)
+            let request = self.syncMOC.performAndWait { self.sut.nextRequest(for: .v0) }
             result.append(request)
 
             if delay == .clearTranscoder {
                 TypingStrategy.clearTranscoderStateForTyping(in: conversation)
                 XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
             } else if delay == .appendMessage {
-                try! conversation.appendText(content: "foo")
+                try conversation.appendText(content: "foo")
                 XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
             }
         }
         return (conversation, result)
     }
 
-    func testThatItDoesReturn_OnlyOne_RequestsWhenReceiving_AnotherIdentical_TypingNotification() {
+    func testThatItDoesReturn_OnlyOne_RequestsWhenReceiving_AnotherIdentical_TypingNotification() throws {
         // when
-        let (conversation, requests) = requestsForSendingNotifications(isTyping: [true, true], delay: .noDelay)
+        let (conversation, requests) = try requestsForSendingNotifications(isTyping: [true, true], delay: .noDelay)
         XCTAssertEqual(requests.count, 2)
-        let request1 = requests.first!
-        let request2 = requests.last!
+        let request1 = try XCTUnwrap(requests.first)
+        let request2 = try XCTUnwrap(requests.last)
 
         // then
         XCTAssertTrue(isExpected(request: request1, for: conversation, isTyping: true))
         XCTAssertNil(request2)
     }
 
-    func testThatItDoesReturn_Another_RequestsWhenReceiving_AnotherIdentical_TypingNotificationAfterTheFirstOneIsCleared() {
-        // when
-        let (conversation, requests) = requestsForSendingNotifications(isTyping: [true, true], delay: .clearTranscoder)
-        XCTAssertEqual(requests.count, 2)
-        let request1 = requests.first!
-        let request2 = requests.last!
-
-        // then
-        XCTAssertTrue(isExpected(request: request1, for: conversation, isTyping: true))
-        XCTAssertTrue(isExpected(request: request2, for: conversation, isTyping: true))
+    func testThatItDoesReturn_Another_RequestsWhenReceiving_AnotherIdentical_TypingNotificationAfterTheFirstOneIsCleared() throws {
+        try internalTestThatItDoesReturn_Two_RequestsWhenReceiving_AnotherTypingNotification(isTypingRequest1: true, isTypingRequest2: true, delay: .clearTranscoder)
     }
 
-    func testThatItDoesReturn_Another_RequestsWhenReceiving_AnotherIdentical_TypingNotificationAfterAppendingAMessage() {
-        // when
-        let (conversation, requests) = requestsForSendingNotifications(isTyping: [true, true], delay: .appendMessage)
-        XCTAssertEqual(requests.count, 2)
-        let request1 = requests.first!
-        let request2 = requests.last!
-
-        // then
-        XCTAssertTrue(isExpected(request: request1, for: conversation, isTyping: true))
-        XCTAssertTrue(isExpected(request: request2, for: conversation, isTyping: true))
+    func testThatItDoesReturn_Another_RequestsWhenReceiving_AnotherIdentical_TypingNotificationAfterAppendingAMessage() throws {
+        try internalTestThatItDoesReturn_Two_RequestsWhenReceiving_AnotherTypingNotification(isTypingRequest1: true,
+                                                                                             isTypingRequest2: true,
+                                                                                             delay: .appendMessage)
     }
 
-    func testThatItDoesReturn_Two_RequestsWhenReceiving_AnotherDifferent_TypingNotification() {
-        // when
-        let (conversation, requests) = requestsForSendingNotifications(isTyping: [true, false], delay: .noDelay)
-        XCTAssertEqual(requests.count, 2)
-        let request1 = requests.first!
-        let request2 = requests.last!
-
-        // then
-        XCTAssertTrue(isExpected(request: request1, for: conversation, isTyping: true))
-        XCTAssertTrue(isExpected(request: request2, for: conversation, isTyping: false))
+    func testThatItDoesReturn_Two_RequestsWhenReceiving_AnotherDifferent_TypingNotification() throws {
+        try internalTestThatItDoesReturn_Two_RequestsWhenReceiving_AnotherTypingNotification(isTypingRequest1: true,
+                                                                                             isTypingRequest2: false,
+                                                                                             delay: .noDelay)
     }
 
-    func testThatItDoesReturn_Two_RequestsWhenReceiving_AnotherIdentical_TypingNotification_AfterADelay() {
+    func testThatItDoesReturn_Two_RequestsWhenReceiving_AnotherIdentical_TypingNotification_AfterADelay() throws {
+        try internalTestThatItDoesReturn_Two_RequestsWhenReceiving_AnotherTypingNotification(isTypingRequest1: true,
+                                                                                             isTypingRequest2: true,
+                                                                                             delay: .delay)
+    }
+
+    func internalTestThatItDoesReturn_Two_RequestsWhenReceiving_AnotherTypingNotification(isTypingRequest1: Bool,
+                                                                                          isTypingRequest2: Bool,
+                                                                                          delay: TestTyping) throws {
         // when
-        let (conversation, requests) = requestsForSendingNotifications(isTyping: [true, true], delay: .delay)
+        let (conversation, requests) = try requestsForSendingNotifications(isTyping: [isTypingRequest1, isTypingRequest2], delay: delay)
+
         XCTAssertEqual(requests.count, 2)
-        let request1 = requests.first!
-        let request2 = requests.last!
+        let request1 = try XCTUnwrap(requests.first)
+        let request2 = try XCTUnwrap(requests.last)
 
         // then
-        XCTAssertTrue(isExpected(request: request1, for: conversation, isTyping: true))
-        XCTAssertTrue(isExpected(request: request2, for: conversation, isTyping: true))
+        XCTAssertTrue(isExpected(request: request1, for: conversation, isTyping: isTypingRequest1))
+        XCTAssertTrue(isExpected(request: request2, for: conversation, isTyping: isTypingRequest2))
     }
 
     func testThatLocalTypingNotificationDoesNotInterferesWithRemoteNotifications() {
