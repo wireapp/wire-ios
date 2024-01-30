fecbd3cccb35b1b03a013d7a5baa6726c27c7b26
Markus Faßbender <markus.fassbender@wire.com>
2024-01-30 10:22:56 +0100

    remove not used papply

--- wire-ios-utilities/Source/Public/Papply.swift
+++ wire-ios-utilities/Source/Public/Papply.swift
@@ -31,27 +31,3 @@ public func papply<A, B, C>(
         ab2c(a, b)
     }
 }
-
-public func papply<A, B, C, D>(
-    _ abc2d: @escaping (A, B, C) -> D, _ a: A
-    ) -> (B, C) -> D {
-    return { b, c in
-        abc2d(a, b, c)
-    }
-}
-
-public func papply<A, B, C, D, E>(
-    _ abcd2e: @escaping (A, B, C, D) -> E, _ a: A
-    ) -> (B, C, D) -> E {
-    return { b, c, d in
-        abcd2e(a, b, c, d)
-    }
-}
-
-public func papply<A, B, C, D, E, F>(
-    _ abcde2f: @escaping (A, B, C, D, E) -> F, _ a: A
-    ) -> (B, C, D, E) -> F {
-    return { b, c, d, e in
-        abcde2f(a, b, c, d, e)
-    }
-}
--- wire-ios-utilities/Tests/PapplyTests.swift
+++ wire-ios-utilities/Tests/PapplyTests.swift
@@ -40,38 +40,4 @@ final class PartialApplicationTests: XCTestCase {
         // Then
         XCTAssertEqual(partial("2"), "12")
     }
-
-    func testThatItPartiallyApplies_3ParameterFunction() {
-        // Given
-        let foo: (String, String, String) -> String = { $0 + $1 + $2 }
-
-        // When
-        let partial = papply(foo, "1")
-
-        // Then
-        XCTAssertEqual(partial("2", "3"), "123")
-    }
-
-    func testThatItPartiallyApplies_4ParameterFunction() {
-        // Given
-        let foo: (String, String, String, String) -> String = { $0 + $1 + $2 + $3 }
-
-        // When
-        let partial = papply(foo, "1")
-
-        // Then
-        XCTAssertEqual(partial("2", "3", "4"), "1234")
-    }
-
-    func testThatItPartiallyApplies_5ParameterFunction() {
-        // Given
-        let foo: (String, String, String, String, String) -> String = { $0 + $1 + $2 + $3 + $4 }
-
-        // When
-        let partial = papply(foo, "1")
-
-        // Then
-        XCTAssertEqual(partial("2", "3", "4", "5"), "12345")
-    }
-
 }
