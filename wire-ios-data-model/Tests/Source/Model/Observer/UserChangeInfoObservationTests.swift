96598fa8ae25c88c13b5fcde1d6c4e06432f88ef
Christoph Aldrian <caldrian@users.noreply.github.com>
2024-02-02 12:39:23 +0100

    chore: rename UserChangeObserver to UserObserver [WPB-765] (#926)

--- wire-ios-data-model/Tests/Source/Model/Observer/UserChangeInfoObservationTests.swift
+++ wire-ios-data-model/Tests/Source/Model/Observer/UserChangeInfoObservationTests.swift
@@ -19,7 +19,7 @@
 import Foundation
 @testable import WireDataModel
 
-class UserObserverTests: NotificationDispatcherTestBase {
+final class UserChangeInfoObservationTests: NotificationDispatcherTestBase {
 
     let UserClientsKey = "clients"
 
@@ -44,49 +44,53 @@ class UserObserverTests: NotificationDispatcherTestBase {
 
     let userInfoChangeKeys: [UserInfoChangeKey] = UserInfoChangeKey.allCases
 
-    var userObserver: UserObserver!
+    var userObserver: MockUserObserver!
 
     override func setUp() {
         super.setUp()
-        userObserver = UserObserver()
+        userObserver = MockUserObserver()
     }
 
     override func tearDown() {
         userObserver = nil
         super.tearDown()
     }
-}
 
-extension UserObserverTests {
+    // MARK: - Tests
 
     func checkThatItNotifiesTheObserverOfAChange(_ user: ZMUser, modifier: (ZMUser) -> Void, expectedChangedField: UserInfoChangeKey) {
         checkThatItNotifiesTheObserverOfAChange(user, modifier: modifier, expectedChangedFields: [expectedChangedField])
     }
 
-    func checkThatItNotifiesTheObserverOfAChange(_ user: ZMUser, modifier: (ZMUser) -> Void, expectedChangedFields: [UserInfoChangeKey]) {
-
+    func checkThatItNotifiesTheObserverOfAChange(
+        _ user: ZMUser,
+        modifier: (ZMUser) -> Void,
+        expectedChangedFields: [UserInfoChangeKey],
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
         // given
         self.uiMOC.saveOrRollback()
-        XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+        XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5), file: file, line: line)
 
         self.token = UserChangeInfo.add(observer: userObserver, for: user, in: self.uiMOC)
 
         // when
         modifier(user)
-        XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+        XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5), file: file, line: line)
 
         self.uiMOC.saveOrRollback()
-        XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+        XCTAssert(waitForAllGroupsToBeEmpty(withTimeout: 0.5), file: file, line: line)
 
         // then
         let changeCount = userObserver.notifications.count
-        XCTAssertEqual(changeCount, 1)
+        XCTAssertEqual(changeCount, 1, file: file, line: line)
 
         // and when
         self.uiMOC.saveOrRollback()
 
         // then
-        XCTAssertEqual(userObserver.notifications.count, changeCount, "Should not have changed further once")
+        XCTAssertEqual(userObserver.notifications.count, changeCount, "Should not have changed further once", file: file, line: line)
 
         guard let changes = userObserver.notifications.first else { return }
         changes.checkForExpectedChangeFields(userInfoKeys: Set(userInfoChangeKeys.map {$0.rawValue}),
--- wire-ios/Wire-iOS/Sources/UserInterface/SelfProfile/ProfileHeaderViewController.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/SelfProfile/ProfileHeaderViewController.swift
@@ -329,9 +329,9 @@ final class ProfileHeaderViewController: UIViewController {
     }
 }
 
-// MARK: - ZMUserObserver
+// MARK: - ZMUserObserving
 
-extension ProfileHeaderViewController: ZMUserObserver {
+extension ProfileHeaderViewController: UserObserving {
 
     func userDidChange(_ changeInfo: UserChangeInfo) {
 
