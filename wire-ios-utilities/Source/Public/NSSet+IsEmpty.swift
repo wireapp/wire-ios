e6d84f0363598f4d76b67617218fa613628f5db7
Markus Faßbender <markus.fassbender@wire.com>
2024-02-06 10:03:08 +0100

    chore: remove old wire utilities (#930)

--- wire-ios-utilities/Source/Public/NSSet+IsEmpty.swift
+++ wire-ios-utilities/Source/Public/NSSet+IsEmpty.swift
@@ -18,26 +18,6 @@
 
 import Foundation
 
-public struct Enumerator: Sequence {
-
-    let enumerable: NSFastEnumeration
-
-    public init(_ object: NSFastEnumeration) {
-        self.enumerable = object
-    }
-
-    public func makeIterator() -> NSFastEnumerationIterator {
-        return NSFastEnumerationIterator(self.enumerable)
-    }
-
-    /**
-     /!\ Be careful using this: Tries to generate a finite array in memory out of potentially infinite enumeration /!\
-     */
-    public func allObjects() -> [AnyObject] {
-        var array: [AnyObject] = []
-        for object in self {
-            array.append(object as AnyObject)
-        }
-        return array
-    }
+extension NSSet {
+    @objc public var isEmpty: Bool { count == 0 }
 }
