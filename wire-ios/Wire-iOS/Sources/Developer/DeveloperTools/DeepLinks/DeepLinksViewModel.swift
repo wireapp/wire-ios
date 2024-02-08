db03666c2414b200d3fc158839996d66dd74bffd
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2024-02-08 14:39:14 +0100

    feat: add dev tool to open deep links (#951)

---
+++ wire-ios/Wire-iOS/Sources/Developer/DeveloperTools/DeepLinks/DeepLinksViewModel.swift
@@ -0,0 +1,71 @@
+//
+// Wire
+// Copyright (C) 2024 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+import WireSyncEngine
+
+final class DeepLinksViewModel: ObservableObject {
+
+    enum Error: LocalizedError {
+
+        case invalidLink
+
+        var errorDescription: String? {
+            switch self {
+            case .invalidLink:
+                return "The deeplink you have entered is invalid."
+            }
+        }
+
+    }
+
+    let router: AppRootRouter?
+    let onDismiss: (() -> Void)?
+
+    @Published
+    var isShowingAlert = false
+
+    @Published
+    var error: Error?
+
+    // MARK: - Life cycle
+
+    init(
+        router: AppRootRouter? = nil,
+        onDismiss: (() -> Void)? = nil
+    ) {
+        self.router = router
+        self.onDismiss = onDismiss
+    }
+
+    // MARK: - Actions
+
+    func openLink(urlString: String) {
+        guard
+            let url = URL(string: urlString),
+            (try? URLAction(url: url)) != nil
+        else {
+            error = .invalidLink
+            isShowingAlert = true
+            return
+        }
+
+        onDismiss?()
+        _ = router?.openDeepLinkURL(url)
+    }
+}
