35165b9caf35dd6f7405add53902c22ea47bb406
François Benaiteau <francois.benaiteau@gmail.com>
2024-02-08 23:21:48 +0100

    code review

--- wire-ios-data-model/Source/Model/ConversationRole/ParticipantRole.swift
+++ wire-ios-data-model/Source/Model/ConversationRole/ParticipantRole.swift
@@ -23,6 +23,7 @@ let ZMParticipantRoleRoleValueKey           = #keyPath(ParticipantRole.role)
 @objcMembers
 final public class ParticipantRole: ZMManagedObject {
 
+    /// - Note: conversation is optional dbut we should make sure it's always created with one using `create(managedObjectContext:user:conversation:)`
     @NSManaged public var conversation: ZMConversation?
     /// - Note: user is optional but we should make sure it's always created with one using `create(managedObjectContext:user:conversation:)`
     @NSManaged public var user: ZMUser?
