03173e8e1e3f0e2fd8f8ca135defc51de4d5023e
Markus Faßbender <markus.fassbender@wire.com>
2024-02-12 10:08:07 +0100

    add todo violation

--- wire-ios-notification-engine/WireNotificationEngineTestHost/AppDelegate.swift
+++ wire-ios-notification-engine/WireNotificationEngineTestHost/AppDelegate.swift
@@ -11,6 +11,8 @@ import UIKit
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    // TODO: test local commit
+
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     return true
