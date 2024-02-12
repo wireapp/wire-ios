ce1c5a01e9ccbb94cecc8e5ab9d4e02ab193a544
Markus Faßbender <markus.fassbender@wire.com>
2024-02-12 10:10:34 +0100

    change file

--- wire-ios-notification-engine/WireNotificationEngineTestHost/AppDelegate.swift
+++ wire-ios-notification-engine/WireNotificationEngineTestHost/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    // TODO: test local commit
+    // TODO: test local commit -
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
