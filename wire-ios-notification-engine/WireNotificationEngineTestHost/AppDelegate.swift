356913953df1fefa20289505f09a209dde0720bd
Markus Faßbender <markus.fassbender@wire.com>
2024-02-12 10:21:46 +0100

    add evil test

--- wire-ios-notification-engine/WireNotificationEngineTestHost/AppDelegate.swift
+++ wire-ios-notification-engine/WireNotificationEngineTestHost/AppDelegate.swift
@@ -11,6 +11,8 @@ import UIKit
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    // TODO: another test
+
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     return true
