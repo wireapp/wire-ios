ba077c9b7da9cef3373acfdbc49beceade2353aa
Markus Faßbender <markus.fassbender@wire.com>
2024-02-13 11:07:22 +0100

    throw error if user is not found

--- wire-ios-data-model/Source/MLS/OneOnOne/OneOnOneResolver.swift
+++ wire-ios-data-model/Source/MLS/OneOnOne/OneOnOneResolver.swift
@@ -38,6 +38,7 @@ public protocol OneOnOneResolverInterface {
 
 public enum OneOnOneResolverError: Error {
 
+    case otherUserNotFound
     case migratorNotFound
 
 }
@@ -70,8 +71,6 @@ public final class OneOnOneResolver: OneOnOneResolverInterface {
     // MARK: - Methods
 
     public func resolveAllOneOnOneConversations(in context: NSManagedObjectContext) async throws {
-        // TODO: [WPB-5812] implement
-
         let users: [ZMUser] = try await context.perform {
             let request = NSFetchRequest<ZMUser>(entityName: ZMUser.entityName())
             request.predicate = NSPredicate(format: "oneOnOneConversation != nil")
@@ -89,8 +88,7 @@ public final class OneOnOneResolver: OneOnOneResolverInterface {
         in context: NSManagedObjectContext
     ) async throws -> OneOnOneConversationResolution {
         guard let otherUser = await context.perform({ ZMUser.fetch(with: userID, in: context) }) else {
-            // TODO: [WPB-5812] throw error? was ignored before
-            return .noAction
+            throw OneOnOneResolverError.otherUserNotFound
         }
 
         return try await resolveOneOnOneUserConversation(otherUser, in: context)
