f7927648cb03ce4138a6064b16228ae9b23e6aa8
Markus Faßbender <markus.fassbender@wire.com>
2024-02-13 16:51:26 +0100

    throw error in case user can not be found

--- wire-ios-data-model/Source/MLS/OneOnOne/OneOnOneResolver.swift
+++ wire-ios-data-model/Source/MLS/OneOnOne/OneOnOneResolver.swift
@@ -33,6 +33,7 @@ public protocol OneOnOneResolverInterface {
 
 public enum OneOnOneResolverError: Error {
 
+    case otherUserNotFound
     case migratorNotFound
 
 }
@@ -65,8 +66,6 @@ public final class OneOnOneResolver: OneOnOneResolverInterface {
     // MARK: - Methods
 
     public func resolveAllOneOnOneConversations(in context: NSManagedObjectContext) async throws {
-        // TODO: [WPB-111] implement
-
         let users: [ZMUser] = try await context.perform {
             let request = NSFetchRequest<ZMUser>(entityName: ZMUser.entityName())
             request.predicate = NSPredicate(format: "oneOnOneConversation != nil")
@@ -75,8 +74,8 @@ public final class OneOnOneResolver: OneOnOneResolverInterface {
 
         for user in users {
             guard let userID = await context.perform({ user.qualifiedID }) else {
-                assertionFailure("required to have a qualifiedID")
-                return
+                WireLogger.conversation.error("required to have a user's qualifiedID to resolve 1-1 conversation!")
+                throw OneOnOneResolverError.otherUserNotFound
             }
 
             try await resolveOneOnOneConversation(with: userID, in: context)
@@ -88,7 +87,7 @@ public final class OneOnOneResolver: OneOnOneResolverInterface {
         with userID: QualifiedID,
         in context: NSManagedObjectContext
     ) async throws -> OneOnOneConversationResolution {
-        WireLogger.conversation.debug("resolving one on one with user: \(userID)")
+        WireLogger.conversation.debug("resolving 1-1 conversation with user: \(userID)")
 
         let messageProtocol = await protocolSelector.getProtocolForUser(
             with: userID,
@@ -111,7 +110,7 @@ public final class OneOnOneResolver: OneOnOneResolverInterface {
             return .archivedAsReadOnly
 
         case .mls:
-            WireLogger.conversation.debug("should resolve to mls one on one")
+            WireLogger.conversation.debug("should resolve to mls 1-1 conversation")
 
             guard let migrator else {
                 throw OneOnOneResolverError.migratorNotFound
@@ -125,7 +124,7 @@ public final class OneOnOneResolver: OneOnOneResolverInterface {
             return .migratedToMLSGroup(identifier: mlsGroupIdentifier)
 
         case .proteus:
-            WireLogger.conversation.debug("should resolve to proteus one on one")
+            WireLogger.conversation.debug("should resolve to proteus 1-1 conversation")
             return .noAction
 
         // This should never happen:
