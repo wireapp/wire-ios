2c9710819d75d7fac9eadf50cdcea180ae512870
Markus Faßbender <markus.fassbender@wire.com>
2024-02-14 11:32:18 +0100

    add domain for users to avoid failure on mls migration

--- wire-ios-sync-engine/Tests/Source/Integration/IntegrationTest.swift
+++ wire-ios-sync-engine/Tests/Source/Integration/IntegrationTest.swift
@@ -299,6 +299,7 @@ extension IntegrationTest {
             selfUser.password = IntegrationTest.SelfUserPassword
             selfUser.identifier = self.currentUserIdentifier.transportString()
             selfUser.phone = ""
+            selfUser.domain = "local@domain.com"
             selfUser.accentID = 2
             session.addProfilePicture(to: selfUser)
             session.addV3ProfilePicture(to: selfUser)
@@ -320,6 +321,7 @@ extension IntegrationTest {
             let user1 = session.insertUser(withName: "Extra User1")
             user1.email = "user1@example.com"
             user1.phone = "6543"
+            user1.domain = "local@domain.com"
             user1.accentID = 3
             session.addProfilePicture(to: user1)
             session.addV3ProfilePicture(to: user1)
@@ -328,12 +330,14 @@ extension IntegrationTest {
             let user2 = session.insertUser(withName: "Extra User2")
             user2.email = "user2@example.com"
             user2.phone = "4534"
+            user2.domain = "local@domain.com"
             user2.accentID = 1
             self.user2 = user2
 
             let user3 = session.insertUser(withName: "Extra User3")
             user3.email = "user3@example.com"
             user3.phone = "340958"
+            user2.domain = "local@domain.com"
             user3.accentID = 4
             session.addProfilePicture(to: user3)
             session.addV3ProfilePicture(to: user3)
@@ -342,6 +346,7 @@ extension IntegrationTest {
             let user4 = session.insertUser(withName: "Extra User4")
             user4.email = "user4@example.com"
             user4.phone = "2349857"
+            user4.domain = "local@domain.com"
             user4.accentID = 7
             session.addProfilePicture(to: user4)
             session.addV3ProfilePicture(to: user4)
@@ -350,6 +355,7 @@ extension IntegrationTest {
             let user5 = session.insertUser(withName: "Extra User5")
             user5.email = "user5@example.com"
             user5.phone = "555466434325"
+            user5.domain = "local@domain.com"
             user5.accentID = 7
             self.user5 = user5
 
