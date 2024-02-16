18ee4aa32c5b66d3eedb09ea63f0fd1eae8e0b2e
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2024-02-16 12:03:45 +0100

    fix: CountryCodeTableViewControllerTests

--- wire-ios/Wire-iOS Tests/CountryCodeTableViewControllerTests.swift
+++ wire-ios/Wire-iOS Tests/CountryCodeTableViewControllerTests.swift
@@ -19,7 +19,7 @@
 import XCTest
 @testable import Wire
 
-final class CountryCodeTableViewControllerTests: XCTestCase {
+final class CountryCodeTableViewControllerTests: BaseSnapshotTestCase {
 
     var sut: CountryCodeTableViewController!
 
