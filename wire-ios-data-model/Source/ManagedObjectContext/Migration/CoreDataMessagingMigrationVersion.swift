a08c27dad31cfa62398840c71aaed3e1b81f7277
François Benaiteau <francois.benaiteau@gmail.com>
2024-02-20 22:43:17 +0100

    regenerate version 2.113.0 test db

--- wire-ios-data-model/Source/ManagedObjectContext/Migration/CoreDataMessagingMigrationVersion.swift
+++ wire-ios-data-model/Source/ManagedObjectContext/Migration/CoreDataMessagingMigrationVersion.swift
@@ -29,7 +29,7 @@ enum CoreDataMessagingMigrationVersion: String, CaseIterable {
     // MARK: -
 
     // Note: add new versions here in first position!
-    case version2_114 = "zmessaging2.114.0"
+//    case version2_114 = "zmessaging2.114.0"
     case version2_113 = "zmessaging2.113.0"
     case version2_112 = "zmessaging2.112.0"
     case version2_111 = "zmessaging2.111.0"
@@ -67,10 +67,10 @@ enum CoreDataMessagingMigrationVersion: String, CaseIterable {
 
     var nextVersion: Self? {
         switch self {
-        case .version2_114:
-            return nil
+//        case .version2_114:
+//            return nil
         case .version2_113:
-            return .version2_114
+            return nil // .version2_114
         case .version2_112:
             return .version2_113
         case .version2_111:
