a52833cf4c1179b9d3b78912a3ca060dfc9f7bbc
KaterinaWire <katerina@wire.com>
2024-02-22 09:42:17 +0100

    roll back changes

--- wire-ios/Tests/Mocks/MockConversation+factory.swift
+++ wire-ios/Tests/Mocks/MockConversation+factory.swift
@@ -68,4 +68,14 @@ extension MockConversation {
 
         return mockConversation
     }
+
+    @objc (willAccessValueForKey:)
+    func willAccessValue(forKey: String) {
+
+    }
+
+    @objc (didAccessValueForKey:)
+    func didAccessValue(forKey: String) {
+
+    }
 }
