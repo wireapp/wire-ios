0b465b16e7ee33bde44365454beedc4f934da8a7
Markus Faßbender <markus.fassbender@wire.com>
2024-03-07 11:03:49 +0100

    fix warning return argument

--- wire-ios-transport/Source/Public/ZMUpdateEvent.swift
+++ wire-ios-transport/Source/Public/ZMUpdateEvent.swift
@@ -354,10 +354,12 @@ extension ZMUpdateEvent {
 
 extension ZMUpdateEvent {
     override open func isEqual(_ object: Any?) -> Bool {
-        guard let other = object as? ZMUpdateEvent else { return false }
-        return
-        (self.uuid == other.uuid) &&
-        (self.type == other.type) &&
-        (self.payload as NSDictionary).isEqual(to: other.payload)
+        guard let other = object as? ZMUpdateEvent else {
+            return false
+        }
+
+        return (self.uuid == other.uuid)
+            && (self.type == other.type)
+            && (self.payload as NSDictionary).isEqual(to: other.payload)
     }
 }
