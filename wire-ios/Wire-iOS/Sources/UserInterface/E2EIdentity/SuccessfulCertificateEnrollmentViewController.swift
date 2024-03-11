3f49fcc10a8efe864d946fe659e86dc0f5d453cd
Christoph Aldrian <christoph.aldrian@wire.com>
2024-03-11 17:27:56 +0100

    fix: remove not needed constraints

--- wire-ios/Wire-iOS/Sources/UserInterface/E2EIdentity/SuccessfulCertificateEnrollmentViewController.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/E2EIdentity/SuccessfulCertificateEnrollmentViewController.swift
@@ -16,9 +16,8 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-import Foundation
-import WireSyncEngine
 import SwiftUI
+import WireSyncEngine
 
 final class SuccessfulCertificateEnrollmentViewController: AuthenticationStepViewController {
     var certificateDetails: String = ""
@@ -50,13 +49,11 @@ final class SuccessfulCertificateEnrollmentViewController: AuthenticationStepVie
         return label
     }()
 
-    private let shieldImageView: UIImageView = {
-        let guestUserIconColor = SemanticColors.Icon.foregroundDefault
+    private let shieldImageView = {
         let imageView = UIImageView(image: .init(resource: .E_2_EI.Enrollment.certificateValid))
         imageView.accessibilityIdentifier = "shieldImageView"
         imageView.isAccessibilityElement = false
         imageView.contentMode = .scaleAspectFit
-
         return imageView
     }()
 
@@ -145,26 +142,24 @@ final class SuccessfulCertificateEnrollmentViewController: AuthenticationStepVie
         NSLayoutConstraint.activate([
             // title Label
             titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
-            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
+            view.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: 32),
 
             // shield image view
             shieldImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            shieldImageView.heightAnchor.constraint(equalToConstant: 64),
-            shieldImageView.widthAnchor.constraint(equalToConstant: 64),
 
             // confirmation button
             confirmationButton.heightAnchor.constraint(equalToConstant: 56),
 
             // stackView
             stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            view.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 24),
             stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
 
             // certificate details button
             certificateDetailsButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
-            certificateDetailsButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            view.trailingAnchor.constraint(equalTo: certificateDetailsButton.trailingAnchor, constant: 24),
             certificateDetailsButton.heightAnchor.constraint(equalToConstant: 32),
-            certificateDetailsButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -64)
+            view.bottomAnchor.constraint(equalTo: certificateDetailsButton.bottomAnchor, constant: 64)
         ])
     }
 
