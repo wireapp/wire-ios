b3b4b74667b1fb4dc1fcf2ca0362626adae5e378
KaterinaWire <katerina@wire.com>
2024-03-13 00:00:16 +0100

    clean up

--- wire-ios/Wire-iOS/Sources/UserInterface/Settings/DeviceManagement/ClientTableViewCellModel.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/Settings/DeviceManagement/ClientTableViewCellModel.swift
@@ -55,8 +55,5 @@ struct ClientTableViewCellModel {
         let mlsThumbPrint = userClient.mlsThumbPrint?.fingerprintStringWithSpaces ?? ""
         mlsThumbprintLabelText = mlsThumbPrint.isNonEmpty ? DeviceDetailsSection.Mls.thumbprint(mlsThumbPrint) : ""
         e2eIdentityStatus = userClient.e2eIdentityCertificate?.status
-        print(userClient.e2eIdentityCertificate)
-        print(e2eIdentityStatus)
-        print("/////")
     }
 }
