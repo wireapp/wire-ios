4b34e569c20db5aeeef8e26ec3d6ec7bf2429d61
Jacob Persson <7156+typfel@users.noreply.github.com>
2024-03-21 22:26:55 +0100

    test: assert that key material timestamp is updated when processing welcome message

--- wire-ios-request-strategy/Tests/Sources/Payloads/Processing/Helpers/MLSEventProcessorTests.swift
+++ wire-ios-request-strategy/Tests/Sources/Payloads/Processing/Helpers/MLSEventProcessorTests.swift
@@ -28,6 +28,7 @@ class MLSEventProcessorTests: MessagingTestBase {
     var mlsServiceMock: MockMLSServiceInterface!
     var conversationServiceMock: MockConversationServiceInterface!
     var oneOnOneResolverMock: MockOneOnOneResolverInterface!
+    var staleKeyMaterialDetectorMock: MockStaleMLSKeyDetectorProtocol!
 
     var conversation: ZMConversation!
     var qualifiedID: QualifiedID!
@@ -50,6 +51,9 @@ class MLSEventProcessorTests: MessagingTestBase {
         conversationServiceMock.syncConversationQualifiedID_MockMethod = { _ in }
         conversationServiceMock.syncConversationIfMissingQualifiedID_MockMethod = { _ in }
 
+        staleKeyMaterialDetectorMock = .init()
+        staleKeyMaterialDetectorMock.keyingMaterialUpdatedFor_MockMethod = { _ in }
+
         syncMOC.performGroupedBlockAndWait {
             self.syncMOC.mlsService = self.mlsServiceMock
             self.conversation = ZMConversation.insertNewObject(in: self.syncMOC)
@@ -59,7 +63,10 @@ class MLSEventProcessorTests: MessagingTestBase {
             self.conversation.messageProtocol = .mls
         }
 
-        sut = MLSEventProcessor(conversationService: conversationServiceMock)
+        sut = MLSEventProcessor(
+            conversationService: conversationServiceMock,
+            staleKeyMaterialDetector: staleKeyMaterialDetectorMock
+        )
     }
 
     override func tearDown() {
@@ -94,7 +101,7 @@ class MLSEventProcessorTests: MessagingTestBase {
         )
 
         // Then
-        // TODO jacob: assert that it updates key material date
+        XCTAssertEqual(staleKeyMaterialDetectorMock.keyingMaterialUpdatedFor_Invocations.count, 1)
         XCTAssertEqual(mlsServiceMock.uploadKeyPackagesIfNeeded_Invocations.count, 1)
         XCTAssertEqual(conversationServiceMock.syncConversationIfMissingQualifiedID_Invocations, [qualifiedID])
         XCTAssertTrue(oneOnOneResolverMock.resolveOneOnOneConversationWithIn_Invocations.isEmpty)
@@ -136,7 +143,7 @@ class MLSEventProcessorTests: MessagingTestBase {
         )
 
         // Then
-        // TODO jacob: assert that it updates key material date
+        XCTAssertEqual(staleKeyMaterialDetectorMock.keyingMaterialUpdatedFor_Invocations.count, 1)
         XCTAssertEqual(mlsServiceMock.uploadKeyPackagesIfNeeded_Invocations.count, 1)
         XCTAssertEqual(conversationServiceMock.syncConversationIfMissingQualifiedID_Invocations, [qualifiedID])
         XCTAssertEqual(oneOnOneResolverMock.resolveOneOnOneConversationWithIn_Invocations.count, 1)
