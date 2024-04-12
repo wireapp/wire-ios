930789f6ca7375945c4361f7eeb6e0c552d32aa8
Christoph Aldrian <caldrian@users.noreply.github.com>
2024-04-12 09:38:56 +0200

    fix: update file headers in wire-ios-cryptobox (#814)

--- wire-ios-cryptobox/WireCryptobox/cbox.h
+++ wire-ios-cryptobox/WireCryptobox/cbox.h
@@ -1,4 +1,6 @@
-// Copyright (C) 2015 Wire Swiss GmbH <support@wire.com>
+//
+// Wire
+// Copyright (C) 2024 Wire Swiss GmbH
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -7,11 +9,12 @@
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
 
 #ifndef CRYPTOBOX_H
 #define CRYPTOBOX_H
