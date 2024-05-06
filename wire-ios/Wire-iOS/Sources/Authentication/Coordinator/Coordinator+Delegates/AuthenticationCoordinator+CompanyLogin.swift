9416e87cf29ac7b1ca6d7419b6c50a2cc8fa7979
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2024-05-06 10:04:57 +0200

    lint

--- wire-ios/Wire-iOS/Sources/Authentication/Coordinator/Coordinator+Delegates/AuthenticationCoordinator+CompanyLogin.swift
+++ wire-ios/Wire-iOS/Sources/Authentication/Coordinator/Coordinator+Delegates/AuthenticationCoordinator+CompanyLogin.swift
@@ -16,9 +16,9 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
+import SwiftUI
 import UIKit
 import WireTransport
-import SwiftUI
 
 extension AuthenticationCoordinator: CompanyLoginControllerDelegate {
 
