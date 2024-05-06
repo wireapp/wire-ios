052a122415e7f8119387ac61fc65b561428cc5a1
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2024-05-06 15:38:16 +0200

    lint

--- wire-ios/Wire-iOS/Sources/Developer/DeveloperTools/DebugActions/DeveloperDebugActionsViewModel.swift
+++ wire-ios/Wire-iOS/Sources/Developer/DeveloperTools/DebugActions/DeveloperDebugActionsViewModel.swift
@@ -43,7 +43,7 @@ final class DeveloperDebugActionsViewModel: ObservableObject {
             .init(title: "Break next quick sync", action: breakNextQuickSync),
             .init(title: "Update Conversation to mixed protocol", action: updateConversationProtocolToMixed),
             .init(title: "Update Conversation to MLS protocol", action: updateConversationProtocolToMLS),
-            .init(title: "Update MLS migration status", action: updateMLSMigrationStatus),
+            .init(title: "Update MLS migration status", action: updateMLSMigrationStatus)
         ]
     }
 
