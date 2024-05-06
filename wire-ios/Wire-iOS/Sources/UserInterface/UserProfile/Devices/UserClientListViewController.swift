530aa6e07b85ce1e102f7f861fa2497626f92da8
KaterinaWire <katerina@wire.com>
2024-05-06 17:01:05 +0200

    use ProfileDeviceDetailsView for other user device details

--- wire-ios/Wire-iOS/Sources/UserInterface/UserProfile/Devices/UserClientListViewController.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/UserProfile/Devices/UserClientListViewController.swift
@@ -173,7 +173,7 @@ final class UserClientListViewController: UIViewController,
             client: client,
             contextProvider: contextProvider
         )
-        let detailsViewController = DeviceInfoViewController(rootView: DeviceDetailsView(viewModel: viewModel))
+        let detailsViewController = DeviceInfoViewController(rootView: ProfileDeviceDetailsView(viewModel: viewModel))
         navigationController.pushViewController(detailsViewController, animated: true)
     }
 
