a98d1637a4c6d2da3b128dce80076ad4347c489a
Christoph Aldrian <christoph.aldrian@wire.com>
2024-05-15 13:52:59 +0200

    cleanup

--- wire-ios/Wire-iOS UnitTests/UserInterface/PermissionDeniedHint/IsNotificationAuthorizationStatusDeniedUseCase/ShouldPresentNotificationPermissionHintUseCaseTests.swift
+++ wire-ios/Wire-iOS UnitTests/UserInterface/PermissionDeniedHint/IsNotificationAuthorizationStatusDeniedUseCase/ShouldPresentNotificationPermissionHintUseCaseTests.swift
@@ -16,8 +16,8 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-import WireRequestStrategySupport // TODO: should be WireUtilitiesSupport
 import WireTesting
+import WireUtilitiesSupport
 import XCTest
 
 @testable import Wire
