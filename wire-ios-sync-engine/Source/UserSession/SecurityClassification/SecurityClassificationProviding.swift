16b90839c2b55c28359dbf25e4292f5175dbacc4
Markus Faßbender <markus.fassbender@wire.com>
2024-05-16 15:17:02 +0200

    chore: remove `AppDelegate+Network` and use protocol (#1424)

--- wire-ios-sync-engine/Source/UserSession/SecurityClassification/SecurityClassificationProviding.swift
+++ wire-ios-sync-engine/Source/UserSession/SecurityClassification/SecurityClassificationProviding.swift
@@ -16,6 +16,7 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
+// sourcery: AutoMockable
 public protocol SecurityClassificationProviding {
 
     func classification(
