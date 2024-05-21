8e5357e31961c7032f08db380b1f6862186352e0
Markus Faßbender <markus.fassbender@wire.com>
2024-05-21 14:50:40 +0200

    implement WireAPIPlugin

--- WireAPI/Package.swift
+++ WireAPI/Package.swift
@@ -32,12 +32,29 @@ let package = Package(
             name: "WireAPI"
         ),
         .target(
-            name: "WireAPISupport"
+            name: "WireAPISupport",
+            dependencies: [
+                "WireAPI"
+            ],
+            plugins: [
+                .plugin(name: "WireAPIPlugin")
+            ]
+        ),
+        .binaryTarget(
+            name: "sourcery2",
+            url: "https://github.com/krzysztofzablocki/Sourcery/releases/download/2.1.7/sourcery-2.1.7.artifactbundle.zip",
+            checksum: "b54ff217c78cada3f70d3c11301da03a199bec87426615b8144fc9abd13ac93b"
+        ),
+        .plugin(
+            name: "WireAPIPlugin",
+            capability: .buildTool(),
+            dependencies: ["sourcery2"]
         ),
         .testTarget(
             name: "WireAPITests",
             dependencies: [
                 "WireAPI",
+                "WireAPISupport",
                 .product(
                     name: "SnapshotTesting",
                     package: "swift-snapshot-testing"
