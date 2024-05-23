e391ca67590682333ca63dcee30dcf219cafde6e
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2024-05-23 16:03:59 +0200

    chore: groom

--- wire-ios/Wire-iOS/Sources/UserInterface/Conversation Options/CreateSecureGuestLink/CreateSecureGuestLinkViewController.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/Conversation Options/CreateSecureGuestLink/CreateSecureGuestLinkViewController.swift
@@ -206,8 +206,22 @@ class CreateSecureGuestLinkViewController: UIViewController, CreatePasswordSecur
         ]
 
         // Keyboard notifications
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(
+                keyboardWillShow
+            ),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(
+                keyboardWillHide
+            ),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
     }
 
     override func viewWillAppear(_ animated: Bool) {
