e061d46f6eea6421efa182ffa37171942f80e606
Markus Faßbender <markus.fassbender@wire.com>
2024-05-24 09:20:19 +0200

    format string

--- SourceryPlugin/Plugins/SourceryPlugin/SourceryPlugin.swift
+++ SourceryPlugin/Plugins/SourceryPlugin/SourceryPlugin.swift
@@ -66,14 +66,13 @@ extension SourceryPlugin: BuildToolPlugin {
             .first
 
         guard let configuration else {
-            Diagnostics.error(
-"""
-No configurations found for target \(target.name). If you would like to generate sources for this \
-target include a `\(Constant.configFileName)` either in:
-1. root of package
-2. target directory
-3. target directory subfolder named 'Sourcery'
-"""
+            Diagnostics.error("""
+                No configurations found for target \(target.name). If you would like to generate sources for this \
+                target include a `\(Constant.configFileName)` either in:
+                1. root of package
+                2. target directory
+                3. target directory subfolder named 'Sourcery'
+                """
             )
             return []
         }
