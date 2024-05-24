1f183d70ed6f7ea45a46f0ee745573bfdcd54b7b
Markus Faßbender <markus.fassbender@wire.com>
2024-05-24 16:32:38 +0200

    chore: unify sourcery execution [WPB-9377] (#1455)

--- scripts/Package.swift
+++ scripts/Package.swift
@@ -7,9 +7,11 @@ import PackageDescription
 // 
 // 1. Go to GitHub repository, the latest release.
 // 2. Copy url of <*.artifactbundle.zip> and enter here as 'url'.
-// 3. Download <*.artifactbundle.zip>, open terminal, go to the folder like '~/Downloads'.
+// 3. Download <*.artifactbundle.zip>, open terminal, go to the folder like '~/Downloads/'.
 // 4. Run 'swift package compute-checksum <*.artifactbundle.zip>', copy the checksum and enter here as 'checksum'.
 // 5. Save this file.
+// 6. Go to repository './scripts/'.
+// 7. Run `swift package update` to fetch new artifacts.
 
 let package = Package(
     name: "Scripts",
@@ -23,11 +25,6 @@ let package = Package(
           name: "swiftgen",
           url: "https://github.com/SwiftGen/SwiftGen/releases/download/6.6.2/swiftgen-6.6.2.artifactbundle.zip",
           checksum: "7586363e24edcf18c2da3ef90f379e9559c1453f48ef5e8fbc0b818fbbc3a045"
-        ),
-        .binaryTarget(
-            name: "sourcery",
-            url: "https://github.com/krzysztofzablocki/Sourcery/releases/download/2.1.7/sourcery-2.1.7.artifactbundle.zip",
-            checksum: "b54ff217c78cada3f70d3c11301da03a199bec87426615b8144fc9abd13ac93b"
         )
     ]
 )
