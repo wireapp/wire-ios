efd479a4e2378b30a09cd592c67e147a452bdf8b
François Benaiteau <francois.benaiteau@wire.com>
2024-05-29 18:10:15 +0200

    add tests for ConnectionsRepository

---
+++ wire-ios-sync-engine/Tests/Source/Repositories/ConnectionsRepositoryTests.swift
@@ -0,0 +1,100 @@
+//
+// Wire
+// Copyright (C) 2024 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+@testable import WireAPI
+import WireAPISupport
+import WireDataModelSupport
+@testable import WireSyncEngine
+import WireSyncEngineSupport
+import XCTest
+
+final class ConnectionsRepositoryTests: XCTestCase {
+
+    var sut: ConnectionsRepository!
+    var connectionsAPI: MockConnectionsAPI!
+
+    var stack: CoreDataStack!
+    let coreDataStackHelper = CoreDataStackHelper()
+    let modelHelper = ModelHelper()
+
+    var context: NSManagedObjectContext {
+        stack.syncContext
+    }
+
+    override func setUp() async throws {
+        try await super.setUp()
+        stack = try await coreDataStackHelper.createStack()
+        connectionsAPI = MockConnectionsAPI()
+        sut = ConnectionsRepository(connectionsAPI: connectionsAPI,
+                                    context: context)
+    }
+
+    override func tearDown() async throws {
+        stack = nil
+        connectionsAPI = nil
+        sut = nil
+        try coreDataStackHelper.cleanupDirectory()
+        try await super.tearDown()
+    }
+
+    // MARK: - Tests
+
+    func testPullConnections_GivenConnectionDoesNotExist() async throws {
+        // Mock
+        let connection = WireAPI.Connection(senderId: Scaffolding.member1ID.uuid,
+                                            receiverId: Scaffolding.member2ID.uuid,
+                                            receiverQualifiedId: Scaffolding.member2ID,
+                                            conversationId: Scaffolding.conversationID.uuid,
+                                            qualifiedConversationId: Scaffolding.conversationID,
+                                            lastUpdate: Scaffolding.lastUpdate,
+                                            status: Scaffolding.connectionStatus)
+
+        connectionsAPI.getConnections_MockValue = .init(fetchPage: { _ in
+            return WireAPI.PayloadPager.Page(element: [connection], hasMore: false, nextStart: "first")
+        })
+
+        // When
+        try await sut.pullConnections()
+
+        // Then
+        try await context.perform { [context] in
+            // There is a team in the database.
+            let storedConnection = try XCTUnwrap(ZMConnection.fetch(userID: Scaffolding.member2ID.uuid, domain: Scaffolding.member2ID.domain, in: context))
+
+            XCTAssertFalse(storedConnection.needsToBeUpdatedFromBackend)
+            XCTAssertEqual(storedConnection.lastUpdateDateInGMT, connection.lastUpdate)
+
+            XCTAssertEqual(storedConnection.to.remoteIdentifier, connection.receiverId)
+            XCTAssertEqual(storedConnection.to.domain, connection.qualifiedConversationId?.domain)
+            XCTAssertEqual(storedConnection.status, ZMConnectionStatus.accepted)
+
+            let relatedConversation = try XCTUnwrap(storedConnection.to.oneOnOneConversation)
+            XCTAssertEqual(relatedConversation.remoteIdentifier, connection.qualifiedConversationId?.uuid)
+            XCTAssertEqual(relatedConversation.domain, connection.qualifiedConversationId?.domain)
+
+        }
+    }
+}
+
+private enum Scaffolding {
+    static let member1ID = WireAPI.QualifiedID(uuid: UUID(), domain: String.randomDomain())
+    static let conversationID = WireAPI.QualifiedID(uuid: UUID(), domain: String.randomDomain())
+    static let member2ID = WireAPI.QualifiedID(uuid: UUID(), domain: String.randomDomain())
+    static let lastUpdate = Date()
+    static let connectionStatus = ConnectionStatus.accepted
+}
