e900f49e60ef4a5ab0d0486169f4d7f4e462df3b
François Benaiteau <francois.benaiteau@wire.com>
2024-05-29 21:45:34 +0200

    add documentation and refactor

--- wire-ios-sync-engine/Source/Repositories/ConnectionsRepositiory.swift
+++ wire-ios-sync-engine/Source/Repositories/ConnectionsRepositiory.swift
@@ -45,6 +45,8 @@ final class ConnectionsRepository: ConnectionsRepositoryProtocol {
         self.context = context
     }
 
+    /// Retrieve from backend and store connections location
+
     public func pullConnections() async throws {
         let connectionsPager = try await connectionsAPI.getConnections()
 
@@ -59,40 +61,69 @@ final class ConnectionsRepository: ConnectionsRepositoryProtocol {
         }
     }
 
-    private func storeConnection(_ connection: Connection) async throws {
+    /// Save connection and related objects to local storage.
+    /// - Parameter connectionPayload: connection object from WireAPI
+
+    private func storeConnection(_ connectionPayload: Connection) async throws {
         try await context.perform { [self] in
 
-            guard let userID = connection.receiverId ?? connection.receiverQualifiedId?.uuid else {
-                throw ConnectionsRepositoryError.missingReceiverId
-            }
+            let connection = try storedConnection(from: connectionPayload)
 
-            let storedConnection = ZMConnection.fetchOrCreate(
-                userID: userID,
-                domain: connection.receiverQualifiedId?.domain,
-                in: context
-            )
+            let conversation = try storedConversation(from: connectionPayload, with: connection)
 
-            guard let conversationID = connection.conversationId ?? connection.qualifiedConversationId?.uuid else {
-                throw ConnectionsRepositoryError.missingConversationId
-            }
-
-            let conversation = ZMConversation.fetchOrCreate(
-                with: conversationID,
-                domain: connection.qualifiedConversationId?.domain,
-                in: context
-            )
-
-            conversation.needsToBeUpdatedFromBackend = true
-            conversation.lastModifiedDate = connection.lastUpdate
-            conversation.addParticipantAndUpdateConversationState(user: storedConnection.to, role: nil)
-
-            storedConnection.to.oneOnOneConversation = conversation
-            storedConnection.status = status(from: connection.status)
-            storedConnection.lastUpdateDateInGMT = connection.lastUpdate
+            connection.to.oneOnOneConversation = conversation
 
             try context.save()
         }
     }
+
+    /// Create or update conversation related to the connection's sender
+    /// - Parameters:
+    ///   - connection: connection payload from WireAPI
+    ///   - storedConnection: ZMConnection object stored locally
+    /// - Returns: conversation object stored locally
+
+    private func storedConversation(from connection: Connection, with storedConnection: ZMConnection) throws -> ZMConversation {
+
+        guard let conversationID = connection.conversationId ?? connection.qualifiedConversationId?.uuid else {
+            throw ConnectionsRepositoryError.missingConversationId
+        }
+
+        let conversation = ZMConversation.fetchOrCreate(
+            with: conversationID,
+            domain: connection.qualifiedConversationId?.domain,
+            in: context
+        )
+
+        conversation.needsToBeUpdatedFromBackend = true
+        conversation.lastModifiedDate = connection.lastUpdate
+        conversation.addParticipantAndUpdateConversationState(user: storedConnection.to, role: nil)
+        return conversation
+    }
+
+    /// Create or update  connection locally related to the connection's sender
+    /// - Parameter connection: connection payload from WireAPI
+    /// - Returns: connection object stored locally
+
+    private func storedConnection(from connection: Connection) throws -> ZMConnection {
+        guard let userID = connection.receiverId ?? connection.receiverQualifiedId?.uuid else {
+            throw ConnectionsRepositoryError.missingReceiverId
+        }
+
+        let storedConnection = ZMConnection.fetchOrCreate(
+            userID: userID,
+            domain: connection.receiverQualifiedId?.domain,
+            in: context
+        )
+
+        storedConnection.status = status(from: connection.status)
+        storedConnection.lastUpdateDateInGMT = connection.lastUpdate
+        return storedConnection
+    }
+
+    /// Converts ConnectionStatus to stored connectionStatus
+    /// - Parameter connectionStatus: WireAPI's ConnectionStatus
+    /// - Returns: stored ConnectionStatus
 
     private func status(from connectionStatus: ConnectionStatus) -> ZMConnectionStatus {
         switch connectionStatus {
