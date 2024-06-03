f2ab0ecf4009b93ed5bcb278364446c576c8131a
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2024-06-03 14:02:17 +0200

    lint

--- WireAPI/Tests/WireAPITests/EventDecoding/FederationEventDecodingTests.swift
+++ WireAPI/Tests/WireAPITests/EventDecoding/FederationEventDecodingTests.swift
@@ -16,8 +16,8 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-import XCTest
 @testable import WireAPI
+import XCTest
 
 final class FederationEventDecodingTests: XCTestCase {
 
