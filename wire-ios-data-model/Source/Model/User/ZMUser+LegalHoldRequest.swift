a75c6fe209578131aeb7203b1f7b4989167472e9
Markus Faßbender <markus.fassbender@wire.com>
2024-06-04 10:01:31 +0200

    chore: rename `SelfUserType` (#1526)

--- wire-ios-data-model/Source/Model/User/ZMUser+LegalHoldRequest.swift
+++ wire-ios-data-model/Source/Model/User/ZMUser+LegalHoldRequest.swift
@@ -21,7 +21,7 @@ import WireCryptobox
 
 private let log = ZMSLog(tag: "UserClient")
 
-public typealias SelfUserType = UserType & EditableUserType & SelfLegalHoldSubject
+public typealias SelfUserLegalHoldable = UserType & EditableUserType & SelfLegalHoldSubject
 
 /**
  * A protocol for objects that provide the legal hold status for the self user.
