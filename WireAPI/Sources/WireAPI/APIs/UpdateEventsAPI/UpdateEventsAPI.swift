a8386dc5c4ddfdbc6df7392a0a4f8a0c9cc6f728
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2024-06-04 11:54:27 +0200

    Apply suggestions from code review
    Co-authored-by: François Benaiteau <netbe@users.noreply.github.com>

--- WireAPI/Sources/WireAPI/APIs/UpdateEventsAPI/UpdateEventsAPI.swift
+++ WireAPI/Sources/WireAPI/APIs/UpdateEventsAPI/UpdateEventsAPI.swift
@@ -20,12 +20,13 @@ import Foundation
 
 // sourcery: AutoMockable
 /// An API access object for endpoints concerning update events.
+
 public protocol UpdateEventsAPI {
 
     /// Get the last (most recent) update event for the self client.
     ///
     /// - Parameter selfClientID: The id of the self client.
-    /// - Returns: An update evelope containing the last update event.
+    /// - Returns: An update envelope containing the last update event.
 
     func getLastUpdateEvent(selfClientID: String) async throws -> UpdateEventEnvelope
 
