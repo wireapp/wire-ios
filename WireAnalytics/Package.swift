c947641c9fc2cf1e81eb40227852299694d1b05a
Markus Faßbender <markus.fassbender@wire.com>
2024-06-07 14:16:05 +0200

    add wireanalytics package

---
+++ WireAnalytics/Package.swift
@@ -0,0 +1,50 @@
+// swift-tools-version: 5.10
+
+import Foundation
+import PackageDescription
+
+let package = Package(
+    name: "WireAnalytics",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "WireAnalytics",
+            targets: ["WireAnalytics"]
+        ),
+        .library(
+            name: "WireAnalyticsTracker",
+            targets: ["WireAnalyticsTracker"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/DataDog/dd-sdk-ios.git", exact: "2.12.0")
+    ],
+    targets: [
+        .target(
+            name: "WireAnalytics",
+            dependencies: resolveWireAnalyticsDependencies()
+        ),
+        .target(
+            name: "WireAnalyticsTracker",
+            dependencies: [
+                .product(name: "DatadogCore", package: "dd-sdk-ios"),
+                .product(name: "DatadogCrashReporting", package: "dd-sdk-ios"),
+                .product(name: "DatadogLogs", package: "dd-sdk-ios"),
+                .product(name: "DatadogRUM", package: "dd-sdk-ios"),
+                .product(name: "DatadogTrace", package: "dd-sdk-ios")
+            ]
+        )
+    ]
+)
+
+func resolveWireAnalyticsDependencies() -> [Target.Dependency] {
+    // if ProcessInfo.processInfo.environment["DATADOG_IMPORT"] != nil {
+    if false {
+        return ["WireAnalyticsTracker"]
+    } else {
+        return []
+    }
+}
---
+++ WireAnalytics/Sources/WireAnalyticsTracker/DatadogTrackerBuilder.swift
@@ -0,0 +1,54 @@
+//
+// Wire
+// Copyright (C) 2024 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import DatadogLogs
+import Foundation
+
+public struct DatadogTrackerBuilder {
+
+    private let bundle: Bundle
+    // private let environment: BackendEnvironment
+    private let level: LogLevel
+
+    public init(
+        bundle: Bundle = .main,// .wireCommonComponents,
+        // environment: BackendEnvironment = .shared,
+        level: LogLevel = .debug
+    ) {
+        self.bundle = bundle
+        // self.environment = environment
+        self.level = level
+    }
+
+    public func build() -> DatadogTracker? {
+//        guard
+//            let appID = bundle.infoForKey("DatadogAppId"),
+//            let clientToken = bundle.infoForKey("DatadogClientToken")
+//        else {
+//            assertionFailure("missing Datadog appID and clientToken - logging disabled")
+//            return nil
+//        }
+
+        return DatadogTracker(
+            appID: "",
+            clientToken: "",
+            // environment: environment,
+            level: level
+        )
+    }
+}
