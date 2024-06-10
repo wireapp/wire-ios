4189798e2c132a1e2dad419a8583a2997b6a7554
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2024-06-10 09:45:09 +0200

    use separate objects to decode feature configs

--- WireAPI/Sources/WireAPI/APIs/UpdateEventsAPI/Event decoding/UpdateEvent+FeatureConfig.swift
+++ WireAPI/Sources/WireAPI/APIs/UpdateEventsAPI/Event decoding/UpdateEvent+FeatureConfig.swift
@@ -35,52 +35,52 @@ extension UpdateEvent {
 
             switch featureName {
             case "appLock":
-                let config = try container.decodeAppLockConfig()
+                let config = try AppLockFeatureConfigDecoder().decode(from: container)
                 let event = FeatureConfigUpdateEvent(featureConfig: .appLock(config))
                 self = .featureConfig(.update(event))
 
             case "classifiedDomains":
-                let config = try container.decodeClassifiedDomainsConfig()
+                let config = try ClassifiedDomainsFeatureConfigDecoder().decode(from: container)
                 let event = FeatureConfigUpdateEvent(featureConfig: .classifiedDomains(config))
                 self = .featureConfig(.update(event))
 
             case "conferenceCalling":
-                let config = try container.decodeConferenceCallingConfig()
+                let config = try ConferenceCallingFeatureConfigDecoder().decode(from: container)
                 let event = FeatureConfigUpdateEvent(featureConfig: .conferenceCalling(config))
                 self = .featureConfig(.update(event))
 
             case "conversationGuestLinks":
-                let config = try container.decodeConversationGuestLinksConfig()
+                let config = try ConversationGuestLinksFeatureConfigDecoder().decode(from: container)
                 let event = FeatureConfigUpdateEvent(featureConfig: .conversationGuestLinks(config))
                 self = .featureConfig(.update(event))
 
             case "digitalSignature":
-                let config = try container.decodeDigitalSignatureConfig()
+                let config = try DigitalSignatureFeatureConfigDecoder().decode(from: container)
                 let event = FeatureConfigUpdateEvent(featureConfig: .digitalSignature(config))
                 self = .featureConfig(.update(event))
 
             case "e2ei":
-                let config = try container.decodeEndToEndIdentityConfig()
+                let config = try EndToEndIdentityFeatureConfigDecoder().decode(from: container)
                 let event = FeatureConfigUpdateEvent(featureConfig: .endToEndIdentity(config))
                 self = .featureConfig(.update(event))
 
             case "fileSharing":
-                let config = try container.decodeFileSharingConfig()
+                let config = try FileSharingFeatureConfigDecoder().decode(from: container)
                 let event = FeatureConfigUpdateEvent(featureConfig: .fileSharing(config))
                 self = .featureConfig(.update(event))
 
             case "mls":
-                let config = try container.decodeMLSConfig()
+                let config = try MLSFeatureConfigDecoder().decode(from: container)
                 let event = FeatureConfigUpdateEvent(featureConfig: .mls(config))
                 self = .featureConfig(.update(event))
 
             case "mlsMigration":
-                let config = try container.decodeMLSMigrationConfig()
+                let config = try MLSMigrationFeatureConfigDecoder().decode(from: container)
                 let event = FeatureConfigUpdateEvent(featureConfig: .mlsMigration(config))
                 self = .featureConfig(.update(event))
 
             case "selfDeletingMessages":
-                let config = try container.decodeSelfDeletingMessagesConfig()
+                let config = try SelfDeletingMessagesFeatureConfigDecoder().decode(from: container)
                 let event = FeatureConfigUpdateEvent(featureConfig: .selfDeletingMessages(config))
                 self = .featureConfig(.update(event))
 
@@ -89,244 +89,6 @@ extension UpdateEvent {
                 self = .featureConfig(.update(event))
             }
         }
-    }
-
-}
-
-private enum FeatureConfigEventCodingKeys: String, CodingKey {
-
-    case name
-    case payload = "data"
-
-}
-
-private struct FeatureWithoutConfig: Decodable {
-
-    let status: FeatureConfigStatus
-
-}
-
-private struct FeatureWithConfig<Config: Decodable>: Decodable {
-
-    let status: FeatureConfigStatus
-    let config: Config
-
-}
-
-// MARK: - App lock update
-
-private extension KeyedDecodingContainer<FeatureConfigEventCodingKeys> {
-
-    func decodeAppLockConfig() throws -> AppLockFeatureConfig {
-        let payload = try decode(
-            FeatureWithConfig<AppLockConfig>.self,
-            forKey: .payload
-        )
-
-        return AppLockFeatureConfig(
-            status: payload.status,
-            isMandatory: payload.config.enforceAppLock,
-            inactivityTimeoutInSeconds: payload.config.inactivityTimeoutSecs
-        )
-    }
-
-    private struct AppLockConfig: Decodable {
-
-        let enforceAppLock: Bool
-        let inactivityTimeoutSecs: UInt
-
-    }
-
-}
-
-// MARK: - Classified domains update
-
-private extension KeyedDecodingContainer<FeatureConfigEventCodingKeys> {
-
-    func decodeClassifiedDomainsConfig() throws -> ClassifiedDomainsFeatureConfig {
-        let payload = try decode(
-            FeatureWithConfig<ClassifiedDomainsConfig>.self,
-            forKey: .payload
-        )
-
-        return ClassifiedDomainsFeatureConfig(
-            status: payload.status,
-            domains: payload.config.domains
-        )
-    }
-
-    private struct ClassifiedDomainsConfig: Decodable {
-
-        let domains: Set<String>
-
-    }
-
-}
-
-// MARK: - Conference calling update
-
-private extension KeyedDecodingContainer<FeatureConfigEventCodingKeys> {
-
-    func decodeConferenceCallingConfig() throws -> ConferenceCallingFeatureConfig {
-        let payload = try decode(
-            FeatureWithoutConfig.self,
-            forKey: .payload
-        )
-
-        return ConferenceCallingFeatureConfig(status: payload.status)
-    }
-
-}
-
-// MARK: - Conversation guest links update
-
-private extension KeyedDecodingContainer<FeatureConfigEventCodingKeys> {
-
-    func decodeConversationGuestLinksConfig() throws -> ConversationGuestLinksFeatureConfig {
-        let payload = try decode(
-            FeatureWithoutConfig.self,
-            forKey: .payload
-        )
-
-        return ConversationGuestLinksFeatureConfig(status: payload.status)
-    }
-
-}
-
-// MARK: - Digital signature update
-
-private extension KeyedDecodingContainer<FeatureConfigEventCodingKeys> {
-
-    func decodeDigitalSignatureConfig() throws -> DigitalSignatureFeatureConfig {
-        let payload = try decode(
-            FeatureWithoutConfig.self,
-            forKey: .payload
-        )
-
-        return DigitalSignatureFeatureConfig(status: payload.status)
-    }
-
-}
-
-// MARK: - End to end identity update
-
-private extension KeyedDecodingContainer<FeatureConfigEventCodingKeys> {
-
-    func decodeEndToEndIdentityConfig() throws -> EndToEndIdentityFeatureConfig {
-        let payload = try decode(
-            FeatureWithConfig<EndToEndIdentityConfig>.self,
-            forKey: .payload
-        )
-
-        return EndToEndIdentityFeatureConfig(
-            status: payload.status,
-            acmeDiscoveryURL: payload.config.acmeDiscoveryUrl,
-            verificationExpiration: payload.config.verificationExpiration
-        )
-    }
-
-    private struct EndToEndIdentityConfig: Decodable {
-
-        let acmeDiscoveryUrl: String?
-        let verificationExpiration: UInt
-
-    }
-
-}
-
-// MARK: - File sharing update
-
-private extension KeyedDecodingContainer<FeatureConfigEventCodingKeys> {
-
-    func decodeFileSharingConfig() throws -> FileSharingFeatureConfig {
-        let payload = try decode(
-            FeatureWithoutConfig.self,
-            forKey: .payload
-        )
-
-        return FileSharingFeatureConfig(status: payload.status)
-    }
-
-}
-
-// MARK: - MLS update
-
-private extension KeyedDecodingContainer<FeatureConfigEventCodingKeys> {
-
-    func decodeMLSConfig() throws -> MLSFeatureConfig {
-        let payload = try decode(
-            FeatureWithConfig<MLSConfig>.self,
-            forKey: .payload
-        )
-
-        return MLSFeatureConfig(
-            status: payload.status,
-            protocolToggleUsers: payload.config.protocolToggleUsers,
-            defaultProtocol: payload.config.defaultProtocol,
-            allowedCipherSuites: payload.config.allowedCipherSuites,
-            defaultCipherSuite: payload.config.defaultCipherSuite,
-            supportedProtocols: payload.config.supportedProtocols
-        )
-    }
-
-    private struct MLSConfig: Decodable {
-
-        let protocolToggleUsers: Set<UUID>
-        let defaultProtocol: MessageProtocol
-        let allowedCipherSuites: [MLSCipherSuite]
-        let defaultCipherSuite: MLSCipherSuite
-        let supportedProtocols: Set<MessageProtocol>
-
-    }
-
-}
-
-// MARK: - MLS migration update
-
-private extension KeyedDecodingContainer<FeatureConfigEventCodingKeys> {
-
-    func decodeMLSMigrationConfig() throws -> MLSMigrationFeatureConfig {
-        let payload = try decode(
-            FeatureWithConfig<MLSMigrationConfig>.self,
-            forKey: .payload
-        )
-
-        return MLSMigrationFeatureConfig(
-            status: payload.status,
-            startTime: payload.config.startTime,
-            finaliseRegardlessAfter: payload.config.finaliseRegardlessAfter
-        )
-    }
-
-    private struct MLSMigrationConfig: Decodable {
-
-        let startTime: Date?
-        let finaliseRegardlessAfter: Date?
-
-    }
-
-}
-
-// MARK: - Self deleting msessages update
-
-private extension KeyedDecodingContainer<FeatureConfigEventCodingKeys> {
-
-    func decodeSelfDeletingMessagesConfig() throws -> SelfDeletingMessagesFeatureConfig {
-        let payload = try decode(
-            FeatureWithConfig<SelfDeletingMessagesConfig>.self,
-            forKey: .payload
-        )
-
-        return SelfDeletingMessagesFeatureConfig(
-            status: payload.status,
-            enforcedTimeoutSeconds: payload.config.enforcedTimeoutSeconds
-        )
-    }
-
-    private struct SelfDeletingMessagesConfig: Decodable {
-
-        let enforcedTimeoutSeconds: UInt
-
     }
 
 }
