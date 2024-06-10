911fd78735edaba72269fce9766de89ce6690991
Markus Faßbender <markus.fassbender@wire.com>
2024-06-10 12:01:42 +0200

    update log

--- wire-ios/Wire-iOS/Sources/AppDelegate.swift
+++ wire-ios/Wire-iOS/Sources/AppDelegate.swift
@@ -113,7 +113,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         WireLogger.appDelegate.info("application:willFinishLaunchingWithOptions \(String(describing: launchOptions)) (applicationState = \(application.applicationState.rawValue))")
 
         WireAnalytics.shared?.enable()
-        WireAnalytics.shared?.info("start app", attributes: nil)
+        WireAnalytics.shared?.info("start app")
 
         // Initial log line to indicate the client version and build
         WireLogger.appDelegate.info(
