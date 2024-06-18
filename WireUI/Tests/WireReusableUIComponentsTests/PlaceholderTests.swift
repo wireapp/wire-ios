9859b3b2a6f00a450b5b05b59f12f20e82b64d53
Christoph Aldrian <christoph.aldrian@wire.com>
2024-06-18 12:49:29 +0200

    fix test setup

--- WireUI/Tests/WireReusableUIComponentsTests/PlaceholderTests.swift
+++ WireUI/Tests/WireReusableUIComponentsTests/PlaceholderTests.swift
@@ -15,3 +15,10 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
+
+import XCTest
+
+final class PlaceholderTests: XCTestCase {
+
+    func testNothing() {}
+}
