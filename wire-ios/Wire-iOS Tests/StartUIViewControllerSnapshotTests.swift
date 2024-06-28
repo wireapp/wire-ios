b32390e25003d7fd6ed88b296893232e49ecc55e
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2024-06-28 10:22:47 +0200

    refactor: Update StartUIViewControllerSnapshotTests

--- wire-ios/Wire-iOS Tests/StartUIViewControllerSnapshotTests.swift
+++ wire-ios/Wire-iOS Tests/StartUIViewControllerSnapshotTests.swift
@@ -49,45 +49,59 @@ final class MockAddressBookHelper: NSObject, AddressBookHelperProtocol {
 
 final class StartUIViewControllerSnapshotTests: CoreDataSnapshotTestCase {
 
-    var sut: StartUIViewController!
-    var mockAddressBookHelper: MockAddressBookHelper!
-    var userSession: UserSessionMock!
+    // MARK: - Properties
+
+    private var snapshotHelper: SnapshotHelper!
+    private var sut: StartUIViewController!
+    private var mockAddressBookHelper: MockAddressBookHelper!
+    private var userSession: UserSessionMock!
+
+    // MARK: - setUp
 
     override func setUp() {
         super.setUp()
+        snapshotHelper = SnapshotHelper()
         mockAddressBookHelper = MockAddressBookHelper()
         SelfUser.provider = selfUserProvider
         userSession = UserSessionMock()
     }
 
+    // MARK: - tearDown
+
     override func tearDown() {
+        snapshotHelper = nil
         sut = nil
         mockAddressBookHelper = nil
         SelfUser.provider = nil
         userSession = nil
+
         super.tearDown()
     }
+
+    // MARK: - Helper Method
 
     func setupSut() {
         sut = StartUIViewController(addressBookHelperType: MockAddressBookHelper.self, userSession: userSession)
         sut.view.backgroundColor = SemanticColors.View.backgroundDefault
-        sut.overrideUserInterfaceStyle = .dark
 
         // Set the size for the SUT to match iPhone 14 dimensions
         let screenSize = CGSize(width: 390, height: 844)
         sut.view.frame = CGRect(origin: .zero, size: screenSize)
     }
 
+    // MARK: - Snapshot Tests
+
     func testForWrappedInNavigationViewController() {
         nonTeamTest {
             setupSut()
 
             let navigationController = UIViewController().wrapInNavigationController(navigationControllerClass: NavigationController.self)
-            navigationController.overrideUserInterfaceStyle = .dark
 
             navigationController.pushViewController(sut, animated: false)
 
-            verify(matching: sut.view)
+            snapshotHelper
+                .withUserInterfaceStyle(.dark)
+                .verify(matching: sut.view)
         }
     }
 
@@ -95,7 +109,9 @@ final class StartUIViewControllerSnapshotTests: CoreDataSnapshotTestCase {
         nonTeamTest {
             setupSut()
 
-            verify(matching: sut.view)
+            snapshotHelper
+                .withUserInterfaceStyle(.dark)
+                .verify(matching: sut.view)
         }
     }
 
@@ -104,7 +120,9 @@ final class StartUIViewControllerSnapshotTests: CoreDataSnapshotTestCase {
         teamTest {
             setupSut()
 
-            verify(matching: sut.view)
+            snapshotHelper
+                .withUserInterfaceStyle(.dark)
+                .verify(matching: sut.view)
         }
     }
 
@@ -115,7 +133,9 @@ final class StartUIViewControllerSnapshotTests: CoreDataSnapshotTestCase {
 
             setupSut()
 
-            verify(matching: sut.view)
+            snapshotHelper
+                .withUserInterfaceStyle(.dark)
+                .verify(matching: sut.view)
         }
     }
 }
