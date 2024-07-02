5881061b78216b295c39ac1040b7ea714b6f0f35
Markus Faßbender <markus.fassbender@wire.com>
2024-07-02 14:11:23 +0200

    chore: make message logs maintainable - WPB-9517 (#1637)

--- wire-ios-request-strategy/Sources/Object Syncs/Helpers/ProteusMessage.swift
+++ wire-ios-request-strategy/Sources/Object Syncs/Helpers/ProteusMessage.swift
@@ -18,21 +18,8 @@
 
 import Foundation
 
-public protocol ProteusMessage: OTREntity, EncryptedPayloadGenerator, Hashable {
-    var logInformation: LogAttributes { get }
-}
+public protocol ProteusMessage: OTREntity, EncryptedPayloadGenerator {}
 
-extension ZMClientMessage: ProteusMessage {
-
-    public var logInformation: LogAttributes {
-
-        return [
-            LogAttributesKey.nonce.rawValue: self.nonce?.safeForLoggingDescription ?? "<nil>",
-            LogAttributesKey.messageType.rawValue: self.underlyingMessage?.safeTypeForLoggingDescription ?? "<nil>",
-            LogAttributesKey.conversationId.rawValue: self.conversation?.qualifiedID?.safeForLoggingDescription ?? "<nil>"
-        ].merging(LogAttributes.safePublic, uniquingKeysWith: { _, new in new })
-
-    }
-}
+extension ZMClientMessage: ProteusMessage {}
 
 extension ZMAssetClientMessage: ProteusMessage {}
