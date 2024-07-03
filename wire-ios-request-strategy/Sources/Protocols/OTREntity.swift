bf76074e9128e4df00a4647bc2155b05efefe483
Markus Faßbender <markus.fassbender@wire.com>
2024-07-03 11:18:36 +0200

    sort import

--- wire-ios-request-strategy/Sources/Protocols/OTREntity.swift
+++ wire-ios-request-strategy/Sources/Protocols/OTREntity.swift
@@ -16,8 +16,8 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-import WireTransport
 import WireDataModel
+import WireTransport
 
 private let zmLog = ZMSLog(tag: "Dependencies")
 
