3f39544c4b3fedcfacf2f0a089faf1c3d4956e90
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2024-07-08 14:11:37 +0200

    fix: Snapshot tests

--- wire-ios/Wire-iOS/Sources/UserInterface/Settings/ChangingDetails/ChangeEmailViewController.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/Settings/ChangingDetails/ChangeEmailViewController.swift
@@ -115,12 +115,7 @@ final class ChangeEmailViewController: SettingsBaseTableViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setupNavigationBarTitle(EmailAccountSection.Change.title.capitalized)
-        let saveButtonItem: UIBarButtonItem = .createNavigationRightBarButtonItem(title: EmailAccountSection.Change.save.capitalized,
-                                                                                  systemImage: false,
-                                                                                  target: self,
-                                                                                  action: #selector(saveButtonTapped))
-        saveButtonItem.tintColor = UIColor.accent()
-        navigationItem.rightBarButtonItem = saveButtonItem
+
         observerToken = userProfile?.add(observer: self)
     }
 
@@ -154,6 +149,13 @@ final class ChangeEmailViewController: SettingsBaseTableViewController {
 
         emailPasswordCell.textField.setBackgroundColor(.clear)
         emailPasswordCell.textField.setTextColor(.white)
+
+        let saveButtonItem: UIBarButtonItem = .createNavigationRightBarButtonItem(title: EmailAccountSection.Change.save.capitalized,
+                                                                                  systemImage: false,
+                                                                                  target: self,
+                                                                                  action: #selector(saveButtonTapped))
+        saveButtonItem.tintColor = UIColor.accent()
+        navigationItem.rightBarButtonItem = saveButtonItem
 
         updateSaveButtonState()
     }
