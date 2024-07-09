04d21307ec3480ed7d037caefefb00b356f85dee
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2024-07-09 11:16:42 +0200

    doc: Explain why file is needed

--- WireAnalytics/Sources/WireAnalyticsSupport/WireAnalyticsSupport.swift
+++ WireAnalytics/Sources/WireAnalyticsSupport/WireAnalyticsSupport.swift
@@ -16,4 +16,6 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-import Foundation
+// This target generates mocks via 'sourcery'. It uses the plugin configured in `Package.swift`.
+// The generated mocks are processed from the sandbox directory and are not visible in the project folder:
+// https://github.com/apple/swift-package-manager/blob/main/Documentation/Plugins.md#implementing-the-build-tool-plugin-script
