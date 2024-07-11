09cf62fdff14a0757b147e6a8f5cc48ab709f67c
Christoph Aldrian <christoph.aldrian@wire.com>
2024-07-11 16:11:35 +0200

    translate group queue extension to Swift

--- wire-ios-utilities/Source/Public/NSManagedObjectContext+WireUtilities.h
+++ wire-ios-utilities/Source/Public/NSManagedObjectContext+WireUtilities.h
@@ -16,60 +16,15 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-#import <Foundation/Foundation.h>
-#import <CoreData/CoreData.h>
-#import <WireSystem/WireSystem.h>
+@import CoreData;
 
 NS_ASSUME_NONNULL_BEGIN
 
-@class DispatchGroupContext;
-
-@interface NSManagedObjectContext (ZMSGroupQueue) <ZMSGroupQueue>
-
-@property (nonatomic, readonly, nullable) ZMSDispatchGroup *dispatchGroup;
-
-- (void)performGroupedBlock:(dispatch_block_t)block;
-
-@property (nonatomic, readonly, null_unspecified) DispatchGroupContext *dispatchGroupContext;
-
-- (void)createDispatchGroups;
-
-/// Schedules a notification block to be submitted to the receiver's
-/// queue once all blocks associated with the receiver's group have completed.
-///
-/// If no blocks are associated with the receiver's group (i.e. the group is empty)
-/// then the notification block will be submitted immediately.
-///
-/// The receiver's group will be empty at the time the notification block is submitted to
-/// the receiver's queue.
-///
-/// @sa  dispatch_group_notify()
-- (void)notifyWhenGroupIsEmpty:(dispatch_block_t _Null_unspecified)block;
-
-/// Adds a group to the receiver. All blocks associated with the receiver's group will
-/// also be associated with this group.
-///
-/// This is used for testing. It is not thread safe.
-- (void)addGroup:(ZMSDispatchGroup *_Null_unspecified)dispatchGroup NS_SWIFT_NAME(addGroup(_:));
-
-/// List of all groups associated with this context
-- (NSArray <ZMSDispatchGroup*>*_Nonnull)allGroups;
-
-/// Performs a block and wait for completion.
-/// @note: The block is not retained after its execution. This means that if the queue
-/// is not running (e.g. blocked by a deadlock), the block and all its captured variables
-/// will be retained, otherwise it will eventually be released.
-/// @attention: Be *very careful* not to create deadlocks.
-- (void)performGroupedBlockAndWait:(dispatch_block_t _Null_unspecified)block NS_SWIFT_UNAVAILABLE("Use `performGroupedAndWait<T>(_:)`");
+@interface NSManagedObjectContext (executeFetchRequestOrAssert)
 
 /// Executes a fetch request and asserts in case of error
 /// For generic requests in Swift please refer to `func fetchOrAssert<T>(request: NSFetchRequest<T>) -> [T]`
 - (nonnull NSArray *)executeFetchRequestOrAssert:(nonnull NSFetchRequest *)request NS_SWIFT_UNAVAILABLE("Use `try fetch(request)` instead!");
-
-- (NSArray<ZMSDispatchGroup*>*_Nonnull)enterAllGroups;
-- (void)leaveAllGroups:(NSArray <ZMSDispatchGroup*>*_Null_unspecified)groups;
-
-@property (nonatomic) int pendingSaveCounter;
 
 @end
 
