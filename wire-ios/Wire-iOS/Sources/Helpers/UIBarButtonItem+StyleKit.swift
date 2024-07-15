9c2461d7e0026906301950b811381d4bafd2259c
David-Henner <david.henner@wire.com>
2024-07-15 11:48:05 +0200

    fix: `ConversationViewController` back button hang WPB-8976 (#1547)
    Co-authored-by: Sam Wyndham <sam.wyndham@wire.com>

--- wire-ios/Wire-iOS/Sources/Helpers/UIBarButtonItem+StyleKit.swift
+++ wire-ios/Wire-iOS/Sources/Helpers/UIBarButtonItem+StyleKit.swift
@@ -38,6 +38,13 @@ extension UIBarButtonItem {
         )
     }
 
+    func setIcon(_ icon: StyleKitIcon) {
+        image = icon.makeImage(
+            size: .tiny,
+            color: IconColors.foregroundDefaultBlack
+        )
+    }
+
     static func createCloseItem() -> UIBarButtonItem {
         let item = UIBarButtonItem(icon: .cross, target: nil, action: nil)
         item.tintColor = IconColors.foregroundDefault
