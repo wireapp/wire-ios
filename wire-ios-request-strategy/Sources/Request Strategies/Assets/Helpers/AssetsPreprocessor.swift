f8af382f2c913c4d79b26016165c5b97ab7cbbe7
François Benaiteau <francois.benaiteau@wire.com>
2024-07-16 15:26:48 +0200

    Revert "Revert "more debug assets""
    bbf2a57e8999024cbca92448aa386b1336b43d95

--- wire-ios-request-strategy/Sources/Request Strategies/Assets/Helpers/AssetsPreprocessor.swift
+++ wire-ios-request-strategy/Sources/Request Strategies/Assets/Helpers/AssetsPreprocessor.swift
@@ -53,6 +53,7 @@ import Foundation
     }
 
     public func objectsDidChange(_ object: Set<NSManagedObject>) {
+        WireLogger.assets.debug("objectsDidChange")
         processObjects(object)
     }
 
@@ -62,6 +63,7 @@ import Foundation
     }
 
     public func addTrackedObjects(_ objects: Set<NSManagedObject>) {
+        WireLogger.assets.debug("addTrackedObjects")
         processObjects(objects)
     }
 
@@ -82,8 +84,10 @@ import Foundation
         for asset in message.assets where asset.hasOriginal {
             if asset.needsPreprocessing, let imageOperations = imageAssetPreprocessor?.operations(forPreprocessingImageOwner: AssetImageOwnerAdapter(asset: asset)) {
                 processingGroup.enter()
+                WireLogger.assets.debug("asset image op", attributes: [LogAttributesKey.nonce.rawValue: message.nonce?.safeForLoggingDescription ?? "<nil>"])
                 imageProcessingQueue.addOperations(imageOperations, waitUntilFinished: false)
             } else {
+                WireLogger.assets.debug("asset encrypt file", attributes: [LogAttributesKey.nonce.rawValue: message.nonce?.safeForLoggingDescription ?? "<nil>"])
                 asset.encrypt()
             }
         }
@@ -96,6 +100,8 @@ import Foundation
         processingGroup.notify(on: .global()) { [weak self] in
             self?.managedObjectContext.performGroupedBlock {
                 self?.objectsBeingProcessed.remove(message)
+                WireLogger.assets.debug("notifyWhenProcessingIsComplete", attributes: [LogAttributesKey.nonce.rawValue: message.nonce?.safeForLoggingDescription ?? "<nil>"])
+//                message.updateTransferState(.uploaded, synchronize: true)
                 let assetClientMessageSet: Set<AnyHashable> = [#keyPath(ZMAssetClientMessage.transferState)]
                 message.setLocallyModifiedKeys(assetClientMessageSet) // TODO jacob hacky
                 message.managedObjectContext?.saveOrRollback()
@@ -119,6 +125,7 @@ extension AssetsPreprocessor: ZMAssetsPreprocessorDelegate {
 
         managedObjectContext.performGroupedBlock {
             assetImageOwnerAdapter.asset.updateWithPreprocessedData(operation.downsampleImageData, imageProperties: operation.properties)
+            WireLogger.assets.debug("asset image encrypt")
             assetImageOwnerAdapter.asset.encrypt()
         }
     }
@@ -133,6 +140,7 @@ extension AssetsPreprocessor: ZMAssetsPreprocessorDelegate {
 
     public func preprocessingCompleteOperation(for imageOwner: ZMImageOwner) -> Operation? {
         return BlockOperation { [weak self] in
+            WireLogger.assets.debug("preprocessingCompleteOperation")
             self?.processingGroup.leave()
         }
     }
