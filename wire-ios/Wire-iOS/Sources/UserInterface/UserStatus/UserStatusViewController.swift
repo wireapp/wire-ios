cc39138be4cb61a2d6675a4a8a10f82e6d0c7fd8
Christoph Aldrian <christoph.aldrian@wire.com>
2024-07-17 13:20:00 +0200

    get rid of PopoverPresenter 5

--- wire-ios/Wire-iOS/Sources/UserInterface/UserStatus/UserStatusViewController.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/UserStatus/UserStatusViewController.swift
@@ -50,13 +50,13 @@ final class UserStatusViewController: UIViewController {
     override func loadView() {
         let view = UserStatusView(options: options)
         view.userStatus = userStatus
-        view.tapHandler = { [weak self] _ in
-            self?.presentAvailabilityPicker()
+        view.tapHandler = { [weak self] button in
+            self?.presentAvailabilityPicker(button)
         }
         self.view = view
     }
 
-    func presentAvailabilityPicker() {
+    private func presentAvailabilityPicker(_ sender: UIButton) {
         let availabilityChangedHandler = { [weak self] (availability: Availability) in
             guard let self else { return }
 
@@ -69,10 +69,10 @@ final class UserStatusViewController: UIViewController {
         }
 
         let alertViewController = UIAlertController.availabilityPicker(availabilityChangedHandler)
-        alertViewController.configPopover(
-            pointToView: view,
-            popoverPresenter: UIApplication.shared.firstKeyWindow!.rootViewController! as! PopoverPresenter
-        )
+        if let popoverPresentationController = alertViewController.popoverPresentationController {
+            popoverPresentationController.sourceView = sender.superview
+            popoverPresentationController.sourceRect = sender.frame
+        }
         present(alertViewController, animated: true)
     }
 }
