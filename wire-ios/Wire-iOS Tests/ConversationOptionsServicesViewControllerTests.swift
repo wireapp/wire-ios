e4d2ee4ad4e055698a3bc7e3fc9fa3ed34f34bfb
Christoph Aldrian <christoph.aldrian@wire.com>
2024-07-18 17:51:21 +0200

    re-apply changes to tests and extensions

--- wire-ios/Wire-iOS Tests/ConversationOptionsServicesViewControllerTests.swift
+++ wire-ios/Wire-iOS Tests/ConversationOptionsServicesViewControllerTests.swift
@@ -151,7 +151,7 @@ final class ConversationServicesOptionsViewControllerTests: XCTestCase {
         let viewModel = ConversationServicesOptionsViewModel(configuration: config)
 
         // Show the alert
-        let sut = viewModel.setAllowServices(false)
+        let sut = viewModel.setAllowServices(false, sender: .init())
 
         // THEN
         XCTAssertNil(sut)
@@ -166,7 +166,7 @@ final class ConversationServicesOptionsViewControllerTests: XCTestCase {
         _ = ConversationServicesOptionsViewController(viewModel: viewModel)
 
         // Show the alert
-        let sut = viewModel.setAllowServices(false)!
+        let sut = viewModel.setAllowServices(false, sender: .init())!
 
         // THEN
         try verify(matching: sut)
