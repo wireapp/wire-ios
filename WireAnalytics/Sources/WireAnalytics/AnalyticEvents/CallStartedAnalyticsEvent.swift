486576f319766fc9b7ffc80e7818927d0eb1ce8b
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2024-07-19 14:14:31 +0200

    chore: Rename event to match the rest of them

--- WireAnalytics/Sources/WireAnalytics/AnalyticEvents/CallStartedAnalyticsEvent.swift
+++ WireAnalytics/Sources/WireAnalytics/AnalyticEvents/CallStartedAnalyticsEvent.swift
@@ -16,7 +16,7 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-public struct CallStartedEvent: AnalyticsEvent {
+public struct CallStartedAnalyticsEvent: AnalyticsEvent {
 
     public var eventName: String {
         "callStarted"
