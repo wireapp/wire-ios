6dd20b16391923d1c65050686151e876e72f4052
Sam Wyndham <sam.wyndham@wire.com>
2024-07-19 13:57:06 +0200

    Fix AVSClientTests

--- wire-ios-sync-engine/Tests/Source/Calling/AVSClientTests.swift
+++ wire-ios-sync-engine/Tests/Source/Calling/AVSClientTests.swift
@@ -19,8 +19,20 @@
 import Foundation
 @testable import WireSyncEngine
 import XCTest
+@_spi(MockBackendInfo)
+import WireTransport
 
-class AVSClientTests: XCTestCase {
+final class AVSClientTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        BackendInfo.enableMocking()
+        BackendInfo.isFederationEnabled = false
+    }
+
+    override func tearDown() {
+        BackendInfo.resetMocking()
+        super.tearDown()
+    }
 
     func testThatItSupportsAVSIdentifier() {
         // Given
