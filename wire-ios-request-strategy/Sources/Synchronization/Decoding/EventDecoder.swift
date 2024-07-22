1ca4d547a327208e66e6403161fec5a8c7071396
François Benaiteau <francois.benaiteau@wire.com>
2024-07-22 21:24:36 +0200

    Revert "add db constraint"
    6e76c02ef10d37c1b23525b09d9751940294c344

--- wire-ios-request-strategy/Sources/Synchronization/Decoding/EventDecoder.swift
+++ wire-ios-request-strategy/Sources/Synchronization/Decoding/EventDecoder.swift
@@ -288,13 +288,12 @@ extension EventDecoder {
                 index: Int64(idx) + startIndex + 1,
                 publicKeys: publicKeys
             )
+        }
 
-            do {
-                // we save for event in case there's a duplicate event's payload, we don't want to store it (db constraint)
-                try self.eventMOC.save()
-            } catch {
-                WireLogger.updateEvent.error("Failed to save stored event: \(error.localizedDescription)", attributes: event.logAttributes)
-            }
+        do {
+            try self.eventMOC.save()
+        } catch {
+            WireLogger.updateEvent.critical("Failed to save stored update events: \(error.localizedDescription)")
         }
     }
 
