43ba06789088b36c0a84e7edf38765d41fa6fc35
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2024-07-23 08:53:08 +0200

    Introduce a new method to create LeftBarButtonItem

---
+++ WireUI/Sources/WireReusableUIComponents/APIExtensions/UIKit/LeftBarButtonItem.swift
@@ -0,0 +1,57 @@
+//
+// Wire
+// Copyright (C) 2024 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import UIKit
+
+public extension UIBarButtonItem {
+
+    static func createNavigationLeftBarButtonItem(
+        title: String,
+        action: UIAction
+    ) -> UIBarButtonItem {
+        
+        let buttonFont = UIFont.systemFont(ofSize: 17, weight: .regular)
+
+        let leftBarButtonItem = UIBarButtonItem(
+            title: title,
+            primaryAction: action
+        )
+
+        let buttonStates: [UIControl.State] = [
+            .normal,
+            .highlighted,
+            .disabled,
+            .selected,
+            .focused,
+            .application,
+            .reserved
+        ]
+
+        buttonStates.forEach { buttonState in
+            leftBarButtonItem.setTitleTextAttributes(
+                [
+                    .font: buttonFont,
+                    .foregroundColor: UIColor.label
+                ],
+                for: buttonState
+            )
+        }
+
+        return leftBarButtonItem
+    }
+}
