85881c5fe03eaeadd224293bd0ebe355ed888c39
Christoph Aldrian <caldrian@users.noreply.github.com>
2024-07-24 17:39:56 +0200

    chore: remove remaining Objective C code from WireSystem - WPB-7306 (#1743)

---
+++ wire-ios-system/Source/ZMAssertionDumpFile.swift
@@ -0,0 +1,52 @@
+//
+// Wire
+// Copyright (C) 2024 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+
+@objc(ZMAssertionDumpFile) @objcMembers
+public final class AssertionDumpFile: NSObject {
+
+    @available(*, unavailable)
+    override public init() {
+        fatalError()
+    }
+
+    public static var url: URL {
+        get throws {
+            let applicationSupportDirectory = try FileManager.default.url(
+                for: .applicationSupportDirectory,
+                in: .userDomainMask,
+                appropriateFor: nil,
+                create: true
+            )
+
+            return applicationSupportDirectory.appendingPathComponent("last_assertion.log")
+        }
+    }
+
+    public static func write(content: String) throws {
+        var dumpFile = try url
+        try Data().write(to: dumpFile)
+
+        var resourceValues = URLResourceValues()
+        resourceValues.isExcludedFromBackup = true
+        try dumpFile.setResourceValues(resourceValues)
+
+        try Data(content.utf8).write(to: dumpFile, options: .atomic)
+    }
+}
--- wire-ios-system/Source/ZMSLogging.h
+++ wire-ios-system/Source/ZMSLogging.h
@@ -17,58 +17,25 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <WireSystem/ZMSAsserts.h>
 
-/// Log levels
-typedef NS_CLOSED_ENUM(int8_t, ZMLogLevel) {
-    ZMLogLevelPublic = 0,
-    ZMLogLevelError,
-    ZMLogLevelWarn,
-    ZMLogLevelInfo,
-    ZMLogLevelDebug,
-};
-
-
-/**
- 
- Logging
- 
- 
- This is for zmessaging logging.
- 
- 
- How to use:
- 
- At the top of the file define the log level like so:
- 
- @code
- static NSString* ZMLogTag ZM_UNUSED = "Network";
- @endcode
- 
- The @c ZMLogError() etc. then work just like @c NSLog() does.
- 
- 
- You can set a (symbolic) breakpoint at ZMLogDebugger to stop automatically when a warning or error level message is logged.
- 
- **/
 #define ZMLogPublic(format, ...) ZMLogWithLevelAndTag(ZMLogLevelPublic, ZMLogTag, format, ##__VA_ARGS__)
 #define ZMLogError(format, ...) ZMLogWithLevel(ZMLogLevelError, format, ##__VA_ARGS__)
 #define ZMLogWarn(format, ...) ZMLogWithLevel(ZMLogLevelWarn, format, ##__VA_ARGS__)
 #define ZMLogInfo(format, ...) ZMLogWithLevelAndTag(ZMLogLevelInfo, ZMLogTag, format, ##__VA_ARGS__)
 #define ZMLogDebug(format, ...) ZMLogWithLevelAndTag(ZMLogLevelDebug, ZMLogTag, format, ##__VA_ARGS__)
 
-
-#define ZMLogWithLevelAndTag(level, tag, format, ...) \
+#define ZMLogWithLevelAndTag(level, tag_, format, ...) \
     do { \
-        ZMLog(tag, __FILE__, __LINE__, level, format, ##__VA_ARGS__); \
+        NSString *message = [[NSString alloc] initWithFormat:format, ##__VA_ARGS__]; \
+        [ZMSLog logWithLevel:level message:^NSString * _Nonnull { \
+            return message; \
+        } tag:tag_ file:[NSString stringWithUTF8String:__FILE__] line:(NSUInteger)__LINE__]; \
     } while (0)
 
 #define ZMLogWithLevel(level, format, ...) \
     do { \
-        ZMLog(0, __FILE__, __LINE__, level, format, ##__VA_ARGS__); \
+        NSString *message = [[NSString alloc] initWithFormat:format, ##__VA_ARGS__]; \
+        [ZMSLog logWithLevel:level message:^NSString * _Nonnull { \
+            return message; \
+        } tag:0 file:[NSString stringWithUTF8String:__FILE__] line:(NSUInteger)__LINE__]; \
     } while (0)
-
-/// Logs an assert
-FOUNDATION_EXTERN void ZMDebugAssertMessage(NSString *tag, char const * const assertion, char const * const filename, int linenumber, char const *format, ...) __attribute__((format(printf,5,6)));
-/// Logs a message
-FOUNDATION_EXTERN void ZMLog(NSString *tag, char const * const filename, int linenumber, ZMLogLevel logLevel, NSString *format, ...) NS_FORMAT_FUNCTION(5,6);
