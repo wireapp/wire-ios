b5869546a1f3f1bdcc96ebe0e8b8ad335419b5b6
Christoph Aldrian <christoph.aldrian@wire.com>
2024-07-24 18:45:42 +0200

    create WireSystemPackage

---
+++ WireSystemPackage/Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WireSystemPackage",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "WireSystemPackage",
+            type: .dynamic,
+            targets: ["WireSystemPackage"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/apple/swift-docc-plugin",
+            from: "1.1.0"
+        )
+    ],
+    targets: [
+        .target(
+            name: "WireSystemPackage",
+            swiftSettings: swiftSettings
+        ),
+        .testTarget(
+            name: "WireSystemPackageTests",
+            dependencies: [
+                "WireSystemPackage"
+            ]
+        )
+    ]
+)
+
+let swiftSettings: [SwiftSetting] = [
+    .enableUpcomingFeature("ExistentialAny")
+]
