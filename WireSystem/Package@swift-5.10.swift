48716df6d1568ad32038b9843e9127fa8da1fb7c
Christoph Aldrian <christoph.aldrian@wire.com>
2024-07-25 12:47:43 +0200

    fix directory structure for WireSystem

--- WireSystem/Package@swift-5.10.swift
+++ WireSystem/Package@swift-5.10.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,23 +8,23 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [.iOS(.v15), .macOS(.v12)],
     products: [
-        .library(name: "WireSystemPackage", type: .dynamic, targets: ["WireSystemPackage"]),
-        .library(name: "WireSystemPackageSupport", type: .dynamic, targets: ["WireSystemPackageSupport"])
+        .library(name: "WireSystemPackage", type: .dynamic, targets: ["WireSystem"]),
+        .library(name: "WireSystemPackageSupport", type: .dynamic, targets: ["WireSystemSupport"])
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
         .package(path: "../SourceryPlugin")
     ],
     targets: [
-        .target(name: "WireSystemPackage", swiftSettings: swiftSettings),
+        .target(name: "WireSystem", swiftSettings: swiftSettings),
         .testTarget(
-            name: "WireSystemPackageTests",
-            dependencies: ["WireSystemPackage"]
+            name: "WireSystemTests",
+            dependencies: ["WireSystem"]
         ),
 
         .target(
-            name: "WireSystemPackageSupport",
-            dependencies: ["WireSystemPackage"],
+            name: "WireSystemSupport",
+            dependencies: ["WireSystem"],
             swiftSettings: swiftSettings,
             plugins: [
                 .plugin(name: "SourceryPlugin", package: "SourceryPlugin")
@@ -34,5 +34,7 @@ let package = Package(
 )
 
 let swiftSettings: [SwiftSetting] = [
-    .enableUpcomingFeature("ExistentialAny")
+    .enableUpcomingFeature("ExistentialAny"),
+    .enableUpcomingFeature("GlobalConcurrency"),
+    .enableExperimentalFeature("StrictConcurrency")
 ]
