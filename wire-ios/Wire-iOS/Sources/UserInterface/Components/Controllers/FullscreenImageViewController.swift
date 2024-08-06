e7200fe690a245ca995e7c7f284c71984f9df852
Sam Wyndham <samwyndham@users.noreply.github.com>
2024-08-06 14:39:21 +0200

    Remove `FullscreenImageViewController.assetWriteInProgress`

--- wire-ios/Wire-iOS/Sources/UserInterface/Components/Controllers/FullscreenImageViewController.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/Components/Controllers/FullscreenImageViewController.swift
@@ -76,7 +76,6 @@ final class FullscreenImageViewController: UIViewController {
     private let longPressGestureRecognizer = UILongPressGestureRecognizer()
 
     private var isShowingChrome = true
-    private var assetWriteInProgress = false
     private var forcePortraitMode = false {
         didSet {
             UIViewController.attemptRotationToDeviceOrientation()
