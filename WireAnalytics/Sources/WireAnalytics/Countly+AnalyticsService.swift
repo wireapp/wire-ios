3f37bb1867a291670ea2b94d66e4af57383fa402
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2024-08-09 14:08:48 +0200

    Address code review comment

--- WireAnalytics/Sources/WireAnalytics/Countly+AnalyticsService.swift
+++ WireAnalytics/Sources/WireAnalytics/Countly+AnalyticsService.swift
@@ -29,7 +29,7 @@ extension Countly: AnalyticsService {
     }
 
     func changeDeviceID(_ id: String) {
-        changeDeviceID(withMerge: id)
+        changeDeviceIDWithoutMerge(id)
     }
 
     func setUserValue(_ value: String?, forKey key: String) {
