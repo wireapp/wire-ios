f93f8b4147d9d6b5dd72512cf3928835853d2fc2
KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
2024-08-09 09:44:23 +0200

    feat: terminate the SFT 1:1 call once the other person hangup the call - WPB-7152 (#1772)
    Co-authored-by: David-Henner <david.henner@wire.com>
    Co-authored-by: John Nguyen <28632506+johnxnguyen@users.noreply.github.com>

--- wire-ios-sync-engine/Source/Calling/WireCallCenterV3+MLS.swift
+++ wire-ios-sync-engine/Source/Calling/WireCallCenterV3+MLS.swift
@@ -98,6 +98,17 @@ extension WireCallCenterV3 {
     private func leaveStaleConferenceIfNeeded(conversationID: AVSIdentifier) {
         guard
             let viewContext = uiMOC,
+            let conversation = ZMConversation.fetch(
+                with: conversationID.identifier,
+                domain: conversationID.domain,
+                in: viewContext
+            ),
+            conversation.conversationType == .group
+        else {
+            return
+        }
+
+        guard
             let syncContext = viewContext.zm_sync,
             let selfClient = ZMUser.selfUser(in: viewContext).selfClient(),
             let selfClientID = MLSClientID(userClient: selfClient),
@@ -155,6 +166,32 @@ extension WireCallCenterV3 {
                     )
                 } catch {
                     WireLogger.calling.error("failed to leave subconversation: \(String(reflecting: error))")
+                }
+            }
+        }
+    }
+
+    func deleteSubconversation(conversationID: AVSIdentifier) {
+        guard
+            let viewContext = uiMOC,
+            let syncContext = viewContext.zm_sync,
+            let parentIDs = mlsParentIDS(for: conversationID)
+        else {
+            return
+        }
+
+        syncContext.perform {
+            guard let mlsService = syncContext.mlsService else {
+                WireLogger.calling.error("failed to delete subconversation: mlsService is missing")
+                assertionFailure("mlsService is nil")
+                return
+            }
+
+            Task {
+                do {
+                    try await mlsService.deleteSubgroup(parentQualifiedID: parentIDs.qualifiedID)
+                } catch {
+                    WireLogger.calling.error("failed to delete subconversation: \(String(reflecting: error))")
                 }
             }
         }
