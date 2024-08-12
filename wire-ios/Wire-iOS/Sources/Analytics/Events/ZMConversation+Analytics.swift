8a6a65b6f7c2a8c6cfd4cf3b0b6f7020babcacea
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2024-08-12 12:41:46 +0200

    fix: SwiftLint warning

--- wire-ios/Wire-iOS/Sources/Analytics/Events/ZMConversation+Analytics.swift
+++ wire-ios/Wire-iOS/Sources/Analytics/Events/ZMConversation+Analytics.swift
@@ -50,6 +50,7 @@ extension ZMConversation {
         return ConversationType.type(self)?.analyticsTypeString
     }
 
+    // swiftlint:disable todo_requires_jira_link
     // TODO: move to DM
     // swiftlint:enable todo_requires_jira_link
     /// Whether the conversation includes at least 1 service user.
