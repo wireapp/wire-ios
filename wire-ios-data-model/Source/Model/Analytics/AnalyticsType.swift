aa6ac4b43c33e0dc47f52c1c7d9f20c57f82d773
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2024-08-13 10:34:17 +0200

    chore:  Remove Deprecated Analytics Code - WPB-10553 (#1787)

--- wire-ios-data-model/Source/Model/Analytics/AnalyticsType.swift
+++ wire-ios-data-model/Source/Model/Analytics/AnalyticsType.swift
@@ -29,32 +29,3 @@ public protocol AnalyticsType: NSObjectProtocol {
     @objc(persistedAttributesForEvent:)
     func persistedAttributes(for event: String) -> [String: NSObject]?
 }
-
-// Used for debugging only
-@objc public final class DebugAnalytics: NSObject, AnalyticsType {
-
-    public func tagEvent(_ event: String) {
-        print(Date(), "[ANALYTICS]", #function, event)
-    }
-
-    public func tagEvent(_ event: String, attributes: [String: NSObject]) {
-        print(Date(), "[ANALYTICS]", #function, event, attributes)
-    }
-
-    var eventAttributes = [String: [String: NSObject]]()
-
-    public func setPersistedAttributes(_ attributes: [String: NSObject]?, for event: String) {
-        if let attributes {
-            eventAttributes[event] = attributes
-        } else {
-            eventAttributes.removeValue(forKey: event)
-        }
-        print(Date(), "[ANALYTICS]", #function, event, eventAttributes[event] ?? [:])
-    }
-
-    public func persistedAttributes(for event: String) -> [String: NSObject]? {
-        let value = eventAttributes[event] ?? [:]
-        print(Date(), "[ANALYTICS]", #function, event, value)
-        return value
-    }
-}
