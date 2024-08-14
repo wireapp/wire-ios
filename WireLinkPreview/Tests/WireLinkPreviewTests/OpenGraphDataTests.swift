1b66fec5cdea4e628d4ea265a5006c6c3ec23246
Christoph Aldrian <christoph.aldrian@wire.com>
2024-08-14 17:02:21 +0200

    final test class

--- WireLinkPreview/Tests/WireLinkPreviewTests/OpenGraphDataTests.swift
+++ WireLinkPreview/Tests/WireLinkPreviewTests/OpenGraphDataTests.swift
@@ -16,10 +16,11 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-@testable import WireLinkPreview
 import XCTest
 
-class OpenGraphDataTests: XCTestCase {
+@testable import WireLinkPreview
+
+final class OpenGraphDataTests: XCTestCase {
 
     func testThatItCreatesAValidOpenGraphDataFromPropertyMapping() {
         // given
