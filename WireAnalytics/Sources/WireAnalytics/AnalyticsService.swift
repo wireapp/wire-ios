dd8cf1c23d66fdb358c3b76637ffa6b8a47e70ad
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2024-08-15 14:54:49 +0200

    Simplify implemention by introducing a boolean

--- WireAnalytics/Sources/WireAnalytics/AnalyticsService.swift
+++ WireAnalytics/Sources/WireAnalytics/AnalyticsService.swift
@@ -34,15 +34,16 @@ protocol AnalyticsService {
     /// Ends the current analytics session.
     func endSession()
 
-    /// Changes the device ID used for analytics without merging the data.
+    /// Changes the device ID used for analytics.
     ///
-    /// - Parameter id: The new device ID.
-    func changeDeviceIDWithoutMerge(_ id: String)
-
-    /// Changes the device ID used for analytics and merges the data.
+    /// - Parameters:
+    ///   - id: The new device ID to be used.
+    ///   - withMerge: A Boolean indicating whether to merge the data associated with the previous device ID into the new device ID.
+    ///     - If `true`, the data from the old device ID will be merged with the new device ID.
+    ///     - If `false`, the data will not be merged, and the new device ID will start with separate data.
     ///
-    /// - Parameter id: The new device ID.
-    func changeDeviceIDWithMerge(_ id: String)
+    /// - Note: If the `withMerge` parameter is set to `false`, the data associated with the previous device ID will not be transferred to the new device ID, resulting in separate analytics tracking.
+    func changeDeviceID(_ id: String, withMerge: Bool)
 
     /// Sets a user value for a given key in the analytics service.
     ///
--- WireAnalytics/Sources/WireAnalytics/Countly+AnalyticsService.swift
+++ WireAnalytics/Sources/WireAnalytics/Countly+AnalyticsService.swift
@@ -28,8 +28,12 @@ extension Countly: AnalyticsService {
         start(with: config)
     }
 
-    func changeDeviceIDWithMerge(_ id: String) {
-        changeDeviceIDWithoutMerge(id)
+    func changeDeviceID(_ id: String, withMerge: Bool) {
+        if withMerge {
+            changeDeviceID(withMerge: id)
+        } else {
+            changeDeviceIDWithoutMerge(id)
+        }
     }
 
     func setUserValue(_ value: String?, forKey key: String) {
