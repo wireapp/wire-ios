5bbc1fd919edaae81fdef0e182d6d4f3b0de2bbf
Sam Wyndham <samwyndham@users.noreply.github.com>
2024-09-02 15:08:22 +0200

    Improve message send cancelled copy

--- wire-ios/Wire-iOS/Generated/Strings+Generated.swift
+++ wire-ios/Wire-iOS/Generated/Strings+Generated.swift
@@ -2083,8 +2083,8 @@ internal enum L10n {
           internal static let generalReason = L10n.tr("Localizable", "content.system.failedtosend_message.general_reason", fallback: "Message could not be sent due to connectivity issues.")
           /// Retry
           internal static let retry = L10n.tr("Localizable", "content.system.failedtosend_message.retry", fallback: "Retry")
-          /// Message was not send as the upload was cancelled by the user.
-          internal static let userCancelledUploadReason = L10n.tr("Localizable", "content.system.failedtosend_message.user_cancelled_upload_reason", fallback: "Message was not send as the upload was cancelled by the user.")
+          /// Message was not sent as the upload was cancelled.
+          internal static let userCancelledUploadReason = L10n.tr("Localizable", "content.system.failedtosend_message.user_cancelled_upload_reason", fallback: "Message was not sent as the upload was cancelled.")
         }
         internal enum FailedtosendParticipants {
           /// Plural format key: "%#@lu_number_of_participants@"
