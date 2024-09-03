d8ca5ebe3ca7cd333b6ca4ba12fd69e81905e1ce
François Benaiteau <francois.benaiteau@wire.com>
2024-09-03 17:25:41 +0200

    wip

--- wire-ios-sync-engine/Source/SessionManager/APIVersionResolver.swift
+++ wire-ios-sync-engine/Source/SessionManager/APIVersionResolver.swift
@@ -72,10 +72,12 @@ final class APIVersionResolver {
         WireLogger.environment.info("received api version response")
 
         guard response.result == .success else {
-            WireLogger.environment.warn("api version response was not success, falling back to v0")
-            BackendInfo.apiVersion = .v0
-            BackendInfo.domain = "wire.com"
-            BackendInfo.isFederationEnabled = false
+            guard response.httpStatus == 404 else {
+                BackendInfo.apiVersion = .v0
+                
+                return
+            }
+            WireLogger.environment.warn("api version response was not successful")
             return
         }
 
@@ -83,7 +85,7 @@ final class APIVersionResolver {
             let data = response.rawData,
             let payload = APIVersionResponsePayload(data)
         else {
-            fatalError()
+            fatal("Couldn't parse api version response payload")
         }
 
         let backendProdVersions = Set(payload.supported.compactMap(APIVersion.init(rawValue:)))
