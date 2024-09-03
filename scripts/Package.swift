a282ba5e206fc07b0dbbf9ad0d17b49d4f575749
Christoph Aldrian <christoph.aldrian@wire.com>
2024-09-03 09:48:25 +0200

    try to fix config file

--- scripts/Package.swift
+++ scripts/Package.swift
@@ -27,11 +27,6 @@ let package = Package(
             checksum: "722a705de1cf4e0e07f2b7d2f9f631f3a8b2635a0c84cce99f9677b38aa4a1d6"
         ),
         .binaryTarget(
-            name: "swiftgen",
-            url: "https://github.com/SwiftGen/SwiftGen/releases/download/6.6.2/swiftgen-6.6.2.artifactbundle.zip",
-            checksum: "7586363e24edcf18c2da3ef90f379e9559c1453f48ef5e8fbc0b818fbbc3a045"
-        ),
-        .binaryTarget(
             name: "swiftformat",
             url: "https://github.com/nicklockwood/SwiftFormat/releases/download/0.54.0/swiftformat.artifactbundle.zip",
             checksum: "edf4ed2f1664ad621ae71031ff915e0c6ef80ad66e87ea0e5a10c3580a27a6dd"
