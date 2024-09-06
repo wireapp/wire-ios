96b9f0e1696457b6e35777051564c3aecac7c86b
François Benaiteau <netbe@users.noreply.github.com>
2024-09-06 14:49:14 +0200

    Update wire-ios-sync-engine/Source/UserSession/ZMUserSession/ZMUserSession.swift
    Co-authored-by: Christoph Aldrian <caldrian@users.noreply.github.com>

--- wire-ios-sync-engine/Source/UserSession/ZMUserSession/ZMUserSession.swift
+++ wire-ios-sync-engine/Source/UserSession/ZMUserSession/ZMUserSession.swift
@@ -635,7 +635,7 @@ public final class ZMUserSession: NSObject {
     // might be replaced by something more elegant
     public func processConversationEvents(_ events: [ZMUpdateEvent], completion: (() -> Void)?) {
         WaitingGroupTask(context: self.syncContext) { [weak self] in
-            guard let self else { return }
+            guard let self else { return completion?() }
             await self.conversationEventProcessor.processAndSaveConversationEvents(events)
             completion?()
         }
