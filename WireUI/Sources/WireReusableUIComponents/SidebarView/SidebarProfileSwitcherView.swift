c87b54e1b9fa82715eb6aa2d7b37c2ca02fd5da0
Christoph Aldrian <christoph.aldrian@wire.com>
2024-09-10 08:44:15 +0200

    chore: create sidebar view - WPB-9727

---
+++ WireUI/Sources/WireReusableUIComponents/SidebarView/SidebarProfileSwitcherView.swift
@@ -0,0 +1,111 @@
+//
+// Wire
+// Copyright (C) 2024 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import SwiftUI
+import WireDesign
+
+struct SidebarProfileSwitcherView<AccountImageView>: View
+    where AccountImageView: View {
+
+    @State private var accountImageDiameter: CGFloat = 0
+
+    let displayName: String
+    let username: String
+    let accountImageView: () -> AccountImageView
+
+    var body: some View {
+        HStack {
+            accountImageView()
+                .frame(width: accountImageDiameter, height: accountImageDiameter)
+
+            VStack(alignment: .leading) {
+                Text(displayName)
+                    .font(.headline)
+                    .foregroundStyle(Color(ColorTheme.Backgrounds.onSurface))
+                Text(username)
+                    .font(.subheadline)
+                    .foregroundStyle(Color(ColorTheme.Base.secondaryText))
+            }
+            .background(GeometryReader { geometryProxy in
+                Color.clear.preference(
+                    key: ProfileSwitcherHeightKey.self,
+                    value: geometryProxy.size.height
+                )
+            })
+            .onPreferenceChange(ProfileSwitcherHeightKey.self) { height in
+                accountImageDiameter = height
+            }
+        }
+    }
+}
+
+extension SidebarProfileSwitcherView {
+
+    init(
+        _ displayName: String,
+        _ username: String,
+        _ accountImageView: @escaping () -> AccountImageView
+    ) {
+        self.init(
+            displayName: displayName,
+            username: username,
+            accountImageView: accountImageView
+        )
+    }
+}
+
+private struct ProfileSwitcherHeightKey: PreferenceKey {
+    static var defaultValue: CGFloat { 0 }
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        // probably never called
+        value = nextValue()
+    }
+}
+
+// MARK: - Previews
+
+#Preview("Personal Account") {
+    SidebarProfileSwitcherView(displayName: "Firstname Lastname", username: "@username") {
+        AccountImageViewRepresentable(
+            accountImage: .from(solidColor: .brown),
+            isTeamAccount: false,
+            availability: .available
+        )
+    }
+}
+
+#Preview("Team Account") {
+    SidebarProfileSwitcherView(displayName: "Firstname Lastname", username: "@username") {
+        AccountImageViewRepresentable(
+            accountImage: .from(solidColor: .orange),
+            isTeamAccount: true,
+            availability: .busy
+        )
+    }
+}
+
+private extension UIImage {
+
+    // TODO: look for all copies and move the code into WireUtilities or WireSystem
+    static func from(solidColor color: UIColor) -> UIImage {
+        UIGraphicsImageRenderer(size: .init(width: 1, height: 1)).image { rendererContext in
+            color.setFill()
+            rendererContext.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+        }
+    }
+}
---
+++ WireUI/Tests/WireUIFoundationTests/Main/MainSplitViewControllerTests.swift
@@ -0,0 +1,174 @@
+//
+// Wire
+// Copyright (C) 2024 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import SwiftUI
+import WireTestingPackage
+import XCTest
+
+@testable import WireUIFoundation
+
+final class MainSplitViewControllerTests: XCTestCase {
+
+    private var sut: MainSplitViewController!
+    private var sidebar: UIViewController!
+    private var conversationList: UIViewController!
+    private var conversation: UIViewController!
+    private var noConversationPlaceholder: UIViewController!
+    private var tabContainer: UIViewController!
+
+    private var snapshotHelper: SnapshotHelper!
+
+    @MainActor
+    override func setUp() async throws {
+        let hostingController: (String, Color) -> UIHostingController<AnyView> = { text, backgroundColor in
+            UIHostingController(
+                rootView: AnyView(
+                    VStack {
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            Text(text)
+                                .ignoresSafeArea()
+                            Spacer()
+                        }
+                        Spacer()
+                    }.background(backgroundColor)
+                )
+            )
+        }
+        sidebar = hostingController("Sidebar", .gray)
+        conversationList = hostingController("Conversation List", .purple)
+        conversation = hostingController("Conversation", .blue)
+        noConversationPlaceholder = hostingController("No Conversation Selected", .brown)
+        tabContainer = hostingController("Tab Container", .cyan)
+        sut = .init(
+            sidebar: sidebar,
+            noConversationPlaceholder: noConversationPlaceholder,
+            tabContainer: tabContainer
+        )
+        sut.conversationList = conversationList
+        sut.conversation = conversation
+
+        snapshotHelper = .init()
+            .withPerceptualPrecision(1)
+            .withSnapshotDirectory(relativeTo: #file)
+    }
+
+    override func tearDown() async throws {
+        snapshotHelper = nil
+        sut = nil
+        tabContainer = nil
+        noConversationPlaceholder = nil
+        conversation = nil
+        conversationList = nil
+        sidebar = nil
+    }
+
+    @MainActor
+    func testInitializationWithConversation() {
+        XCTAssert(sut.sidebar === sidebar)
+        XCTAssert(sut.conversationList === conversationList)
+        XCTAssert(sut.conversation === conversation)
+        XCTAssert(sut.tabContainer === tabContainer)
+    }
+
+    @MainActor
+    func testPlaceholderIsNotReturnedAsConversation() {
+        // Given
+        sut = .init(
+            sidebar: sidebar,
+            noConversationPlaceholder: noConversationPlaceholder,
+            tabContainer: tabContainer
+        )
+        sut.conversationList = conversationList
+
+        // Then
+        XCTAssertNil(sut.conversation)
+        let secondaryNavigationController = sut.viewController(for: .secondary) as! UINavigationController
+        XCTAssert(secondaryNavigationController.viewControllers[0] === noConversationPlaceholder)
+    }
+
+    @MainActor
+    func testConversationListIsReleased() {
+        // When
+        weak var conversationList = conversationList
+        self.conversationList = nil
+        sut.conversationList = nil
+
+        // Then
+        XCTAssertEqual(conversationList, nil)
+    }
+
+    @MainActor
+    func testConversationIsReleased() async {
+        // When
+        weak var conversation = conversation
+        self.conversation = nil
+        sut.conversation = nil
+
+        // Then
+        await Task.yield()
+        XCTAssertEqual(conversation, nil)
+    }
+
+    @available(iOS 17.0, *) @MainActor
+    func testSidebarAppearanceLandscape() {
+        let window = UIWindow(frame: .init(x: 0, y: 0, width: 1_024, height: 768))
+        window.rootViewController = sut
+        window.makeKeyAndVisible()
+        sut.traitOverrides.horizontalSizeClass = .regular
+        sut.preferredDisplayMode = .twoBesideSecondary
+
+        snapshotHelper
+            .withUserInterfaceStyle(.light)
+            .verify(matching: sut.view, named: "light")
+        snapshotHelper
+            .withUserInterfaceStyle(.dark)
+            .verify(matching: sut.view, named: "dark")
+
+        window.isHidden = true
+    }
+
+    @available(iOS 17.0, *) @MainActor
+    func testSidebarAppearancePortrait() {
+        let window = UIWindow(frame: .init(x: 0, y: 0, width: 768, height: 1_024))
+        window.rootViewController = sut
+        window.makeKeyAndVisible()
+        sut.traitOverrides.horizontalSizeClass = .regular
+        sut.preferredDisplayMode = .twoBesideSecondary
+
+        snapshotHelper
+            .withUserInterfaceStyle(.light)
+            .verify(matching: sut.view, named: "light")
+        snapshotHelper
+            .withUserInterfaceStyle(.dark)
+            .verify(matching: sut.view, named: "dark")
+
+        window.isHidden = true
+    }
+
+    @MainActor
+    func testCompactAppearance() {
+        snapshotHelper
+            .withUserInterfaceStyle(.light)
+            .verify(matching: sut, named: "light")
+        snapshotHelper
+            .withUserInterfaceStyle(.dark)
+            .verify(matching: sut, named: "dark")
+    }
+}
