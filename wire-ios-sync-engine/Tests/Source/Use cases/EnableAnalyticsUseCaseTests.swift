f70c1393436cf6fb324003201485c823bcbbad24
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2024-09-12 14:20:01 +0200

    Fix SwiftLint Warnings

--- wire-ios-sync-engine/Tests/Source/Use cases/EnableAnalyticsUseCaseTests.swift
+++ wire-ios-sync-engine/Tests/Source/Use cases/EnableAnalyticsUseCaseTests.swift
@@ -16,8 +16,8 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-import WireAnalyticsSupport
 import WireAnalytics
+import WireAnalyticsSupport
 import XCTest
 
 @testable import WireSyncEngine
