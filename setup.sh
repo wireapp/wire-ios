4be56038b0f29abf0aa54f890449ae6d4856b271
KaterinaWire <katerina@wire.com>
2024-10-01 14:01:33 +0200

    undo

--- setup.sh
+++ setup.sh
@@ -70,8 +70,8 @@ fi
 echo ""
 
 echo "ℹ️  Resolve Swift Packages for Scripts..."
-( cd $REPO_ROOT && xcodebuild -resolvePackageDependencies -clonedSourcePackagesDirPath "$PACKAGES_DIR" )
-#xcrun --sdk macosx swift package --package-path scripts resolve
+#( cd $REPO_ROOT && xcodebuild -resolvePackageDependencies -clonedSourcePackagesDirPath "$PACKAGES_DIR" )
+xcrun --sdk macosx swift package --package-path scripts resolve
 #xcrun --sdk macosx swift package --package-path SourceryPlugin resolve
 echo ""
 
