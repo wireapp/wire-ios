159fefa42aed1af74567d0ae39d1cb053fa16a1a
Christoph Aldrian <christoph.aldrian@wire.com>
2024-10-07 17:14:41 +0200

    fix: accent color picker presentation - WPB-10141

--- wire-ios/Wire-iOS/Sources/UserInterface/Settings/AccentColorPicker.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/Settings/AccentColorPicker.swift
@@ -16,12 +16,15 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-import Inject
 import SwiftUI
 import WireCommonComponents
 import WireDesign
 import WireFoundation
 import WireSyncEngine
+
+#if canImport(Inject)
+import Inject
+#endif
 
 struct AccentColorPicker: View {
 
@@ -29,36 +32,34 @@ struct AccentColorPicker: View {
     var selectedColor: AccentColor
     private let colorViewSize: CGFloat = 28
 
+#if canImport(Inject)
     @ObserveInjection var inject
+#endif
 
     let onColorSelect: ((AccentColor) -> Void)?
 
     var body: some View {
-        // The NavigationView is needed here to properly set the font size for the navigation bar title.
-        // Currently, there's no other way to set the font size of the navigation bar title to the desired value.
-        // Without the NavigationView, the title would be too small.
-        NavigationView {
-            List(AccentColor.allCases, id: \.self) { color in
-                cell(for: color)
-                    .listRowBackground(Color(SemanticColors.View.backgroundUserCell))
-                    .onTapGesture {
-                        self.selectedColor = color
-                        onColorSelect?(color)
-                    }
-            }
-            .listStyle(.plain)
-            .modifier(ListBackgroundStyleModifier())
-            .background(Color(SemanticColors.View.backgroundDefault))
-        }
-        .enableInjection()
-        .toolbar {
-            ToolbarItem(placement: .principal) {
-                VStack {
-                    Text(L10n.Localizable.Self.Settings.AccountPictureGroup.color.capitalized)
-                        .font(.textStyle(.h3))
+#if canImport(Inject)
+        accentColorList
+            .enableInjection()
+#else
+        accentColorList
+#endif
+    }
+
+    @ViewBuilder
+    private var accentColorList: some View {
+        List(AccentColor.allCases, id: \.self) { color in
+            cell(for: color)
+                .listRowBackground(Color(SemanticColors.View.backgroundUserCell))
+                .onTapGesture {
+                    self.selectedColor = color
+                    onColorSelect?(color)
                 }
-            }
         }
+        .listStyle(.plain)
+        .modifier(ListBackgroundStyleModifier())
+        .background(Color(SemanticColors.View.backgroundDefault))
     }
 
     @ViewBuilder
