c90ea1f4db147864656a430185adad430391b8d2
Christoph Aldrian <christoph.aldrian@wire.com>
2024-10-08 13:48:50 +0200

    fix TODO

--- WireUI/Sources/WireMainNavigation/MainTabBarController.swift
+++ WireUI/Sources/WireMainNavigation/MainTabBarController.swift
@@ -18,7 +18,7 @@
 
 import SwiftUI
 
-// TODO: rotating the call screen doesn't seem to work <-
+// TODO: [WPB-11448] Bug: The call screen doesn't rotate to landscape
 
 /// A subclass of `UITabBarController` which configures its `viewControllers` property to match
 /// ``MainTabBarControllerContent``'s cases. After initialization each tab contains an empty navigation controller.
