2d70dd6119be06a12a3ee84f24530f3a03edb32d
Christoph Aldrian <christoph.aldrian@wire.com>
2024-10-23 23:02:34 +0200

    fix swift-docc-plugin url

--- WireFoundation/Package.swift
+++ WireFoundation/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "WireTestingPackage", targets: ["WireTestingPackage"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0"),
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.17.4"),
         .package(path: "../SourceryPlugin")
     ],
