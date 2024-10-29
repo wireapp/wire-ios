c9290738588bb30a4a949d036ee6b7c40f8a4adc
François Benaiteau <francois.benaiteau@wire.com>
2024-10-29 16:22:57 +0100

    fix test_GroupsWithStaleKeyingMaterial

--- wire-ios-data-model/Source/Utilis/Date+Helpers.swift
+++ wire-ios-data-model/Source/Utilis/Date+Helpers.swift
@@ -20,13 +20,19 @@ import Foundation
 
 public extension Date {
 
+    private static var customCalendar: Calendar = {
+        var calendar = Calendar.current
+        calendar.timeZone = TimeZone(secondsFromGMT: 0)!
+        return calendar
+    }()
+    
     /// The number of days from this date til now.
     ///
     /// If this date is in the future, the return value will be 0.
 
     var ageInDays: Int {
         let now = Date()
-        return Calendar.current.dateComponents([.day], from: self, to: now).day!
+        return Self.customCalendar.dateComponents([.day], from: self, to: now).day!
     }
 
     /// Whether the date is after the current instant.
