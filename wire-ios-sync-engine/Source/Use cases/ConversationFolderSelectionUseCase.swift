358b66820ab0eef0f5bfc4a67baf2db3e3525677
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2024-11-01 14:42:14 +0100

    Make protocol public
    Co-authored-by: Christoph Aldrian <caldrian@users.noreply.github.com>

--- wire-ios-sync-engine/Source/Use cases/ConversationFolderSelectionUseCase.swift
+++ wire-ios-sync-engine/Source/Use cases/ConversationFolderSelectionUseCase.swift
@@ -20,7 +20,7 @@ import WireDataModel
 
 // MARK: - ConversationFolderSelectionUseCaseProtocol
 
-protocol ConversationFolderSelectionUseCaseProtocol {
+public protocol ConversationFolderSelectionUseCaseProtocol {
 
     func invoke<Conversation: ToFolderMovableConversation>(
         folder: LabelType,
