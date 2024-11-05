6d5784f58d5761147558339ef59531db1c69bdc9
El-Fitz <8971906+El-Fitz@users.noreply.github.com>
2024-11-05 19:05:39 +0100

    feat: implement individual to team migration UI

---
+++ WireUI/Sources/WireIndividualToTeamUI/Components/ContainerView.swift
@@ -0,0 +1,73 @@
+//
+// Wire
+// Copyright (C) 2024 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+
+//
+// Wire
+// Copyright (C) 2024 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import SwiftUI
+
+struct ContainerView<Content: View>: View {
+    private let content: Content
+
+    private let step: Int
+    private let stepCount: Int
+    private let stepTitle: String
+
+    init(
+        @ViewBuilder content: () -> Content,
+        step: Int,
+        stepCount: Int,
+        stepTitle: String
+    ) {
+        self.content = content()
+        self.step = step
+        self.stepCount = stepCount
+        self.stepTitle = stepTitle
+    }
+
+    var body: some View {
+        VStack {
+            Text("Step \(step) of \(stepCount)")
+                .font(.caption)
+            Spacer()
+                .frame(height: 12)
+            Text(stepTitle)
+                .font(.title2)
+            Spacer(minLength: 36)
+            content
+        }
+        .padding(.horizontal, 16)
+        .padding(.bottom, 24)
+    }
+}
