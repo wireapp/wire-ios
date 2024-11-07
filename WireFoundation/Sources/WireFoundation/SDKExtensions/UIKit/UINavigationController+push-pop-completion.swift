d8a46e6311f7d083f6ff5b231befd5b3f37364df
Christoph Aldrian <christoph.aldrian@wire.com>
2024-11-07 10:18:15 +0100

    UINavigationController extensions

---
+++ WireFoundation/Sources/WireFoundation/SDKExtensions/UIKit/UINavigationController+push-pop-completion.swift
@@ -0,0 +1,70 @@
+//
+// Wire
+// Copyright (C) 2024 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import SwiftUI
+
+public extension UINavigationController {
+
+    func setViewControllers(
+        _ viewControllers: [UIViewController],
+        animated: Bool,
+        completion: @escaping () -> Void
+    ) {
+        fatalError() // TODO: finish
+        //setViewControllers(<#T##viewControllers: [UIViewController]##[UIViewController]#>, animated: <#T##Bool#>)
+    }
+
+    func pushViewController(
+        _ viewController: UIViewController,
+        animated: Bool,
+        completion: @escaping () -> Void
+    ) {
+        pushViewController(viewController, animated: animated)
+
+        guard animated, let coordinator = transitionCoordinator else {
+            return DispatchQueue.main.async { completion() }
+        }
+
+        coordinator.animate(alongsideTransition: nil) { _ in completion() }
+    }
+
+    func pushViewController(_ viewController: UIViewController, animated: Bool) async {
+        await withCheckedContinuation { continuation in
+            pushViewController(viewController, animated: animated, completion: continuation.resume)
+        }
+    }
+
+    func popViewController(
+        animated: Bool,
+        completion: @escaping () -> Void
+    ) {
+        popViewController(animated: animated)
+
+        guard animated, let coordinator = transitionCoordinator else {
+            return DispatchQueue.main.async { completion() }
+        }
+
+        coordinator.animate(alongsideTransition: nil) { _ in completion() }
+    }
+
+    func popViewController(animated: Bool) async {
+        await withCheckedContinuation { continuation in
+            popViewController(animated: animated, completion: continuation.resume)
+        }
+    }
+}
