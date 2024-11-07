ccdaed85327a98dcb5ffed29f18bb09e4ecf5b78
Christoph Aldrian <christoph.aldrian@wire.com>
2024-11-07 19:45:40 +0100

    temporary changes

--- WireFoundation/Package.swift
+++ WireFoundation/Package.swift
@@ -48,7 +48,12 @@ let package = Package(
             ],
             path: "./Sources/WireTesting"
         ),
-        .plugin(name: "SnapshotTestReferenceDirectoryPlugin", capability: .buildTool())
+        .plugin(name: "SnapshotTestReferenceDirectoryPlugin", capability: .buildTool(), dependencies: []),
+        .binaryTarget(
+            name: "swiftgen.artifactbundle",
+            url: "https://github.com/SwiftGen/SwiftGen/releases/download/6.6.2/swiftgen-6.6.2.artifactbundle.zip",
+            checksum: "7586363e24edcf18c2da3ef90f379e9559c1453f48ef5e8fbc0b818fbbc3a045"
+        )
     ]
 )
 
