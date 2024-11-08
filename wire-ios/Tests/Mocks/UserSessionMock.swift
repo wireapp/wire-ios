18806a236ab871b72cb459183e8734560be574af
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2024-11-08 10:35:36 +0100

    Implement mock for UserSessionMock

--- wire-ios/Tests/Mocks/UserSessionMock.swift
+++ wire-ios/Tests/Mocks/UserSessionMock.swift
@@ -337,6 +337,10 @@ final class UserSessionMock: UserSession {
         SubmitCallQualitySurveyUseCase(analyticsEventTracker: nil)
     }
 
+    func makeConversationFolderSelectionUseCase() -> ConversationFolderSelectionUseCaseProtocol {
+        ConversationFolderSelectionUseCase()
+    }
+
     var e2eiFeature: Feature.E2EI = Feature.E2EI(status: .enabled)
 
     var mlsFeature: Feature.MLS = Feature.MLS(
