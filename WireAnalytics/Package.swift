c812fedd5c2a03f26245582d7ca5662c10a08855
Christoph Aldrian <christoph.aldrian@wire.com>
2024-11-12 15:46:00 +0100

    delete imports

--- WireAnalytics/Package.swift
+++ WireAnalytics/Package.swift
@@ -3,7 +3,7 @@
 import Foundation
 import PackageDescription
 
-let wireDatadogTarget = Target.target(name: "WireDatadog")
+let isDatadogEnabled = ProcessInfo.processInfo.environment["ENABLE_DATADOG"] == "true"
 
 let package = Package(
     name: "WireAnalytics",
@@ -14,6 +14,7 @@ let package = Package(
         .library(name: "WireAnalyticsSupport", targets: ["WireAnalyticsSupport"])
     ],
     dependencies: [
+        .package(url: "https://github.com/DataDog/dd-sdk-ios.git", exact: "2.18.0"),
         .package(url: "https://github.com/Countly/countly-sdk-ios.git", exact: "24.4.2"),
         .package(path: "../SourceryPlugin")
     ],
@@ -24,7 +25,16 @@ let package = Package(
                 .product(name: "Countly", package: "countly-sdk-ios")
             ]
         ),
-        wireDatadogTarget,
+        .target(
+            name: "WireDatadog",
+            dependencies: isDatadogEnabled ? [
+                .product(name: "DatadogCore", package: "dd-sdk-ios"),
+                .product(name: "DatadogCrashReporting", package: "dd-sdk-ios"),
+                .product(name: "DatadogLogs", package: "dd-sdk-ios"),
+                .product(name: "DatadogRUM", package: "dd-sdk-ios"),
+                .product(name: "DatadogTrace", package: "dd-sdk-ios")
+            ] : []
+        ),
         .target(
             name: "WireAnalyticsSupport",
             dependencies: ["WireAnalytics"],
@@ -37,23 +47,18 @@ let package = Package(
         ),
         .testTarget(
             name: "WireAnalyticsTests",
-            dependencies: ["WireAnalytics", "WireAnalyticsSupport"]
+            dependencies: [
+                "WireAnalytics",
+                "WireAnalyticsSupport"
+            ] + (
+                // adding this line fixes the warning that "dd-sdk-ios" is not used
+                isDatadogEnabled
+                ? []
+                : [.product(name: "DatadogCore", package: "dd-sdk-ios")]
+            )
         )
     ]
 )
-
-if ProcessInfo.processInfo.environment["ENABLE_DATADOG"] == "true" {
-    package.dependencies += [
-        .package(url: "https://github.com/DataDog/dd-sdk-ios.git", exact: "2.18.0")
-    ]
-    wireDatadogTarget.dependencies += [
-        .product(name: "DatadogCore", package: "dd-sdk-ios"),
-        .product(name: "DatadogCrashReporting", package: "dd-sdk-ios"),
-        .product(name: "DatadogLogs", package: "dd-sdk-ios"),
-        .product(name: "DatadogRUM", package: "dd-sdk-ios"),
-        .product(name: "DatadogTrace", package: "dd-sdk-ios")
-    ]
-}
 
 for target in package.targets {
     target.swiftSettings = (target.swiftSettings ?? []) + [
