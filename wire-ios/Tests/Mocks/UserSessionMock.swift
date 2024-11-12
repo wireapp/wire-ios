07c4e20e3b258371a5da380315a8e1a25b7871a0
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2024-11-12 10:06:27 +0100

    feat: Implement ConversationFolderCreationUseCase for Folder Creation - WPB-12171 (#2142)
    Co-authored-by: Christoph Aldrian <caldrian@users.noreply.github.com>

--- wire-ios/Tests/Mocks/UserSessionMock.swift
+++ wire-ios/Tests/Mocks/UserSessionMock.swift
@@ -337,6 +337,10 @@ final class UserSessionMock: UserSession {
         SubmitCallQualitySurveyUseCase(analyticsEventTracker: nil)
     }
 
+    func makeConversationFolderCreationUseCase() -> CreateConversationFolderUseCaseProtocol {
+        CreateConversationFolderUseCase(managedObjectContext: self.syncContext)
+    }
+
     var e2eiFeature: Feature.E2EI = Feature.E2EI(status: .enabled)
 
     var mlsFeature: Feature.MLS = Feature.MLS(
