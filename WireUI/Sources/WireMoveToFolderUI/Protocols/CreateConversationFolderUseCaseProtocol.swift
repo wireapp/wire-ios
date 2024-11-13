6c3d433da47e38a44aaa7b184bd7aaf0d4b62b41
Agisilaos Tsaraboulidis <agtsaraboulidis@gmail.com>
2024-11-13 14:07:43 +0100

    Rename `CreateConversationFolderUseCaseType` to `CreateConversationFolderUseCaseProtocol`

--- WireUI/Sources/WireMoveToFolderUI/Protocols/CreateConversationFolderUseCaseProtocol.swift
+++ WireUI/Sources/WireMoveToFolderUI/Protocols/CreateConversationFolderUseCaseProtocol.swift
@@ -17,7 +17,7 @@
 //
 
 /// Protocol for handling creating a folder
-public protocol CreateConversationFolderUseCaseType {
+public protocol CreateConversationFolderUseCaseProtocol {
     /// Creates a folder
     /// - Parameters:
     ///   - name: The name of the folder to create
--- wire-ios/Wire-iOS/Sources/UserInterface/Folders/CreateConversationFolderUseCase+CreateConversationFolderUseCaseType.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/Folders/CreateConversationFolderUseCase+CreateConversationFolderUseCaseType.swift
@@ -20,7 +20,7 @@ import WireDataModel
 import WireMoveToFolderUI
 import WireSyncEngine
 
-extension CreateConversationFolderUseCase: @retroactive CreateConversationFolderUseCaseType {
+extension CreateConversationFolderUseCase: @retroactive CreateConversationFolderUseCaseProtocol {
 
     public func invoke(name: String) async throws -> Folder {
         guard let labelType = try await self.fetchLabelType(for: name) else {
