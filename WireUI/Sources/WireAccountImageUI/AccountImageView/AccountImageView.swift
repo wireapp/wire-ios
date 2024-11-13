14f111426a4f6ce10ee8034f399ed42075245b6d
KaterinaWire <katerina@wire.com>
2024-11-13 12:49:43 +0100

    clean up

--- WireUI/Sources/WireAccountImageUI/AccountImageView/AccountImageView.swift
+++ WireUI/Sources/WireAccountImageUI/AccountImageView/AccountImageView.swift
@@ -139,7 +139,6 @@ public final class AccountImageView: UIView {
         let accountImageViewWrapper = UIView()
         accountImageViewWrapper.translatesAutoresizingMaskIntoConstraints = false
         accountImageViewWrapper.clipsToBounds = true
-        accountImageViewWrapper.accessibilityLabel = "Profile picture"
         addSubview(accountImageViewWrapper)
         NSLayoutConstraint.activate([
             // make sure it's in the center, even if the surrounding view is not a square
