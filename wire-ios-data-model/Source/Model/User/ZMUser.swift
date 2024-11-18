33766efb84959e30a93b17cf2be4cfe181ff41dd
KaterinaWire <katerina@wire.com>
2024-11-18 17:02:39 +0100

    replace var with func

--- wire-ios-data-model/Source/Model/User/ZMUser.swift
+++ wire-ios-data-model/Source/Model/User/ZMUser.swift
@@ -499,7 +499,7 @@ extension ZMUser: UserConnections {
 
         let mlsService = syncContext.performAndWait { syncContext.mlsService }
         let migrator = mlsService.map(OneOnOneMigrator.init(mlsService:))
-        let resolver = OneOnOneResolver(migrator: migrator, isMLSEnabled: isMLSEnabled)
+        let resolver = OneOnOneResolver(migrator: migrator, isMLSEnabled: isMLSEnabled())
 
         accept(
             oneOnOneResolver: resolver,
