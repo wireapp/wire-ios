15db04a14dae2bef211276a100877f3f35a64d0b
François Benaiteau <netbe@users.noreply.github.com>
2024-11-19 09:04:43 +0100

    feat: empty state for search - WPB-11341 (#2152)
    Co-authored-by: KaterinaWire <katerina@wire.com>
    Co-authored-by: Christoph Aldrian <christoph.aldrian@wire.com>

--- wire-ios/Wire-iOS/Sources/UserInterface/ConversationList/ListContent/ConversationListContentController/ViewModel/ConversationListViewModel.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/ConversationList/ListContent/ConversationListContentController/ViewModel/ConversationListViewModel.swift
@@ -218,7 +218,12 @@ final class ConversationListViewModel: NSObject {
     }
 
     // Local copies of the lists.
-    var sections: [Section] = []
+    private var sections: [Section] = []
+
+    var isEmptyList: Bool {
+        let totalItems = sections.map { $0.items.count }.reduce(0, +)
+        return totalItems == 0
+    }
 
     private typealias DiffKitSection = ArraySection<Int, SectionItem>
 
