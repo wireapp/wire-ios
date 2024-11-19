d69884afe0ba032c876d89acba4e083d2e6abd52
Jullian Mercier <31648126+jullianm@users.noreply.github.com>
2024-11-19 09:37:11 +0100

    add event raw implementation, create rename event processor tests - WPB-10177

--- WireDomain/Sources/WireDomain/Event Processing/ConversationEventProcessor/ConversationRenameEventProcessor.swift
+++ WireDomain/Sources/WireDomain/Event Processing/ConversationEventProcessor/ConversationRenameEventProcessor.swift
@@ -33,8 +33,38 @@ protocol ConversationRenameEventProcessorProtocol {
 struct ConversationRenameEventProcessor: ConversationRenameEventProcessorProtocol {
 
     func processEvent(_: ConversationRenameEvent) async throws {
-        // TODO: [WPB-10177]
-        assertionFailure("not implemented yet")
+        
+//        await context.perform {
+//             let conversation = ZMConversation.fetchOrCreate(
+//                 with: event.conversationID.uuid,
+//                 domain: event.conversationID.domain,
+//                 in: context
+//             )
+//
+//             let sender = ZMUser.fetchOrCreate(
+//                 with: event.senderID.uuid,
+//                 domain: event.senderID.domain,
+//                 in: context
+//             )
+//
+//             let nameDidChange = conversation.userDefinedName != event.newName
+//             conversation.userDefinedName = event.newName
+//
+//             if nameDidChange {
+//                 let message = ZMSystemMessage(
+//                     nonce: UUID(),
+//                     managedObjectContext: context
+//                 )
+//
+//                 message.systemMessageType = .conversationNameChanged
+//                 message.visibleInConversation = conversation
+//                 message.serverTimestamp = event.timestamp
+//                 message.users = [sender]
+//                 message.text = event.newName
+//
+//                 conversation.updateTimestampsAfterUpdatingMessage(message)
+//             }
+//         }
     }
 
 }
