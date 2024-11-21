e62ba6c3ce7cad2f3e0650478a0d9fab27ccba08
Christoph Aldrian <christoph.aldrian@wire.com>
2024-11-21 15:31:01 +0100

    try to fix WireTestingTests

--- wire-ios-testing/Source/Public/Mock Data Model/ZMMockEntity.h
+++ wire-ios-testing/Source/Public/Mock Data Model/ZMMockEntity.h
@@ -16,7 +16,7 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-@import CoreData;
+#import <CoreData/CoreData.h>
 
 @interface ZMMockEntity : NSManagedObject
 
--- wire-ios-testing/Tests/WireTestingTests.m
+++ wire-ios-testing/Tests/WireTestingTests.m
@@ -16,7 +16,8 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-#import <WireTesting/WireTesting.h>
+@import WireSystem;
+@import WireTesting;
 
 @interface WireTestingTests : ZMTBaseTest
 
