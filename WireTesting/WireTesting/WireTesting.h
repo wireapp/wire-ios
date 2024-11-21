7bab3329d2ac1e2f836b89b4854f48205583d702
Christoph Aldrian <christoph.aldrian@wire.com>
2024-11-21 17:19:07 +0100

    link XCTest

--- WireTesting/WireTesting/WireTesting.h
+++ WireTesting/WireTesting/WireTesting.h
@@ -16,7 +16,7 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-#import <Foundation/Foundation.h>
+#import <XCTest/XCTest.h>
 
 //! Project version number for WireTesting.
 FOUNDATION_EXPORT double WireTestingVersionNumber;
