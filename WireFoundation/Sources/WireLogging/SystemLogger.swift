eebc803a5ed272ffb0d07effd59e036da5f353d5
Christoph Aldrian <christoph.aldrian@wire.com>
2024-12-03 18:52:03 +0100

    move SystemLogger

--- WireFoundation/Sources/WireLogging/SystemLogger.swift
+++ WireFoundation/Sources/WireLogging/SystemLogger.swift
@@ -16,9 +16,9 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-import Foundation
+public import Foundation
+
 import OSLog
-import WireLogging
 
 public protocol FileLoggerDestination {
     var log: URL? { get }
