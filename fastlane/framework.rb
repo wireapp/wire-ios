cdeb6d0d6eedeea7831ba5a4a97122f23070a645
Christoph Aldrian <christoph.aldrian@wire.com>
2024-12-04 16:50:01 +0100

    fix framework.rb

--- fastlane/framework.rb
+++ fastlane/framework.rb
@@ -20,11 +20,12 @@ class Framework
             "wire-ios-transport",
             "wire-ios-utilities",
             "wire-ios-ziphy",
-            "WireAPI",
             "WireAnalytics",
+            "WireAPI",
             "WireDomain",
-            "WireUI",
             "WireFoundation",
+            "WireLogging",
+            "WireUI",
         ]
 
         frameworks = all_folders.reduce({}) do |hash, framework| 
@@ -36,43 +37,58 @@ class Framework
         frameworks["wire-ios"].add_dependency(frameworks["wire-ios-share-engine"])
         frameworks["wire-ios"].add_dependency(frameworks["wire-ios-sync-engine"])
         frameworks["wire-ios"].add_dependency(frameworks["WireFoundation"])
-        
-        
+        frameworks["wire-ios"].add_dependency(frameworks["WireLogging"])
+
         frameworks["wire-ios-notification-engine"].add_dependency(frameworks["wire-ios-request-strategy"])
+        frameworks["wire-ios-notification-engine"].add_dependency(frameworks["WireLogging"])
+
         frameworks["wire-ios-sync-engine"].add_dependency(frameworks["wire-ios-request-strategy"])
         frameworks["wire-ios-sync-engine"].add_dependency(frameworks["WireAPI"])
-        frameworks["wire-ios-sync-engine"].add_dependency(frameworks["WireDomain"])
         frameworks["wire-ios-sync-engine"].add_dependency(frameworks["WireAnalytics"])
+        frameworks["wire-ios-sync-engine"].add_dependency(frameworks["WireDomain"])
+        frameworks["wire-ios-sync-engine"].add_dependency(frameworks["WireLogging"])
+
         frameworks["wire-ios-share-engine"].add_dependency(frameworks["wire-ios-request-strategy"])
+        frameworks["wire-ios-share-engine"].add_dependency(frameworks["WireLogging"])
 
         frameworks["wire-ios-request-strategy"].add_dependency(frameworks["wire-ios-data-model"])
         frameworks["wire-ios-request-strategy"].add_dependency(frameworks["WireAPI"])
-        
+        frameworks["wire-ios-request-strategy"].add_dependency(frameworks["WireLogging"])
+
         frameworks["wire-ios-data-model"].add_dependency(frameworks["wire-ios-cryptobox"])
         frameworks["wire-ios-data-model"].add_dependency(frameworks["wire-ios-images"])
         frameworks["wire-ios-data-model"].add_dependency(frameworks["wire-ios-link-preview"])
         frameworks["wire-ios-data-model"].add_dependency(frameworks["wire-ios-protos"])
         frameworks["wire-ios-data-model"].add_dependency(frameworks["wire-ios-transport"])
         frameworks["wire-ios-data-model"].add_dependency(frameworks["WireFoundation"])
-        
+        frameworks["wire-ios-data-model"].add_dependency(frameworks["WireLogging"])
+
         frameworks["wire-ios-mocktransport"].add_dependency(frameworks["wire-ios-testing"])
         frameworks["wire-ios-mocktransport"].add_dependency(frameworks["wire-ios-cryptobox"])
         frameworks["wire-ios-mocktransport"].add_dependency(frameworks["wire-ios-protos"])
 
         frameworks["wire-ios-cryptobox"].add_dependency(frameworks["wire-ios-utilities"])
+
         frameworks["wire-ios-transport"].add_dependency(frameworks["wire-ios-utilities"])
+        frameworks["wire-ios-transport"].add_dependency(frameworks["WireLogging"])
+
         frameworks["wire-ios-link-preview"].add_dependency(frameworks["wire-ios-utilities"])
+
         frameworks["wire-ios-images"].add_dependency(frameworks["wire-ios-utilities"])
 
         frameworks["wire-ios-utilities"].add_dependency(frameworks["wire-ios-system"])
         frameworks["wire-ios-utilities"].add_dependency(frameworks["WireFoundation"])
-        
+        frameworks["wire-ios-utilities"].add_dependency(frameworks["WireLogging"])
+
         frameworks["wire-ios-testing"].add_dependency(frameworks["wire-ios-system"])
-        
+
+        frameworks["wire-ios-system"].add_dependency(frameworks["WireLogging"])
+
         frameworks["WireDomain"].add_dependency(frameworks["wire-ios-transport"])
         frameworks["WireDomain"].add_dependency(frameworks["wire-ios-data-model"])
         frameworks["WireDomain"].add_dependency(frameworks["WireAPI"])
         frameworks["WireDomain"].add_dependency(frameworks["WireFoundation"])
+        frameworks["WireDomain"].add_dependency(frameworks["WireLogging"])
 
         frameworks["WireAPI"].add_dependency(frameworks["WireFoundation"])
 
