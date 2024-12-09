a1a3f2feea85ef485ff1d854603f73666916479c
François Benaiteau <francois.benaiteau@wire.com>
2024-12-09 21:25:09 +0100

    fix ziphy

--- fastlane/framework.rb
+++ fastlane/framework.rb
@@ -128,6 +128,8 @@ class Framework
             "WireUIAll" # use a custom scheme that includes all targets of WireUI, fastlane does not found WireUI-Package
         when "WireAPI"
             "WireAPIAll" # if a package has multiple targets, fastlane does not found <Package>-Package
+        when "wire-ios-ziphy"
+            "Ziphy"
         when "WireDomain"
             name
         when "WireAnalytics"
