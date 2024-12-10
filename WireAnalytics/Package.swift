ee274d6daed52447b439de79920fa17f2749ec0c
Christoph Aldrian <christoph.aldrian@wire.com>
2024-12-10 14:01:41 +0100

    use env var ENABLE_COUNTLY

--- WireAnalytics/Package.swift
+++ WireAnalytics/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 // You can enable/disable Datadog for debugging by overriding the boolean.
 let isDatadogEnabled = hasEnvironmentVariable("ENABLE_DATADOG", "true")
-let isCountlyEnabled = true // TODO: [WPB-11285] use env variable to set
+let isCountlyEnabled = hasEnvironmentVariable("ENABLE_COUNTLY", "true")
 
 let package = Package(
     name: "WireAnalytics",
