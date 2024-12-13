23f4c49ca1b3bf319a838c305fd86363bf3f0114
Christoph Aldrian <christoph.aldrian@wire.com>
2024-12-13 21:29:40 +0100

    add additional package files

--- WireDeveloperTools/Package.swift
+++ WireDeveloperTools/Package.swift
@@ -4,8 +4,13 @@ import PackageDescription
 
 let package = Package(
     name: "WireDeveloperTools",
+    platforms: [.iOS(.v16), .macOS(.v12)],
     products: [
         .library(name: "WireDeveloperTools", targets: ["WireDeveloperTools"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0"),
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.17.4"),
     ],
     targets: [
         .target(name: "WireDeveloperTools"),
@@ -13,5 +18,14 @@ let package = Package(
             name: "WireDeveloperToolsTests",
             dependencies: ["WireDeveloperTools"]
         ),
-    ]
+    ],
+    swiftLanguageModes: [.v6]
 )
+
+for target in package.targets {
+    target.swiftSettings = (target.swiftSettings ?? []) + [
+        .enableUpcomingFeature("InternalImportsByDefault"),
+        .enableUpcomingFeature("FullTypedThrows"),
+        .enableUpcomingFeature("ExistentialAny")
+    ]
+}
