4306fba616a9f8e7c6557541097f38cd51260e93
El-Fitz <8971906+El-Fitz@users.noreply.github.com>
2024-12-13 17:30:08 +0100

    chore: remove leftover todo

--- wire-ios/Wire-iOS/Sources/UserInterface/SelfProfile/SelfProfileViewController.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/SelfProfile/SelfProfileViewController.swift
@@ -305,7 +305,6 @@ final class SelfProfileViewController: UIViewController {
     }
 
     private func navigateToTeam() {
-        // TODO: [WPB-11968] navigate to team
         URL.manageTeam(source: .settings).open()
     }
 
