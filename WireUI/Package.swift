13735f123e374f2437ef0899d238845d46859fb8
Christoph Aldrian <christoph.aldrian@wire.com>
2024-12-13 21:13:06 +0100

    undo changes

--- WireUI/Package.swift
+++ WireUI/Package.swift
@@ -13,7 +13,6 @@ let package = Package(
         .library(name: "WireAccountImageUI", targets: ["WireAccountImageUI"]),
         .library(name: "WireConversationListUI", targets: ["WireConversationListUI"]),
         .library(name: "WireDesign", targets: ["WireDesign"]),
-        .library(name: "WireDeveloperToolsUI", targets: ["WireDeveloperToolsUI"]),
         .library(name: "WireFolderPickerUI", targets: ["WireFolderPickerUI"]),
         .library(name: "WireIndividualToTeamMigrationUI", targets: ["WireIndividualToTeamMigrationUI"]),
         .library(name: "WireMainNavigationUI", targets: ["WireMainNavigationUI"]),
@@ -44,8 +43,6 @@ let package = Package(
 
         .target(name: "WireDesign", dependencies: ["WireFoundation"]),
         .testTarget(name: "WireDesignTests", dependencies: ["WireDesign"]),
-
-        .target(name: "WireDeveloperToolsUI"),
 
         .target(name: "WireFolderPickerUI", dependencies: ["WireReusableUIComponents"]),
 
--- wire-ios/Wire-iOS/Sources/Developer/DeveloperTools/DeveloperToolsView.swift
+++ wire-ios/Wire-iOS/Sources/Developer/DeveloperTools/DeveloperToolsView.swift
@@ -110,7 +110,7 @@ private struct TextItemCell: View {
 struct DeveloperToolsView_Previews: PreviewProvider {
 
     static var previews: some View {
-        NavigationStack {
+        NavigationView {
             DeveloperToolsView(viewModel: DeveloperToolsViewModel())
         }
     }
