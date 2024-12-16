a2724c3a60310810145023079f998efd5a41132c
Christoph Aldrian <christoph.aldrian@wire.com>
2024-12-16 11:11:03 +0100

    improve structure of predefined analytics events

---
+++ WireAnalytics/Sources/WireAnalytics/Events/AnalyticsEvent+resultBuilder.swift
@@ -0,0 +1,64 @@
+//
+// Wire
+// Copyright (C) 2024 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+public extension AnalyticsEvent {
+
+    @resultBuilder
+    struct SegmentationEntryBuilder {
+
+        public static func buildBlock(_ components: [SegmentationEntry]...) -> [SegmentationEntry] {
+            components.flatMap { $0 }
+        }
+
+        public static func buildExpression(_ expression: SegmentationEntry) -> [SegmentationEntry] {
+            [expression]
+        }
+
+        public static func buildExpression(_ expression: [SegmentationEntry]) -> [SegmentationEntry] {
+            expression
+        }
+
+        public static func buildOptional(_ components: [SegmentationEntry]?) -> [SegmentationEntry] {
+            components ?? []
+        }
+
+        public static func buildEither(first components: [SegmentationEntry]) -> [SegmentationEntry] {
+            components
+        }
+
+        public static func buildEither(second components: [SegmentationEntry]) -> [SegmentationEntry] {
+            components
+        }
+    }
+
+    /// Create a new `AnalyticsEvent`.
+    ///
+    /// - Parameters:
+    ///   - name: A unique name.
+    ///   - segmentation: Additional metadata.
+
+    init(
+        _ name: String,
+        @SegmentationEntryBuilder segmentation: () -> [SegmentationEntry]
+    ) {
+        self.init(
+            name: name,
+            segmentation: segmentation()
+        )
+    }
+}
---
+++ WireAnalytics/Sources/WireAnalytics/Events/Predefined/AnalyticsEvent+calling.swift
@@ -0,0 +1,74 @@
+//
+// Wire
+// Copyright (C) 2024 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+public extension AnalyticsEvent {
+
+    enum Calling {
+
+        /// An event tracking when the user initiates a call.
+        ///
+        /// - Parameters:
+        ///   - isVideo: Whether video is enabled.
+        ///   - conversationType: The type of conversation.
+        ///
+        /// - Returns: A call initialized analytics event.
+
+        static func callInitialized(
+            isVideo: Bool,
+            conversationType: ConversationType
+        ) -> AnalyticsEvent {
+            AnalyticsEvent(
+                name: "calling.initiated_call",
+                segmentation: [
+                    .isVideoCall(isVideo),
+                    .groupType(conversationType)
+                ]
+            )
+        }
+
+        /// An event tracking when the user joins a call.
+        ///
+        /// - Parameters:
+        ///   - isVideo: Whether video is enabled.
+        ///   - conversationType: The type of conversation.
+        ///
+        /// - Returns: A call joined analytics event.
+
+        static func callJoined(
+            isVideo: Bool,
+            conversationType: ConversationType
+        ) -> AnalyticsEvent {
+            AnalyticsEvent(
+                name: "calling.joined_call",
+                segmentation: [
+                    .isVideoCall(isVideo),
+                    .groupType(conversationType)
+                ]
+            )
+        }
+
+        /// An event tracking the call quality when the user end the call.
+        /// - Parameter review: The Review containing score, reason or duration related to the call
+        /// - Returns: A callQualitySurvey analytics event.
+
+        static func callQualitySurvey(_ review: CallQualitySurveyReview) -> AnalyticsEvent {
+            .init(name: "calling.call_quality_review", segmentation: review.segmentation)
+        }
+
+    }
+}
