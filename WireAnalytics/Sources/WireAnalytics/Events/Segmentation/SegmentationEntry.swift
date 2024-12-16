46e4dd402277539bc82ffc5866cd2556d0937554
Christoph Aldrian <christoph.aldrian@wire.com>
2024-12-16 10:12:57 +0100

    var to let

--- WireAnalytics/Sources/WireAnalytics/Events/Segmentation/SegmentationEntry.swift
+++ WireAnalytics/Sources/WireAnalytics/Events/Segmentation/SegmentationEntry.swift
@@ -24,8 +24,8 @@
 
 public struct SegmentationEntry: Hashable, Sendable {
 
-    public var key: String
-    public var value: String
+    public let key: String
+    public let value: String
 
     public init(key: String, value: String) {
         self.key = key
