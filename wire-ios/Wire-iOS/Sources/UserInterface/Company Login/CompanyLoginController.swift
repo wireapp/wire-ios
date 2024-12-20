0f79611f0bced5b89077712d8b9158abffd0cb76
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2024-12-20 13:02:50 +0100

    add log

--- wire-ios/Wire-iOS/Sources/UserInterface/Company Login/CompanyLoginController.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/Company Login/CompanyLoginController.swift
@@ -332,6 +332,7 @@ extension CompanyLoginController {
                     guard didConfirm else { return }
                     sessionManager.switchBackend(to: backendEnvironment) { error in
                         if let error {
+                            WireLogger.authentication.error("sso login flow failed due to backend switch error: \(String(describing: error))")
                             self.presentCompanyLoginAlert(error: .unknown)
                         }
 
