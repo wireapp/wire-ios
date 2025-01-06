51b0e8cc3fbe7f13d769f33898fb81eee57e6d08
Sam Wyndham <samwyndham@users.noreply.github.com>
2025-01-06 15:42:18 +0100

    fix: Call notifications from self user - WPB-14925 (#2349)

--- wire-ios-notification-engine/Sources/NotificationSession.swift
+++ wire-ios-notification-engine/Sources/NotificationSession.swift
@@ -508,9 +508,10 @@ extension NotificationSession: PushNotificationStrategyDelegate {
 
         // Should not handle a call if the caller is a self user and it's an incoming call or call end.
         // The caller can be the same as the self user if it's a rejected call or answered elsewhere.
-        if let selfUserID = ZMUser.selfUser(in: context).remoteIdentifier,
-           let callerID = callContent.callerID,
-           callerID == selfUserID,
+        let selfUser = ZMUser.selfUser(in: context)
+        if let callerID = callContent.callerID,
+           callerID.identifier == selfUser.remoteIdentifier,
+           callerID.domain == selfUser.domain,
            callContent.isIncomingCall || callContent.isEndCall {
             WireLogger.calling.info("should not handle call event: self call")
             return nil
@@ -599,7 +600,7 @@ extension NotificationSession {
             guard
                 let callState = callEventContent.callState,
                 let callerID = callEventContent.callerID,
-                let caller = ZMUser.fetch(with: callerID, domain: event.senderDomain, in: context),
+                let caller = ZMUser.fetch(with: callerID.identifier, domain: callerID.domain, in: context),
                 caller != ZMUser.selfUser(in: context),
                 !isEventTimedOut(currentTimestamp: currentTimestamp, eventTimestamp: event.timestamp)
             else {
