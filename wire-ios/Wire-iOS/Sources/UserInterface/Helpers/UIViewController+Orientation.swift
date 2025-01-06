bc7bf8f804167652b461259bab270dad7fc95f92
Sam Wyndham <samwyndham@users.noreply.github.com>
2025-01-06 16:59:28 +0100

    Update `wr_supportedInterfaceOrientations` to return `all` for iPad, otherwise `portrait`.
    The current implementation doesn't really make sense because:
    
    - In the case of iPad the app attempts to adjust supported orientations based on horizontal size class.
    - In iPad fullscreen the size class is always `regular`.
    - If the app supports multitasking, `supportedInterfaceOrientations` is never called because multitasking apps must support all interface orientations.

--- wire-ios/Wire-iOS/Sources/UserInterface/Helpers/UIViewController+Orientation.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/Helpers/UIViewController+Orientation.swift
@@ -20,16 +20,14 @@ import UIKit
 
 extension UIViewController {
 
-    /// return the default supported interface orientations of a view controller
-    /// return .all only if the idiom is .pad and size class is .regular
+    /// The default supported interface orientations of a view controller
+    /// - returns: `.all` for iPad otherwise `.portrait`.
     var wr_supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        switch (UIDevice.current.userInterfaceIdiom, traitCollection.horizontalSizeClass) {
-        case (.pad, .regular),
-             // Notice: for iPad with iOS9 in landscape mode, horizontalSizeClass is .unspecified (it is .regular in iOS11).
-             (.pad, .unspecified):
-            .all
+        switch UIDevice.current.userInterfaceIdiom {
+        case .pad:
+            return .all
         default:
-            .portrait
+            return .portrait
         }
     }
 }
