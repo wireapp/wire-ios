b40944b1ea06c1711ebc95d3da0de95120e1f164
François Benaiteau <netbe@users.noreply.github.com>
2025-01-07 23:49:29 +0100

    feat: simulcast support - WPB-11481 (#2251)

--- wire-ios-sync-engine/Source/Calling/WireCallCenterV3.swift
+++ wire-ios-sync-engine/Source/Calling/WireCallCenterV3.swift
@@ -909,7 +909,7 @@ public extension WireCallCenterV3 {
     /// - Parameters:
     ///   - conversationId: The identifier of the conversation where the video call is hosted.
     ///   - clients: The list of clients for which AVS should load video streams.
-    func requestVideoStreams(conversationId: AVSIdentifier, clients: [AVSClient]) {
+    func requestVideoStreams(conversationId: AVSIdentifier, clients: [AVSClientVideoStream]) {
         let videoStreams = AVSVideoStreams(conversationId: conversationId.serialized, clients: clients)
         avsWrapper.requestVideoStreams(videoStreams, conversationId: conversationId)
     }
