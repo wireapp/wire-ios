019b0a156bd73753492179924df82a1117eda593
Christoph Aldrian <christoph.aldrian@wire.com>
2025-01-10 14:18:18 +0100

    delete not needed files

--- WireDomain/Package.swift
+++ WireDomain/Package.swift
@@ -7,42 +7,15 @@ let package = Package(
     name: "WireDomainPackage",
     platforms: [.iOS(.v16), .macOS(.v12)],
     products: [
-        .library(name: "WireDomainAPI", targets: ["WireDomainAPI"]),
-        .library(name: "WireDomainPackage", targets: ["WireDomainPkg"]),
-        .library(name: "WireDomainPackageSupport", targets: ["WireDomainPkgSupport"])
+        .library(name: "WireDomainAPI", targets: ["WireDomainAPI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0"),
-        .package(path: "../WirePlugins"),
-        .package(name: "WireAPI", path: "../WireAPI"),
-        .package(name: "WireAnalytics", path: "../WireAnalytics"),
-        .package(name: "WireFoundation", path: "../WireFoundation")
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0")
     ],
     targets: [
         .target(
             name: "WireDomainAPI",
             path: "./Sources/WireDomainAPI"
-        ),
-        .target(
-            name: "WireDomainPkg",
-            dependencies: ["WireAPI", "WireAnalytics"],
-            path: "./Sources/Package"
-        ),
-        .target(
-            name: "WireDomainPkgSupport",
-            dependencies: ["WireDomainPkg"],
-            path: "./Sources/PackageSupport",
-            plugins: [
-                .plugin(name: "SourceryPlugin", package: "WirePlugins")
-            ]
-        ),
-        .testTarget(
-            name: "WireDomainPkgTests",
-            dependencies: [
-                "WireDomainPkg",
-                .product(name: "WireTestingPackage", package: "WireFoundation")
-            ],
-            path: "./Tests/PackageTests"
         )
     ]
 )
