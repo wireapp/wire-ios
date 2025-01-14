3c24a58f08bc5f840427d729763211e5741b6670
François Benaiteau <francois.benaiteau@wire.com>
2025-01-14 22:21:51 +0100

    remove test

--- wire-ios-sync-engine/Tests/Source/Synchronization/ZMOperationLoopTests.m
+++ wire-ios-sync-engine/Tests/Source/Synchronization/ZMOperationLoopTests.m
@@ -482,20 +482,6 @@
               };
 }
 
-- (NSDictionary *)encryptedPushPayload
-{
-    return @{
-             @"aps" : @{@"alert": @{@"loc-args": @[],
-                                    @"loc-key": @"push.notification.new_message"}
-                        },
-             @"data": @{
-                     @"data" : @"70XpQ4qri2D4YCU7lvSjaqk+SgN/s4dDv/J8uMUel0xY8quNetPF8cMXskAZwBI9EArjMY/NupWo8Bar14GHi9ISzlOswDsoQ6BQiFsEdnv4shT+ZpJ+wghmPF+sxWhys9048ny6WiSqywUNzsUPjDrudAAiG4bPjS2FjMou2/o7FpCg7+6p8fcSYCcvQllv6P8oidVbMlpnT1Bs7fK6fz9ceq6H3L+BKZai82H7gc6nxSS5Gjf56qvDqdc3J9jTowpdjyqHGO26YahMQtDf4tn6KuTSp4OG1qLPk6jFf4xO2q/WrxV2dnoXGXWbIZ4cnohkeA85QxMhpM9pIGAbZ58fRUt9fPXm6PmX3rqQY7MSv4TV1fLyb5Zqo/yqQbcE2qS/dJKRrzwW5MWlKVWfacuNRZnansMMGUYyt7iRpD/E8PdtSfW7QO/02Evureor7MqQ8AYf6Ivt3Ksf1wplXne0zl8CT5GMeExB7DLfyr8T1xK6H+u3y29FmI9/T01la5cbIq/E83Yh2LTNo3X4eOfZ6mhC0EIC8YEyo/0x2IHsLyCAjzvIFfTSD8tOpa1yQTBSQ3mGGDWiPJ3f6OypQFj+vY13Bq9WZoL9Q+UbYbxdzkaYILaX2UakZ5OafQ7nH0WslvfzjRsdYoruTGDV+E8mXB2JOZh9ij2PT8fWsyJJ9DqKg5Iw2EPfUlXBv3pXIpZuL6+g8c2von092bV2pHTWkPE4A2yvw3LTzI8e9puOr5K87JUQHdR7mfXYifErW+9TRrmBibF5wKZtVl97UOFOps4/ZXU9i6Lr0qKKMdX3iruo7o3fYcbJTajb+sZLttDPsKnJHnnMxJUB3D+I1UuA35hL6Fy2wLj2mRNAzWuitNj9MSDUhDHU42+bZnap",
-                     @"mac": @"ZGe7fjgAEvTjfSSv2MuDHQe7BCRj2NT7qg8OAm8JZyI=",
-                     @"type": @"cipher"
-                     }
-             };
-}
-
 - (void)testThatItForwardsEventsFromSilentPushesToThePushNotificationStatus
 {
     // given
@@ -513,19 +499,6 @@
     XCTAssertTrue(self.pushNotificationStatus.hasEventsToFetch);
 }
 
-
-- (void)testThatItForwardsEventsFromEncryptedPushesToThePushNotificationStatus
-{
-    // given
-    NSDictionary *pushPayload = [self encryptedPushPayload];
-    
-    // when
-    [self.sut fetchEventsFromPushChannelPayload:pushPayload completionHandler:^{}];
-    WaitForAllGroupsToBeEmpty(0.5);
-    
-    // then
-    XCTAssertTrue(self.pushNotificationStatus.hasEventsToFetch);
-}
 
 - (void)testThatItForwardsNoticeNotificationsToThePushNotificationStatus
 {
