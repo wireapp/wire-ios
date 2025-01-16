6f8fa810dc4ecb2f09d795ff2e440ab2bfe33d3a
Christoph Aldrian <christoph.aldrian@wire.com>
2025-01-16 17:27:56 +0100

    copy some code

--- wire-ios-sync-engine/Source/Use cases/ImportBackupUseCase.swift
+++ wire-ios-sync-engine/Source/Use cases/ImportBackupUseCase.swift
@@ -16,8 +16,92 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
+import Foundation
+import WireSystem
+
 struct ImportBackupUseCase: ImportBackupUseCaseProtocol {
+
+    var activeUserSession: ZMUserSession
+    var dispatchGroup: ZMSDispatchGroup
+
+    private let workerQueue = DispatchQueue(label: "import-backup")
+
     func invoke(url: URL, password: String) async throws {
-        fatalError()
+
+        let account = activeUserSession.account
+
+        // Verify the imported file has the correct file extension.
+        guard BackupFileExtensions(rawValue: url.pathExtension) != nil else {
+            throw BackupError.invalidFileExtension
+        }
+
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, any Error>) in
+            workerQueue.async(group: dispatchGroup) { [weak self] in
+//                guard let self else {
+//                    completion(.failure(NSError(
+//                        userSessionErrorCode: .unknownError,
+//                        userInfo: ["reason": "SessionManager.self is `nil` in restoreFromBackup"]
+//                    )))
+//                    return
+//                }
+//
+//                let decryptedURL = SessionManager.temporaryURL(for: location)
+//
+//                WireLogger.localStorage.debug("coordinated file access at: \(location.absoluteString)")
+//
+//                do {
+//                    try SessionManager.decrypt(
+//                        from: location,
+//                        to: decryptedURL,
+//                        password: password,
+//                        accountId: userId
+//                    )
+//                } catch ChaCha20Poly1305.StreamEncryption.EncryptionError.decryptionFailed {
+//                    return complete(.failure(BackupError.decryptionError))
+//
+//                } catch ChaCha20Poly1305.StreamEncryption.EncryptionError.keyGenerationFailed {
+//                    return complete(.failure(BackupError.keyCreationFailed))
+//
+//                } catch {
+//                    return complete(.failure(error))
+//                }
+//
+//                let url = SessionManager.unzippedBackupURL(for: location)
+//
+//                guard decryptedURL.unzip(to: url) else {
+//                    return complete(.failure(BackupError.compressionError))
+//                }
+//
+//                CoreDataStack.importLocalStorage(
+//                    accountIdentifier: userId,
+//                    from: url,
+//                    applicationContainer: sharedContainerURL,
+//                    dispatchGroup: dispatchGroup
+//                ) { result in
+//                    completion(result.map { _ in })
+//                }
+            }
+        }
     }
+}
+
+// MARK: -
+
+/// There are some external apps that users can use to transfer backup files, which can modify their attachments and
+/// change the underscore with a dash. For this reason, we accept 2 types of file extensions to restore conversations.
+private enum BackupFileExtensions: String, CaseIterable {
+    case fileExtensionWithUnderscore = "ios_wbu"
+    case fileExtensionWithHyphen = "ios-wbu"
+}
+
+// MARK: -
+
+private enum BackupError: Error {
+//    case notAuthenticated
+//    case noActiveAccount
+//    case compressionError
+    case invalidFileExtension
+//    case keyCreationFailed
+//    case decryptionError
+//    case unknown
 }
