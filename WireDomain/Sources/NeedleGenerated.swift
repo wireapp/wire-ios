b81ad18acd7cecb94a178853722e2362c106585c
Jullian Mercier <31648126+jullianm@users.noreply.github.com>
2025-01-20 16:43:27 +0100

    integrate Needle

---
+++ WireDomain/Sources/NeedleGenerated.swift
@@ -0,0 +1,45 @@
+
+
+import Combine
+import NeedleFoundation
+import WireAPI
+import WireDataModel
+
+// swiftlint:disable unused_declaration
+private let needleDependenciesHash : String? = nil
+
+// MARK: - Traversal Helpers
+
+private func parent1(_ component: NeedleFoundation.Scope) -> NeedleFoundation.Scope {
+    return component.parent
+}
+
+// MARK: - Providers
+
+#if !NEEDLE_DYNAMIC
+
+
+#else
+
+
+#endif
+
+private func factoryEmptyDependencyProvider(_ component: NeedleFoundation.Scope) -> AnyObject {
+    return EmptyDependencyProvider(component: component)
+}
+
+// MARK: - Registration
+private func registerProviderFactory(_ componentPath: String, _ factory: @escaping (NeedleFoundation.Scope) -> AnyObject) {
+    __DependencyProviderRegistry.instance.registerDependencyProviderFactory(for: componentPath, factory)
+}
+
+#if !NEEDLE_DYNAMIC
+
+
+#endif
+
+public func registerProviderFactories() {
+#if !NEEDLE_DYNAMIC
+    
+#endif
+}
