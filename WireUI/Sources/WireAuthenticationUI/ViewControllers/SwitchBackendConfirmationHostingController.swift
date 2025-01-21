5be5c76794bf71a69520037bf789949747f33391
KaterinaWire <katerina@wire.com>
2025-01-21 17:23:21 +0100

    fix swiftformat issues

--- WireUI/Sources/WireAuthenticationUI/ViewControllers/SwitchBackendConfirmationHostingController.swift
+++ WireUI/Sources/WireAuthenticationUI/ViewControllers/SwitchBackendConfirmationHostingController.swift
@@ -56,9 +56,9 @@ private extension UISheetPresentationController.Detent.Identifier {
     var oppositeValue: UISheetPresentationController.Detent.Identifier {
         switch self {
         case .medium:
-                .large
+            .large
         case .large:
-                .medium
+            .medium
         default:
             fatalError("Unsupported value")
         }
