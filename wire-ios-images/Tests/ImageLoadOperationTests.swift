e6f6238a7bc124454fb846580fedab2fa0475afc
Sam Wyndham <samwyndham@users.noreply.github.com>
2025-01-24 16:45:28 +0100

    Migrate `ImageLoadOperationTests.testThatItLoadsJPEGData()`

--- wire-ios-images/Tests/ImageLoadOperationTests.swift
+++ wire-ios-images/Tests/ImageLoadOperationTests.swift
@@ -16,12 +16,39 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
+//import MobileCoreServices
+import UniformTypeIdentifiers;
 import WireTesting
 import XCTest
 
 @testable import WireImages
 
 final class ImageLoadOperationTests: ZMTBaseTest {
+
+    func testThatItLoadsJPEGData() throws {
+        // given
+        let imageData = try XCTUnwrap(data(forResource: "unsplash_medium_exif_2", extension: "jpg"))
+        let sut = try XCTUnwrap(ZMImageLoadOperation(imageData: imageData))
+        expectation(for: NSPredicate(format: "isFinished == YES"), evaluatedWith: sut, handler: nil)
+
+        // when
+        sut.start()
+        waitForExpectations(timeout: 5)
+
+        // then
+        let tiffDictionary = try XCTUnwrap(
+            sut.sourceImageProperties[kCGImagePropertyTIFFDictionary] as? [AnyHashable: Any]
+        )
+        XCTAssertNotNil(sut.cgImage)
+        XCTAssertEqual(tiffDictionary[kCGImagePropertyTIFFOrientation] as? Int, 2)
+        XCTAssertEqual(sut.sourceImageProperties[kCGImagePropertyOrientation] as? Int, 2)
+        XCTAssertEqual(sut.sourceImageProperties[kCGImagePropertyPixelHeight] as? Double, 346)
+        XCTAssertEqual(sut.sourceImageProperties[kCGImagePropertyPixelWidth] as? Double, 531)
+        XCTAssertEqual(sut.originalImageData, imageData)
+        XCTAssertEqual(sut.computedImageProperties.mimeType, UTType.jpeg.identifier)
+        XCTAssertEqual(sut.tiffOrientation, 2);
+        XCTAssertEqual(sut.computedImageProperties.size, CGSize(width: 531, height: 346))
+    }
 
     func testThatItDoesNotLoadWhenCancelled() throws {
         // given
