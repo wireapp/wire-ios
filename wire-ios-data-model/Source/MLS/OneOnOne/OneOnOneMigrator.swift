19f81fe38e603969bcba69fa08a4948964eea40c
François Benaiteau <francois.benaiteau@wire.com>
2025-01-24 09:08:58 +0100

    Revert "fix message is inserted on new mls conversations"
    48f12e11af44fd3eeea9018ef6d19beb796e7e94

--- wire-ios-data-model/Source/MLS/OneOnOne/OneOnOneMigrator.swift
+++ wire-ios-data-model/Source/MLS/OneOnOne/OneOnOneMigrator.swift
@@ -169,10 +169,6 @@ public struct OneOnOneMigrator: OneOnOneMigratorInterface {
             if !proteusConversations.isEmpty {
                 // update just to be sure
                 mlsConversation.needsToBeUpdatedFromBackend = true
-            } else {
-                // we migrated from proteus, let's inform the user
-                let sender = ZMUser.selfUser(in: context)
-                mlsConversation.appendMLSMigrationFinalizedSystemMessage(sender: sender, at: .now)
             }
 
             // switch active conversation
