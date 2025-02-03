fc088579327c8d8dff660ad36da13f44e2716c2a
Sam Wyndham <samwyndham@users.noreply.github.com>
2025-02-03 10:43:41 +0100

    Create `WireAuthentication` SPM

---
+++ WireAuthentication/Tests/WireAuthenticationTests/WireAuthenticationTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+@testable import WireAuthentication
+
+final class WireAuthenticationTests: XCTestCase {
+    func testExample() throws {
+        XCTFail()
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
