337766a53698282195616446897aa612eb3f3f1f
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2025-02-06 14:07:07 +0100

    refactor: add sync to pull self user settings - WPB-10801

---
+++ WireDomain/Tests/WireDomainTests/Synchronization/PullSelfUserSettingsSyncTests.swift
@@ -0,0 +1,60 @@
+//
+// Wire
+// Copyright (C) 2025 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import WireAPISupport
+import XCTest
+@testable import WireAPI
+@testable import WireDomain
+@testable import WireDomainSupport
+
+final class PullSelfUserSettingsSyncTests: XCTestCase {
+
+    private var sut: PullSelfUserSettingsSync!
+    private var api: MockUserPropertiesAPI!
+    private var store: MockUserLocalStoreProtocol!
+
+    override func setUp() async throws {
+        api = MockUserPropertiesAPI()
+        store = MockUserLocalStoreProtocol()
+        sut = PullSelfUserSettingsSync(api: api, store: store)
+    }
+
+    override func tearDown() async throws {
+        api = nil
+        store = nil
+        sut = nil
+    }
+
+    func testPull() async throws {
+        // Mock
+        api.areReadReceiptsEnabledClosure = { true }
+        store.updateSelfUserReadReceiptsIsReadReceiptsEnabledIsReadReceiptsEnabledChangedRemotely_MockMethod = { _, _ in }
+
+        // When
+        try await sut.pull()
+
+        // Then
+        XCTAssertEqual(api.areReadReceiptsEnabledCallsCount, 1)
+
+        let storeInvocations = store.updateSelfUserReadReceiptsIsReadReceiptsEnabledIsReadReceiptsEnabledChangedRemotely_Invocations
+        try XCTAssertCount(storeInvocations, count: 1)
+        XCTAssertEqual(storeInvocations[0].isReadReceiptsEnabled, true)
+        XCTAssertEqual(storeInvocations[0].isReadReceiptsEnabledChangedRemotely, false)
+    }
+
+}
