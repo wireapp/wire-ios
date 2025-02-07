8e4d8776702d3816df31dbc90038c33e2703f09c
KaterinaWire <katerina@wire.com>
2025-02-07 12:42:18 +0100

    add documentation

--- WireAPI/Sources/WireAPI/APIs/AuthenticationAPI/AuthenticationAPI.swift
+++ WireAPI/Sources/WireAPI/APIs/AuthenticationAPI/AuthenticationAPI.swift
@@ -21,6 +21,14 @@ import Foundation
 public protocol AuthenticationAPI {
 
     /// Login via email
+    ///
+    /// - Parameters:
+    ///   - email: Email address of the account
+    ///   - password: Password
+    ///   - verificationCode: The verification code is sent to the given user’s email address,
+    ///   this is an optional field and depends on the team/server settings.
+    ///   - label: An optional label to associate with the access token.
+    /// - Returns: HTTP cookie, a valid access token.
 
     func login(
         email: String,
