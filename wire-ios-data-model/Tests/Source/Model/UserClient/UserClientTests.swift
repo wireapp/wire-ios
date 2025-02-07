e2449ac7628c1a179afdb40d7a64616b9acb5d39
François Benaiteau <francois.benaiteau@wire.com>
2025-02-07 13:10:52 +0100

    Revert "chore: remove unused files - WPB-8605 (#2221)"
    This reverts commit fa8a97deae5b9480dd031eeabf37f2e8e2f8b735.

--- wire-ios-data-model/Tests/Source/Model/UserClient/UserClientTests.swift
+++ wire-ios-data-model/Tests/Source/Model/UserClient/UserClientTests.swift
@@ -122,6 +122,7 @@ final class UserClientTests: ZMBaseManagedObjectTest {
             ZMUserClientMarkedToDeleteKey,
             ZMUserClientNumberOfKeysRemainingKey,
             ZMUserClientMissingKey,
+            ZMUserClientNeedsToUpdateSignalingKeysKey,
             ZMUserClientNeedsToUpdateCapabilitiesKey,
             UserClient.needsToUploadMLSPublicKeysKey
         ])
@@ -548,6 +549,43 @@ extension UserClientTests {
         // then
         XCTAssertFalse(otherClient.verified)
     }
+}
+
+// MARK: SignalingStore
+
+extension UserClientTests {
+
+    func testThatItDeletesExistingSignalingKeys() {
+
+        // given
+        let selfClient = createSelfClient()
+        selfClient.apsVerificationKey = Data()
+        selfClient.apsDecryptionKey = Data()
+
+        XCTAssertNotNil(selfClient.apsVerificationKey)
+        XCTAssertNotNil(selfClient.apsDecryptionKey)
+
+        // when
+        UserClient.resetSignalingKeysInContext(uiMOC)
+
+        // then
+        XCTAssertNil(selfClient.apsVerificationKey)
+        XCTAssertNil(selfClient.apsDecryptionKey)
+    }
+
+    func testThatItSetsKeysNeedingToBeSynced() {
+
+        // given
+        let selfClient = createSelfClient()
+
+        // when
+        UserClient.resetSignalingKeysInContext(uiMOC)
+
+        // then
+        XCTAssertTrue(selfClient.needsToUploadSignalingKeys)
+        XCTAssertTrue(selfClient.keysThatHaveLocalModifications.contains(ZMUserClientNeedsToUpdateSignalingKeysKey))
+    }
+
 }
 
 // MARK: Capabilities
