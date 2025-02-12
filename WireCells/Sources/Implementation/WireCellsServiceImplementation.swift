31a180aefe385bd0c984124bac5298bf449afb54
El-Fitz <8971906+El-Fitz@users.noreply.github.com>
2025-02-12 16:34:30 +0100

    fix: update WireCellsAPI import

--- WireCells/Sources/Implementation/WireCellsServiceImplementation.swift
+++ WireCells/Sources/Implementation/WireCellsServiceImplementation.swift
@@ -16,7 +16,7 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-import API
+import WireCellsAPI
 import AWSClientRuntime
 @preconcurrency import AWSS3
 import AWSSDKIdentity
