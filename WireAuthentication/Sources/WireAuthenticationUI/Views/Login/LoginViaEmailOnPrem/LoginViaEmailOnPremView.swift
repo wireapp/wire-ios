98534dcf3a4a1584c3bcc312dc65959e143ec6cd
KaterinaWire <katerina@wire.com>
2025-02-13 17:33:01 +0100

    temp

--- WireAuthentication/Sources/WireAuthenticationUI/Views/Login/LoginViaEmailOnPrem/LoginViaEmailOnPremView.swift
+++ WireAuthentication/Sources/WireAuthenticationUI/Views/Login/LoginViaEmailOnPrem/LoginViaEmailOnPremView.swift
@@ -91,7 +91,9 @@ package struct LoginViaEmailOnPremView: View {
                 showCustomBackendAlert.toggle()
             }, label: {
                 Text(L10n.OnPremUserLogin.title(viewModel.backendName))
-                    .foregroundColor(ColorTheme.Buttons.Secondary.onEnabled.color) + Text(" ") + Text(Image(systemName: "info.circle"))
+                    .foregroundColor(ColorTheme.Buttons.Secondary.onEnabled.color)
+              + Text(" ")
+              + Text(Image(systemName: "info.circle"))
                     .foregroundColor(.gray)
             })
             .multilineTextAlignment(.center)
