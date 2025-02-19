cac0443ce48ca749bcdea91d5310bfb3c84d3a8a
Sarah <sarah.hutchins@wire.com>
2025-02-19 18:19:44 +0100

    Fixing the backup/restore flow

--- wire-ios-automation/ios/src/main/java/com/wearezeta/auto/ios/pages/BackupRestorePage.java
+++ wire-ios-automation/ios/src/main/java/com/wearezeta/auto/ios/pages/BackupRestorePage.java
@@ -9,11 +9,38 @@ public class BackupRestorePage extends IOSPage {
   @iOSXCUITFindBy(accessibility = "Back Up Now")
   WebElement backupAction;
 
+  @iOSXCUITFindBy(accessibility = "Restore from Backup")
+  WebElement restoreAction;
+
+  @iOSXCUITFindBy(accessibility = "Proceed")
+  WebElement proceed;
+
+  @iOSXCUITFindBy(accessibility = "password input")
+  WebElement password;
+
+  @iOSXCUITFindBy(accessibility = "Continue")
+  WebElement continueButton;
+
   public BackupRestorePage(WebDriver driver) {
     super(driver);
   }
 
   public void startBackUp() {
     backupAction.click();
+  }
+
+  public void startRestore() {
+    restoreAction.click();
+  }
+
+  public void restoreProceed() {
+    proceed.click();
+  }
+
+  public void inputRestorePassword(String backupPassword) {
+    waitUntilElementVisible(password);
+    password.sendKeys(backupPassword);
+    waitUntilElementClickable(continueButton);
+    continueButton.click();
   }
 }
