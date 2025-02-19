b05e848f761baa19109aa0eca228207702eaa2e2
Christoph Aldrian <caldrian@users.noreply.github.com>
2025-02-19 12:03:44 +0100

    chore: support API v8 - WPB-15840 (#2562)

--- wire-ios-request-strategy/Sources/Request Strategies/MLS/Actions/FetchBackendMLSPublicKeysActionHandler.swift
+++ wire-ios-request-strategy/Sources/Request Strategies/MLS/Actions/FetchBackendMLSPublicKeysActionHandler.swift
@@ -32,7 +32,7 @@ class FetchBackendMLSPublicKeysActionHandler: ActionHandler<FetchBackendMLSPubli
             action.fail(with: .endpointUnavailable)
             return nil
 
-        case .v5, .v6, .v7:
+        case .v5, .v6, .v7, .v8:
             return ZMTransportRequest(
                 path: "/mls/public-keys",
                 method: .get,
