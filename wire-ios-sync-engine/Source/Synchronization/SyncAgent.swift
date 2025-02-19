226d55492a3aa682322e7eb5b797a604158b19df
Jullian Mercier <31648126+jullianm@users.noreply.github.com>
2025-02-19 12:47:29 +0100

    fix compilation error

--- wire-ios-sync-engine/Source/Synchronization/SyncAgent.swift
+++ wire-ios-sync-engine/Source/Synchronization/SyncAgent.swift
@@ -112,7 +112,7 @@ final class SyncAgent: NSObject {
     /// Perform an incremental sync.
 
     func performIncrementalSync() async throws {
-        await legacySyncStatus.performQuickSync()
+        await legacySyncStatus.performQuickSync(isRecovering: false)
     }
 
 }
@@ -140,7 +140,7 @@ extension SyncAgent: ZMSyncStateDelegate {
         delegate?.syncAgentDidStartLegacyIncrementalSync(self)
     }
 
-    func didFinishQuickSync() {
+    func didFinishQuickSync(isRecovering: Bool) {
         WireLogger.sync.debug("did start finish incremental sync")
         delegate?.syncAgentDidFinishLegacyIncrementalSync(self)
     }
