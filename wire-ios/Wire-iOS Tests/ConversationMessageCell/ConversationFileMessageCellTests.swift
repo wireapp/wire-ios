b0e15af48c29fdaa07ef75c3680f764cb2cb9e3f
Christoph Aldrian <christoph.aldrian@wire.com>
2025-02-28 14:08:57 +0100

    update snapshot images

--- wire-ios/Wire-iOS Tests/ConversationMessageCell/ConversationFileMessageCellTests.swift
+++ wire-ios/Wire-iOS Tests/ConversationMessageCell/ConversationFileMessageCellTests.swift
@@ -20,7 +20,7 @@ import XCTest
 
 @testable import Wire
 
-final class ConversationFileMessageTests: ConversationMessageSnapshotTestCase { // TODO: check snapshots
+final class ConversationFileMessageTests: ConversationMessageSnapshotTestCase {
 
     var message: MockMessage!
     var mockSelfUser: MockUserType!
