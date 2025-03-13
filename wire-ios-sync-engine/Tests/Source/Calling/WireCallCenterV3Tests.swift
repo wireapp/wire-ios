f0d8cc2009de5b05fdccfaf73e90d6f10193c6db
François Benaiteau <francois.benaiteau@wire.com>
2025-03-13 10:40:36 +0100

    Revert "revert: simulcast support - WPB-11481 (#2475)"
    This reverts commit 156311e06bc0d41056115e3345f27c21c055997a.

--- wire-ios-sync-engine/Tests/Source/Calling/WireCallCenterV3Tests.swift
+++ wire-ios-sync-engine/Tests/Source/Calling/WireCallCenterV3Tests.swift
@@ -2367,7 +2367,9 @@ extension WireCallCenterV3Tests {
         let clients = [
             AVSClient(userId: selfUserID, clientId: clientId1),
             AVSClient(userId: otherUserID, clientId: clientId2)
-        ]
+        ].map { client in
+            AVSClientVideoStream(client: client, quality: .low)
+        }
 
         let expectedResult = AVSVideoStreams(conversationId: conversationId.serialized, clients: clients)
 
