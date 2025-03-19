d909c39dec0ef7cf456f1f6b26954b47e0aef803
François Benaiteau <francois.benaiteau@wire.com>
2025-03-19 11:15:36 +0100

    fix

--- WireAuthentication/Sources/WireAuthenticationUI/Views/Root/RootView.swift
+++ WireAuthentication/Sources/WireAuthenticationUI/Views/Root/RootView.swift
@@ -20,16 +20,16 @@ import SwiftUI
 import WireAuthenticationAPI
 
 package struct RootView: View {
-
+    
     package typealias Factory =
-        DetermineAuthMethodBuilder &
-        LoginViaEmailOnPremBuilder &
-        LoginViaSSOBuilder &
-        NoHistoryViewBuilder
-
+    DetermineAuthMethodBuilder &
+    LoginViaEmailOnPremBuilder &
+    LoginViaSSOBuilder &
+    NoHistoryViewBuilder
+    
     @StateObject var viewModel: RootViewModel
     let factory: any Factory
-
+    
     package init(
         viewModel: RootViewModel,
         factory: any Factory
@@ -37,58 +37,62 @@ package struct RootView: View {
         self._viewModel = StateObject(wrappedValue: viewModel)
         self.factory = factory
     }
-
+    
     package var body: some View {
         BackgroundView()
-            .sheet(item: $viewModel.modalDestination) { sheet in
-                switch sheet {
-                case .authFlow:
-                    NavigationStack(path: $viewModel.path) {
-                        factory.determineAuthMethodView()
-                    }
-                case let .onPremiseAuthFlow(environmentType, backendConfig, backendMetadata):
-                    NavigationStack(path: $viewModel.path) {
-                        factory.determineAuthMethodView(
-                            environmentType: environmentType,
-                            backendConfig: backendConfig,
-                            backendMetadata: backendMetadata
-                        )
-                    }
-                case let .noHistory(
-                    authenticationResult,
-                    didDetectDomainConflict
-                ):
-                    factory.noHistoryView(
-                        authenticationResult: authenticationResult,
-                        didDetectDomainConflict: didDetectDomainConflict
-                    )
-                case let .onPremiseLogin(
-                    email,
-                    environmentType,
-                    backendConfig,
-                    backendMetadata
-                ):
-                    factory.loginViaEmailOnPremView(
-                        email: email,
-                        environmentType: environmentType,
-                        backendConfig: backendConfig,
-                        backendMetadata: backendMetadata
-                    )
-                case let .ssoLogin(
-                    ssoURL,
-                    backendEnvironment
-                ):
-                    factory.loginViaSSOView(
-                        ssoURL: ssoURL,
-                        backendEnvironment: backendEnvironment
-                    )
-                }
+            .universalSheet(item: $viewModel.modalDestination) { item in
+                sheetContent(for: item)
             }
     }
-
+    
+    @ViewBuilder
+    private func sheetContent(for sheet: RootView.ModalDestination) -> some View {
+        switch sheet {
+        case .authFlow:
+            NavigationStack(path: $viewModel.path) {
+                factory.determineAuthMethodView()
+            }
+        case let .onPremiseAuthFlow(environmentType, backendConfig, backendMetadata):
+            NavigationStack(path: $viewModel.path) {
+                factory.determineAuthMethodView(
+                    environmentType: environmentType,
+                    backendConfig: backendConfig,
+                    backendMetadata: backendMetadata
+                )
+            }
+        case let .noHistory(
+            authenticationResult,
+            didDetectDomainConflict
+        ):
+            factory.noHistoryView(
+                authenticationResult: authenticationResult,
+                didDetectDomainConflict: didDetectDomainConflict
+            )
+        case let .onPremiseLogin(
+            email,
+            environmentType,
+            backendConfig,
+            backendMetadata
+        ):
+            factory.loginViaEmailOnPremView(
+                email: email,
+                environmentType: environmentType,
+                backendConfig: backendConfig,
+                backendMetadata: backendMetadata
+            )
+        case let .ssoLogin(
+            ssoURL,
+            backendEnvironment
+        ):
+            factory.loginViaSSOView(
+                ssoURL: ssoURL,
+                backendEnvironment: backendEnvironment)
+        }
+    }
+    
     package enum ModalDestination: Identifiable, Hashable {
         public var id: Self { self }
-
+        
         case authFlow
         case onPremiseAuthFlow(
             environmentType: BackendEnvironmentType,
@@ -110,9 +114,66 @@ package struct RootView: View {
             backendEnvironment: WireAuthenticationBackendEnvironment
         )
     }
-
 }
 
 #Preview {
     MockDependencies().rootView
+}
+
+extension View {
+    
+    nonisolated public func universalSheet<Item, Content>(
+        item: Binding<Item?>,
+        onDismiss: (() -> Void)? = nil,
+        @ViewBuilder content: @escaping (Item) -> Content
+    ) -> some View where Item: Identifiable, Content: View {
+        self.modifier(UniversalSheetModifier(item: item, onDismiss: onDismiss, content: content))
+    }
+}
+
+struct UniversalSheetModifier<Item: Identifiable, SheetContent: View>: ViewModifier {
+    
+    @Binding var item: Item?
+    var onDismiss: (() -> Void)?
+    var content: (Item) -> SheetContent
+    
+    @ViewBuilder
+    func body(content: Content) -> some View {
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            content.overlaySheet(item: $item, onDismiss: onDismiss, content: self.content)
+        } else {
+            content.sheet(item: $item, onDismiss: onDismiss, content: self.content)
+        }
+    }
+}
+
+extension View {
+    
+    
+    nonisolated public func overlaySheet<Item, Content>(
+        item: Binding<Item?>,
+        onDismiss: (() -> Void)? = nil,
+        @ViewBuilder content: @escaping (Item) -> Content
+    ) -> some View where Item: Identifiable, Content: View {
+        self.overlay(
+            Group {
+                if let value = item.wrappedValue {
+                    ZStack {
+                        Color.clear
+                            .contentShape(Rectangle())
+                            .allowsHitTesting(false) // Prevents background blocking taps
+                        
+                        content(value)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            .contentShape(Rectangle()) // Ensures only visible parts are interactive
+                            .allowsHitTesting(true) // Ensures taps go directly to content
+                    }
+                    .transition(.opacity)
+                }
+            }
+                .animation(.easeInOut, value: item.wrappedValue != nil)
+        )
+    }
 }
