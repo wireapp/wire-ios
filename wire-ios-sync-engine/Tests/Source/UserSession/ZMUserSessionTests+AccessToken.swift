a5a61f7b6dc0e76ff21ce1e371d54811b0bb2b7a
Jullian Mercier <31648126+jullianm@users.noreply.github.com>
2025-03-19 12:10:23 +0100

    add UTs for non renewed token

--- wire-ios-sync-engine/Tests/Source/UserSession/ZMUserSessionTests+AccessToken.swift
+++ wire-ios-sync-engine/Tests/Source/UserSession/ZMUserSessionTests+AccessToken.swift
@@ -24,12 +24,14 @@ import WireTransport
 
 final class ZMUserSessionTests_AccessToken: ZMUserSessionTestsBase {
     var selfClient: UserClient!
+    var otherClient: UserClient!
 
     override func setUp() {
         super.setUp()
 
         syncMOC.performGroupedAndWait {
-            self.selfClient = setupSelfClient(inMoc: syncMOC)
+            self.selfClient = createSelfClient()
+            self.otherClient = createClient(for: ZMUser.selfUser(in: syncMOC))
         }
         XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
     }
@@ -37,6 +39,7 @@ final class ZMUserSessionTests_AccessToken: ZMUserSessionTestsBase {
     override func tearDown() {
         super.tearDown()
         selfClient = nil
+        otherClient = nil
     }
 
     func test_itRenewsAccessTokenAfterClientRegistration_StartingFromApiV3() {
@@ -46,6 +49,18 @@ final class ZMUserSessionTests_AccessToken: ZMUserSessionTestsBase {
                     userClient: self.selfClient,
                     apiVersion: $0,
                     shouldRenew: $0 > .v2
+                )
+            }
+        }
+    }
+
+    func test_itDoesNotRenewAccessTokenAfterClientRegistration_StartingFromApiV3() {
+        syncMOC.performAndWait {
+            APIVersion.allCases.forEach {
+                test_accessTokenRenewalAfterClientRegistration(
+                    userClient: self.otherClient,
+                    apiVersion: $0,
+                    shouldRenew: false
                 )
             }
         }
