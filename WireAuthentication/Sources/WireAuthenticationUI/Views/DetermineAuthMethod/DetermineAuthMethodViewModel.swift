7abba0a1e13dd7f383682d26980c6aead6b28e39
François Benaiteau <francois.benaiteau@wire.com>
2025-04-02 17:03:28 +0200

    code review

--- WireAuthentication/Sources/WireAuthenticationUI/Views/DetermineAuthMethod/DetermineAuthMethodViewModel.swift
+++ WireAuthentication/Sources/WireAuthenticationUI/Views/DetermineAuthMethod/DetermineAuthMethodViewModel.swift
@@ -253,7 +253,7 @@ package final class DetermineAuthMethodViewModel: ObservableObject {
                 // No op
                 break
             case .noDefaultCodeAvailable:
-                router.popToRoot() // in case backend changed, we force SwiftUI to re-render the navigation
+                router.popToRoot() // clear the navigation stack before replacing root
                 router.presentSheet(.authFlow(backendInfo: backendInfo))
             case let .authenticationFailed(samlError):
                 WireLogger.authentication.error(
