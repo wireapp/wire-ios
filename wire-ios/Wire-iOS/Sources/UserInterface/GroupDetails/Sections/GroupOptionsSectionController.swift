045f650c4461add56af5c4fa0d06e86744fcc917
Dzmitry Simkin <dzmitry.simkin.ext@wire.com>
2025-04-02 12:03:26 +0200

    Do not show access option for not admins

--- wire-ios/Wire-iOS/Sources/UserInterface/GroupDetails/Sections/GroupOptionsSectionController.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/GroupDetails/Sections/GroupOptionsSectionController.swift
@@ -42,7 +42,7 @@ final class GroupOptionsSectionController: GroupDetailsSectionController {
             by user: UserType
         ) -> Bool {
             switch self {
-            case .channelAccess: conversation.groupType == .channel
+            case .channelAccess: user.canManageTeam && conversation.groupType == .channel
             case .notifications: user.canModifyNotificationSettings(in: conversation)
             case .guests:        user.canModifyAccessControlSettings(in: conversation)
             case .services:      user.canModifyAccessControlSettings(in: conversation) && conversation.botCanBeAdded
