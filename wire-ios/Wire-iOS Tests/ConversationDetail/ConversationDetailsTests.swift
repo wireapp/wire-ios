ca57402e50c46766a26229d55b042686ec93844c
Dzmitry Simkin <dzmitry.simkin.ext@wire.com>
2025-04-07 17:43:32 +0200

    Add tests

---
+++ wire-ios/Wire-iOS Tests/ConversationDetail/ConversationDetailsTests.swift
@@ -0,0 +1,72 @@
+//
+// Wire
+// Copyright (C) 2025 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Foundation
+import XCTest
+import WireTestingPackage
+@testable import Wire
+
+final class ConversationDetailsTests: XCTestCase {
+    
+    lazy var conversation = {
+        let conversation = MockGroupDetailsConversation()
+        conversation.isChannel = true
+        return conversation
+    }()
+    
+    let user = {
+        let user = MockUserType()
+        user.canManageTeam = false
+        user.isGroupAdminInConversation = false
+        return user
+    }()
+    
+    func testAccessOptionNotAllowedForGroup() {
+        user.canManageTeam = true
+        conversation.isChannel = false
+    
+        XCTAssertFalse(GroupOptionsSectionController.Option.channelAccess
+            .accessible(in: conversation, by: user))
+    }
+    
+    func testAccessOptionAllowed_ForChannel_andTeamAdmin() {
+        user.canManageTeam = true
+        
+        XCTAssertTrue(GroupOptionsSectionController.Option.channelAccess
+            .accessible(in: conversation, by: user))
+    }
+    
+    func testAccessOptionNotAllowed_ForChannel_Member() {
+        XCTAssertFalse(GroupOptionsSectionController.Option.channelAccess
+            .accessible(in: conversation, by: user))
+    }
+    
+    func testAccessOptionAllowed_ForChannel_MemberButTeamOwner() {
+        user.canManageTeam = true
+        
+        XCTAssertTrue(GroupOptionsSectionController.Option.channelAccess
+            .accessible(in: conversation, by: user))
+    }
+    
+    func testAccessOptionAllowed_ForChannel_ChannelOwner() {
+        user.isGroupAdminInConversation = true
+        
+        XCTAssertTrue(GroupOptionsSectionController.Option.channelAccess
+            .accessible(in: conversation, by: user))
+    }
+}
