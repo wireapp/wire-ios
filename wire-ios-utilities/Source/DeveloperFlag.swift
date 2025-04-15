bfa6a4635b4149155205ad3caa24bbf7bfd16bf2
KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
2025-04-15 20:33:03 +0200

    fix: Unable to send MLS messages - WPB-16922 (#2902)

--- wire-ios-utilities/Source/DeveloperFlag.swift
+++ wire-ios-utilities/Source/DeveloperFlag.swift
@@ -26,6 +26,7 @@ public enum DeveloperFlag: String, CaseIterable {
     case proteusViaCoreCrypto
     case forceDatabaseLoadingFailure
     case ignoreIncomingEvents
+    case skipMLSMessagesDecryption
     case debugDuplicateObjects
     case decryptAndStoreEventsSleep
     case forceCRLExpiryAfterOneMinute
@@ -46,6 +47,9 @@ public enum DeveloperFlag: String, CaseIterable {
 
         case .ignoreIncomingEvents:
             "Turn on to ignore incoming update events"
+
+        case .skipMLSMessagesDecryption:
+            "Turn on to skip MLS message decryption"
 
         case .debugDuplicateObjects:
             "Turn on to have actions to insert duplicate users, conversations, teams"
