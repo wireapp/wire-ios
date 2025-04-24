5a4cf22649841f8644957a61e436851f5140c1f2
Christoph Aldrian <christoph.aldrian@wire.com>
2025-04-24 13:58:05 +0200

    fix package configs

--- WireBackup/Package.swift
+++ WireBackup/Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         .library(name: "WireBackup", targets: ["WireBackup"])
     ],
     dependencies: [
+        .package(url: "https://github.com/rickclephas/KMP-NativeCoroutines.git", exact: "1.0.0-ALPHA-27"),
         .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0"),
         .package(path: "../WireFoundation"),
         .package(path: "../WirePlugins")
@@ -17,6 +18,8 @@ let package = Package(
         .target(
             name: "WireBackup",
             dependencies: [
+                "KaliumBackup",
+                .product(name: "KMPNativeCoroutinesAsync", package: "KMP-NativeCoroutines"),
                 "WireFoundation"
             ]
         ),
@@ -33,12 +36,22 @@ let package = Package(
                 "WireBackup",
                 "WireBackupSupport"
             ]
+        ),
+
+        .binaryTarget(
+            name: "KaliumBackup",
+            url: "https://media.githubusercontent.com/media/wireapp/wire-ios/ef4ecddb36eb5fe9c743b4fe48f224d8ef846532/WireDomain/Frameworks/KaliumBackup.xcframework.zip?download=true",
+            checksum: "e6165410781ef8207a8d5bdc02bd04f092517da37e9e5bb57879610685c31806"
         )
+        // .binaryTarget(
+        //     name: "KaliumBackup",
+        //     path: "../../wire-android/kalium/backup/build/XCFrameworks/release/KaliumBackup.xcframework"
+        // )
     ],
     swiftLanguageModes: [.v6]
 )
 
-for target in package.targets where target.name != "Clibsodium" {
+for target in package.targets where target.type != .binary {
     target.swiftSettings = (target.swiftSettings ?? []) + [
         .enableUpcomingFeature("InternalImportsByDefault"),
         .enableUpcomingFeature("ExistentialAny")
--- WireDomain/Package.swift
+++ WireDomain/Package.swift
@@ -16,18 +16,15 @@ let package = Package(
         .package(path: "../WireAPI"),
         .package(path: "../WireFoundation"),
         .package(path: "../WireLogging"),
-        .package(path: "../WirePlugins"),
-        .package(url: "https://github.com/rickclephas/KMP-NativeCoroutines.git", exact: "1.0.0-ALPHA-27")
+        .package(path: "../WirePlugins")
     ],
     targets: [
         .target(
             name: "WireDomainPackage",
             dependencies: [
-                "KaliumBackup",
                 "WireAPI",
                 "WireLogging",
-                "WireFoundation",
-                .product(name: "KMPNativeCoroutinesAsync", package: "KMP-NativeCoroutines")
+                "WireFoundation"
             ]
         ),
         .target(
@@ -42,21 +39,11 @@ let package = Package(
                 "WireDomainPackageSupport",
                 .product(name: "WireFoundationSupport", package: "WireFoundation")
             ]
-        ),
-
-        .binaryTarget(
-            name: "KaliumBackup",
-            url: "https://media.githubusercontent.com/media/wireapp/wire-ios/ef4ecddb36eb5fe9c743b4fe48f224d8ef846532/WireDomain/Frameworks/KaliumBackup.xcframework.zip?download=true",
-            checksum: "e6165410781ef8207a8d5bdc02bd04f092517da37e9e5bb57879610685c31806"
         )
-        // .binaryTarget(
-        //     name: "KaliumBackup",
-        //     path: "../../wire-android/kalium/backup/build/XCFrameworks/release/KaliumBackup.xcframework"
-        // )
     ]
 )
 
-for target in package.targets where target.type != .binary {
+for target in package.targets {
     target.swiftSettings = [
         .enableUpcomingFeature("InternalImportsByDefault"),
         .enableUpcomingFeature("ExistentialAny")
