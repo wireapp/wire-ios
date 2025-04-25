00f516d2e5142673b807659638a157094a948374
Jullian Mercier <31648126+jullianm@users.noreply.github.com>
2025-04-25 15:29:33 +0200

    fix sync status bar not going away on sync failure

--- wire-ios-sync-engine/Source/Synchronization/SyncAgent.swift
+++ wire-ios-sync-engine/Source/Synchronization/SyncAgent.swift
@@ -70,7 +70,10 @@ final class SyncAgent: NSObject {
             do {
                 try await performSync()
             } catch {
-                WireLogger.sync.error("failed to perform sync: \(String(describing: error))")
+                delegate?.syncAgentDidFailSyncing(
+                    self,
+                    error: error
+                )
             }
         }
     }
@@ -112,10 +115,9 @@ final class SyncAgent: NSObject {
                 WireLogger.sync.error("failed to perform new initial sync: \(String(describing: error))")
                 throw error
             }
-
+            // Incremental sync automatically follows the slow sync.
             try await performIncrementalSync()
         } else {
-            // Incremental sync automatically follows the slow sync.
             legacySyncStatus.forceSlowSync()
         }
     }
