d912d3898941dac62d70b377220a9b35c636ca11
Christoph Aldrian <christoph.aldrian@wire.com>
2025-04-28 16:19:59 +0200

    delete gray colors from catalog

--- WireUI/Sources/WireDesign/Colors/ColorTheme.swift
+++ WireUI/Sources/WireDesign/Colors/ColorTheme.swift
@@ -173,10 +173,23 @@ public enum ColorTheme {
 private extension UIColor {
 
     convenience init(light: ColorResource, dark: ColorResource) {
+        self.init(light: .init(resource: light), dark: .init(resource: dark))
+    }
+
+    convenience init(light: UIColor, dark: ColorResource) {
+        self.init(light: light, dark: .init(resource: dark))
+    }
+
+    convenience init(light: ColorResource, dark: UIColor) {
+        self.init(light: .init(resource: light), dark: dark)
+    }
+
+    convenience init(light: UIColor, dark: UIColor) {
         self.init { traits in
-            .init(resource: traits.userInterfaceStyle == .dark ? dark : light)
+            traits.userInterfaceStyle == .dark ? dark : light
         }
     }
+
 }
 
 public extension UIColor {
