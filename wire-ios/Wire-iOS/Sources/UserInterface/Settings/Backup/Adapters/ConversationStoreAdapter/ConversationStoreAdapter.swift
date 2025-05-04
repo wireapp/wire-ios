8a362b32fc08f0d9107e68b803c1b54afef87416
Christoph Aldrian <christoph.aldrian@wire.com>
2025-05-05 01:02:32 +0200

    minor corrections

--- wire-ios/Wire-iOS/Sources/UserInterface/Settings/Backup/Adapters/ConversationStoreAdapter/ConversationStoreAdapter.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/Settings/Backup/Adapters/ConversationStoreAdapter/ConversationStoreAdapter.swift
@@ -26,7 +26,7 @@ struct ConversationStoreAdapter<ConversationLocalStore>: ConversationStoreProtoc
     where ConversationLocalStore: ConversationLocalStoreProtocol {
     typealias QualifiedID = WireFoundation.QualifiedID
 
-    /// The context to call `perform(schedule:_:)` on.
+    /// The context to call `perform(schedule:_:)` on if needed.
     let context: NSManagedObjectContext
     let conversationLocalStore: ConversationLocalStore
 
