bd68589e7792d40998140f752dda08d76dfbe4ec
Christoph Aldrian <christoph.aldrian@wire.com>
2025-05-05 01:28:17 +0200

    fix crash

--- wire-ios/Wire-iOS/Sources/UserInterface/Settings/Backup/Adapters/ConversationStoreAdapter/ConversationStoreAdapter.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/Settings/Backup/Adapters/ConversationStoreAdapter/ConversationStoreAdapter.swift
@@ -61,7 +61,7 @@ extension ConversationStoreAdapter where ConversationLocalStore == WireDomain.Co
         self.context = context
         self.conversationLocalStore = ConversationLocalStore(
             context: context,
-            mlsService: context.mlsService,
+            mlsService: context.performAndWait { context.mlsService },
             messageLocalStore: MessageLocalStore(context: context)
         )
     }
