03324e8af2a8d357f998315bf48e083dbfe4a9ff
Christoph Aldrian <christoph.aldrian@wire.com>
2025-05-16 12:07:47 +0200

    update reference

--- WireBackup/Package.swift
+++ WireBackup/Package.swift
@@ -40,8 +40,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "KaliumBackup",
-            url: "https://github.com/wireapp/kalium/releases/download/backup%2F0.0.1/KaliumBackup.xcframework.zip",
-            checksum: "03e6e6398f65e0366a772b0b3ec710bd74d9e0482739781f649bcbd89be482ac"
+            url: "https://github.com/wireapp/kalium/releases/download/backup%2F0.0.2/KaliumBackup.xcframework.zip",
+            checksum: "1bfe39d56c89414d5c9c330ef2a250a2a2e837106e06bad2f89f4a38f927ea43"
         )
     ],
     swiftLanguageModes: [.v6]
