fa943323a76cd5de738631dce3fa3cccbb787b3b
KaterinaWire <katerina@wire.com>
2025-05-19 16:11:31 +0200

    chore: update checksum for CC 6.0.1

--- WireCoreCrypto/Package.swift
+++ WireCoreCrypto/Package.swift
@@ -22,14 +22,14 @@ let package = Package(
         .binaryTarget(
             name: "WireCoreCrypto",
             url: "https://github.com/wireapp/core-crypto/releases/download/v6.0.1/WireCoreCrypto.xcframework.zip",
-            checksum: "d291cf8ef997b1414448890446893465bb2147f23b396e92e6a8098de948b7f9"
+            checksum: "a75e11dd3ff4ec4ec6f455d8512a9f0b0d8b98dba12f62672ed97aa4c2072e81"
         ),
         // this is an internal dependency to WireCoreCrypto but currently needs to explictly
         // added as a dependency due to limitations of Swift packages.
         .binaryTarget(
             name: "WireCoreCryptoUniffi",
             url: "https://github.com/wireapp/core-crypto/releases/download/v6.0.1/WireCoreCryptoUniffi.xcframework.zip",
-            checksum: "4931c7473c83e157f5c89a6e6dda9a087d746e97f9b0a4443b106cb56e5b8789"
+            checksum: "93113420d7194ea38e5b7a6a4ab4827e7e6793f01d3e6b6df103db9e56732a03"
         )
     ]
 )
