a853fb6cf4a30561a83a16c204966bbdbbd6ae96
Christoph Aldrian <christoph.aldrian@wire.com>
2025-05-20 15:10:11 +0200

    clean up

--- WireDomain/Sources/WireDomain/Repositories/Message/Protocols/MessageLocalStoreProtocol.swift
+++ WireDomain/Sources/WireDomain/Repositories/Message/Protocols/MessageLocalStoreProtocol.swift
@@ -16,10 +16,8 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-public import CoreData
-
-import Foundation
-import WireDataModel
+public import Foundation
+public import WireDataModel
 
 // sourcery: AutoMockable
 /// Facilitate access to message related domain objects.
