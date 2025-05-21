e581d2be3fc47559b9c21e8e207f1379c9ccb62e
Jullian Mercier <31648126+jullianm@users.noreply.github.com>
2025-05-21 10:14:30 +0200

    fix: incremental sync observer crash - WPB-17644 (#3039)

--- wire-ios-sync-engine/Source/Synchronization/IncrementalSyncObserver.swift
+++ wire-ios-sync-engine/Source/Synchronization/IncrementalSyncObserver.swift
@@ -105,6 +105,7 @@ final class IncrementalSyncObserver: IncrementalSyncObserverProtocol {
                 cancellable = $decryptionState.sink { newDecryptionState in
                     if newDecryptionState == .done {
                         continuation.resume()
+                        cancellable?.cancel()
                     }
                 }
             }
