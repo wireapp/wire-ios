e64f6cfd65657e6f3ec4d0c340d62ae16c214849
Christoph Aldrian <christoph.aldrian@wire.com>
2025-05-22 15:22:30 +0200

    restore stubs for tests

--- WireBackup/Tests/WireBackupTests/UseCases/BackupImporterTests.swift
+++ WireBackup/Tests/WireBackupTests/UseCases/BackupImporterTests.swift
@@ -59,4 +59,59 @@ struct BackupImporterTests {
 
     // TODO: [WPB-16658] add more tests
 
+    @Test(arguments: [
+        "android-encrypted",
+        "android-unencrypted",
+        "ios-encrypted",
+        "ios-unencrypted",
+        "web-encrypted",
+        "web-unencrypted"
+    ])
+    func testImportingBackupFilesFromAllPlatforms(resource: String) async throws {
+
+        let workDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory())
+        defer { try? FileManager.default.removeItem(at: workDirectoryURL) }
+
+        let importer = BackupImporter(
+            selfUserID: QualifiedID(
+                id: UUID(uuidString: "cfc7f55a-2ccf-4557-b212-32b2c89bf1a2")!,
+                domain: "staging.zinfra.io"
+            ),
+            workDirectoryURL: workDirectoryURL,
+            fileUnarchiver: ZIPFoundationFileUnarchiver()
+        )
+
+        let backupURL = try #require(Bundle.module.url(forResource: resource, withExtension: "wbu"))
+        let password = resource.hasSuffix("-encrypted") ? password : ""
+        let result = try await importer.importBackup(from: backupURL, using: password)
+        let users = [BackupUser](result.usersPager).compactMap(UserBackupModel.init)
+        let conversations = [BackupConversation](result.conversationsPager).compactMap(ConversationBackupModel.init)
+        let messages = [BackupMessage](result.messagesPager).compactMap(MessageBackupModel.init)
+
+//        #expect(result.totalPagesCount == 3)
+//        #expect(result.usersPager.totalPages == 1)
+//        #expect(result.conversationsPager.totalPages == 1)
+//        #expect(result.messagesPager.totalPages == 1)
+//
+//        let users = result.usersPager.nextPage()
+//        let conversations = result.conversationsPager.nextPage()
+//        let messages = result.messagesPager.nextPage()
+
+        #expect(users.count == 1)
+        if !resource.hasPrefix("web-") { // some mismatch
+            #expect(users.first?.qualifiedID == "CFC7F55A-2CCF-4557-B212-32B2C89BF1A2@staging.zinfra.io")
+            #expect(users.first?.name == "CA Staging 94")
+        }
+        #expect(users.first?.handle == "ca-staging-94")
+
+//        #expect(conversations.size == 3)
+//        #expect(messages.size == 7)
+//
+//        let user = UserBackupModel(users.get(index: 0))
+//        user.
+
+        #expect(Bool(false))
+
+    }
+
 }
