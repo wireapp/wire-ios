a78568721578b3f6087dc8bf4ecde4e9388dbcd0
Christoph Aldrian <christoph.aldrian@wire.com>
2025-05-27 18:53:48 +0200

    finish team account option view

--- WireAuthentication/Sources/WireAuthenticationUI/Views/Registration/AccountTypeSelection/AccountTypeSelectionView.swift
+++ WireAuthentication/Sources/WireAuthenticationUI/Views/Registration/AccountTypeSelection/AccountTypeSelectionView.swift
@@ -17,6 +17,7 @@
 //
 
 import SwiftUI
+import WireDesign
 
 struct AccountTypeSelectorView: View {
 
@@ -42,7 +43,7 @@ struct AccountTypeSelectorView: View {
 
     @ViewBuilder
     private var scrollViewContent: some View {
-        VStack {
+        VStack(spacing: 24) {
             teamAccountView
             personalAccountView
         }
@@ -53,26 +54,71 @@ struct AccountTypeSelectorView: View {
         let background = RoundedRectangle(cornerRadius: 24, style: .circular)
             .stroke(.tint)
         VStack(spacing: 24) {
-            VStack(spacing: 8) {
-                Text(L10n.AccountTypeSelector.OptionTeam.title)
-                    .font(.callout.bold())
-                Text(L10n.AccountTypeSelector.OptionTeam.subtitle)
-            }
-            VStack {
-                Divider()
-                Text(verbatim: "abcd")
-                Divider()
-            }
+            teamAccountTitle
+            teamAccountFeatures
+            teamAccountButton
         }
         .padding(.horizontal, 16)
         .padding(.vertical, 24)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(background)
-        .padding()
+        .padding(.horizontal, 24)
+        .padding(.vertical, 16)
+    }
+
+    @ViewBuilder
+    private var teamAccountTitle: some View {
+        VStack(spacing: 8) {
+            Text(L10n.AccountTypeSelector.OptionTeam.title)
+                .font(.callout.bold())
+                .foregroundStyle(.tint)
+            Text(L10n.AccountTypeSelector.OptionTeam.subtitle)
+        }
+    }
+
+    @ViewBuilder
+    private var teamAccountFeatures: some View {
+        VStack(spacing: 12) {
+            Divider()
+            teamAccountFeature(L10n.AccountTypeSelector.OptionTeam.feature0)
+            Divider()
+            teamAccountFeature(L10n.AccountTypeSelector.OptionTeam.feature1)
+            Divider()
+        }
+        .padding(.horizontal, 16)
+    }
+
+    @ViewBuilder
+    private func teamAccountFeature(_ content: String) -> some View {
+        HStack(alignment: .top, spacing: 8) {
+            Image(.circleCheck)
+                .foregroundStyle(ColorTheme.Base.positive.color)
+                .padding(.vertical, 4)
+            Text(content)
+            Spacer()
+        }
+    }
+
+    @ViewBuilder
+    private var teamAccountButton: some View {
+        Button(L10n.AccountTypeSelector.OptionTeam.button) {
+            print("todo")
+        }
+        .wireButtonStyle(.primary)
+        .bold()
     }
 
     @ViewBuilder
     private var personalAccountView: some View {
+        Text(verbatim: "todo")
+    }
+
+}
+
+private struct OptionView: View {
+
+    var body: some View {
+        EmptyView()
     }
 
 }
