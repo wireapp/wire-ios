0afb62979bc0b89be024b812cb1122123b02ef32
Christoph Aldrian <christoph.aldrian@wire.com>
2025-05-28 07:23:38 +0200

    finish UI

--- WireAuthentication/Sources/WireAuthenticationUI/Views/Registration/AccountTypeSelection/AccountTypeSelectionView.swift
+++ WireAuthentication/Sources/WireAuthenticationUI/Views/Registration/AccountTypeSelection/AccountTypeSelectionView.swift
@@ -49,12 +49,14 @@ struct AccountTypeSelectorView: View {
         }
     }
 
+    // MARK: -
+
     @ViewBuilder
     private var teamAccountView: some View {
         let background = RoundedRectangle(cornerRadius: 24, style: .circular)
             .stroke(.tint)
         VStack(spacing: 24) {
-            teamAccountTitle
+            teamAccountTitles
             teamAccountFeatures
             teamAccountButton
         }
@@ -67,7 +69,7 @@ struct AccountTypeSelectorView: View {
     }
 
     @ViewBuilder
-    private var teamAccountTitle: some View {
+    private var teamAccountTitles: some View {
         VStack(spacing: 8) {
             Text(L10n.AccountTypeSelector.OptionTeam.title)
                 .font(.callout.bold())
@@ -102,23 +104,71 @@ struct AccountTypeSelectorView: View {
     @ViewBuilder
     private var teamAccountButton: some View {
         Button(L10n.AccountTypeSelector.OptionTeam.button) {
-            print("todo")
+            print("[WPB-17453]") // TODO: [WPB-17453] implement flow
         }
         .wireButtonStyle(.primary)
         .bold()
     }
 
+    // MARK: -
+
     @ViewBuilder
     private var personalAccountView: some View {
-        Text(verbatim: "todo")
+        let background = RoundedRectangle(cornerRadius: 24, style: .circular)
+            .stroke(ColorTheme.Strokes.outline.color)
+        VStack(spacing: 24) {
+            personalAccountTitles
+            personalAccountFeatures
+            personalAccountButton
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 24)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(background)
+        .padding(.horizontal, 24)
+        .padding(.vertical, 16)
     }
 
-}
+    @ViewBuilder
+    private var personalAccountTitles: some View {
+        VStack(spacing: 8) {
+            Text(L10n.AccountTypeSelector.OptionPersonal.title)
+                .font(.callout.bold())
+                .foregroundStyle(.tint)
+            Text(L10n.AccountTypeSelector.OptionPersonal.subtitle)
+        }
+    }
 
-private struct OptionView: View {
+    @ViewBuilder
+    private var personalAccountFeatures: some View {
+        VStack(spacing: 12) {
+            Divider()
+            personalAccountFeature(L10n.AccountTypeSelector.OptionPersonal.feature0)
+            Divider()
+            personalAccountFeature(L10n.AccountTypeSelector.OptionPersonal.feature1)
+            Divider()
+        }
+        .padding(.horizontal, 16)
+    }
 
-    var body: some View {
-        EmptyView()
+    @ViewBuilder
+    private func personalAccountFeature(_ content: String) -> some View {
+        HStack(alignment: .top, spacing: 8) {
+            Image(.circleCheck)
+                .foregroundStyle(ColorTheme.Base.positive.color)
+                .padding(.vertical, 4)
+            Text(content)
+            Spacer()
+        }
+    }
+
+    @ViewBuilder
+    private var personalAccountButton: some View {
+        Button(L10n.AccountTypeSelector.OptionPersonal.button) {
+            print("[WPB-17453]") // TODO: [WPB-17453] implement flow
+        }
+        .wireButtonStyle(.secondary)
+        .bold()
     }
 
 }
