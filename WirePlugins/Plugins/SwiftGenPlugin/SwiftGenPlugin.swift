77afa64c1630e3a1cff7bca681dee3a33ac49074
Christoph Aldrian <christoph.aldrian@wire.com>
2025-06-02 09:27:21 +0200

    restore deleted code

--- WirePlugins/Plugins/SwiftGenPlugin/SwiftGenPlugin.swift
+++ WirePlugins/Plugins/SwiftGenPlugin/SwiftGenPlugin.swift
@@ -35,6 +35,24 @@ struct SwiftGenPlugin: BuildToolPlugin {
                 environment: ["GENERATED": outputFilesDirectoryURL.path()],
                 outputFilesDirectory: outputFilesDirectoryURL
             )
+        ] + existentialAnyWorkaround(context: context)
+    }
+
+    /// This workaround is needed because at the time of writing `strings/structured-swift5.stencil` of SwiftGen does
+    /// not support existential any.
+    private func existentialAnyWorkaround(context: PluginContext) -> [Command] { // TODO: [WPB-9200] delete this
+        let outputFilesDirectory = context.pluginWorkDirectoryURL
+        let tmpOutputFilesDirectory = outputFilesDirectory.appending(path: "tmp", directoryHint: .isDirectory)
+        let outputFile = outputFilesDirectory.appending(path: "Strings+Generated.swift", directoryHint: .notDirectory)
+        try? FileManager.default.createDirectory(at: tmpOutputFilesDirectory, withIntermediateDirectories: true)
+        return [
+            .prebuildCommand(
+                displayName: "Replace CVarArg by any CVarArg",
+                executable: try! context.tool(named: "sed").url,
+                arguments: ["-i", "", "s/CVarArg/any CVarArg/g", outputFile.path()],
+                // fix duplicate build file warning by providing a non-existent path
+                outputFilesDirectory: tmpOutputFilesDirectory
+            )
         ]
     }
 }
