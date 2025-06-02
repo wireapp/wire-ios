9530304b8fb1c06d5232055494f85f6aed416ce2
Christoph Aldrian <christoph.aldrian@wire.com>
2025-06-02 09:07:07 +0200

    fix: warnings in WirePlugins - WPB-17970

--- WirePlugins/Plugins/SwiftGenPlugin/SwiftGenPlugin.swift
+++ WirePlugins/Plugins/SwiftGenPlugin/SwiftGenPlugin.swift
@@ -24,32 +24,16 @@ struct SwiftGenPlugin: BuildToolPlugin {
     /// Entry point for creating build commands for targets in Swift packages.
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
         // regarding the warning: https://github.com/swiftlang/swift-package-manager/issues/7870
-        let configFile = URL(fileURLWithPath: target.directory.appending(subpath: ".swiftgen.yml").string)
-        let outputFilesDirectory = context.pluginWorkDirectoryURL
+        let configFileURL = target.directoryURL.appending(path: ".swiftgen.yml", directoryHint: .notDirectory)
+        let outputFilesDirectoryURL = context.pluginWorkDirectoryURL
         let tool = try context.tool(named: "swiftgen")
         return [
             .prebuildCommand(
                 displayName: "Running \(tool)",
                 executable: tool.url,
-                arguments: ["--config", configFile.path()],
-                environment: ["GENERATED": outputFilesDirectory.path()],
-                outputFilesDirectory: outputFilesDirectory
-            )
-        ] + existentialAnyWorkaround(context: context)
-    }
-
-    /// This workaround is needed because at the time of writing `strings/structured-swift5.stencil` of SwiftGen does
-    /// not support existential any.
-    private func existentialAnyWorkaround(context: PluginContext) -> [Command] {
-        let outputFilesDirectory = context.pluginWorkDirectoryURL
-        let outputFile = outputFilesDirectory.appending(path: "Strings+Generated.swift", directoryHint: .notDirectory)
-        return [
-            .prebuildCommand(
-                displayName: "Replace CVarArg by any CVarArg",
-                executable: try! context.tool(named: "sed").url,
-                arguments: ["-i", "", "s/CVarArg/any CVarArg/g", outputFile.path()],
-                // fix duplicate build file warning by providing a non-existent path
-                outputFilesDirectory: outputFilesDirectory.appending(path: "tmp", directoryHint: .isDirectory)
+                arguments: ["--config", configFileURL.path()],
+                environment: ["GENERATED": outputFilesDirectoryURL.path()],
+                outputFilesDirectory: outputFilesDirectoryURL
             )
         ]
     }
