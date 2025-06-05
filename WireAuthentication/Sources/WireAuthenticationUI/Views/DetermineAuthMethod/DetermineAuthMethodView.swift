4fad20f5c28e582ebd68d8c67e58d2e6ad1e29c1
Christoph Aldrian <christoph.aldrian@wire.com>
2025-06-05 10:39:52 +0200

    add convenience code

--- WireAuthentication/Sources/WireAuthenticationUI/Views/DetermineAuthMethod/DetermineAuthMethodView.swift
+++ WireAuthentication/Sources/WireAuthenticationUI/Views/DetermineAuthMethod/DetermineAuthMethodView.swift
@@ -83,6 +83,9 @@ package struct DetermineAuthMethodView: View {
         .interactiveDismissDisabled()
         .background(ColorTheme.Backgrounds.surface.color)
         .presentationDragIndicator(.hidden)
+        .onAppear { // TODO: revert temporary change
+            viewModel.emailOrSSOCode = "somebody@example.org"
+        }
     }
 
     // MARK: - Views
