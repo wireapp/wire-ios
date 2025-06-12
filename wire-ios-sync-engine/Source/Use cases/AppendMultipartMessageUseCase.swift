76c0279cdf924aa65303161f63715c6c3b89cd33
Sam Wyndham <samwyndham@users.noreply.github.com>
2025-06-12 10:04:16 +0200

    Add "AppendMultipartMessageUseCase"

---
+++ wire-ios-sync-engine/Source/Use cases/AppendMultipartMessageUseCase.swift
@@ -0,0 +1,63 @@
+//
+// Wire
+// Copyright (C) 2025 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import WireAnalytics
+import WireDataModel
+import WireLogging
+
+public protocol AppendMultipartMessageUseCaseProtocol {
+
+    func invoke(
+        text: String,
+        mentions: [Mention],
+        replyingTo: ZMConversationMessage?,
+        in conversation: ZMConversation,
+        fetchLinkPreview: Bool,
+        attachments: [MultipartAttachment]
+    ) throws
+}
+
+public struct AppendMultipartMessageUseCase: AppendMultipartMessageUseCaseProtocol {
+
+    weak var analyticsEventTracker: (any AnalyticsEventTracker)?
+
+    public init(analyticsEventTracker: (any AnalyticsEventTracker)?) {
+        self.analyticsEventTracker = analyticsEventTracker
+    }
+
+    public func invoke(
+        text: String,
+        mentions: [Mention],
+        replyingTo: ZMConversationMessage?,
+        in conversation: ZMConversation,
+        fetchLinkPreview: Bool,
+        attachments: [MultipartAttachment]
+    ) throws {
+        try conversation.appendMultipart(
+            text: text,
+            attachments: attachments,
+            mentions: mentions,
+            replyingTo: replyingTo,
+            fetchLinkPreview: fetchLinkPreview,
+            nonce: UUID()
+        )
+        conversation.draftMessage = nil
+
+        // TODO: Track analytics event
+    }
+}
