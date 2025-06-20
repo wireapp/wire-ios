6a035e57f91fc3a031a5488d023ac23abab116be
Christoph Aldrian <christoph.aldrian@wire.com>
2025-06-20 09:30:59 +0200

    fix import

--- WireDomain/Tests/WireDomainTests/Event Processing/UserEventProcessor/UserConnectionEventProcessorTests.swift
+++ WireDomain/Tests/WireDomainTests/Event Processing/UserEventProcessor/UserConnectionEventProcessorTests.swift
@@ -16,12 +16,12 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-import WireAPI
 import WireDataModel
 import WireDataModelSupport
 import WireDomainSupport
 import WireNetwork
 import XCTest
+
 @testable import WireDomain
 
 final class UserConnectionEventProcessorTests: XCTestCase {
