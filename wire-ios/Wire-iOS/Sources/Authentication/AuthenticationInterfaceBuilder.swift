e79b318d4fac218a260f4ce0d3e051371ad06083
Christoph Aldrian <christoph.aldrian@wire.com>
2025-06-20 15:59:59 +0200

    create RegistrationAnalyticsIDRepository

--- wire-ios/Wire-iOS/Sources/Authentication/AuthenticationInterfaceBuilder.swift
+++ wire-ios/Wire-iOS/Sources/Authentication/AuthenticationInterfaceBuilder.swift
@@ -92,6 +92,12 @@ final class AuthenticationInterfaceBuilder {
             let preferredAPIVersion = BackendInfo.preferredAPIVersion.flatMap {
                 WireNetwork.APIVersion(rawValue: UInt($0.rawValue))
             }
+            let registrationAnalyticsTracker = RegistrationAnalyticsTracker(
+                analyticsServiceConfiguration: AnalyticsServiceConfigurationBuilder.build(),
+                countlyProvider: { CountlyWrapper() },
+                userDefaults: .applicationGroup
+            )
+            let registrationAnalyticsIDRepository = RegistrationAnalyticsIDRepository(userDefaults: .applicationGroup)
             let (rootView, bridge) = assembly.assemble(
                 environmentType: BackendEnvironmentType(environment.environmentType.value),
                 backendConfig: BackendConfig(environment),
@@ -108,11 +114,8 @@ final class AuthenticationInterfaceBuilder {
                 accountsPublisher: CurrentValuePublisher(subject: CurrentValueSubject(accounts)),
                 useLegacyRegistrationFlow: !DeveloperFlag.newRegistration.isOn,
                 isMultibackendEnabled: DeveloperFlag.multibackend.isOn,
-                registrationAnalyticsTracker: RegistrationAnalyticsTracker(
-                    analyticsServiceConfiguration: AnalyticsServiceConfigurationBuilder.build(),
-                    countlyProvider: { CountlyWrapper() },
-                    userDefaults: .applicationGroup
-                )
+                registrationAnalyticsTracker: registrationAnalyticsTracker,
+                registrationAnalyticsIDRepository: registrationAnalyticsIDRepository
             )
             return AuthenticationHostingController(
                 rootView: rootView,
