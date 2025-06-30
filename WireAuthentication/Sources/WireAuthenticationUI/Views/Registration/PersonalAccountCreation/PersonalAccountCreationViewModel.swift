32efe90c0182ac7938da4af1ba9abafffefe939a
Christoph Aldrian <christoph.aldrian@wire.com>
2025-06-30 10:06:12 +0200

    add clearifying comment

--- WireAuthentication/Sources/WireAuthenticationUI/Views/Registration/PersonalAccountCreation/PersonalAccountCreationViewModel.swift
+++ WireAuthentication/Sources/WireAuthenticationUI/Views/Registration/PersonalAccountCreation/PersonalAccountCreationViewModel.swift
@@ -45,6 +45,8 @@ package final class PersonalAccountCreationViewModel: ObservableObject {
     }
 
     var isAnalyticsTrackingAvailable: Bool {
+        // `analyticsEventTracker` will nil if the app is not shipped with Countly credentials.
+        // If credentials are available, we only want to enable Countly for prod and staging backends.
         if analyticsEventTracker != nil, let backendHost = backendURL.host() {
             [
                 // prod
