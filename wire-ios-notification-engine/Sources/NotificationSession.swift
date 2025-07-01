830afb85139e15e5acfa9ed556888ca45047bf3a
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2025-07-01 11:50:26 +0200

    refactor: clean up `AccountManager` and `AccountStore` - WPB-12067 (#3283)
    Co-authored-by: Dzmitry Simkin <dzmitry.simkin.ext@wire.com>

--- wire-ios-notification-engine/Sources/NotificationSession.swift
+++ wire-ios-notification-engine/Sources/NotificationSession.swift
@@ -17,6 +17,7 @@
 //
 
 import Foundation
+import WireDomain
 import WireLogging
 import WireRequestStrategy
 
@@ -126,7 +127,7 @@ public final class NotificationSession {
         minTLSVersion: String?
     ) throws {
         let sharedContainerURL = FileManager.sharedContainerDirectory(for: applicationGroupIdentifier)
-        let accountManager = AccountManager(sharedDirectory: sharedContainerURL)
+        let accountManager = try AccountManager(sharedDirectory: sharedContainerURL)
 
         guard let account = accountManager.account(with: accountIdentifier) else {
             throw InitializationError.noAccount
