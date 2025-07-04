fe12158235a3604a86e3c108df6114cd57157159
François Benaiteau <netbe@users.noreply.github.com>
2025-07-04 14:50:16 +0200

    fix: consumable-notifications enabled - WPB-18587 (#3314)

--- wire-ios-sync-engine/Tests/Source/Synchronization/SyncAgentTests.swift
+++ wire-ios-sync-engine/Tests/Source/Synchronization/SyncAgentTests.swift
@@ -300,6 +300,7 @@ final class SyncAgentTests: XCTestCase, InitialSyncProvider, IncrementalSyncProv
 
     func testPerformIncrementalSync_V3() async throws {
         // Given
+        DeveloperFlag.consumableNotifications.enable(true, storage: .temporary())
         journal[.isConsumableNotificationsEnabled] = true
         journal[.isSyncV2Enabled] = true
 
