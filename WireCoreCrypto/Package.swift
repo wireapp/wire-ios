e1c8ae3f105def4af90fd0da6e2f8641cfe4e78d
Jacob Persson <7156+typfel@users.noreply.github.com>
2025-07-08 17:11:44 +0200

    chore: update CoreCrypto to 7.0.2 - WPB-18348 (#3228)
    Co-authored-by: Dzmitry Simkin <dzmitry.simkin.ext@wire.com>
    Co-authored-by: François Benaiteau <netbe@users.noreply.github.com>
    Co-authored-by: François Benaiteau <francois.benaiteau@wire.com>

--- WireCoreCrypto/Package.swift
+++ WireCoreCrypto/Package.swift
@@ -21,15 +21,15 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WireCoreCrypto",
-            url: "https://github.com/wireapp/core-crypto/releases/download/v6.0.1/WireCoreCrypto.xcframework.zip",
-            checksum: "a75e11dd3ff4ec4ec6f455d8512a9f0b0d8b98dba12f62672ed97aa4c2072e81"
+            url: "https://github.com/wireapp/core-crypto/releases/download/v7.0.2/WireCoreCrypto.xcframework.zip",
+            checksum: "c16e4bd5616b1bf154db64b8d00b16ced69a6b5cd3eb66f61cf9f2feb871a867"
         ),
         // this is an internal dependency to WireCoreCrypto but currently needs to explictly
         // added as a dependency due to limitations of Swift packages.
         .binaryTarget(
             name: "WireCoreCryptoUniffi",
-            url: "https://github.com/wireapp/core-crypto/releases/download/v6.0.1/WireCoreCryptoUniffi.xcframework.zip",
-            checksum: "93113420d7194ea38e5b7a6a4ab4827e7e6793f01d3e6b6df103db9e56732a03"
+            url: "https://github.com/wireapp/core-crypto/releases/download/v7.0.2/WireCoreCryptoUniffi.xcframework.zip",
+            checksum: "669fda445f209b07c784a07dc5fbeda31b0ffb0336e9f13867195f9be8120e16"
         )
     ]
 )
