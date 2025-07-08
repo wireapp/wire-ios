06d79dd294d459ca8ba8ae3bd620ac78c184f968
François Benaiteau <francois.benaiteau@wire.com>
2025-07-08 23:38:40 +0200

    fix typing indicator thread violation

--- WireDomain/Sources/WireDomain/Event Processing/ConversationEventProcessor/ConversationTypingEventProcessor/ConversationTypingEventProcessor.swift
+++ WireDomain/Sources/WireDomain/Event Processing/ConversationEventProcessor/ConversationTypingEventProcessor/ConversationTypingEventProcessor.swift
@@ -47,7 +47,7 @@ struct ConversationTypingEventProcessor: ConversationTypingEventProcessorProtoco
 
         // Since we'll be manipulating managed object IDs in `ConversationTypingUsersTimeout`
         // we need to make sure we have valid, consistent IDs for the user and conversation.
-        conversationLocalStore.obtainPermanentIDs(
+        await conversationLocalStore.obtainPermanentIDs(
             user: user,
             conversation: conversation
         )
