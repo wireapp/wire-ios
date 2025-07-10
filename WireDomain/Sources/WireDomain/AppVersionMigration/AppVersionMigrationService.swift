ad910f502697a66d02b5521e009c9324530f9571
KaterinaWire <katerina@wire.com>
2025-07-10 15:08:41 +0200

    update logs

--- WireDomain/Sources/WireDomain/AppVersionMigration/AppVersionMigrationService.swift
+++ WireDomain/Sources/WireDomain/AppVersionMigration/AppVersionMigrationService.swift
@@ -68,9 +68,9 @@ public final class AppVersionMigrationService {
             do {
                 try await nextMigration.perform()
                 journal.lastCompletedAppVersionMigration = nextMigration.version
-                WireLogger.session.info("Completed migration to version \(nextMigration.version)")
+                WireLogger.appVersionMigration.info("Completed migration to version \(nextMigration.version)")
             } catch {
-                WireLogger.session.error("Failed migration to version \(nextMigration.version): \(error)")
+                WireLogger.appVersionMigration.error("Failed migration to version \(nextMigration.version): \(error)")
                 throw error
             }
         }
