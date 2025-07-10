86dc52693decd58993a9157af8287f89fc9fc874
M S <31733022+findms@users.noreply.github.com>
2025-07-10 15:31:57 +0200

    fix: test personal user registration with new registration flow - WPB-18650 (#3324)
    Co-authored-by: Christoph Aldrian <christoph.aldrian@wire.com>
    Co-authored-by: François Benaiteau <netbe@users.noreply.github.com>

---
+++ wire-ios/WireUITests/Pages/CreatePersonalAccountFormPage.swift
@@ -0,0 +1,84 @@
+//
+// Wire
+// Copyright (C) 2025 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import XCTest
+
+class CreatePersonalAccountFormPage: PageModel {
+
+    override var pageMainElement: XCUIElement {
+        nameTextField
+    }
+
+    var nameTextField: XCUIElement {
+        app.descendants(matching: .any)["Enter your name"].firstMatch
+    }
+
+    var showPasswordButton: XCUIElement {
+        app.descendants(matching: .any)["eye.slash"].firstMatch
+    }
+
+    var passwordField: XCUIElement {
+        app.descendants(matching: .any)["Enter a password"].firstMatch
+    }
+
+    var confirmPasswordField: XCUIElement {
+        app.descendants(matching: .any)["Confirm password"].firstMatch
+    }
+
+    var continueButton: XCUIElement {
+        let elementsQuery = app.scrollViews.otherElements
+        return elementsQuery.buttons["Continue"]
+    }
+
+    var acceptButton: XCUIElement {
+        let elementsQuery = app.otherElements
+        return elementsQuery.buttons["Accept"]
+    }
+
+    func enterName(_ name: String) throws -> CreatePersonalAccountFormPage {
+        nameTextField.tap()
+        nameTextField.typeText(name)
+        return self
+    }
+
+    func enterPassword(_ password: String) throws -> CreatePersonalAccountFormPage {
+        showPasswordButton.tap()
+        passwordField.tap()
+        passwordField.typeText(password)
+        return self
+    }
+
+    func enterConfirmPassword(_ confirmPassword: String) throws -> CreatePersonalAccountFormPage {
+        showPasswordButton.tap()
+        confirmPasswordField.tap()
+        confirmPasswordField.typeText(confirmPassword)
+        return self
+    }
+
+    func tapContinueButton() throws -> CreatePersonalAccountFormPage {
+        continueButton.tap()
+        return self
+    }
+
+    func tapAcceptButton() throws -> VerificationCodePage {
+        _ = acceptButton.waitForExistence(timeout: 10)
+        acceptButton.tap()
+        return try VerificationCodePage()
+    }
+
+}
