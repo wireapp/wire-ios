5d2e40fd382859316cec8600dd6e3a0f2983be99
Christoph Aldrian <christoph.aldrian@wire.com>
2025-07-18 10:47:51 +0200

    fix migration

--- wire-ios-sync-engine/Source/UserSession/ZMUserSession/AppVersionMigrations/AppVersionMigration_4_2_0.swift
+++ wire-ios-sync-engine/Source/UserSession/ZMUserSession/AppVersionMigrations/AppVersionMigration_4_2_0.swift
@@ -22,23 +22,51 @@ import WireDomain
 /// **Issue:**: To simplify the logic, we rely solely on journal value to perform InitialSync or not
 final class AppVersionMigration_4_2_0: AppVersionMigration {
 
+    let appGroupIdentifier: String?
     let lastEventIDRepository: LastEventIDRepositoryInterface
     var journal: JournalProtocol
     let version: SemanticVersion = "4.2.0"
 
-    init(lastEventIDRepository: LastEventIDRepositoryInterface, journal: JournalProtocol) {
+    init(
+        appGroupIdentifier: String?,
+        lastEventIDRepository: LastEventIDRepositoryInterface,
+        journal: JournalProtocol
+    ) {
+        self.appGroupIdentifier = appGroupIdentifier
         self.lastEventIDRepository = lastEventIDRepository
         self.journal = journal
     }
 
     func perform() async throws {
+
         if lastEventIDRepository.fetchLastEventID() == nil {
             journal[.isInitialSyncRequired] = true
         }
 
-        // TODO: clean up old log files
-        // "oslog_dump.log"
-        // "oslog_NSE_dump.log"
-        // "oslog_share_dump.log"
+        deleteOldLogFiles()
+
     }
+
+    /// `FileLoggerDestination` protocol and `LogFileDestination.swift` have been deleted.
+    /// This code cleans up potentially left-over files which could have been deleted by the removed code.
+    private func deleteOldLogFiles() {
+        let fileManager = FileManager.default
+
+        // main app
+        if let cachesDirectory = fileManager.urls(for: .cachesDirectory, in: .userDomainMask).first {
+            let mainAppLogFileURL = cachesDirectory.appendingPathComponent("oslog_dump.log")
+            try? fileManager.removeItem(at: mainAppLogFileURL)
+        }
+
+        // nse
+        guard let appGroupIdentifier else { return }
+        let containerDirectory = FileManager.sharedContainerDirectory(for: appGroupIdentifier)
+        let nseLogFileURL = containerDirectory.appending(path: "oslog_NSE_dump.log", directoryHint: .notDirectory)
+        try? fileManager.removeItem(at: nseLogFileURL)
+
+        // se
+        let seLogFileURL = containerDirectory.appending(path: "oslog_NSE_dump.log", directoryHint: .notDirectory)
+        try? fileManager.removeItem(at: seLogFileURL)
+    }
+
 }
