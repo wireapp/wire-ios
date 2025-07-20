3eb607c4a625f9f5302679158bbf2baf1ae987b9
Dzmitry Simkin <dzmitry.simkin.ext@wire.com>
2025-07-20 23:03:34 +0200

    Improve tests

--- WireNetwork/Tests/WireNetworkTests/APIs/MLSAPI/MLSAPITests_SwiftTesting.swift
+++ WireNetwork/Tests/WireNetworkTests/APIs/MLSAPI/MLSAPITests_SwiftTesting.swift
@@ -26,14 +26,32 @@ class MLSAPITests_SwiftTesting {
     @Test(
         "Test reset MLS broken conversation failures",
         arguments: [
-            (HTTPStatusCode.badRequest, "mls-protocol-error", MLSAPIError.mlsProtocolError(message: "")),
-            (HTTPStatusCode.badRequest, "mls-group-id-not-supported", MLSAPIError.mlsGroupIdNotSupported(message: "")),
+            (
+                HTTPStatusCode.badRequest,
+                "mls-protocol-error",
+                MLSAPIError.mlsProtocolError(message: "")
+            ),
+            (
+                HTTPStatusCode.badRequest,
+                "mls-group-id-not-supported",
+                MLSAPIError.mlsGroupIdNotSupported(message: "")
+            ),
             (
                 HTTPStatusCode.badRequest,
                 "mls-federated-reset-not-supported",
                 MLSAPIError.mlsFederatedResetNotSupported(message: "")
             ),
             (HTTPStatusCode.badRequest, "mls-not-enabled", MLSAPIError.mlsNotEnabled),
+            (
+                HTTPStatusCode.badRequest,
+                "mls-invalid-leaf-node-index",
+                MLSAPIError.mlsInvalidLeafNodeIndex
+            ),
+            (
+                HTTPStatusCode.badRequest,
+                "mls-invalid-leaf-node-signature",
+                MLSAPIError.mlsInvalidLeafNodeSignature
+            ),
 
             (HTTPStatusCode.forbidden, "action-denied", MLSAPIError.actionDenied(message: "")),
             (HTTPStatusCode.forbidden, "access-denied", MLSAPIError.accessDenied(message: "")),
@@ -42,7 +60,6 @@ class MLSAPITests_SwiftTesting {
             (HTTPStatusCode.notFound, "no-conversation", MLSAPIError.noConversation(message: "")),
 
             (HTTPStatusCode.conflict, "mls-stale-message", MLSAPIError.mlsStaleMessage)
-            (HTTPStatusCode.conflict, "mls-stale-message", MLSAPIError.mlsStaleMessageWithMessage(message: ""))
         ]
     )
     func testResetBrokenMLSConversations_Failed(
