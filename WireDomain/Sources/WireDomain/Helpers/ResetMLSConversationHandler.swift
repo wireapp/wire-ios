07c2c974929252e23f3baa978016b98bf906bad2
Dzmitry Simkin <dzmitry.simkin.ext@wire.com>
2025-07-21 15:54:39 +0200

    Remove not used

--- WireDomain/Sources/WireDomain/Helpers/ResetMLSConversationHandler.swift
+++ WireDomain/Sources/WireDomain/Helpers/ResetMLSConversationHandler.swift
@@ -21,7 +21,6 @@ import WireDataModel
 import WireLogging
 import WireNetwork
 
-// sourcery: AutoMockable
 public protocol InitiateResetMLSConversationUseCaseProtocol {
     func invoke(groupID: WireDataModel.MLSGroupID, epoch: Int64) async
 }
