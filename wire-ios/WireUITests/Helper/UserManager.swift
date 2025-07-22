1ce9d1c42fe44fd0f2bec79ece374dee0bc34045
Sam Wyndham <samwyndham@users.noreply.github.com>
2025-07-22 17:02:15 +0200

    Fix compile error in UI tests

--- wire-ios/WireUITests/Helper/UserManager.swift
+++ wire-ios/WireUITests/Helper/UserManager.swift
@@ -39,7 +39,7 @@ class UserManager {
         )
         self.cookieStorage = MockCookieStorage()
         self.authenticationManager = AuthenticationManager(
-            clientID: nil,
+            clientID: "some-client-id",
             cookieStorage: cookieStorage,
             networkService: networkStack.apiNetworkService,
             onAuthenticationFailure: { @Sendable () in }
