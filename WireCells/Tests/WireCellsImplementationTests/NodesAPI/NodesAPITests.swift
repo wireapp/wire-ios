1604ac89a4d2aa2f139eb249957bbc4d9bc8150d
Sam Wyndham <samwyndham@users.noreply.github.com>
2025-07-24 14:28:41 +0200

    feat: use wire auth token for cells - WPB-18754 (#3344)

--- WireCells/Tests/WireCellsImplementationTests/NodesAPI/NodesAPITests.swift
+++ WireCells/Tests/WireCellsImplementationTests/NodesAPI/NodesAPITests.swift
@@ -47,7 +47,7 @@ final class NodesAPITests {
             ),
             restAPI: RestAPI(
                 serverURL: URL(string: "example.com")!,
-                accessToken: "exampleAccessToken"
+                accessToken: AccessTokenProviderMock()
             ),
         )
     }
