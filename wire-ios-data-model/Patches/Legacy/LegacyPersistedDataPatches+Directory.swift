7b43c35fcb3a90581c11c0ea3059bbb09cc5df3a
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2025-07-25 15:53:26 +0200

    delete 58.4.1

--- wire-ios-data-model/Patches/Legacy/LegacyPersistedDataPatches+Directory.swift
+++ wire-ios-data-model/Patches/Legacy/LegacyPersistedDataPatches+Directory.swift
@@ -26,7 +26,6 @@ extension LegacyPersistedDataPatch {
 
     /// List of patches to apply
     static let allPatchesToApply = [
-        LegacyPersistedDataPatch(version: "58.4.1", block: Team.deleteLocalTeamsAndMembers),
         LegacyPersistedDataPatch(version: "62.1.0", block: Member.migrateRemoteIdentifiers),
         LegacyPersistedDataPatch(version: "78.1.0", block: DuplicatedEntityRemoval.removeDuplicated),
         LegacyPersistedDataPatch(version: "81.2.1", block: InvalidClientsRemoval.removeInvalid),
