f2f87b8c00f78cae730f9cebcca34d518fa1a00f
Sam Wyndham <samwyndham@users.noreply.github.com>
2025-07-25 15:24:50 +0200

    [WPB-19048] Create WireData package

---
+++ WireData/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WireData",
+    products: [
+        .library(
+            name: "WireData",
+            targets: ["WireData"]),
+    ],
+    targets: [
+        .target(
+            name: "WireData"),
+        .testTarget(
+            name: "WireDataTests",
+            dependencies: ["WireData"]
+        ),
+    ]
+)
