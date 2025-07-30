26b3c1a29f09ae047341c2f4b6d2702ea0497013
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2025-07-30 16:21:26 +0200

    chore: delete old data patch system - WPB-17719 (#3409)

--- wire-ios-sync-engine/Tests/Source/Notifications/PushNotifications/LocalNotificationDispatcherTests.swift
+++ wire-ios-sync-engine/Tests/Source/Notifications/PushNotifications/LocalNotificationDispatcherTests.swift
@@ -444,36 +444,6 @@ extension LocalNotificationDispatcherTests {
         XCTAssertEqual(scheduledRequests.count, 0)
     }
 
-    func testThatNotifyAvailabilityBehaviourChangedIfNeededSchedulesNotification_WhenNeedsToNotifyAvailabilityBehaviourChangeIsSet(
-    ) {
-        syncMOC.performAndWait {
-            // given
-            selfUser.availability = .away
-            selfUser.needsToNotifyAvailabilityBehaviourChange = [.notification]
-
-            // when
-            sut.notifyAvailabilityBehaviourChangedIfNeeded()
-
-            // then
-            XCTAssertEqual(self.notificationCenter.scheduledRequests.count, 1)
-            XCTAssertEqual(selfUser.needsToNotifyAvailabilityBehaviourChange, [])
-        }
-    }
-
-    func testThatNotifyAvailabilityBehaviourChangedIfNeededDoesNotScheduleNotification_WhenneedsToNotifyAvailabilityBehaviourChangeIsNotSet(
-    ) {
-        syncMOC.performAndWait {
-            // given
-            selfUser.needsToNotifyAvailabilityBehaviourChange = []
-
-            // when
-            sut.notifyAvailabilityBehaviourChangedIfNeeded()
-
-            // then
-            XCTAssertEqual(self.notificationCenter.scheduledRequests.count, 0)
-        }
-    }
-
     // MARK: Updating unread count
 
     func testThatEstimatedUnreadCountIsIncreased_WhenProcessingTextMessage() {
