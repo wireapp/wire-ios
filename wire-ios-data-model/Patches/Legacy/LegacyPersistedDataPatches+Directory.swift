00b14f2ace4bc0a171e58a17ed961a85e0da2986
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2025-07-30 10:36:49 +0200

    delete 198.0.0

--- wire-ios-data-model/Patches/Legacy/LegacyPersistedDataPatches+Directory.swift
+++ wire-ios-data-model/Patches/Legacy/LegacyPersistedDataPatches+Directory.swift
@@ -26,7 +26,6 @@ extension LegacyPersistedDataPatch {
 
     /// List of patches to apply
     static let allPatchesToApply = [
-        LegacyPersistedDataPatch(version: "198.0.0", block: ZMConversation.introduceParticipantRoles),
         LegacyPersistedDataPatch(version: "220.0.4", block: InvalidConnectionRemoval.removeInvalid),
         LegacyPersistedDataPatch(version: "234.0.0", block: TransferApplockKeychain.migrateKeychainItems),
         LegacyPersistedDataPatch(version: "234.1.1", block: InvalidConnectionRemoval.removeInvalid),
