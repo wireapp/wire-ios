23f8e13d44726989fb88f6f00abea20e07b44082
Dzmitry Simkin <dzmitry.simkin.ext@wire.com>
2025-07-30 16:49:27 +0200

    Fix parsing new FF in new network code

--- WireDomain/Sources/WireDomain/Event Processing/ConversationEventProcessor/ConversationMLSResetEventProcessor.swift
+++ WireDomain/Sources/WireDomain/Event Processing/ConversationEventProcessor/ConversationMLSResetEventProcessor.swift
@@ -44,7 +44,7 @@ struct ConversationMLSResetEventProcessor: ConversationMLSResetEventProcessorPro
 
     func processEvent(_ event: ConversationMLSResetEvent) async throws {
 
-        let feature = await featureRepository.fetchAllowGlobalOperations()
+        let feature = await featureRepository.fetchAllowedGlobalOperations()
         guard feature.status == .enabled, feature.config.mlsConversationReset == true else {
             WireLogger.mls.debug(
                 "No need to process reset broken MLS conversation, FF is OFF"
