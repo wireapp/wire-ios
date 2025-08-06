c02d5a049858fc430487733804b710fda14cc2d2
Christoph Aldrian <christoph.aldrian@wire.com>
2025-08-06 11:35:28 +0200

    improve readability

--- WireDomain/Sources/WireDomain/Event Processing/ConversationEventProcessor/ConversationMessageAddEventProcessor/ConversationMLSMessageAddEventProcessor.swift
+++ WireDomain/Sources/WireDomain/Event Processing/ConversationEventProcessor/ConversationMessageAddEventProcessor/ConversationMLSMessageAddEventProcessor.swift
@@ -90,9 +90,16 @@ struct ConversationMLSMessageAddEventProcessor: ConversationMLSMessageAddEventPr
 
         // Parse into GenericMessage
         guard let genericMessage = GenericMessage(decryptedMessage.message), genericMessage.validateFields() else {
-            WireLogger.eventProcessing.warn("Can't read protobuf, abort processing", attributes: logAttributes)
-            return await addInvalidSystemMessage(senderID: senderID, conversationID: conversationID, date: date ?? .now)
-        }
+             WireLogger.eventProcessing.warn(
+                 "Can't read protobuf, abort processing",
+                 attributes: logAttributes
+             )
+             return await addInvalidSystemMessage(
+                 senderID: senderID,
+                 conversationID: conversationID,
+                 date: date ?? .now
+             )
+         }
 
         // Handle calling if there's one.
         if let callEventInfo = getCallEventInfo(
