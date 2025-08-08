92d50d35dadd92a44b159fe251cd8e4508a93aae
Christoph Aldrian <christoph.aldrian@wire.com>
2025-08-08 15:32:03 +0200

    add note

--- WireDomain/Sources/WireDomain/Event Processing/ConversationEventProcessor/ConversationMessageAddEventProcessor/ConversationMessageAddEventProcessorProtocol.swift
+++ WireDomain/Sources/WireDomain/Event Processing/ConversationEventProcessor/ConversationMessageAddEventProcessor/ConversationMessageAddEventProcessorProtocol.swift
@@ -34,7 +34,7 @@ protocol ConversationMessageAddEventProcessorProtocol {
         date: Date
     ) async
 
-    func addInvalidSystemMessage(
+    func addInvalidSystemMessage( // TODO: same text?
         senderID: UserID,
         conversationID: ConversationID,
         date: Date
