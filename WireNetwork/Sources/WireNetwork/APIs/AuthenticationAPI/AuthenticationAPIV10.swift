f8b264470d6ca1627024991beb2d8d74364b65e1
Sam Wyndham <samwyndham@users.noreply.github.com>
2025-08-20 12:53:19 +0200

    fix: versioning of `/get-domain-registration` - WPB-19619 (#3480)

--- WireNetwork/Sources/WireNetwork/APIs/AuthenticationAPI/AuthenticationAPIV10.swift
+++ WireNetwork/Sources/WireNetwork/APIs/AuthenticationAPI/AuthenticationAPIV10.swift
@@ -23,8 +23,7 @@ final class AuthenticationAPIV10: AuthenticationAPIV9 {
     override var apiVersion: APIVersion { .v10 }
 
     override func getDomainRegistration(forEmail email: String) async throws -> DomainRegistrationConfiguration {
-        // no need to have api version prefix
-        let path = "/get-domain-registration"
+        let path = "\(pathPrefix)/get-domain-registration"
         let body = GetDomainRegistrationParametersV8(email: email)
 
         let encodedJSON: Data
