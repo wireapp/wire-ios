82239ea8a1f4b488d4ba10f5da934011c644319f
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2025-08-21 14:50:54 +0200

    format

--- wire-ios/Wire-iOS Tests/Authentication/AuthenticationInterfaceBuilderTests.swift
+++ wire-ios/Wire-iOS Tests/Authentication/AuthenticationInterfaceBuilderTests.swift
@@ -16,8 +16,8 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-import WireTestingPackage
 import WireNetwork
+import WireTestingPackage
 import XCTest
 
 @testable import Wire
