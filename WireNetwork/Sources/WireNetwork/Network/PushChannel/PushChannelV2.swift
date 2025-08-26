c531dab4cb3aa1dd1160bf6ae82a559186c6d37c
François Benaiteau <francois.benaiteau@wire.com>
2025-08-26 12:02:14 +0200

    add logs

--- WireNetwork/Sources/WireNetwork/Network/PushChannel/PushChannelV2.swift
+++ WireNetwork/Sources/WireNetwork/Network/PushChannel/PushChannelV2.swift
@@ -103,7 +103,7 @@ public final class PushChannelV2: PushChannelV2Protocol {
                         if await batchBuffer.count() >= batchSize {
                             let drained = await batchBuffer.drain()
                             continuation.yield(.events(drained))
-                            WireLogger.pushChannel.debug("reached batch of size '\(drained.count)' yield")
+                            WireLogger.pushChannel.debug("reached batch of size '\(drained.count)' yield, batchSize '\(batchSize)'")
                         }
 
                     case .missedEvents:
@@ -114,7 +114,7 @@ public final class PushChannelV2: PushChannelV2Protocol {
                         let drained = await batchBuffer.drain()
                         if !drained.isEmpty {
                             continuation.yield(.events(drained))
-                            WireLogger.pushChannel.debug("reached batch of size '\(drained.count)' yield")
+                            WireLogger.pushChannel.debug("syncMarker reached batch of size '\(drained.count)' yield")
                         }
 
                         continuation.yield(.syncMarker(id: id, deliveryTag: deliveryTag))
@@ -179,7 +179,7 @@ public final class PushChannelV2: PushChannelV2Protocol {
                     try await Task.sleep(for: .seconds(batchInterval))
                     if !(await batchBuffer.isEmpty()) {
                         let drained = await batchBuffer.drain()
-                        WireLogger.pushChannel.debug("yielding batch '\(drained.count)'", attributes: .pushChannelV2)
+                        WireLogger.pushChannel.debug("timeout yielding batch '\(drained.count)'", attributes: .pushChannelV2)
                         continuation.yield(.events(drained))
                     }
                 }
