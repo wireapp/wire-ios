f400eb711e134243a0b22054d4fbb1cf1c5bccbc
Manish Singh <31733022+findms@users.noreply.github.com>
2025-09-03 14:01:58 +0200

    chore: test backup and restore history - WPB-17914 (#3481)

--- wire-ios/WireUITests/Pages/ConversationsPage.swift
+++ wire-ios/WireUITests/Pages/ConversationsPage.swift
@@ -47,6 +47,14 @@ class ConversationsPage: PageModel {
         app.buttons["Block"]
     }
 
+    var videoCallButton: XCUIElement {
+        app.descendants(matching: .any)["videoCallBarButton"].firstMatch
+    }
+
+    var acceptRequestButton: XCUIElement {
+        app.buttons["accept"]
+    }
+
     func openSettings() throws -> SettingsPage {
         settingsButton.tap()
         return try SettingsPage()
@@ -65,15 +73,31 @@ class ConversationsPage: PageModel {
     }
 
     func openPendingRequest() throws -> ConnectionRequestsPage {
-        if conversationCell.waitForExistence(timeout: 7) {
-            conversationCell.tap()
+        XCTAssertTrue(conversationCell.waitForExistence(timeout: 5), "Conversation cell did not appear")
+
+        let maxDuration: TimeInterval = 10
+        let start = Date()
+
+        while !acceptRequestButton.exists, Date().timeIntervalSince(start) < maxDuration {
+            if conversationCell.isHittable {
+                conversationCell.tap()
+            }
+            RunLoop.current.run(until: Date().addingTimeInterval(1.0))
         }
         return try ConnectionRequestsPage()
     }
 
     func openConversation() throws -> ActiveConversationPage {
-        if conversationCell.waitForExistence(timeout: 7) {
-            conversationCell.tap()
+        XCTAssertTrue(conversationCell.waitForExistence(timeout: 5), "Conversation cell did not appear")
+
+        let maxDuration: TimeInterval = 10
+        let start = Date()
+
+        while !videoCallButton.exists, Date().timeIntervalSince(start) < maxDuration {
+            if conversationCell.isHittable {
+                conversationCell.tap()
+            }
+            RunLoop.current.run(until: Date().addingTimeInterval(1.0))
         }
         return try ActiveConversationPage()
     }
@@ -92,5 +116,4 @@ class ConversationsPage: PageModel {
     func getNameLabel() -> String? {
         conversationCell.label
     }
-
 }
