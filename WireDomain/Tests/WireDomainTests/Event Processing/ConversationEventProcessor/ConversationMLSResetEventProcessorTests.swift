7430936e000aa5725c007bd0395899563afc80af
François Benaiteau <francois.benaiteau@wire.com>
2025-09-04 16:45:25 +0200

    fix: add test

--- WireDomain/Tests/WireDomainTests/Event Processing/ConversationEventProcessor/ConversationMLSResetEventProcessorTests.swift
+++ WireDomain/Tests/WireDomainTests/Event Processing/ConversationEventProcessor/ConversationMLSResetEventProcessorTests.swift
@@ -55,8 +55,10 @@ final class ConversationMLSResetEventProcessorTests: XCTestCase {
         zmConversation = conversation
 
         mlsService.wipeGroup_MockMethod = { _ in }
+        mlsService.conversationExistsGroupID_MockValue = false
         conversationLocalStore.fetchConversationIdDomain_MockValue = conversation
         conversationLocalStore.storeMLSConversationPendingJoinNewMLSGroupIDConversation_MockMethod = { _, _ in }
+        conversationLocalStore.storeMLSConversationEstablishedMlsGroupIDConversation_MockMethod = { _, _ in  }
 
         mockResetLockRepository.removeResetInitiatedConversationID_MockMethod = { _ in }
         mockResetLockRepository.wasResetInitiatedConversationID_MockValue = false
@@ -110,6 +112,44 @@ final class ConversationMLSResetEventProcessorTests: XCTestCase {
             }
     }
 
+    func testProcessEvent_AlreadyReset() async throws {
+        // GIVEN
+        mlsService.conversationExistsGroupID_MockValue = true
+
+        // When
+
+        try await sut.processEvent(Scaffolding.event)
+
+        // Then
+
+        XCTAssertEqual(mlsService.wipeGroup_Invocations.count, 1)
+        mlsService.wipeGroup_Invocations
+            .forEach {
+                XCTAssertEqual(
+                    $0,
+                    MLSGroupID(Scaffolding.oldMLSGroupIDData)
+                )
+            }
+
+        XCTAssertEqual(
+            conversationLocalStore.storeMLSConversationPendingJoinNewMLSGroupIDConversation_Invocations.count,
+            0
+        )
+        XCTAssertEqual(
+            conversationLocalStore.storeMLSConversationEstablishedMlsGroupIDConversation_Invocations.count,
+            1
+        )
+
+
+        conversationLocalStore.storeMLSConversationEstablishedMlsGroupIDConversation_Invocations
+            .forEach {
+                XCTAssertEqual(
+                    $0.mlsGroupID,
+                    MLSGroupID(Scaffolding.newMLSGroupIDData)
+                )
+            }
+    }
+
     func testProcessEvent_DoNothingWhenInitiatedFromSameDevice() async throws {
 
         mockResetLockRepository.wasResetInitiatedConversationID_MockValue = true
@@ -140,6 +180,7 @@ final class ConversationMLSResetEventProcessorTests: XCTestCase {
             try await self.sut.processEvent(Scaffolding.event)
         }
     }
+
 
     func testErrorOnWipeGroup() async throws {
 
