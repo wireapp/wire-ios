24184e03ceb60cb8d25c842e8a3118f694999f3f
KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
2025-09-05 14:57:11 +0200

    feat: Revert "fix: iPhone keep ringing after joining the call - WPB-18769 (#3544)

--- WireDomain/Sources/WireDomain/Notifications/Components/NSEClientScope.swift
+++ WireDomain/Sources/WireDomain/Notifications/Components/NSEClientScope.swift
@@ -359,8 +359,7 @@ final class NSEClientScope: Component<NSEClientScopeDependency> {
         )
 
         let validator = ConversationMessageAddEventNotificationBuilder.Validator(
-            conversationLocalStore: conversationLocalStore,
-            userLocalStore: userLocalStore
+            conversationLocalStore: conversationLocalStore
         )
 
         return ConversationMessageAddEventNotificationBuilder(
@@ -536,8 +535,7 @@ final class NSEClientScope: Component<NSEClientScopeDependency> {
         )
 
         let validator = ConversationDeleteEventNotificationBuilder.Validator(
-            conversationLocalStore: conversationLocalStore,
-            userLocalStore: userLocalStore
+            conversationLocalStore: conversationLocalStore
         )
 
         return ConversationDeleteEventNotificationBuilder(
@@ -552,9 +550,7 @@ final class NSEClientScope: Component<NSEClientScopeDependency> {
             userLocalStore: userLocalStore
         )
 
-        let validator = ConversationMessageTimerUpdateEventNotificationBuilder.Validator(
-            userLocalStore: userLocalStore
-        )
+        let validator = ConversationMessageTimerUpdateEventNotificationBuilder.Validator()
 
         return ConversationMessageTimerUpdateEventNotificationBuilder(
             context: context,
