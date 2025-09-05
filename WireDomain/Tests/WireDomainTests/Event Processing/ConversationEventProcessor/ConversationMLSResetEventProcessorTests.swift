a2fd7e27457cb6bab873ad07063ed71f6b3c9f59
François Benaiteau <francois.benaiteau@wire.com>
2025-09-05 09:08:04 +0200

    format

--- WireDomain/Tests/WireDomainTests/Event Processing/ConversationEventProcessor/ConversationMLSResetEventProcessorTests.swift
+++ WireDomain/Tests/WireDomainTests/Event Processing/ConversationEventProcessor/ConversationMLSResetEventProcessorTests.swift
@@ -58,7 +58,7 @@ final class ConversationMLSResetEventProcessorTests: XCTestCase {
         mlsService.conversationExistsGroupID_MockValue = false
         conversationLocalStore.fetchConversationIdDomain_MockValue = conversation
         conversationLocalStore.storeMLSConversationPendingJoinNewMLSGroupIDConversation_MockMethod = { _, _ in }
-        conversationLocalStore.storeMLSConversationEstablishedMlsGroupIDConversation_MockMethod = { _, _ in    }
+        conversationLocalStore.storeMLSConversationEstablishedMlsGroupIDConversation_MockMethod = { _, _ in }
 
         mockResetLockRepository.removeResetInitiatedConversationID_MockMethod = { _ in }
         mockResetLockRepository.wasResetInitiatedConversationID_MockValue = false
