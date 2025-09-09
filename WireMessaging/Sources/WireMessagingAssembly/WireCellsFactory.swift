4804a814f28f9b02679d47d31d0130aabd8c8172
Sam Wyndham <samwyndham@users.noreply.github.com>
2025-09-09 17:11:48 +0200

    Update server URLs

--- WireMessaging/Sources/WireMessagingAssembly/WireCellsFactory.swift
+++ WireMessaging/Sources/WireMessagingAssembly/WireCellsFactory.swift
@@ -33,9 +33,13 @@ public struct WireCellsFactory {
     public init(serverURL: URL, accessToken: any AccessTokenProvider) {
         // TODO: [WPB-18798] Remove serverURL temporary override when there exists a method to obtain the correct URL.
         let serverURL = switch serverURL.host {
-        case "nginz-https.fulu.wire.link":
+        case "prod-nginz-https.wire.com": // Production
+            URL(string: "https://cells-beta.wire.com")!
+        case "staging-nginz-https.zinfra.io": // Staging
+            URL(string: "https://cells.staging.zinfra.io")!
+        case "nginz-https.fulu.wire.link": // Fulu
             URL(string: "https://cells.fulu.wire.link")!
-        case "nginz-https.imai.wire.link":
+        case "nginz-https.imai.wire.link": // Imai
             URL(string: "https://cells.imai.wire.link")!
         default:
             serverURL
