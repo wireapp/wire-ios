bbad873939b75746ba6444672c609168a1267471
LalithaWire <syamala633@gmail.com>
2025-09-10 15:03:25 +0200

    fix: intermittently create button doesnt get enabled to create folder after entering the name - WPB-19888 (#3554)

--- WireUI/Tests/WireMoveToFolderUITests/FolderPickerViewModelTests.swift
+++ WireUI/Tests/WireMoveToFolderUITests/FolderPickerViewModelTests.swift
@@ -27,6 +27,7 @@ final class FolderPickerViewModelTests: XCTestCase {
     private var sut: FolderPickerViewModel!
     private var mockDirectory: MockFolderDirectoryTypeProtocol!
     private var mockUpdateFolderUseCase: MockUpdateConversationFolderUseCase!
+    private var mockCreateFolderUseCase: MockCreateConversationFolderUseCase!
 
     // MARK: - setUp
 
@@ -34,6 +35,7 @@ final class FolderPickerViewModelTests: XCTestCase {
     override func setUp() async throws {
         mockDirectory = MockFolderDirectoryTypeProtocol()
         mockUpdateFolderUseCase = MockUpdateConversationFolderUseCase()
+        mockCreateFolderUseCase = MockCreateConversationFolderUseCase()
     }
 
     // MARK: - tearDown
@@ -43,6 +45,7 @@ final class FolderPickerViewModelTests: XCTestCase {
         sut = nil
         mockDirectory = nil
         mockUpdateFolderUseCase = nil
+        mockCreateFolderUseCase = nil
     }
 
     // MARK: - Initialization
@@ -155,7 +158,8 @@ final class FolderPickerViewModelTests: XCTestCase {
         sut = FolderPickerViewModel(
             conversation: conversation,
             directory: mockDirectory,
-            updateConversationFolderUseCase: mockUpdateFolderUseCase
+            updateConversationFolderUseCase: mockUpdateFolderUseCase,
+            createFolderUseCase: mockCreateFolderUseCase
         )
     }
 }
