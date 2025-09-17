7b2d00379d7172285be86891c54b3ff9b1200b7a
François Benaiteau <francois.benaiteau@wire.com>
2025-09-17 10:31:25 +0200

    format

--- WireDomain/Tests/WireDomainTests/Utilities/JournalTests.swift
+++ WireDomain/Tests/WireDomainTests/Utilities/JournalTests.swift
@@ -67,7 +67,6 @@ class JournalStoreTests {
         #expect(storage.object(forKey: "notAJournalKey") != nil)
     }
 
-
     @Test("Values contain all declared values")
     func values() {
         // Given
