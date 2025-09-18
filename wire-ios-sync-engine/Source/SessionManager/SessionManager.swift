d5d47fae5876cacd7abe6db67405d538434e5302
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2025-09-18 11:57:19 +0200

    Revert "ignore disable global environment changes if multibackend is on"
    This reverts commit f96e2409bab9dd6e9e410ab8f4a0b52cd1d225df.

--- wire-ios-sync-engine/Source/SessionManager/SessionManager.swift
+++ wire-ios-sync-engine/Source/SessionManager/SessionManager.swift
@@ -330,7 +330,6 @@ public final class SessionManager: NSObject, SessionManagerType {
 
     public internal(set) var environment: WireTransport.BackendEnvironment {
         didSet {
-            guard !DeveloperFlag.multibackend.isOn else { return }
             apiVersionResolver = nil
             reachability.tearDown()
             reachability = environment.reachabilityWrapper()
