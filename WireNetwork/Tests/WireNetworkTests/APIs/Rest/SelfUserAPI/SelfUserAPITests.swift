eff3c8b4d5be6e7f4819b05183676c41b5482d1a
François Benaiteau <francois.benaiteau@wire.com>
2025-09-22 15:24:43 +0200

    fix: parse mlsProtocolError

--- WireNetwork/Tests/WireNetworkTests/APIs/Rest/SelfUserAPI/SelfUserAPITests.swift
+++ WireNetwork/Tests/WireNetworkTests/APIs/Rest/SelfUserAPI/SelfUserAPITests.swift
@@ -205,6 +205,23 @@ final class SelfUserAPITests: XCTestCase {
             try await sut.pushSupportedProtocols([.mls])
         }
     }
+    
+    func testPushSupportedProtocols_MLSProtocolErrorFailureResponse_V8() async throws {
+        // Given
+        let errorMessage = "MLS protocol cannot be removed"
+        let apiService = MockAPIServiceProtocol.withError(
+            statusCode: .conflict,
+            label: "mls-protocol-error",
+            message: errorMessage
+        )
+        let sut = SelfUserAPIV8(apiService: apiService)
+
+        // Then
+        await XCTAssertThrowsErrorAsync(SelfUserAPIError.mlsProtocolError(errorMessage), when: {
+            // When
+            try await sut.pushSupportedProtocols([.mls])
+        })
+    }
 }
 
 extension SelfUserAPITests {
@@ -319,5 +336,21 @@ private extension APIVersion {
     func buildAPI(apiService: any APIServiceProtocol) -> any SelfUserAPI {
         let builder = SelfUserAPIBuilder(apiService: apiService)
         return builder.makeAPI(for: self)
+    }
+}
+
+extension SelfUserAPIError: Equatable {
+    public static func == (lhs: SelfUserAPIError, rhs: SelfUserAPIError) -> Bool {
+        switch (lhs, rhs) {
+        case (.selfUserNotFound, .selfUserNotFound):
+            return true
+        case (.mlsProtocolError(let lhsMsg), .mlsProtocolError(let rhsMsg)):
+            return lhsMsg == rhsMsg
+        case (.unsupportedEndpointForAPIVersion, .unsupportedEndpointForAPIVersion):
+            return true
+        default:
+            return false
+        }
+    
     }
 }
