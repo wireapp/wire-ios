ca7d1cb8da09499eb8fb5cb0cd1f8ccb0e79018f
KaterinaWire <katerina@wire.com>
2025-10-01 11:41:31 +0200

    add view and viewModel for meetings

---
+++ wire-ios/Wire-iOS/Sources/UserInterface/Meetings/MeetingsView.swift
@@ -0,0 +1,66 @@
+//
+// Wire
+// Copyright (C) 2025 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import SwiftUI
+
+public struct MeetingsView: View {
+
+    @ObservedObject private var viewModel: MeetingsViewModel
+
+    public init(viewModel: MeetingsViewModel) {
+        self.viewModel = viewModel
+    }
+
+    public var body: some View {
+        VStack(spacing: 12) {
+            Picker("", selection: Binding(
+                get: { viewModel.selectedTab.rawValue },
+                set: { viewModel.selectedTab = MeetingsViewModel.Tab(rawValue: $0) ?? .upcoming }
+            )) {
+                ForEach(MeetingsViewModel.Tab.allCases, id: \.rawValue) { tab in
+                    Text(tab.title).tag(tab.rawValue)
+                }
+            }
+            .pickerStyle(.segmented)
+            //.padding(.horizontal)
+            .padding(.horizontal, 16)
+            .padding(.top, 12)
+            .padding(.bottom, 10)
+            .accessibilityHint("Switch between upcoming and past meetings")
+
+            content
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        }
+        .background(Color(uiColor: .systemBackground))
+    }
+
+    @ViewBuilder
+    private var content: some View {
+        switch viewModel.selectedTab {
+        case .upcoming:
+            Text("Upcoming meetings").frame(maxWidth: .infinity, alignment: .leading).padding(.horizontal)
+        case .past:
+            Text("Past meetings").frame(maxWidth: .infinity, alignment: .leading).padding(.horizontal)
+        }
+    }
+
+}
+
+#Preview {
+    MeetingsView(viewModel: MeetingsViewModel())
+}
--- wire-ios/Wire-iOS/Sources/UserInterface/Meetings/MeetingsViewController.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/Meetings/MeetingsViewController.swift
@@ -22,55 +22,216 @@ import WireDataModel
 import WireDesign
 import WireSyncEngine
 import WireAccountImageUI
+import SwiftUI
+
+//final class MeetingsViewController: UIViewController {
+//    weak var accountImageView: AccountImageView?
+//    private let segmentedControl: UISegmentedControl
+//
+//    init() {
+//        let groupItems: [String] = ["Upcoming", "Past"]
+//        self.segmentedControl = UISegmentedControl(items: groupItems)
+//        super.init(nibName: nil, bundle: nil)
+//    }
+//
+//    @available(*, unavailable)
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) is not supported")
+//    }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        view.accessibilityViewIsModal = true
+//        //view.backgroundColor = .blue
+//    }
+//
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        setupNavigationBar()
+//        configureNavigationBarAppearance()
+//        configureViews()
+//    }
+//
+//    private func configureViews() {
+//        segmentedControl.selectedSegmentIndex = 0
+//
+//        view.addSubview(segmentedControl)
+//        configureConstraints()
+//    }
+//
+//    private func configureConstraints() {
+//        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+//            segmentedControl.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+//            segmentedControl.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+//        ])
+//    }
+//
+//    private func setupNavigationBar() {
+//        setupNavigationBarTitle("Meetings")
+////        let button = AuthenticationNavigationBar.makeBackButton()
+//        //navigationItem.leftBarButtonItem = UIBarButtonItem(customView: button)
+//        setupLeftNavigationBarButtonItems()
+//        setupRightNavigationBarButtonItems()
+//    }
+//
+//    func configureNavigationBarAppearance() {
+//        let appearance = UINavigationBarAppearance()
+//        appearance.configureWithDefaultBackground()
+//        appearance.backgroundColor = ColorTheme.Backgrounds.surface
+//
+//        // Configure appearance for different states
+//        navigationController?.navigationBar.standardAppearance = appearance
+//        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+//        navigationController?.navigationBar.compactAppearance = appearance
+//    }
+//
+//    func setupLeftNavigationBarButtonItems() {
+//
+//        // in the design the left bar button items are very close to each other,
+//        // so we'll use a stack view instead
+//        let stackView = UIStackView()
+//        stackView.spacing = 4
+//
+//        // avatar
+//        let accountImageView = makeAccountImageView()
+//        stackView.addArrangedSubview(accountImageView)
+//        self.accountImageView = accountImageView
+//
+////        // legal hold
+////        switch viewModel.selfUserLegalHoldSubject.legalHoldStatus {
+////        case .disabled:
+////            break
+////        case .pending:
+////            let pendingRequestView = createPendingLegalHoldRequestView()
+////            stackView.addArrangedSubview(pendingRequestView)
+////        case .enabled:
+////            let legalHoldView = createLegalHoldView()
+////            stackView.addArrangedSubview(legalHoldView)
+////        }
+////
+////        // verification status
+////        if viewModel.selfUserStatus.isE2EICertified {
+////            let imageView = UIImageView(image: .init(resource: .certificateValid))
+////            imageView.contentMode = .scaleAspectFit
+////            stackView.addArrangedSubview(imageView)
+////        }
+////        if viewModel.selfUserStatus.isProteusVerified {
+////            let imageView = UIImageView(image: .init(resource: .verifiedShield))
+////            imageView.contentMode = .scaleAspectFit
+////            stackView.addArrangedSubview(imageView)
+////        }
+//
+//        navigationItem.leftBarButtonItems = [.init(customView: stackView)]
+//    }
+//
+//    func setupRightNavigationBarButtonItems() {
+//
+//        let configuration = UIImage.SymbolConfiguration(font: .systemFont(ofSize: 17))
+//        let actionImage = UIImage(systemName: "chevron.forward", withConfiguration: configuration)
+//        let joinNowAction = UIAction(title: "Meet Now", image: actionImage) { _ in }
+//        let scheduleMeetingAction = UIAction(title: "Schedule a Meeting", image: actionImage) { _ in }
+//
+//        let menuChildren = [
+//            joinNowAction,
+//            scheduleMeetingAction
+//        ]
+//        let filterMenu = UIMenu(children: menuChildren)
+//
+//        let symbolConfiguration = UIImage.SymbolConfiguration(weight: .semibold)
+//        let startMeetingImage = UIImage(systemName: "video.fill", withConfiguration: symbolConfiguration)!
+//        let startMeetingButton = UIButton(type: .system)
+//        startMeetingButton.setImage(startMeetingImage, for: .normal)
+//        startMeetingButton.showsMenuAsPrimaryAction = true
+//        startMeetingButton.accessibilityLabel = L10n.Accessibility.ConversationsList.FilterButton.description
+//        startMeetingButton.menu = filterMenu
+//        let startMeetingItem = UIBarButtonItem(customView: startMeetingButton)
+//
+//        navigationItem.rightBarButtonItems = [startMeetingItem]
+//
+//    }
+//
+//    // MARK: - Navigation Bar Items
+//
+//    private func makeAccountImageView() -> AccountImageView {
+//
+//        let accountImageView = AccountImageView()
+////        accountImageView.source = viewModel.accountImageSource
+////        accountImageView.availability = viewModel.selfUserStatus.availability.mapToAccountImageAvailability()
+////        accountImageView.hideProfileNotificationsBadge = viewModel.hideProfileNotificationsBadge
+//        accountImageView.isAccessibilityElement = true
+////        accountImageView.accessibilityValue = L10n.Localizable.ConversationList.Header.SelfTeam
+////            .accessibilityValue(viewModel.userSession.selfUser.name ?? "")
+//        accountImageView.accessibilityHint = L10n.Accessibility.ConversationsList.AccountButton.hint
+//        accountImageView.translatesAutoresizingMaskIntoConstraints = false
+//        accountImageView.widthAnchor.constraint(equalToConstant: 28).isActive = true
+//        accountImageView.heightAnchor.constraint(equalToConstant: 28).isActive = true
+//
+//        let design = AccountImageViewDesign()
+//        accountImageView.imageBorderWidth = design.borderWidth
+//        accountImageView.imageBorderColor = design.borderColor
+//        accountImageView.availableColor = design.availabilityIndicator.availableColor
+//        accountImageView.busyColor = design.availabilityIndicator.busyColor
+//        accountImageView.awayColor = design.availabilityIndicator.awayColor
+//        accountImageView.availabilityIndicatorBackgroundColor = design.availabilityIndicator.backgroundViewColor
+//
+//        accountImageView.translatesAutoresizingMaskIntoConstraints = false
+//        accountImageView.widthAnchor.constraint(equalToConstant: 28).isActive = true
+//        accountImageView.heightAnchor.constraint(equalToConstant: 28).isActive = true
+//
+//        return accountImageView
+//    }
+//
+//}
+//
 
 final class MeetingsViewController: UIViewController {
-    weak var accountImageView: AccountImageView?
-    private let segmentedControl: UISegmentedControl
 
-    init() {
-        let groupItems: [String] = ["Upcoming", "Past"]
-        self.segmentedControl = UISegmentedControl(items: groupItems)
+    private let viewModel: MeetingsViewModel
+    private let hostingController: UIHostingController<MeetingsView>
+    private weak var accountImageView: AccountImageView?
+
+    init(viewModel: MeetingsViewModel = .init()) {
+        self.viewModel = viewModel
+        self.hostingController = UIHostingController(rootView: MeetingsView(viewModel: viewModel))
         super.init(nibName: nil, bundle: nil)
     }
 
     @available(*, unavailable)
-    required init?(coder aDecoder: NSCoder) {
+    required init?(coder: NSCoder) {
         fatalError("init(coder:) is not supported")
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.accessibilityViewIsModal = true
-        //view.backgroundColor = .blue
+        addChild(hostingController)
+        view.addSubview(hostingController.view)
+        hostingController.didMove(toParent: self)
+
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hostingController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+
+        setupNavigationBar()
+        configureNavigationBarAppearance()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        setupNavigationBar()
+        // Keep nav appearance in sync in case parent controller changes it
         configureNavigationBarAppearance()
-        configureViews()
     }
 
-    private func configureViews() {
-        segmentedControl.selectedSegmentIndex = 0
-
-        view.addSubview(segmentedControl)
-        configureConstraints()
-    }
-
-    private func configureConstraints() {
-        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
-            segmentedControl.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
-            segmentedControl.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
-        ])
-    }
+    // MARK: - Navigation Bar
 
     private func setupNavigationBar() {
-        setupNavigationBarTitle("Meetings")
-//        let button = AuthenticationNavigationBar.makeBackButton()
-        //navigationItem.leftBarButtonItem = UIBarButtonItem(customView: button)
+        navigationItem.title = "Meetings"
         setupLeftNavigationBarButtonItems()
         setupRightNavigationBarButtonItems()
     }
@@ -86,101 +247,55 @@ final class MeetingsViewController: UIViewController {
         navigationController?.navigationBar.compactAppearance = appearance
     }
 
-    func setupLeftNavigationBarButtonItems() {
 
-        // in the design the left bar button items are very close to each other,
-        // so we'll use a stack view instead
+
+    private func setupLeftNavigationBarButtonItems() {
         let stackView = UIStackView()
-        stackView.spacing = 4
+        stackView.spacing = 6
+        stackView.axis = .horizontal
+        stackView.alignment = .center
 
-        // avatar
-        let accountImageView = makeAccountImageView()
-        stackView.addArrangedSubview(accountImageView)
-        self.accountImageView = accountImageView
+        let avatar = makeAccountImageView()
+        stackView.addArrangedSubview(avatar)
+        self.accountImageView = avatar
 
-//        // legal hold
-//        switch viewModel.selfUserLegalHoldSubject.legalHoldStatus {
-//        case .disabled:
-//            break
-//        case .pending:
-//            let pendingRequestView = createPendingLegalHoldRequestView()
-//            stackView.addArrangedSubview(pendingRequestView)
-//        case .enabled:
-//            let legalHoldView = createLegalHoldView()
-//            stackView.addArrangedSubview(legalHoldView)
-//        }
-//
-//        // verification status
-//        if viewModel.selfUserStatus.isE2EICertified {
-//            let imageView = UIImageView(image: .init(resource: .certificateValid))
-//            imageView.contentMode = .scaleAspectFit
-//            stackView.addArrangedSubview(imageView)
-//        }
-//        if viewModel.selfUserStatus.isProteusVerified {
-//            let imageView = UIImageView(image: .init(resource: .verifiedShield))
-//            imageView.contentMode = .scaleAspectFit
-//            stackView.addArrangedSubview(imageView)
-//        }
-
-        navigationItem.leftBarButtonItems = [.init(customView: stackView)]
+        let container = UIBarButtonItem(customView: stackView)
+        navigationItem.leftBarButtonItems = [container]
     }
 
-    func setupRightNavigationBarButtonItems() {
-
+    private func setupRightNavigationBarButtonItems() {
         let configuration = UIImage.SymbolConfiguration(font: .systemFont(ofSize: 17))
-        let actionImage = UIImage(systemName: "chevron.forward", withConfiguration: configuration)
-        let joinNowAction = UIAction(title: "Meet Now", image: actionImage) { _ in }
-        let scheduleMeetingAction = UIAction(title: "Schedule a Meeting", image: actionImage) { _ in }
+        let chevron = UIImage(systemName: "chevron.forward", withConfiguration: configuration)
 
-        let menuChildren = [
-            joinNowAction,
-            scheduleMeetingAction
-        ]
-        let filterMenu = UIMenu(children: menuChildren)
+        let meetNow = UIAction(title: "Meet Now", image: chevron) { [weak self] _ in
+            self?.viewModel.meetNowTapped()
+        }
+        let schedule = UIAction(title: "Schedule a Meeting", image: chevron) { [weak self] _ in
+            self?.viewModel.scheduleMeetingTapped()
+        }
+
+        let menu = UIMenu(children: [meetNow, schedule])
 
         let symbolConfiguration = UIImage.SymbolConfiguration(weight: .semibold)
-        let startMeetingImage = UIImage(systemName: "video.fill", withConfiguration: symbolConfiguration)!
-        let startMeetingButton = UIButton(type: .system)
-        startMeetingButton.setImage(startMeetingImage, for: .normal)
-        startMeetingButton.showsMenuAsPrimaryAction = true
-        startMeetingButton.accessibilityLabel = L10n.Accessibility.ConversationsList.FilterButton.description
-        startMeetingButton.menu = filterMenu
-        let startMeetingItem = UIBarButtonItem(customView: startMeetingButton)
+        let video = UIImage(systemName: "video.fill", withConfiguration: symbolConfiguration)
 
-        navigationItem.rightBarButtonItems = [startMeetingItem]
+        let button = UIButton(type: .system)
+        button.setImage(video, for: .normal)
+        button.accessibilityLabel = "Start or schedule a meeting"
+        button.showsMenuAsPrimaryAction = true
+        button.menu = menu
 
+        let item = UIBarButtonItem(customView: button)
+        navigationItem.rightBarButtonItems = [item]
     }
-
-    // MARK: - Navigation Bar Items
 
     private func makeAccountImageView() -> AccountImageView {
-
-        let accountImageView = AccountImageView()
-//        accountImageView.source = viewModel.accountImageSource
-//        accountImageView.availability = viewModel.selfUserStatus.availability.mapToAccountImageAvailability()
-//        accountImageView.hideProfileNotificationsBadge = viewModel.hideProfileNotificationsBadge
-        accountImageView.isAccessibilityElement = true
-//        accountImageView.accessibilityValue = L10n.Localizable.ConversationList.Header.SelfTeam
-//            .accessibilityValue(viewModel.userSession.selfUser.name ?? "")
-        accountImageView.accessibilityHint = L10n.Accessibility.ConversationsList.AccountButton.hint
-        accountImageView.translatesAutoresizingMaskIntoConstraints = false
-        accountImageView.widthAnchor.constraint(equalToConstant: 28).isActive = true
-        accountImageView.heightAnchor.constraint(equalToConstant: 28).isActive = true
-
-        let design = AccountImageViewDesign()
-        accountImageView.imageBorderWidth = design.borderWidth
-        accountImageView.imageBorderColor = design.borderColor
-        accountImageView.availableColor = design.availabilityIndicator.availableColor
-        accountImageView.busyColor = design.availabilityIndicator.busyColor
-        accountImageView.awayColor = design.availabilityIndicator.awayColor
-        accountImageView.availabilityIndicatorBackgroundColor = design.availabilityIndicator.backgroundViewColor
-
-        accountImageView.translatesAutoresizingMaskIntoConstraints = false
-        accountImageView.widthAnchor.constraint(equalToConstant: 28).isActive = true
-        accountImageView.heightAnchor.constraint(equalToConstant: 28).isActive = true
-
-        return accountImageView
+        let v = AccountImageView()
+        v.isAccessibilityElement = true
+        v.accessibilityHint = viewModel.accessibilityHintForAvatar
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.widthAnchor.constraint(equalToConstant: 28).isActive = true
+        v.heightAnchor.constraint(equalToConstant: 28).isActive = true
+        return v
     }
-
 }
-
