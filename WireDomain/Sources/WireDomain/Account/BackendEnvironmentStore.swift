6c19ede7cccc335f9a73a8de3b766ce4840806ee
François Benaiteau <francois.benaiteau@wire.com>
2025-10-17 09:46:45 +0200

    remove comment

--- WireDomain/Sources/WireDomain/Account/BackendEnvironmentStore.swift
+++ WireDomain/Sources/WireDomain/Account/BackendEnvironmentStore.swift
@@ -100,7 +100,6 @@ public struct BackendEnvironmentStore {
             if
                 nsError.domain == NSCocoaErrorDomain,
                 nsError.code == NSFileReadNoSuchFileError {
-                // Why do we return nil ?
                 return nil
             }
 
