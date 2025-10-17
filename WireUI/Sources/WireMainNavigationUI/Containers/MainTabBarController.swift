584b9113089bf3192b4df6a71132a832edfe73a7
Manish Singh <31733022+findms@users.noreply.github.com>
2025-10-17 09:24:38 +0200

    chore: common locator placement - WPB-20289 (#3653)

--- WireUI/Sources/WireMainNavigationUI/Containers/MainTabBarController.swift
+++ WireUI/Sources/WireMainNavigationUI/Containers/MainTabBarController.swift
@@ -17,6 +17,7 @@
 //
 
 import SwiftUI
+import WireLocators
 
 // TODO: [WPB-11448] Bug: The call screen doesn't rotate to landscape
 
@@ -130,7 +131,8 @@ public final class MainTabBarController<
                     image: .init(systemName: "text.bubble"),
                     selectedImage: .init(systemName: "text.bubble.fill")
                 )
-                tabBarItem.accessibilityIdentifier = "bottomBarRecentListButton"
+                tabBarItem
+                    .accessibilityIdentifier = Locators.ConversationsPage.bottomBarRecentListButton.rawValue
                 tabBarItem.accessibilityLabel = String(
                     localized: "tabBar.conversations.description",
                     table: "Accessibility",
@@ -187,7 +189,8 @@ public final class MainTabBarController<
                     image: .init(systemName: "gearshape"),
                     selectedImage: .init(systemName: "gearshape.fill")
                 )
-                tabBarItem.accessibilityIdentifier = "bottomBarSettingsButton"
+                tabBarItem
+                    .accessibilityIdentifier = Locators.ConversationsPage.bottomBarSettingsButton.rawValue
                 tabBarItem.accessibilityLabel = String(
                     localized: "tabBar.settings.description",
                     table: "Accessibility",
