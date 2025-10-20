317ce35079b8e3683dea3da0da4e4995d70e0f92
Christoph Aldrian <christoph.aldrian@wire.com>
2025-10-20 14:46:50 +0200

    revert last commit

--- wire-ios-sync-engine/Source/UserSession/UserSession.swift
+++ wire-ios-sync-engine/Source/UserSession/UserSession.swift
@@ -54,12 +54,6 @@ public protocol UserSession: AnyObject {
 
     var requiresScreenCurtain: Bool { get }
 
-    /// Convenience accessor to `clientSessionComponent?.featureConfigRepository.isFeatureEnabled(_:)`.
-
-    func isFeatureEnabled(
-        feature: Feature
-    ) async -> Bool
-
     /// Whether the app lock on.
 
     var isAppLockActive: Bool { get set }
@@ -322,15 +316,5 @@ public protocol UserSession: AnyObject {
     var resolvedBackendMetadata: BackendMetadataProvider { get }
 
     var isBackendMLSEnabled: Bool { get }
-
-}
-
-public extension UserSession {
-
-    func isFeatureEnabled(
-        _ feature: Feature.Name
-    ) async -> Bool? {
-        await clientSessionComponent?.featureConfigRepository.isFeatureEnabled(feature)
-    }
 
 }
