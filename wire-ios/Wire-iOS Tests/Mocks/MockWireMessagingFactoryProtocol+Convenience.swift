1ac1b4dc12ce720b5fc3d7e7eebc09e243e5983f
Sam Wyndham <samwyndham@users.noreply.github.com>
2025-10-21 15:23:52 +0200

    Fix compile error in tests

--- wire-ios/Wire-iOS Tests/Mocks/MockWireMessagingFactoryProtocol+Convenience.swift
+++ wire-ios/Wire-iOS Tests/Mocks/MockWireMessagingFactoryProtocol+Convenience.swift
@@ -26,7 +26,7 @@ extension MockWireMessagingFactoryProtocol {
         mock.makePublishDraftsUseCaseCellName_MockValue = WireCellsPublishDraftsUseCaseProtocolMock()
         mock.makeRetryUploadDraftUseCaseCellName_MockValue = WireCellsRetryUploadDraftUseCaseProtocolMock()
         mock.makeUploadDraftUseCaseCellName_MockValue = WireCellsUploadDraftUseCaseProtocolMock()
-        mock.makeConversationCellProviderInsets_MockValue = MockConversationCellProviderProtocol()
+        mock.makeConversationCellProviderInsetsProvider_MockValue = MockConversationCellProviderProtocol()
         return mock
     }
 
