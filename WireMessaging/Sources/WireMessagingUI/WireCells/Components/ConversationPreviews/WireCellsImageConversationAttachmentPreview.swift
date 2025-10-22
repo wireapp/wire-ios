9d41c22fe73a699d67207c2ecb94ab3bfed4d197
Sam Wyndham <samwyndham@users.noreply.github.com>
2025-10-22 09:56:17 +0200

    Add WireCellsImageConversationAttachmentPreview

---
+++ WireMessaging/Sources/WireMessagingUI/WireCells/Components/ConversationPreviews/WireCellsImageConversationAttachmentPreview.swift
@@ -0,0 +1,88 @@
+//
+// Wire
+// Copyright (C) 2025 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import SwiftUI
+import WireDesign
+import WireFoundation
+
+struct WireCellsImageConversationAttachmentPreview: View {
+
+    let thumbnailURL: URL?
+    let progress: Double?
+    let isError: Bool
+    let noPreviewMessage: String?
+
+    init(thumbnailURL: URL?, progress: Double?, isError: Bool, noPreviewMessage: String?) {
+        self.thumbnailURL = thumbnailURL
+        self.progress = progress
+        self.isError = isError
+        self.noPreviewMessage = noPreviewMessage
+    }
+
+    var body: some View {
+        WireCellsAttachmentPreview(
+            progress: progress,
+            progressColor: isError ? ColorTheme.Base.error.color : ColorTheme.Base.primary.color
+        ) {
+            ZStack {
+                AsyncImage(url: thumbnailURL, scale: UIScreen.main.scale) { phase in
+                    switch phase {
+                    case .empty where !isError && thumbnailURL != nil:
+                        ProgressView()
+                            .tint(ColorTheme.Backgrounds.surface.color)
+                    case .success(let image):
+                        GeometryReader { geometry in
+                            image
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: geometry.size.width, height: geometry.size.height)
+                        }
+                    default:
+                        if let noPreviewMessage, !isError {
+                            Text(noPreviewMessage)
+                                .wireTextStyle(.subline1)
+                                .foregroundColor(ColorTheme.Backgrounds.surface.color)
+                                .multilineTextAlignment(.center)
+                                .padding()
+                        } else {
+                            EmptyView()
+                        }
+                    }
+                }
+
+                if isError {
+                    WireCellsAttachmentPreviewErrorCircle()
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(ColorTheme.Backdrop.background.color)
+        }
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    WireCellsImageConversationAttachmentPreview(
+        thumbnailURL: nil,
+        progress: 0.5,
+        isError: false,
+        noPreviewMessage: "No preview available"
+    )
+    .environment(\.wireTextStyleMapping, WireTextStyleMapping())
+}
