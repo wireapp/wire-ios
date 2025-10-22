f558f4ae51ed333523148b7d6cf206071b987033
Christoph Aldrian <christoph.aldrian@wire.com>
2025-10-22 11:24:38 +0200

    fix migration and tests

--- wire-ios-data-model/Source/ManagedObjectContext/Migration/CoreDataMessagingMigrationVersion.swift
+++ wire-ios-data-model/Source/ManagedObjectContext/Migration/CoreDataMessagingMigrationVersion.swift
@@ -29,6 +29,7 @@ enum CoreDataMessagingMigrationVersion: String, CoreDataMigrationVersion {
     // MARK: -
 
     // Note: add new versions here in first position!
+    case v132 = "zmessaging2.132.0"
     case v131 = "zmessaging2.131.0"
     case v130 = "zmessaging2.130.0"
     case v129 = "zmessaging2.129.0"
@@ -84,10 +85,10 @@ enum CoreDataMessagingMigrationVersion: String, CoreDataMigrationVersion {
 
     var nextVersion: Self? {
         switch self {
-        case .v131:
+        case .v132:
             nil
-        case .v129, .v130:
-            .v131
+        case .v129, .v130, .v131:
+            .v132
         case .v128:
             .v129
         case .v127:
