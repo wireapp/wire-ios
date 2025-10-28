4b8fc1148428b0a7a5cd4b81e994056b44f74f9b
Jullian Mercier <31648126+jullianm@users.noreply.github.com>
2025-10-28 08:51:17 +0100

    update code comment

--- WireMessaging/Sources/WireMessagingData/WireCells/NodesAPI/NodesAPI.swift
+++ WireMessaging/Sources/WireMessagingData/WireCells/NodesAPI/NodesAPI.swift
@@ -123,7 +123,8 @@ package final actor NodesAPI: NodesAPIProtocol, WireCellsNodesRepositoryProtocol
         do {
             let (nodes, nextOffset) = try await restAPI.getNodes(request)
             return (nodes: nodes.map { $0.toDomainModel() }, nextOffset: nextOffset)
-            // no cells conversations exist on the backend
+            // user not yet created, wire users are "lazily" sync to pydio users the first time they are part of a cells
+            // conversation
         } catch let error as CellsSDK.ErrorResponse where error.httpStatusCode == 401 {
             return (nodes: [], nextOffset: nil)
         } catch {
