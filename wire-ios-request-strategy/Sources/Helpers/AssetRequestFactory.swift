fcdadee7dd1d170a34b5a0d7769309d8453ea4bf
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2025-10-29 13:46:32 +0100

    add v13 in WireTransport

--- wire-ios-request-strategy/Sources/Helpers/AssetRequestFactory.swift
+++ wire-ios-request-strategy/Sources/Helpers/AssetRequestFactory.swift
@@ -97,7 +97,7 @@ public final class AssetRequestFactory: NSObject {
         case .v0, .v1:
             "/assets/v3"
 
-        case .v2, .v3, .v4, .v5, .v6, .v7, .v8, .v9, .v10, .v11, .v12:
+        case .v2, .v3, .v4, .v5, .v6, .v7, .v8, .v9, .v10, .v11, .v12, .v13:
             "/assets"
         }
 
@@ -135,7 +135,7 @@ public final class AssetRequestFactory: NSObject {
         case .v0, .v1:
             "/assets/v3"
 
-        case .v2, .v3, .v4, .v5, .v6, .v7, .v8, .v9, .v10, .v11, .v12:
+        case .v2, .v3, .v4, .v5, .v6, .v7, .v8, .v9, .v10, .v11, .v12, .v13:
             "/assets"
         }
 
