1cde70141cafc271fe10bc19cfc2ed395b07f027
Christoph Aldrian <christoph.aldrian@wire.com>
2025-10-30 18:40:45 +0100

    fix file headers

--- wire-ios-share-engine/Sources/WireLegacyLogging.swift
+++ wire-ios-share-engine/Sources/WireLegacyLogging.swift
@@ -1,6 +1,6 @@
 //
 // Wire
-// Copyright (C) 2024 Wire Swiss GmbH
+// Copyright (C) 2025 Wire Swiss GmbH
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -16,6 +16,8 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-import WireLegacyLogging
+// TODO: delete file
 
-typealias WireLogger = WireLegacyLogging.WireLogger
+// import WireLegacyLogging
+
+// typealias WireLogger = WireLegacyLogging.WireLogger
