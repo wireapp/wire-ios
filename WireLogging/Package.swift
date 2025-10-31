a278cde0115d867f84880736e617783b86f38cdf
Christoph Aldrian <christoph.aldrian@wire.com>
2025-10-31 12:47:48 +0100

    fix more errors

--- WireLogging/Package.swift
+++ WireLogging/Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version: 6.2
 
+import Foundation
 import PackageDescription
 
 let package = Package(
@@ -46,5 +47,7 @@ for target in package.targets {
         .enableUpcomingFeature("InternalImportsByDefault"),
         .enableUpcomingFeature("MemberImportVisibility"),
         .enableUpcomingFeature("StrictMemorySafety"),
-    ]
+        isCI ? .unsafeFlags(["-warnings-as-errors"]) : nil
+    ].compactMap(\.self)
 }
+let isCI = ProcessInfo.processInfo.environment["CI"] != nil
