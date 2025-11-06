7afd8570afe4a46024d8545c0ebb9ea37a068769
Christoph Aldrian <christoph.aldrian@wire.com>
2025-11-06 17:41:49 +0100

    cleanup

--- WireLogging/Tests/WireLoggingTests/WireLogMessage/WireLogInterpolationTests.swift
+++ WireLogging/Tests/WireLoggingTests/WireLogMessage/WireLogInterpolationTests.swift
@@ -34,46 +34,6 @@ struct WireLogInterpolationTests {
         let name = "World" as StaticString
         let message: WireLogMessage = "Hello, \(name)!"
         #expect(message.content == "Hello, World!")
-
-        // TODO: delete
-        let osl = OSLogHandler(subsystem: Bundle.main.bundleIdentifier!)
-        let l = WireTaggedLogger(tag: "test", handler: osl)
-        l.critical("Lorem \("Ipsum")", .eventID("overridden"))
-    }
-
-}
-
-// TODO: move somewhere else? keep in readme/docs?
-private struct CustomType {
-    var eventID = "012345"
-    var sensibleInformation = "Sensitive"
-}
-
-private extension WireLogInterpolation {
-
-    mutating func appendInterpolation(
-        _ customType: CustomType,
-        selfUserID: UUID
-    ) {
-
-        let obfuscationStartIndex = content.index(content.startIndex, offsetBy: 4)
-        let obfuscatedContent = content.replacingCharacters(in: obfuscationStartIndex ... content.endIndex, with: "***")
-        writeText(obfuscatedContent)
-
-        writeAttribute(.eventID(selfUserID.uuidString))
-        writeAttribute(.selfUserID(selfUserID.uuidString))
-    }
-
-}
-
-private extension WireLogAttribute {
-
-    static func eventID(_ value: String) -> WireLogAttribute {
-        .init(key: "event_id", value: value)
-    }
-
-    static func selfUserID(_ value: String) -> WireLogAttribute {
-        .init(key: "self_user_id", value: value)
     }
 
 }
