8a236aff8f4e62f38e9bda5c74a30e00488d0231
KaterinaWire <katerina@wire.com>
2025-11-11 17:34:25 +0100

    update AVS version

--- WireAVS/Package.swift
+++ WireAVS/Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WireAVS",
-            url: "https://github.com/wireapp/wire-avs/releases/download/10.1.28/avs.xcframework.zip",
-            checksum: "111e1598aa781bb20a5c52075cb27386271a4c6fcaec935396db030e701c768b"
+            url: "https://github.com/wireapp/wire-avs/releases/download/10.1.29/avs.xcframework.zip",
+            checksum: "3fd37a4055e8d381e268be5cc1da1d817121dee9330103b14b671a41669b7b99"
         )
     ]
 )
