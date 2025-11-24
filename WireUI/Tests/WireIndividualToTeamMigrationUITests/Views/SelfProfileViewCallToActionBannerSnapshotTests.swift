8011ff92dce16e2c2f3fbbeb91c26be3fb46c84f
Christoph Aldrian <christoph.aldrian@wire.com>
2025-11-24 10:51:29 +0100

    feat: create hint view for when apps are disabled - WPB-21441

--- WireUI/Tests/WireIndividualToTeamMigrationUITests/Views/SelfProfileViewCallToActionBannerSnapshotTests.swift
+++ WireUI/Tests/WireIndividualToTeamMigrationUITests/Views/SelfProfileViewCallToActionBannerSnapshotTests.swift
@@ -35,35 +35,31 @@ final class SelfProfileViewCallToActionBannerSnapshotTests: XCTestCase {
         snapshotHelper = nil
     }
 
-    @MainActor @ViewBuilder static var view: some View {
-        let screenBounds = UIScreen.main.bounds
-
-        SelfProfileViewCallToActionBanner(actionCallback: { _ in })
-            .frame(width: screenBounds.width, height: screenBounds.height)
-    }
-
     @MainActor
     func testColorSchemeVariants() {
-        let view = Self.view
-
+        let screenBounds = UIScreen.main.bounds
+        let sut = SelfProfileViewCallToActionBanner {}
+            .frame(width: screenBounds.width, height: screenBounds.width)
         snapshotHelper
             .withUserInterfaceStyle(.light)
-            .verify(matching: view, named: "light")
+            .verify(matching: sut, named: "light")
         snapshotHelper
             .withUserInterfaceStyle(.dark)
-            .verify(matching: view, named: "dark")
+            .verify(matching: sut, named: "dark")
     }
 
     @MainActor
     func testDynamicTypeVariants() {
-        let view = Self.view
-
+        let screenBounds = UIScreen.main.bounds
+        let sut = SelfProfileViewCallToActionBanner {}
+            .frame(width: screenBounds.width, height: screenBounds.width)
         for dynamicTypeSize in DynamicTypeSize.allCases {
             snapshotHelper
                 .verify(
-                    matching: view.dynamicTypeSize(dynamicTypeSize),
+                    matching: sut.dynamicTypeSize(dynamicTypeSize),
                     named: "\(dynamicTypeSize)"
                 )
         }
     }
+
 }
