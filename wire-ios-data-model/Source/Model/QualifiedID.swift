913205b65965acb971d286e19d881adb50a2d932
François Benaiteau <netbe@users.noreply.github.com>
2025-12-03 21:02:48 +0100

    chore: clean log values - WPB-22183 (#3959)

--- wire-ios-data-model/Source/Model/QualifiedID.swift
+++ wire-ios-data-model/Source/Model/QualifiedID.swift
@@ -56,7 +56,7 @@ public struct QualifiedID: Codable, Equatable, Hashable, CustomDebugStringConver
 
 extension QualifiedID: SafeForLoggingStringConvertible {
     public var safeForLoggingDescription: String {
-        "\(uuid.safeForLoggingDescription) - \(domain.redactedAndTruncated(maxVisibleCharacters: 4, length: 7))"
+        "\(uuid.uuidString.lowercased()) - \(domain)"
     }
 }
 
