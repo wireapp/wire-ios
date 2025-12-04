bcb46959d7008176d02c106b57bb13027e252824
Sam Wyndham <samwyndham@users.noreply.github.com>
2025-12-04 15:31:07 +0100

    Remove from menu when it's in the recycle bin.

--- WireMessaging/Sources/WireMessagingUI/WireCells/Components/Files/FilesItemViewModel.swift
+++ WireMessaging/Sources/WireMessagingUI/WireCells/Components/Files/FilesItemViewModel.swift
@@ -147,7 +147,7 @@ final class FilesItemViewModel: ObservableObject {
     }
 
     var isEditable: Bool {
-        return item.isEditable && !isInRecycleBin
+        item.isEditable && !isInRecycleBin
     }
 
     func open() async {
