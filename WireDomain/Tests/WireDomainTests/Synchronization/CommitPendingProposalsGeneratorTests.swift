b1e6af53273b0d72e3252dfa4931e4b76647ff09
François Benaiteau <francois.benaiteau@wire.com>
2025-12-05 13:52:07 +0100

    wip

---
+++ WireDomain/Tests/WireDomainTests/Synchronization/CommitPendingProposalsGeneratorTests.swift
@@ -0,0 +1,124 @@
+//
+// Wire
+// Copyright (C) 2025 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import Testing
+import WireDataModel
+import WireDataModelSupport
+import WireDomainSupport
+@testable import WireDomain
+
+class CommitPendingProposalsGeneratorTests {
+
+    var sut: CommitPendingProposalsGenerator!
+    let repository: MockConversationRepositoryProtocol
+    let modelHelper = ModelHelper()
+    let coreDataStackHelper = CoreDataStackHelper()
+    let coreDataStack: CoreDataStack
+    var commitPendingProposalItemClosure: ((CommitPendingProposalItem) -> Void)?
+    var mockMLSService: MockMLSServiceInterface!
+    
+    init() async throws {
+        self.repository = MockConversationRepositoryProtocol()
+        self.mockMLSService = MockMLSServiceInterface()
+        self.mockMLSService.subConferenceConversationParentGroupID_MockMethod = { _ in return nil }
+        self.coreDataStack = try await coreDataStackHelper.createStack()
+
+        self.sut = CommitPendingProposalsGenerator(
+            repository: repository,
+            mlsService: mockMLSService,
+            context: coreDataStack.syncContext,
+            onCommitPendingProposals: { item in
+                self.commitPendingProposalItemClosure?(item)
+            }
+        )
+    }
+
+    @Test("It generates an item when a conversation with commitPendingProposalDate set is found")
+    func startGeneratesItem() async throws {
+        // GIVEN
+        let conversationID = QualifiedID.random()
+        await coreDataStack.syncContext.perform { [modelHelper, context = coreDataStack.syncContext] in
+            let selfUser = ZMUser.selfUser(in: context)
+            let conversation = modelHelper.createMLSConversation(
+                id: conversationID.uuid,
+                domain: conversationID.domain,
+                mlsGroupID: .random(),
+                with: [selfUser],
+                in: context
+            )
+            conversation.commitPendingProposalDate = Date()
+        }
+        var items = [CommitPendingProposalItem]()
+        commitPendingProposalItemClosure = { item in
+            items.append(item)
+        }
+        // WHEN
+        await sut.start()
+
+        // THEN
+        #expect(items.count == 1)
+        #expect(items.first?.conversationID == conversationID)
+
+        // WHEN
+        let newConversationID = QualifiedID.random()
+        try await confirmation("generator delivers an update for a new conversation") { confirm in
+            self.commitPendingProposalItemClosure = {  item in
+                items.append(item)
+                confirm()
+            }
+
+            await coreDataStack.syncContext.perform { [modelHelper, context = coreDataStack.syncContext] in
+                let conversation = modelHelper.createMLSConversation(
+                    id: newConversationID.uuid,
+                    domain: newConversationID.domain,
+                    in: context
+                )
+                conversation.commitPendingProposalDate = Date()
+                context.saveOrRollback()
+            }
+            try await Task.sleep(for: .seconds(1))
+        }
+
+        // THEN
+        #expect(items.count == 2)
+        #expect(items.last?.conversationID == newConversationID)
+    }
+
+    @Test("It does not generate an item on conversation insertion")
+    func startDoesNotGenerateItem() async throws {
+        // GIVEN
+        var items = [CommitPendingProposalItem]()
+        commitPendingProposalItemClosure = { item in
+            items.append(item)
+        }
+        let conversationID = QualifiedID.random()
+        _ = await coreDataStack.syncContext.perform { [modelHelper, context = coreDataStack.syncContext] in
+            modelHelper.createMLSConversation(
+                id: conversationID.uuid,
+                domain: conversationID.domain,
+                in: context
+            )
+        }
+
+        // WHEN
+        await sut.start()
+
+        // THEN
+        #expect(items.isEmpty)
+    }
+}
