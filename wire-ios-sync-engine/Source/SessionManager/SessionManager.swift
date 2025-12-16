61bd0fba97ca6602b6c92ede4cbf2cb3a80b578d
KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
2025-12-16 15:55:07 +0100

    fix: Call UI not maximizing after account switch - WPB-20468 (#3897)

--- wire-ios-sync-engine/Source/SessionManager/SessionManager.swift
+++ wire-ios-sync-engine/Source/SessionManager/SessionManager.swift
@@ -341,6 +341,7 @@ public final class SessionManager: NSObject, SessionManagerType {
     let jailbreakDetector: JailbreakDetectorProtocol?
     fileprivate var accountTokens: [UUID: [Any]] = [:]
     fileprivate var memoryWarningObserver: NSObjectProtocol?
+    var conversationVisibleObserver: NSObjectProtocol?
     fileprivate var isSelectingAccount: Bool = false
 
     var proxyCredentials: WireTransport.ProxyCredentials?
