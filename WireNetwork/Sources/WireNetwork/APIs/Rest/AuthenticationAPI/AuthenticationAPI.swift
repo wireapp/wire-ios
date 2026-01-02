a5366c704ca96d420675f93b25c6e87dd446b91d
Manish Singh <findmanish@outlook.com>
2026-01-02 13:44:17 +0100

    formatting

--- WireNetwork/Sources/WireNetwork/APIs/Rest/AuthenticationAPI/AuthenticationAPI.swift
+++ WireNetwork/Sources/WireNetwork/APIs/Rest/AuthenticationAPI/AuthenticationAPI.swift
@@ -29,7 +29,7 @@ public protocol AuthenticationAPI: Sendable {
     ///   - verificationCode: The verification code is sent to the given user’s email address,
     ///   this is an optional field and depends on the team/server settings.
     ///   - label: An optional label to associate with the access token.
-    /// - Returns: HTTP cookie, a valid access token
+    /// - Returns: HTTP cookie, a valid access token.
 
     func login(
         email: String,
