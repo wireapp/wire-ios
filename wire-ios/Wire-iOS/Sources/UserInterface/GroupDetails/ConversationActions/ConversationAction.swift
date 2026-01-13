70783046bf77fdf2cc75ccd1ccb6ae871410c65e
Javier Calatrava <javier.calatrava_llaveria@mail.schwarz>
2026-01-13 12:57:00 +0100

    [PR] Address PR coments

--- wire-ios/Wire-iOS/Sources/UserInterface/GroupDetails/ConversationActions/ConversationAction.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/GroupDetails/ConversationActions/ConversationAction.swift
@@ -209,18 +209,3 @@ extension ZMConversation.Action {
         .init(title: title, style: isDestructive ? .destructive : .default, handler: { _, _ in handler() })
     }
 }
-
-extension UIAlertAction {
-
-    convenience init(
-        title: String?,
-        style: UIAlertAction.Style,
-        accessibilityIdentifier: String?,
-        handler: ((UIAlertAction) -> Void)? = nil
-    ) {
-        self.init(title: title, style: style, handler: handler)
-        if let accessibilityIdentifier {
-            setValue(accessibilityIdentifier, forKey: "accessibilityIdentifier")
-        }
-    }
-}
