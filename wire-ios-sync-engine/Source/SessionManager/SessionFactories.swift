75b25999a8fa593ae02851f59a9d909787a399d9
François Benaiteau <netbe@users.noreply.github.com>
2026-01-21 12:33:35 +0100

    chore: remove legacy sync - WPB-18928 (#4024)

--- wire-ios-sync-engine/Source/SessionManager/SessionFactories.swift
+++ wire-ios-sync-engine/Source/SessionManager/SessionFactories.swift
@@ -148,7 +148,6 @@ open class AuthenticatedSessionFactory {
         let userSession = userSessionBuilder.build()
         userSession.setup(
             apiVersion: nil,
-            eventProcessor: nil,
             strategyDirectory: nil,
             syncStrategy: nil,
             operationLoop: nil,
