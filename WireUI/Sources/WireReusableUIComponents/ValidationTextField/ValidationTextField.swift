3d8a4f1113c49fb94caa66c3b25a2694249c6e10
Wilhelm Oks <50032051+WilhelmOks@users.noreply.github.com>
2026-01-21 17:57:24 +0100

    feat: rename/create file/folder check for invalid chars - WPB-22585 (#4164)

--- WireUI/Sources/WireReusableUIComponents/ValidationTextField/ValidationTextField.swift
+++ WireUI/Sources/WireReusableUIComponents/ValidationTextField/ValidationTextField.swift
@@ -92,6 +92,10 @@ public struct ValidationTextField: View {
                         lineWidth: 1
                     )
             )
+            .background {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundStyle(ColorTheme.Backgrounds.backgroundVariant.color)
+            }
 
             if shouldShowErrorMessage, let errorMessage {
                 Text(errorMessage)
@@ -142,12 +146,20 @@ private struct ValidationTextField_PreviewView: View {
     @StateObject var viewModel: ValidationTextField_PreviewViewModel
 
     var body: some View {
-        ValidationTextField(
-            title: "Username",
-            placeholder: "Enter username",
-            textInput: $viewModel.text,
-            errorMessage: $viewModel.errorMessage
-        ).padding()
+        VStack {
+            ValidationTextField(
+                title: "Username",
+                placeholder: "Enter username",
+                textInput: $viewModel.text,
+                errorMessage: $viewModel.errorMessage
+            )
+        }
+        .padding()
+        .frame(maxHeight: .infinity)
+        .background {
+            ColorTheme.Backgrounds.background.color
+                .ignoresSafeArea(.all)
+        }
     }
 }
 
