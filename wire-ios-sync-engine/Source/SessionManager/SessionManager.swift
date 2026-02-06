3dd698e5a7a65fe96c2fe09dddf3c23a98b8b255
Sam Wyndham <samwyndham@users.noreply.github.com>
2026-02-06 12:53:42 +0100

    fix: crash loading multiple user session for same account concurrently - WPB-22928 (#4218)

--- wire-ios-sync-engine/Source/SessionManager/SessionManager.swift
+++ wire-ios-sync-engine/Source/SessionManager/SessionManager.swift
@@ -381,6 +381,10 @@ public final class SessionManager: NSObject, SessionManagerType {
 
     private let sharedUserDefaults: UserDefaults
 
+    /// Used by `withSession(for:newEnvironment:notifyAboutMigration:)` to avoid creating & loading multiple sessions
+    /// for the same account concurrently.
+    private let withSessionTaskManager = NonReentrantTaskManager<ZMUserSession?, Never>()
+
     // MARK: - Life cycle
 
     public override init() {
@@ -993,10 +997,13 @@ public final class SessionManager: NSObject, SessionManagerType {
         notifyAboutMigration: Bool = false
     ) async -> ZMUserSession? {
         WireLogger.sessionManager.debug("Request to load session for \(account)")
+
         if let session = backgroundUserSessions[account.userIdentifier] {
             WireLogger.sessionManager.debug("Session for \(account) is already loaded")
             return session
-        } else {
+        }
+
+        return await withSessionTaskManager.performIfNeeded { @MainActor [self] in
             do {
                 let loader = try UserSessionLoader(
                     account: account,
