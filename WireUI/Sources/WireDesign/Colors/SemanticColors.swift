ef0bd9a86578f09b799519782ca637daf7bc55b8
Lalitha <syamala633@gmail.com>
2026-02-09 16:36:54 +0100

    Adjust dark mode color scheme

--- WireUI/Sources/WireDesign/Colors/SemanticColors.swift
+++ WireUI/Sources/WireDesign/Colors/SemanticColors.swift
@@ -283,7 +283,7 @@ public enum SemanticColors {
     }
 
     public enum ChatBubble {
-        public static let backgroundOtherMessage = UIColor(light: .gray30, dark: .gray100)
+        public static let backgroundOtherMessage = UIColor(light: .gray30, dark: .gray90)
         public static let foregroundOwnMessage = UIColor(light: .white, dark: .black)
         public static let foregroundOtherMessage = UIColor(light: .black, dark: .white)
     }
