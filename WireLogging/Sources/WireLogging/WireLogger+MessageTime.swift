2092b38f6fc2a0190848ae415048a7b03cb0866a
Sam Wyndham <samwyndham@users.noreply.github.com>
2026-02-09 10:36:44 +0100

    Allow customizing the duration attribute key

--- WireLogging/Sources/WireLogging/WireLogger+MessageTime.swift
+++ WireLogging/Sources/WireLogging/WireLogger+MessageTime.swift
@@ -22,10 +22,11 @@ public extension WireLogger {
 
     func measureTime<T>(
         label: String,
+        durationAttributeKey: LogAttributesKey = .syncDuration,
         attributes: LogAttributes = [:],
         block: () async throws -> T
     ) async throws -> T {
-        let context = measureTimeStart(label: label, attributes: attributes)
+        let context = measureTimeStart(label: label, durationAttributeKey: durationAttributeKey, attributes: attributes)
         let result = try await block()
         measureTimeEnd(context: context)
         return result
@@ -33,10 +34,11 @@ public extension WireLogger {
 
     func measureTime<T, E: Error>(
         label: String,
+        durationAttributeKey: LogAttributesKey = .syncDuration,
         attributes: LogAttributes = [:],
         block: () throws(E) -> T
     ) throws(E) -> T {
-        let context = measureTimeStart(label: label, attributes: attributes)
+        let context = measureTimeStart(label: label, durationAttributeKey: durationAttributeKey, attributes: attributes)
         let result = try block()
         measureTimeEnd(context: context)
         return result
@@ -47,23 +49,31 @@ public extension WireLogger {
     private struct Context {
         let start: Date
         let label: String
+        let durationAttributeKey: LogAttributesKey
         let attributes: LogAttributes
     }
 
     private func measureTimeStart(
         label: String,
+        durationAttributeKey: LogAttributesKey,
         attributes: LogAttributes
     ) -> Context {
         let startMessage = "starting \(label)"
         info(startMessage, attributes: attributes)
-        return Context(start: Date.now, label: label, attributes: attributes)
+
+        return Context(
+            start: Date.now,
+            label: label,
+            durationAttributeKey: durationAttributeKey,
+            attributes: attributes
+        )
     }
 
     private func measureTimeEnd(context: Context) {
         let durationInSeconds = context.start.timeIntervalSinceNow.magnitude
         var updatedAttributes = context.attributes
         let formattedDuration = String(format: "%.2f", durationInSeconds)
-        updatedAttributes[.syncDuration] = formattedDuration
+        updatedAttributes[context.durationAttributeKey] = formattedDuration
         let completedMessage = "completed \(context.label)"
         info(completedMessage, attributes: updatedAttributes)
     }
