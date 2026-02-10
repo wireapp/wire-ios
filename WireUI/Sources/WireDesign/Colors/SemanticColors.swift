842e9c82a72ffde921777f15b9333f3143348eae
Lalitha <syamala633@gmail.com>
2026-02-10 14:56:59 +0100

    "updated font color inside chatbubbles as per Figma link"

--- WireUI/Sources/WireDesign/Colors/SemanticColors.swift
+++ WireUI/Sources/WireDesign/Colors/SemanticColors.swift
@@ -284,7 +284,7 @@ public enum SemanticColors {
 
     public enum ChatBubble {
         public static let backgroundOtherMessage = UIColor(light: .gray30, dark: .gray90)
-        public static let foregroundOwnMessage = UIColor(light: .white, dark: .black)
+        public static let foregroundOwnMessage = UIColor(light: .black, dark: .white)
         public static let foregroundOtherMessage = UIColor(light: .black, dark: .white)
     }
 }
