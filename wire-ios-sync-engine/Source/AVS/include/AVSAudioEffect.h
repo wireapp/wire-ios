9d16e9d23a9cf856152c5851cdd98a6266f9cd11
Christoph Aldrian <caldrian@users.noreply.github.com>
2026-02-12 12:12:34 +0100

    fix: copyright year - WPB-22589 (#4306)

--- wire-ios-sync-engine/Source/AVS/include/AVSAudioEffect.h
+++ wire-ios-sync-engine/Source/AVS/include/AVSAudioEffect.h
@@ -1,6 +1,6 @@
 //
 // Wire
-// Copyright (C) 2025 Wire Swiss GmbH
+// Copyright (C) 2026 Wire Swiss GmbH
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -64,5 +64,3 @@ typedef NS_ENUM(NSInteger, AVSAudioEffectType) {
 - (int)applyEffectWav:(id<AVSAudioEffectProgressDelegate>)delegate inFile: (NSString *)inWavFileName outFile: (NSString *)outWavFileName effect: (AVSAudioEffectType) effect nr_flag:(bool)reduce_noise;
 
 @end
-
-
