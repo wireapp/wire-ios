93dd95d575f7c1d87e1e183f4eead4edde757ca7
Christoph Aldrian <christoph.aldrian@wire.com>
2026-02-13 17:01:50 +0100

    add TODO

--- wire-ios/Wire-iOS/Sources/UserInterface/UserProfile/ProfileViewControllerViewModel.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/UserProfile/ProfileViewControllerViewModel.swift
@@ -185,7 +185,7 @@ final class ProfileViewControllerViewModel: NSObject, ProfileViewControllerViewM
     func startOneToOneConversation() {
         viewModelDelegate?.startAnimatingActivity()
 
-        userSession.createTeamOneOnOne(with: user) { [weak self] in
+        userSession.createTeamOneOnOne(with: user) { [weak self] in // TODO: use this for starting conversations with apps?
             self?.viewModelDelegate?.stopAnimatingActivity()
 
             switch $0 {
