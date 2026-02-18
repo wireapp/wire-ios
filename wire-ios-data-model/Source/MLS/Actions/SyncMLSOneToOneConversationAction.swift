5a56927d4105ab5a2841b06b5025895c31f8ce9d
François Benaiteau <netbe@users.noreply.github.com>
2026-02-18 08:54:40 +0100

    fix: triggering MLS resets for a federated conversation will establish a broken group - WPB-22510 (#4304)

--- wire-ios-data-model/Source/MLS/Actions/SyncMLSOneToOneConversationAction.swift
+++ wire-ios-data-model/Source/MLS/Actions/SyncMLSOneToOneConversationAction.swift
@@ -20,7 +20,7 @@ import Foundation
 
 public struct SyncMLSOneToOneConversationAction: EntityAction {
 
-    public typealias Result = (MLSGroupID, BackendMLSPublicKeys?)
+    public typealias Result = (QualifiedID, MLSGroupID, BackendMLSPublicKeys?)
     public typealias Failure = SyncMLSOneToOneConversationActionError
 
     public let userID: UUID
