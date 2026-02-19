2be351c22aa1c1872b9ca6fd9bd8719226da12cc
Manish Singh <31733022+findms@users.noreply.github.com>
2026-02-19 14:47:26 +0100

    chore: test-MentionUserInGroup - WPB-23471 (#4309)

--- wire-ios/WireUITests/Pages/ConversationsPage.swift
+++ wire-ios/WireUITests/Pages/ConversationsPage.swift
@@ -64,6 +64,14 @@ class ConversationsPage: PageModel {
         app.buttons[Locators.ConversationsPage.accountProfileImageView.rawValue]
     }
 
+    var mentionStatus: XCUIElement {
+        app.otherElements[Locators.ConversationsPage.status.rawValue]
+    }
+
+    var loadBar: XCUIElement {
+        app.descendants(matching: .any)[Locators.ConversationsPage.loadBar.rawValue]
+    }
+
     func openSettings() throws -> SettingsPage {
         settingsButton.tap()
         return try SettingsPage()
@@ -75,7 +83,7 @@ class ConversationsPage: PageModel {
     }
 
     func openUserProfilePage() throws -> UserProfilePage {
-        try VerifyUserIsOnConversationsTab()
+        try letTheSyncFinish()
         accountProfileImageView.waitAndTap()
         return try UserProfilePage()
     }
@@ -86,6 +94,7 @@ class ConversationsPage: PageModel {
     }
 
     func openPendingRequest() throws -> ConnectionRequestsPage {
+        try letTheSyncFinish()
         XCTAssertTrue(conversationCell.waitForExistence(timeout: 5), "Conversation cell did not appear")
 
         let maxDuration: TimeInterval = 10
@@ -101,6 +110,7 @@ class ConversationsPage: PageModel {
     }
 
     func openConversation() throws -> ActiveConversationPage {
+        try letTheSyncFinish()
         XCTAssertTrue(conversationCell.waitForExistence(timeout: 5), "Conversation cell did not appear")
 
         let maxDuration: TimeInterval = 10
@@ -130,11 +140,7 @@ class ConversationsPage: PageModel {
         conversationCell.label
     }
 
-    // Problem: Opening the user profile can occasionally fail while sync is in progress after login
-    // Workaround: Assert the presence of some UI elements to give the sync time to complete.
-    func VerifyUserIsOnConversationsTab() throws {
-        XCTAssertTrue(plusButtonToCreateGroup.exists)
-        XCTAssertTrue(archivedButton.exists)
-        XCTAssert(settingsButton.exists)
+    func letTheSyncFinish() throws {
+        loadBar.waitToDisappear()
     }
 }
