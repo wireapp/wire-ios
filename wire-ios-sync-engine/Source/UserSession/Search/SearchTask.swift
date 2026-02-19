8c02a540cf300226feaca21aecdf5290591f76d2
Christoph Aldrian <christoph.aldrian@wire.com>
2026-02-19 13:36:57 +0100

    delete unneeded code

--- wire-ios-sync-engine/Source/UserSession/Search/SearchTask.swift
+++ wire-ios-sync-engine/Source/UserSession/Search/SearchTask.swift
@@ -148,15 +148,6 @@ public final class SearchTask {
                     return { _ in }
                 }
             }
-            taskGroup.addTask {
-                do {
-                    return try await self.listAllApps()
-                } catch {
-                    let errorType = Swift.type(of: error)
-                    WireLogger.search.error("failed to perform app lookup: \(String(describing: errorType))")
-                    return { _ in }
-                }
-            }
 
             var result = SearchResult()
             while let aggregator = await taskGroup.next() {
@@ -429,94 +420,6 @@ public final class SearchTask {
     }
 
     // MARK: -
-
-    func listAllApps() async throws -> SearchResultAggregator {
-
-        func getTeamID() async -> UUID? {
-            let searchContext = contextProvider.newBackgroundContext()
-            return await searchContext.perform {
-                ZMUser.selfUser(in: searchContext).team?.remoteIdentifier
-            }
-        }
-
-        guard
-            let apiVersion,
-            apiVersion >= .v15,
-            case let .search(searchRequest) = type,
-            searchRequest.query.string.isEmpty,
-            !searchRequest.searchOptions.contains(.localResultsOnly),
-            searchRequest.searchOptions.contains(.apps),
-            let teamID = await getTeamID()
-        else {
-            return { _ in }
-        }
-
-        let apps = try await teamsAPI.getApps(for: teamID) // [WireNetwork.App(name: "WPB-20362 App 0", category: "developer", description: "some other app", accentID: 5, assets: [])]
-
-        try Task.checkCancellation()
-
-        let viewContext = contextProvider.viewContext
-        let searchUsers = await viewContext.perform { [searchUsersCache] in
-            apps.compactMap { app in
-                guard let id = app.id else { return ZMSearchUser?.none }
-
-                let domain = filteredContact.qualifiedID?.domain
-                let localUser = ZMUser.fetch(with: id, domain: domain, in: viewContext)
-
-                if let cachedSearchUser = searchUsersCache?.object(forKey: id as NSUUID) {
-                    cachedSearchUser.user = localUser
-                    return cachedSearchUser
-
-                } else {
-                    let accentColorRawValue = filteredContact.accentID.flatMap(Int16.init(exactly:))
-                    let accentColor = accentColorRawValue.flatMap(AccentColor.init(rawValue:))
-                    return ZMSearchUser(
-                        viewContext: viewContext,
-                        name: filteredContact.name,
-                        handle: filteredContact.handle,
-                        accentColor: accentColor.map(ZMAccentColor.from(accentColor:)),
-                        remoteIdentifier: filteredContact.id,
-                        domain: domain,
-                        teamIdentifier: filteredContact.team,
-                        user: localUser,
-                        searchUsersCache: searchUsersCache,
-                        type: .init(filteredContact.type)
-                    )
-                }
-            }
-        }
-
-        try Task.checkCancellation()
-
-        let searchOptions = searchRequest.searchOptions
-        let includeActiveTeamMembers = searchOptions.contains(.teamMembers) &&
-            searchOptions.isDisjoint(with: .excludeNonActiveTeamMembers)
-        let partialResult = await viewContext.perform { [searchUsersCache] in
-            SearchResult(
-                context: viewContext,
-                contacts: [],
-                teamMembers: includeActiveTeamMembers ? searchUsers.filter(\.isTeamMember) : [],
-                directory: searchUsers.filter { !$0.isConnected && !$0.isTeamMember },
-                conversations: [],
-                apps: searchUsers.filter(\.isApp),
-                bots: [],
-                searchUsersCache: searchUsersCache
-            )
-        }
-
-        try Task.checkCancellation()
-
-        if searchRequest.searchOptions.contains(.teamMembers) {
-            return try await performTeamMembershipLookup(on: partialResult, searchRequest: searchRequest)
-        } else if searchForApps {
-            return { $0 = $0.union(withAppsResult: partialResult) }
-        } else {
-            return { $0 = $0.union(withDirectoryResult: partialResult) }
-        }
-         */
-    }
-
-// MARK: -
 
     func performUserLookup() async throws -> SearchResultAggregator {
         guard case var .lookup(qualifiedID) = type else {
