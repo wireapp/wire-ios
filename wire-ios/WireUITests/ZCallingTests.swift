d1dba10e1d4db38e3800b75ed499efcbcb075d5c
Manish Singh <findmanish@outlook.com>
2026-02-26 14:37:16 +0100

    more info about comment added

--- wire-ios/WireUITests/ZCallingTests.swift
+++ wire-ios/WireUITests/ZCallingTests.swift
@@ -20,7 +20,7 @@ import WireFoundation
 import WireLocators
 import XCTest
 
-/// Prefixed 'Z' in the class name to run these tests at the end
+/// Prefixed 'Z' in the class name to run these tests at the end, just to avoid if this test may impact others due to alert keep showing
 final class ZCallingTests: WireUITestCase {
 
     struct GroupCallSetupResponse {
