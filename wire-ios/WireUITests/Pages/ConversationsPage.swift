85ebca125ffb9a3da222fc016482d5185e91cb9d
François Benaiteau <francois.benaiteau@wire.com>
2026-03-02 21:59:21 +0100

    fix identifiers

--- wire-ios/WireUITests/Pages/ConversationsPage.swift
+++ wire-ios/WireUITests/Pages/ConversationsPage.swift
@@ -84,8 +84,8 @@ class ConversationsPage: PageModel {
         app.descendants(matching: .any)[Locators.ActiveConversationPage.videoCallBarButton.rawValue].firstMatch
     }
 
-    var acceptRequestButton: XCUIElement {
-        app.buttons[Locators.ConversationsPage.connectionRequestsCell.rawValue]
+    var connectionsRequestCell: XCUIElement {
+        app.cells[Locators.ConversationsPage.connectionRequestsCell.rawValue]
     }
 
     var accountProfileImageView: XCUIElement {
@@ -128,7 +128,7 @@ class ConversationsPage: PageModel {
         let maxDuration: TimeInterval = 10
         let start = Date()
 
-        while !acceptRequestButton.exists, Date().timeIntervalSince(start) < maxDuration {
+        while !connectionsRequestCell.exists, Date().timeIntervalSince(start) < maxDuration {
             if conversationCell.isHittable {
                 conversationCell.tap()
             }
@@ -164,9 +164,9 @@ class ConversationsPage: PageModel {
         return self
     }
     
-    func acceptConnection() throws -> ConversationsPage {
-        acceptRequestButton.tap()
-        return self
+    func tapConnectionRequestsCell() throws -> ConnectionRequestsPage {
+        connectionsRequestCell.tap()
+        return try ConnectionRequestsPage()
     }
 
     func getNameLabel() -> String? {
