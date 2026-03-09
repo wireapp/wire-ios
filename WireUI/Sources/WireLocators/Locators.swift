830371d68bc2dfda78027cfa9fd37c32bc7a33dd
Jullian Mercier <jllnmercier@gmail.com>
2026-03-09 10:52:13 +0100

    lint and format

--- WireUI/Sources/WireLocators/Locators.swift
+++ WireUI/Sources/WireLocators/Locators.swift
@@ -315,9 +315,9 @@ public enum Locators {
         case classificationBanner = "ClassificationBanner"
 
     }
-    
+
     public enum WireDrive {
-        
+
         public enum FilesFilterPage: String {
             case saveButton
             case cancelButton
