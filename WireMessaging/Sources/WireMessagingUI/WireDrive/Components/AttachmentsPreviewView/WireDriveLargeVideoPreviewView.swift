09b4c32eaae330de1982f89123d2dbbaf3f7b387
Jullian Mercier <jllnmercier@gmail.com>
2026-03-10 12:08:19 +0100

    remove optional

--- WireMessaging/Sources/WireMessagingUI/WireDrive/Components/AttachmentsPreviewView/WireDriveLargeVideoPreviewView.swift
+++ WireMessaging/Sources/WireMessagingUI/WireDrive/Components/AttachmentsPreviewView/WireDriveLargeVideoPreviewView.swift
@@ -36,7 +36,7 @@ struct WireDriveLargeVideoPreviewView: View {
     let progress: Double?
     let downloadError: Bool
     let url: URL?
-    var imageAspectRatio: CGFloat? = defaultAspectRatio
+    var imageAspectRatio: CGFloat = defaultAspectRatio
     let duration: String?
 
     @Environment(\.wireAccentColor) private var wireAccentColor
@@ -88,7 +88,7 @@ struct WireDriveLargeVideoPreviewView: View {
             case let .success(image):
                 image
                     .resizable()
-                    .aspectRatio(imageAspectRatio ?? Self.defaultAspectRatio, contentMode: .fit)
+                    .aspectRatio(imageAspectRatio, contentMode: .fit)
                     .overlay {
                         PlayIcon()
                             .disabled(false)
@@ -105,7 +105,7 @@ struct WireDriveLargeVideoPreviewView: View {
     private func previewContainer(@ViewBuilder content: () -> some View)
         -> some View {
         Color.clear
-            .aspectRatio(imageAspectRatio ?? Self.defaultAspectRatio, contentMode: .fit)
+            .aspectRatio(imageAspectRatio, contentMode: .fit)
             .background(alignment: .top) {
                 content()
             }
