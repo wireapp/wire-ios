c6a6b37637e1ea24ec99afc8f0e7b375a2a65d15
Javier Calatrava <javier.calatrava_llaveria@mail.schwarz>
2026-03-10 12:19:49 +0100

    Addressed PR comments

--- wire-ios/Wire-iOS Tests/ConversationMessageCell/ConversationTextMessageCellTests.swift
+++ wire-ios/Wire-iOS Tests/ConversationMessageCell/ConversationTextMessageCellTests.swift
@@ -46,12 +46,13 @@ final class ConversationTextMessageCellTests: XCTestCase {
     // MARK: - Tests
 
     func testConfigureForMention() {
+        // Given
         let rawText = "Hola @Bruno"
         let mentionRange = (rawText as NSString).range(of: "@Bruno")
         let mention = Mention(range: mentionRange, user: otherUser)
-
+        // When
         applyConfig(text: rawText, mentions: [mention])
-
+        // Then
         let attrs = getAttributes(at: mentionRange)
         XCTAssertNotNil(attrs?[.link])
         XCTAssertEqual(attrs?[.underlineStyle] as? Int, 0, "Mentions must not be underlined")
@@ -59,13 +60,14 @@ final class ConversationTextMessageCellTests: XCTestCase {
     }
 
     func testConfigureForLink() {
+        // Given
         let rawText = "Check google.com"
         let linkRange = (rawText as NSString).range(of: "google.com")
         let linkURL = URL(string: "https://google.com")!
         let linkResult = NSTextCheckingResult.linkCheckingResult(range: linkRange, url: linkURL)
-
+        // When
         applyConfig(text: rawText, links: [linkResult])
-
+        // Then
         let attrs = getAttributes(at: linkRange)
         XCTAssertNotNil(attrs?[.link])
         XCTAssertEqual(attrs?[.underlineStyle] as? Int, NSUnderlineStyle.single.rawValue)
@@ -73,6 +75,7 @@ final class ConversationTextMessageCellTests: XCTestCase {
     }
 
     func testConfigureForAddress() {
+        // Given
         let addressText = "6, Plaza de Cataluña, Barcelona, Eixample, Barcelona, 08007"
         let fullText = "Mi dirección es \(addressText)"
         let range = (fullText as NSString).range(of: addressText)
@@ -81,25 +84,25 @@ final class ConversationTextMessageCellTests: XCTestCase {
             range: range,
             components: [.city: "Barcelona", .zip: "08007"]
         )
-
+        // When
         applyConfig(text: fullText, links: [addressResult])
-
+        // Then
         let attrs = getAttributes(at: range)
         let urlString = (attrs?[.link] as? URL)?.absoluteString ?? ""
-
         XCTAssertTrue(urlString.hasPrefix("http://maps.apple.com/?q="))
         XCTAssertTrue(urlString.contains("08007"))
         XCTAssertEqual(attrs?[.underlineStyle] as? Int, NSUnderlineStyle.single.rawValue)
     }
 
     func testConfigureForPhoneNumber() {
+        // Given
         let phoneNumber = "123456789"
         let fullText = "Llamame al \(phoneNumber)"
         let range = (fullText as NSString).range(of: phoneNumber)
         let phoneResult = NSTextCheckingResult.phoneNumberCheckingResult(range: range, phoneNumber: phoneNumber)
-
+        // When
         applyConfig(text: fullText, links: [phoneResult])
-
+        // Then
         let attrs = getAttributes(at: range)
         XCTAssertEqual((attrs?[.link] as? URL)?.absoluteString, "tel:\(phoneNumber)")
         XCTAssertEqual(attrs?[.underlineStyle] as? Int, NSUnderlineStyle.single.rawValue)
