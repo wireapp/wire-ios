dfb2087e31b23e7d698d0d8f161cb6a10de0f913
Christoph Aldrian <christoph.aldrian@wire.com>
2026-03-13 10:09:14 +0100

    cleanup and fixes

--- wire-ios-data-model/Source/Model/User/ServiceUser.swift
+++ wire-ios-data-model/Source/Model/User/ServiceUser.swift
@@ -16,12 +16,6 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
-@objc
 public protocol UserSearchResult: UserType { // TODO: still needed?
     var summary: String? { get }
-}
-
-extension ZMUser { // TODO: delete
-    static let servicesMustBeMentioned = false
-    static let serviceMentionKeyword = "@bots"
 }
