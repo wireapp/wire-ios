fa3e00bf9e7749a24de5c726cb830a976e6475b0
Christoph Aldrian <christoph.aldrian@wire.com>
2026-03-17 14:49:52 +0100

    prepare for creating app model

---
+++ WireData/Sources/WireData/Models/App.swift
@@ -0,0 +1,39 @@
+//
+// Wire
+// Copyright (C) 2026 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+import CoreData
+
+/// Additional information about an app.
+
+public final class App: NSManagedObject {
+
+    /// The name of the associated Core Data entity.
+
+    public static let entityName = "App"
+
+    // TODO: fix doc
+    /// The identifier of the asset on the Wire Cells backend.
+
+    @NSManaged public var category: String
+
+    // TODO: fix doc
+    /// The identifier of the asset on the Wire Cells backend.
+
+    @NSManaged public var description: String
+
+}
