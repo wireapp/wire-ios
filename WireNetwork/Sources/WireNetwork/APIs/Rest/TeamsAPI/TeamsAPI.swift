af50c3ecad5e24597fcf07d0f3a1e3ae70475713
Christoph Aldrian <christoph.aldrian@wire.com>
2026-03-18 14:44:23 +0100

    clean up getApp

--- WireNetwork/Sources/WireNetwork/APIs/Rest/TeamsAPI/TeamsAPI.swift
+++ WireNetwork/Sources/WireNetwork/APIs/Rest/TeamsAPI/TeamsAPI.swift
@@ -95,13 +95,6 @@ public protocol TeamsAPI {
         ) async throws -> UUID
     #endif
 
-    /// Fetches details of an app in a team by app id.
-
-    func getApp(
-        for teamID: Team.ID,
-        with id: UUID
-    ) async throws -> TmpApp
-
     func getWhitelistedBots(
         for teamID: Team.ID,
         with prefix: String
