9ace68ef536b5e758e457c23b72854861c8c0b90
KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
2026-03-18 11:45:52 +0100

    Commit with unresolved merge conflicts

--- WireAVS/Package.swift
+++ WireAVS/Package.swift
@@ -19,7 +19,10 @@ let package = Package(
             name: "WireAVS",
             url: "https://github.com/wireapp/wire-avs/releases/download/10.1.51/avs.xcframework.zip",
             checksum: "9e7d7c0dd553a5b624d8bb29f2d40997fe4e8c2fae59930ad7febe3d6dee237e"
+<<<<<<< HEAD
 
+=======
+>>>>>>> e86a1847b4 (chore: bump AVS version - WPB-24126 (#4448))
         )
     ]
 )
