cb1dabe4449253b5511ead56f61fccc861e435cb
François Benaiteau <netbe@users.noreply.github.com>
2026-03-26 16:02:21 +0100

    fix: disallow copy/paste on WireAuthentication - WPB-24299 (#4487)
    Co-authored-by: Christoph Aldrian <christoph.aldrian@wire.com>
    Co-authored-by: Christoph Aldrian <caldrian@users.noreply.github.com>
    Co-authored-by: David-Henner <david.henner@wire.com>

--- WireAuthentication/Sources/WireAuthenticationUI/Views/Login/LoginViaEmail/LoginViaEmailView.swift
+++ WireAuthentication/Sources/WireAuthenticationUI/Views/Login/LoginViaEmail/LoginViaEmailView.swift
@@ -129,12 +129,11 @@ package struct LoginViaEmailView: View {
         LabeledTextField(
             placeholder: Strings.CloudUserLogin.InputEmail.placeholder,
             title: Strings.CloudUserLogin.InputEmail.title,
-            string: $viewModel.email
+            string: $viewModel.email,
+            keyboardType: .emailAddress,
+            textContentType: .username
         )
-        .autocapitalization(.none)
         .autocorrectionDisabled()
-        .textContentType(.username)
-        .keyboardType(.emailAddress)
         .disabled(viewModel.isEmailPrefilled)
     }
 
@@ -232,12 +231,11 @@ package struct LoginViaEmailView: View {
             LabeledTextField(
                 placeholder: "jane@example.com",
                 title: Strings.ProxyCredentials.InputEmail.title,
-                string: $viewModel.proxyUsername
+                string: $viewModel.proxyUsername,
+                keyboardType: .emailAddress,
+                textContentType: .username
             )
-            .autocapitalization(.none)
             .autocorrectionDisabled()
-            .textContentType(.username)
-            .keyboardType(.emailAddress)
 
             PasswordField(
                 password: $viewModel.proxyPassword,
