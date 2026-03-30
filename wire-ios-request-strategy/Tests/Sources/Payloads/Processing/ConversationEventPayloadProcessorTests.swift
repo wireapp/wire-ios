88691978cfb3341a385a06558bec9db75de41696
Javier Calatrava <javier.calatrava_llaveria@mail.schwarz>
2026-03-30 17:24:12 +0200

    Revert unit test

--- wire-ios-request-strategy/Tests/Sources/Payloads/Processing/ConversationEventPayloadProcessorTests.swift
+++ wire-ios-request-strategy/Tests/Sources/Payloads/Processing/ConversationEventPayloadProcessorTests.swift
@@ -207,7 +207,7 @@ final class ConversationEventPayloadProcessorTests: MessagingTestBase {
                 domain: qualifiedID.domain,
                 in: self.syncMOC
             )
-            XCTAssertNil(conversation?.lastMessage?.systemMessageData?.systemMessageType)
+            XCTAssertEqual(conversation?.lastMessage?.systemMessageData?.systemMessageType, .newConversation)
         }
     }
 
