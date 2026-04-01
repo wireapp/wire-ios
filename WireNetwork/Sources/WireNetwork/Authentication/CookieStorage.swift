44616147e7aaf50a9b55df7c5b51d12f5a3423e2
Sam Wyndham <samwyndham@users.noreply.github.com>
2026-04-01 17:31:59 +0200

    Refactor `CookieStorage`

--- WireNetwork/Sources/WireNetwork/Authentication/CookieStorage.swift
+++ WireNetwork/Sources/WireNetwork/Authentication/CookieStorage.swift
@@ -43,30 +43,6 @@ public actor CookieStorage: CookieStorageProtocol {
     private let cookieEncryptionKey: Data
     private let keychain: any KeychainProtocol
 
-    private lazy var baseQuery: Set<KeychainQueryItem> = [
-        .service("Wire: Credentials for wire.com"),
-        .account(userID.uuidString),
-        .itemClass(.genericPassword)
-    ]
-
-    private lazy var fetchQuery: Set<KeychainQueryItem> = {
-        var result = baseQuery
-        result.insert(.returningData(true))
-        return result
-    }()
-
-    private func addQuery(cookieData: Data) -> Set<KeychainQueryItem> {
-        var result = updateQuery(cookieData: cookieData)
-        result.insert(.accessible(.afterFirstUnlock))
-        return result
-    }
-
-    private func updateQuery(cookieData: Data) -> Set<KeychainQueryItem> {
-        var result = baseQuery
-        result.insert(.data(cookieData.base64EncodedData()))
-        return result
-    }
-
     public init(
         userID: UUID,
         cookieEncryptionKey: Data,
@@ -86,8 +62,14 @@ public actor CookieStorage: CookieStorageProtocol {
     /// - Parameter cookies: The cookies to store.
 
     public func storeCookies(_ cookies: [HTTPCookie]) throws {
-        let cookieData = try HTTPCookieCodec.encodeCookies(cookies)
-        try storeCookieData(cookieData)
+        let cookieData = try Self.encodeAndEncryptCookies(cookies, key: cookieEncryptionKey)
+
+        // The typical case is updating so try that first.
+        do {
+            try keychain.updateItem(query: baseQuery(), attributesToUpdate: [.data(cookieData)])
+        } catch let KeychainError.errorStatus(status) where status == errSecItemNotFound {
+            try keychain.addItem(query: addQuery(cookieData: cookieData))
+        }
     }
 
     /// Fetch stored cookies.
@@ -99,11 +81,11 @@ public actor CookieStorage: CookieStorageProtocol {
     /// - Returns: The stored cookies.
 
     public func fetchCookies() throws -> [HTTPCookie] {
-        guard let cookieData = try fetchCookieData() else {
+        guard let data: Data = try keychain.fetchItem(query: fetchQuery()) else {
             return []
         }
 
-        return try HTTPCookieCodec.decodeData(cookieData)
+        return try Self.decryptAndDecodeCookies(data, key: cookieEncryptionKey)
     }
 
     /// Remove stored cookies from the keychain.
@@ -115,67 +97,66 @@ public actor CookieStorage: CookieStorageProtocol {
     /// - Throws: An error if the keychain deletion fails.
 
     public func removeCookies() throws {
-        try keychain.deleteItem(query: baseQuery)
+        try keychain.deleteItem(query: baseQuery())
     }
 
-    // MARK: - Cookie data
+    // MARK: - Queries
 
-    private func storeCookieData(_ cookieData: Data) throws {
-        let encryptedCookieData: Data
+    private func baseQuery() -> Set<KeychainQueryItem> {
+        [
+            .service("Wire: Credentials for wire.com"),
+            .account(userID.uuidString),
+            .itemClass(.genericPassword)
+        ]
+    }
+
+    private func fetchQuery() -> Set<KeychainQueryItem> {
+        var query = baseQuery()
+        query.insert(.returningData(true))
+        return query
+    }
+
+    private func addQuery(cookieData: Data) -> Set<KeychainQueryItem> {
+        var query = baseQuery()
+        query.insert(.data(cookieData))
+        query.insert(.accessible(.afterFirstUnlock))
+        return query
+    }
+
+    // MARK: - Cookie encoding / decoding
+
+    private static func encodeAndEncryptCookies(_ cookies: [HTTPCookie], key: Data) throws -> Data {
+        let cookieData = try HTTPCookieCodec.encodeCookies(cookies)
+
+        let encryptedData: Data
         do {
-            encryptedCookieData = try AES256Crypto.encryptAllAtOnceWithPrefixedIV(
+            encryptedData = try AES256Crypto.encryptAllAtOnceWithPrefixedIV(
                 plaintext: cookieData,
-                key: cookieEncryptionKey
+                key: key
             ).data
         } catch {
             throw Failure.failedToEncryptCookie(error)
         }
 
-        // The typical case is updating so try that first.
-        do {
-            try updateCookieInKeychain(encryptedCookieData)
-        } catch let KeychainError.errorStatus(status) where status == errSecItemNotFound {
-            try addCookieToKeychain(encryptedCookieData)
-        }
+        return encryptedData.base64EncodedData()
     }
 
-    private func fetchCookieData() throws -> Data? {
-        guard let encryptedCookieData = try fetchCookieDataFromKeychain() else {
-            return nil
+    private static func decryptAndDecodeCookies(_ base64Data: Data, key: Data) throws -> [HTTPCookie] {
+        guard let encryptedData = Data(base64Encoded: base64Data) else {
+            throw Failure.malformedCookieData
         }
 
+        let cookieData: Data
         do {
-            return try AES256Crypto.decryptAllAtOnceWithPrefixedIV(
-                ciphertext: AES256Crypto.PrefixedData(data: encryptedCookieData),
-                key: cookieEncryptionKey
+            cookieData = try AES256Crypto.decryptAllAtOnceWithPrefixedIV(
+                ciphertext: AES256Crypto.PrefixedData(data: encryptedData),
+                key: key
             )
         } catch {
             throw Failure.failedToDecryptCookie(error)
         }
-    }
 
-    // MARK: - Keychain
-
-    private func addCookieToKeychain(_ cookieData: Data) throws {
-        let query = addQuery(cookieData: cookieData)
-        try keychain.addItem(query: query)
-    }
-
-    private func updateCookieInKeychain(_ cookieData: Data) throws {
-        let updateQuery: Set<KeychainQueryItem> = [.data(cookieData.base64EncodedData())]
-        try keychain.updateItem(query: baseQuery, attributesToUpdate: updateQuery)
-    }
-
-    private func fetchCookieDataFromKeychain() throws -> Data? {
-        guard let base64CookieData: Data = try keychain.fetchItem(query: fetchQuery) else {
-            return nil
-        }
-
-        guard let cookieData = Data(base64Encoded: base64CookieData) else {
-            throw Failure.malformedCookieData
-        }
-
-        return cookieData
+        return try HTTPCookieCodec.decodeData(cookieData)
     }
 
 }
