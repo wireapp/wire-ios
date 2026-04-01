35acc1d525b0defddefb042387e03cb22df5b9fd
François Benaiteau <netbe@users.noreply.github.com>
2026-04-01 08:12:38 +0200

    Update wire-ios-data-model/Source/MLS/MLSService.swift
    Co-authored-by: David-Henner <david.henner@wire.com>

--- wire-ios-data-model/Source/MLS/MLSService.swift
+++ wire-ios-data-model/Source/MLS/MLSService.swift
@@ -378,7 +378,7 @@ public final class MLSService: MLSServiceInterface {
                 return MLSUser(from: selfUser, localDomain: self.localDomain)
             }
             // make sure we have the selfUser but only once
-            // and add self at last so for 1-1 if other user don't have key packages we don't depleet our keypackages
+            // add self in last position so - if the other user doesn't have key packages for 1-1 - we don't deplete our keypackages
             // for nothing
             let usersWithSelfUser = users.filter { $0 != mlsSelfUser } + [mlsSelfUser]
             try await addMembersToConversation(with: usersWithSelfUser, for: groupID)
