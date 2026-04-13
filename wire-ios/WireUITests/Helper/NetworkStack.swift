5fdf4fc0314d09653630a53ec6d2d8fe939d9528
Manish Singh <31733022+findms@users.noreply.github.com>
2026-04-13 16:29:37 +0200

    fix: failing to build app due to a newly added parameter - WPB-24695 (#4560)

--- wire-ios/WireUITests/Helper/NetworkStack.swift
+++ wire-ios/WireUITests/Helper/NetworkStack.swift
@@ -51,11 +51,12 @@ final class NetworkStack {
     )
 
     lazy var apiNetworkService: NetworkService = {
-        let service = NetworkService(baseURL: backendEnvironment.url, serverTrustValidator: serverTrustValidator)
         let config = urlSessionConfigurationFactory.makeRESTAPISessionConfiguration()
-        let session = URLSession(configuration: config, delegate: service, delegateQueue: nil)
-        service.configure(with: session)
-        return service
+        return NetworkService(
+            baseURL: backendEnvironment.url,
+            urlSessionConfiguration: config,
+            serverTrustValidator: serverTrustValidator
+        )
     }()
 
     private lazy var serverTrustValidator = ServerTrustValidator(
