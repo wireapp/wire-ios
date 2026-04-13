24f01a9229a39c8468593e87b1b3734401b59606
Manish Singh <findmanish@outlook.com>
2026-04-13 13:16:09 +0200

    added more description

--- WireUI/Sources/WireLocators/AutoPrefixedEnum.swift
+++ WireUI/Sources/WireLocators/AutoPrefixedEnum.swift
@@ -19,7 +19,7 @@
 protocol AutoPrefixedEnum: RawRepresentable, CaseIterable where RawValue == String {}
 
 extension AutoPrefixedEnum {
-    // TODO: [WPB-24694]
+    // TODO: [WPB-24694] need to update app locators based on this file
     public var rawValue: String { "\(self)" }
 
     public init?(rawValue: String) {
