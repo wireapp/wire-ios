5039c7f5c0e8acb5c08c98e7ab30b906a1c86e7d
Manish Singh <31733022+findms@users.noreply.github.com>
2026-04-13 15:39:19 +0200

    fix: locators for critical flow in 4.16.2 run - WPB-24695 (#4556)

--- WireUI/Sources/WireLocators/AutoPrefixedEnum.swift
+++ WireUI/Sources/WireLocators/AutoPrefixedEnum.swift
@@ -19,7 +19,8 @@
 protocol AutoPrefixedEnum: RawRepresentable, CaseIterable where RawValue == String {}
 
 extension AutoPrefixedEnum {
-    public var rawValue: String { "\(Self.self).\(self)" }
+    // TODO: [WPB-24694] need to update app locators based on this file
+    public var rawValue: String { "\(self)" }
 
     public init?(rawValue: String) {
         guard let match = Self.allCases.first(where: { $0.rawValue == rawValue }) else { return nil }
