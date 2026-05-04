0d3ea0ab43ff2fbf5de61eed49325b61601559ed
Christoph Aldrian <christoph.aldrian@wire.com>
2026-05-04 14:19:33 +0200

    revert

--- WireDeveloperTools/Tests/WireDeveloperToolsUITests/WireDeveloperToolsUITests.swift
+++
@@ -1,7 +0,0 @@
-import Testing
-
-@testable import WireDeveloperToolsUI
-
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
