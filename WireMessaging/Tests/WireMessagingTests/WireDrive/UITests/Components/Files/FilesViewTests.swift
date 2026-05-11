9005fad935eded36d06e654f1cd5d9dd3107ba1b
François Benaiteau <francois.benaiteau@wire.com>
2026-05-11 15:37:18 +0200

    fix tests

--- WireMessaging/Tests/WireMessagingTests/WireDrive/UITests/Components/Files/FilesViewTests.swift
+++ WireMessaging/Tests/WireMessagingTests/WireDrive/UITests/Components/Files/FilesViewTests.swift
@@ -17,6 +17,7 @@
 //
 
 import Combine
+import Network
 import SwiftUI
 import WireDesign
 import WireFoundation
@@ -48,6 +49,7 @@ final class FilesViewTests: XCTestCase {
     private var makeAssetAvailableOfflineUseCase: WireDriveMakeAssetAvailableOfflineUseCase!
     private var removeAssetAvailableOfflineUseCase: WireDriveRemoveAssetAvailableOfflineUseCase!
     private var fetchOfflineAvailableAssetsUseCase: WireDriveFetchOfflineAvailableAssetsUseCase!
+    private var networkMonitor: NetworkMonitor!
 
     private let record: Bool? = nil
 
@@ -115,6 +117,9 @@ final class FilesViewTests: XCTestCase {
         fetchOfflineAvailableAssetsUseCase = WireDriveFetchOfflineAvailableAssetsUseCase(
             localAssetRepository: localAssetsRepository
         )
+
+        networkMonitor = NetworkMonitor(monitor: MockNWPathMonitoring())
+        networkMonitor.currentStatus = .connected
     }
 
     @MainActor
@@ -132,6 +137,7 @@ final class FilesViewTests: XCTestCase {
         updatePublicLinkExpiration = nil
         updatePublicLinkPassword = nil
         driveConversationsUseCase = nil
+        networkMonitor = nil
     }
 
     @MainActor
@@ -391,7 +397,8 @@ final class FilesViewTests: XCTestCase {
             nodesRepository: nodesRepository,
             fileCache: MockFileCache(),
             isBrowsing: false,
-            accentColorProvider: { .default }
+            accentColorProvider: { .default },
+            networkMonitor: networkMonitor
         )
 
         filesViewModel.state = state
@@ -432,4 +439,10 @@ private extension FilesItemViewModel {
         )
     }
 
+}
+
+private final class MockNWPathMonitoring: NWPathMonitoring {
+    var pathUpdateHandler: (@Sendable (NWPath) -> Void)?
+
+    func start(queue: DispatchQueue) {}
 }
