f59661e693368cd1ecbb3631bfd65ff7ad1fd21f
Sam Wyndham <samwyndham@users.noreply.github.com>
2026-05-19 15:14:39 +0300

    Remove ZMCookieKey duplication

--- wire-ios-utilities/Source/UserDefaults+Shared.swift
+++ wire-ios-utilities/Source/UserDefaults+Shared.swift
@@ -25,8 +25,7 @@ public extension UserDefaults {
     static var existingCookiesKey: Data? {
         guard let defaults = UserDefaults.shared() else { return nil }
 
-        let cookiesKeyKey = "ZMCookieKey"
-        return defaults.data(forKey: cookiesKeyKey)
+        return defaults.data(forKey: ZMCookieKeyKey)
     }
 
 }
