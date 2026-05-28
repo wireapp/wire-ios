60eb230ae5e19dc88ffce8cc9a0a572b0fdd3c76
Manish Singh <31733022+findms@users.noreply.github.com>
2026-05-28 17:15:55 +0200

    chore: test - update name and username - WPB-25498 (#4722)

--- WireUI/Sources/WireLocators/Locators.swift
+++ WireUI/Sources/WireLocators/Locators.swift
@@ -260,6 +260,14 @@ public enum Locators {
         case save
     }
 
+    public enum UsernameUpdatePage: String {
+
+        case usernameField
+        case save = "Save"
+        case username = "Username"
+        case handleTextField
+    }
+
     public enum NewConversationPage: String {
 
         case createNewGroupButton
