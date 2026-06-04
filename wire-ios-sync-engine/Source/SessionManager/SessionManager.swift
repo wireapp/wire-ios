f2b494265591b4ddfcba805ad80d082c87ecb102
Christoph Aldrian <christoph.aldrian@wire.com>
2026-06-04 10:55:32 +0200

    revert some changes

--- wire-ios-sync-engine/Source/SessionManager/SessionManager.swift
+++ wire-ios-sync-engine/Source/SessionManager/SessionManager.swift
@@ -172,13 +172,6 @@ public protocol SessionManagerSwitchingDelegate: AnyObject {
     func confirmSwitchingAccount(completion: @escaping (Bool) -> Void)
 }
 
-public extension Notification.Name {
-    /// Posted by `SessionManager` after the active user session changes. The notification's
-    /// `object` is the new active `ZMUserSession?` (may be `nil`).
-    static let sessionManagerActiveUserSessionDidChange = Notification
-        .Name("SessionManagerActiveUserSessionDidChange")
-}
-
 @objc
 public protocol ForegroundNotificationResponder: AnyObject {
     @MainActor
@@ -307,11 +300,6 @@ public final class SessionManager: NSObject, SessionManagerType {
             requireInternal(oldSession === $0.activeUserSession, "Invalid state updating active user session")
             $0.activeUserSession = newSession
         }
-
-        NotificationCenter.default.post(
-            name: .sessionManagerActiveUserSessionDidChange,
-            object: newSession
-        )
     }
 
     public var backgroundUserSessions: [UUID: ZMUserSession] {
