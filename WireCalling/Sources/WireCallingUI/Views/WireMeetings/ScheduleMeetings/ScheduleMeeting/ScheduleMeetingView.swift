4d15dfadfd1cd60a3927ebe8f362e44e2cc935b3
Christoph Aldrian <christoph.aldrian@wire.com>
2026-06-09 14:38:53 +0200

    delete unused code

--- WireCalling/Sources/WireCallingUI/Views/WireMeetings/ScheduleMeetings/ScheduleMeeting/ScheduleMeetingView.swift
+++ WireCalling/Sources/WireCallingUI/Views/WireMeetings/ScheduleMeetings/ScheduleMeeting/ScheduleMeetingView.swift
@@ -35,13 +35,8 @@ struct ScheduleMeetingView: View {
 
     var body: some View {
         NavigationStack {
-            if #available(iOS 17, *) {
-                formContent
-                    .listSectionSpacing(.compact)
-            } else {
-                formContent
-                    .listStyle(.insetGrouped)
-            }
+            formContent
+                .listSectionSpacing(.compact)
         }
     }
 
