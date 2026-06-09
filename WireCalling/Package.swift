3cb87fc7cc471ae788c8354334b01d4b61c8f595
Christoph Aldrian <christoph.aldrian@wire.com>
2026-06-09 15:08:21 +0200

    use .v17 enum case

--- WireCalling/Package.swift
+++ WireCalling/Package.swift
@@ -7,7 +7,7 @@ let WireTestingPackage = Target.Dependency.product(name: "WireTestingPackage", p
 let package = Package(
     name: "WireCalling",
     defaultLocalization: "en",
-    platforms: [.iOS("17.0"), .macOS(.v12)],
+    platforms: [.iOS(.v17), .macOS(.v12)],
     products: [
         .library(name: "WireCallingDomain", targets: ["WireCallingDomain"]),
         .library(name: "WireCallingAssembly", targets: ["WireCallingAssembly"]),
