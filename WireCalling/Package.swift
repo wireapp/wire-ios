14a6e59559a9f55b4c5175d8c5aded2683609fc6
Christoph Aldrian <christoph.aldrian@wire.com>
2026-06-12 15:51:30 +0200

    stencil

--- WireCalling/Package.swift
+++ WireCalling/Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version: 6.2
 
+import Foundation
 import PackageDescription
 
 let WireTestingPackage = Target.Dependency.product(name: "WireTestingPackage", package: "WireFoundation")
@@ -81,9 +82,17 @@ for target in package.targets {
     }
 }
 
-for target in package.targets {
+// open --env CI wire-ios-mono.xcworkspace
+// or
+// CI= swift build
+let isCI = ProcessInfo.processInfo.environment["CI"] != nil
+
+for target in package.targets where target.type != .binary {
     target.swiftSettings = (target.swiftSettings ?? []) + [
+        .enableUpcomingFeature("ExistentialAny"),
         .enableUpcomingFeature("InternalImportsByDefault"),
-        .enableUpcomingFeature("ExistentialAny")
-    ]
+        .enableUpcomingFeature("MemberImportVisibility"),
+        .enableUpcomingFeature("StrictMemorySafety"),
+        isCI ? .unsafeFlags(["-warnings-as-errors"]) : nil
+    ].compactMap(\.self)
 }
