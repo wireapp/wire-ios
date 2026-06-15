0928a2b8d580610a9a223e59e45b7991662ceba5
Sam Wyndham <samwyndham@users.noreply.github.com>
2026-06-15 13:46:31 +0200

    Introduce `withBackgroundTask(name:executer:operation:)`

---
+++ wire-ios-system/Source/BackgroundTasks.swift
@@ -0,0 +1,38 @@
+//
+// Wire
+// Copyright (C) 2026 Wire Swiss GmbH
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+//
+
+public protocol BackgroundTaskExecuter {
+    func execute<T>(name: String?, operation: () async throws -> T) async throws -> T
+}
+
+func withBackgroundTask<T>(
+    name: String? = nil,
+    executer: any BackgroundTaskExecuter,
+    operation: sending @escaping @isolated(any) () async throws -> T
+) async throws -> T {
+    try await executer.execute(name: name, operation: operation)
+}
+
+/// A `BackgroundTaskExecuter` that simply executes the operation without any background task management.
+struct PassthroughTaskExecuter: BackgroundTaskExecuter {
+
+    func execute<T>(name: String?, operation: () async throws -> T) async throws -> T {
+        try await operation()
+    }
+
+}
