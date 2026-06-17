3706516e2f01137cf2733e8bf906c2ab201c1ced
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2026-06-17 12:14:21 +0200

    wip: root view stub

--- wire-ios/WireShareExtensionCore/Views/RootView.swift
+++ wire-ios/WireShareExtensionCore/Views/RootView.swift
@@ -20,13 +20,79 @@ import SwiftUI
 
 public struct RootView: View {
 
-    public init() {}
+    // Selected account
+
+    @State
+    private var conversations: [ConversationModel]
+
+    @State
+    private var accounts: [Account]
+
+    @State
+    private var selectedAccount: Account
+
+    public init(accounts: [Account], conversations: [ConversationModel]) {
+        self.accounts = accounts
+        self.conversations = conversations
+        self.selectedAccount = accounts[0]
+    }
 
     public var body: some View {
-        Text("Hello, World!")
+        NavigationStack {
+            form.navigationDestination(for: ConversationModel.self) { conversation in
+                ComposeMessageView()
+            }
+            .navigationTitle("Send to")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel", systemImage: "xmark") {
+                        print("Cancel")
+                    }
+                }
+            }
+        }
+    }
+
+    @ViewBuilder
+    private var form: some View {
+        Form {
+            Section {
+                Picker("Account", selection: $selectedAccount) {
+                    ForEach(accounts, id: \.self) { account in
+                        Text(account.name)
+                    }
+                }
+            }
+
+            Section("Conversations") {
+                ForEach(conversations, id: \.self) { conversation in
+                    NavigationLink(value: conversation) {
+                        Text(conversation.name)
+                    }
+                }
+            }
+        }
     }
 }
 
+public struct Account: Hashable {
+
+    let id: UUID
+    let name: String
+}
+
 #Preview {
-    RootView()
+    RootView(
+        accounts: [
+            Account(id: UUID(), name: "Sam"),
+            Account(id: UUID(), name: "John")
+        ],
+        conversations: [
+        ConversationModel(id: UUID(), name: "One"),
+        ConversationModel(id: UUID(), name: "Two"),
+        ConversationModel(id: UUID(), name: "Three"),
+        ConversationModel(id: UUID(), name: "Four"),
+        ConversationModel(id: UUID(), name: "Five"),
+    ])
 }
