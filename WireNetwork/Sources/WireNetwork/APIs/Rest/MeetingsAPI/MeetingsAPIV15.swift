ac45fd4e091251c4417c2a76834a3c3a081d2829
Christoph Aldrian <christoph.aldrian@wire.com>
2026-06-23 13:32:03 +0200

    fixes

--- WireNetwork/Sources/WireNetwork/APIs/Rest/MeetingsAPI/MeetingsAPIV15.swift
+++ WireNetwork/Sources/WireNetwork/APIs/Rest/MeetingsAPI/MeetingsAPIV15.swift
@@ -16,6 +16,8 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
+import Foundation
+
 class MeetingsAPIV15: MeetingsAPIV14 {
 
     override var apiVersion: APIVersion {
