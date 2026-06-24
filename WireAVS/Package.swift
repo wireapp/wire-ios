9735a391037a1d145f3eed990b24f9882fbf4414
François Benaiteau <netbe@users.noreply.github.com>
2026-06-24 10:16:36 +0200

    Commit with unresolved merge conflicts

--- WireAVS/Package.swift
+++ WireAVS/Package.swift
@@ -17,8 +17,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WireAVS",
+<<<<<<< HEAD
             url: "https://github.com/wireapp/wire-avs/releases/download/10.4.5/avs.xcframework.zip",
             checksum: "2061baefbc05c6d27e8243d34c2f24e3f4f2277625aaeaf332c0b2c0a0ef8110"
+=======
+            url: "https://github.com/wireapp/wire-avs/releases/download/10.3.23/avs.xcframework.zip",
+            checksum: "1b93474d2a7b2978674e145d41129c6eb346493b468182a43cb61298f9447f9c"
+>>>>>>> ba33b8d428 (chore: bump AVS 10.3.23 - WPB-25715 🍒 (#4900))
         )
     ]
 )
