49a23c22c10d048a220a81f1deb9a1415e4dae21
Manish Singh <31733022+findms@users.noreply.github.com>
2026-06-25 16:26:58 +0200

    fix: changed the tag with specific commit to build testservice jar - WPB-26093 (#4895)

--- wire-ios/WireUITests/ZCallingTests.swift
+++ wire-ios/WireUITests/ZCallingTests.swift
@@ -158,11 +158,8 @@ final class ZCallingTests: WireUITestCase {
             .resumeCallUI()
 
         // THEN
-        let activeConversationPage = try ongoingCallPage.hangUpOngoingCall()
+        _ = try ongoingCallPage.hangUpOngoingCall()
+            .verifyNoCallOngoingAfterHangUp()
 
-        XCTAssertTrue(
-            activeConversationPage.openOngoingCallButton.waitForNonExistence(timeout: 4),
-            "Ongoing call still visible after hanging up the call"
-        )
     }
 }
