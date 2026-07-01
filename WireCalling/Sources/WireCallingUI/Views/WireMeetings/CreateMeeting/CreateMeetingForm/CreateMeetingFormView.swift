6f401b002f6c26db65d589c7ccb9298b9317a06d
Christoph Aldrian <christoph.aldrian@wire.com>
2026-07-01 10:27:52 +0200

    add notes

--- WireCalling/Sources/WireCallingUI/Views/WireMeetings/CreateMeeting/CreateMeetingForm/CreateMeetingFormView.swift
+++ WireCalling/Sources/WireCallingUI/Views/WireMeetings/CreateMeeting/CreateMeetingForm/CreateMeetingFormView.swift
@@ -16,6 +16,9 @@
 // along with this program. If not, see http://www.gnu.org/licenses/.
 //
 
+// TODO: initial focus state title field
+// TODO: meeting start next full hour
+
 import SwiftUI
 import WireCallingDomain
 import WireDesign
