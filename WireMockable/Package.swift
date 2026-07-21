1397b57184c80ed12a0d044ec9cbc879853f557a
Christoph Aldrian <christoph.aldrian@wire.com>
2026-07-21 12:50:23 +0200

    create new package

--- WireMockable/Package.swift
+++ WireMockable/Package.swift
@@ -1,43 +1,52 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import CompilerPluginSupport
 import PackageDescription
+import CompilerPluginSupport
 
 let package = Package(
     name: "WireMockable",
-    platforms: [.iOS(.v17), .macOS(.v14)],
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
-        .library(name: "WireMockable", targets: ["WireMockable"])
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "WireMockable",
+            targets: ["WireMockable"]
+        ),
+        .executable(
+            name: "WireMockableClient",
+            targets: ["WireMockableClient"]
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "601.0.0")
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "603.0.0-latest"),
     ],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        // Macro implementation that performs the source transformation of a macro.
         .macro(
             name: "WireMockableMacros",
             dependencies: [
-                .product(name: "SwiftSyntax", package: "swift-syntax"),
                 .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
-                .product(name: "SwiftDiagnostics", package: "swift-syntax")
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
             ]
         ),
-        .target(
-            name: "WireMockable",
-            dependencies: ["WireMockableMacros"]
-        ),
+
+        // Library that exposes a macro as part of its API, which is used in client programs.
+        .target(name: "WireMockable", dependencies: ["WireMockableMacros"]),
+
+        // A client of the library, which is able to use the macro in its own code.
+        .executableTarget(name: "WireMockableClient", dependencies: ["WireMockable"]),
+
+        // A test target used to develop the macro implementation.
         .testTarget(
             name: "WireMockableTests",
             dependencies: [
                 "WireMockableMacros",
-                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax")
+                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
             ]
-        )
-    ]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
 )
-
-for target in package.targets {
-    target.swiftSettings = (target.swiftSettings ?? []) + [
-        .enableUpcomingFeature("ExistentialAny")
-    ]
-}
