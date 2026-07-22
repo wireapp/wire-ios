5630b25f925aa1dedc2dab177734514e5bf6ded7
Christoph Aldrian <christoph.aldrian@wire.com>
2026-07-22 15:19:54 +0200

    clean before measuring

--- measure.sh
+++ measure.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -Eeuo pipefail
 
+xcodebuild clean \
+  -workspace wire-ios-mono.xcworkspace \
+  -scheme Wire-iOS \
+  -skipPackagePluginValidation \
+  -skipMacroValidation
+
 time \
 xcodebuild build-for-testing \
   -workspace wire-ios-mono.xcworkspace \
