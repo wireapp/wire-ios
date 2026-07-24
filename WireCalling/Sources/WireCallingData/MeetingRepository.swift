004895deb54a25d5bc3fd0f4b918a1ab3f003b11
Christoph Aldrian <christoph.aldrian@wire.com>
2026-07-24 14:58:40 +0200

    resolve comment

--- WireCalling/Sources/WireCallingData/MeetingRepository.swift
+++ WireCalling/Sources/WireCallingData/MeetingRepository.swift
@@ -112,7 +112,7 @@ public final class MeetingRepository: MeetingRepositoryProtocol {
             await localStore.storeMeeting(meeting)
         } else {
             // The meeting no longer exists on the backend.
-            await localStore.deleteMeeting(id: id) // TODO: need to delete the conversation?
+            await localStore.deleteMeeting(id: id)
         }
         changeBroadcaster.broadcast()
         guard let meeting else { return nil }
