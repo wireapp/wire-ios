ce0a90ca04212b5d9f919b8c74099a338838ce32
Christoph Aldrian <christoph.aldrian@wire.com>
2026-07-27 15:08:55 +0200

    v30

--- WireAVS/Package.swift
+++ WireAVS/Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WireAVS",
-            url: "https://github.com/wireapp/wire-avs/releases/download/10.4.28/avs.xcframework.zip",
-            checksum: "ceaefc593a00ae9a168611aeabf13de0f97d0cb6fe50c26ac40b289c98e3b7d6"
+            url: "https://github.com/wireapp/wire-avs/releases/download/10.4.30/avs.xcframework.zip",
+            checksum: "6cd1897701716a719547288f9b2263c4ae5c004aa9409d6aadb9a2ade26a31a8"
         )
     ]
 )
