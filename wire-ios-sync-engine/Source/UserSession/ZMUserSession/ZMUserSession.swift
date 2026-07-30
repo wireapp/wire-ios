26be2de936c68c6e67f855917e2c89257350d13b
Christoph Aldrian <christoph.aldrian@wire.com>
2026-07-30 13:49:25 +0200

    move meeting to first occurrence

--- wire-ios-sync-engine/Source/UserSession/ZMUserSession/ZMUserSession.swift
+++ wire-ios-sync-engine/Source/UserSession/ZMUserSession/ZMUserSession.swift
@@ -1337,9 +1337,7 @@ extension ZMUserSession: SyncAgentDelegate {
             while mlsFeature.isEnabled,
                   isBackendMLSEnabled,
                   await MainActor.run(body: { [application] in application.applicationState == .active }) {
-                guard await mlsService.recoverPendingConversationBatchIfNeeded() else {
-                    break
-                }
+                guard await mlsService.recoverPendingConversationBatchIfNeeded() else { break }
             }
             await resolveOneOnOneConversationsIfNeeded()
             await recurringActionService.performActionsIfNeeded()
