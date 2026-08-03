ed0c5277aaa4f88a9253c8e68cf98b2049eca1de
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2026-08-03 15:57:16 +0200

    chore: bump Core Crypto to 10.1.1 - WPB-27753 (#5094)

--- WireCoreCrypto/Package.swift
+++ WireCoreCrypto/Package.swift
@@ -21,15 +21,15 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WireCoreCrypto",
-            url: "https://github.com/wireapp/core-crypto/releases/download/v10.0.0/WireCoreCrypto.xcframework.zip",
-            checksum: "edd996f1146eaa3f1d99dc01c73c49337f41ed512c601c90f0a07606fb420761"
+            url: "https://github.com/wireapp/core-crypto/releases/download/v10.1.1/WireCoreCrypto.xcframework.zip",
+            checksum: "67444590076124d73cf1524c035c2d4a7be1d2122cc53c99e75dee23128951cd"
         ),
         // this is an internal dependency to WireCoreCrypto but currently needs to explicitly
         // added as a dependency due to limitations of Swift packages.
         .binaryTarget(
             name: "WireCoreCryptoUniffi",
-            url: "https://github.com/wireapp/core-crypto/releases/download/v10.0.0/WireCoreCryptoUniffi.xcframework.zip",
-            checksum: "277ddd0f20143a768540152c1bb7d463a5185b73e568299668db1f6d7e3e9031"
+            url: "https://github.com/wireapp/core-crypto/releases/download/v10.1.1/WireCoreCryptoUniffi.xcframework.zip",
+            checksum: "a9583c7c8fda3306ec5577652c4b24e4717c632b45e58b808d904ed344231585"
         )
     ]
 )
