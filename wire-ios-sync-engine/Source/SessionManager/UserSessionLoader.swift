2d32cf21e11c8fe5c34a88a132b70a6875746d66
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2026-08-12 07:32:02 +0000

    fix: mls migration doesn't start after relaunch - WPB-27049 (#5113)

--- wire-ios-sync-engine/Source/SessionManager/UserSessionLoader.swift
+++ wire-ios-sync-engine/Source/SessionManager/UserSessionLoader.swift
@@ -489,6 +489,7 @@ final class UserSessionLoader {
             apiVersion: WireTransport.APIVersion(rawValue: Int32(backendMetadata.apiVersion.rawValue))
         )
         let recurringActionService = RecurringActionService(
+            userID: accountID,
             storage: sharedUserDefaults,
             dateProvider: .system
         )
