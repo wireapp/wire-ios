89a47ab55c554e86df75dfedd5c2a2d59df9fbdb
Christoph Aldrian <christoph.aldrian@wire.com>
2026-09-11 15:08:11 +0200

    minor fix

--- wire-ios/Wire-iOS/Sources/AuthenticatedRouter.swift
+++ wire-ios/Wire-iOS/Sources/AuthenticatedRouter.swift
@@ -189,6 +189,11 @@ extension AuthenticatedRouter: AuthenticatedRouterProtocol {
             _zClientViewController?.showConversationList()
         case .meetings:
             Task { @MainActor in
+                // The meetings tab is only installed when the feature is enabled at setup
+                // (see ZClientViewController.setupSplitViewController). A notification
+                // created while enabled can be tapped after the feature was disabled;
+                // guarding here avoids selecting a tab that isn't in the tab bar.
+                guard zClientViewController.userSession.isMeetingsEnabled else { return }
                 await zClientViewController.mainCoordinator.showMeetings()
             }
         case let .userProfile(user):
