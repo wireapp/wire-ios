490a4e811d3397f0f10b35fbc79470513e76f826
François Benaiteau <netbe@users.noreply.github.com>
2026-09-17 09:12:18 +0200

    Clean up Locators.swift by removing merge conflict markers
    Removed unnecessary merge conflict markers and cleaned up the code structure.

--- WireUI/Sources/WireLocators/Locators.swift
+++ WireUI/Sources/WireLocators/Locators.swift
@@ -677,7 +677,6 @@ public enum Locators {
 
     }
 
-<<<<<<< HEAD
     public enum SSOWebLoginPage: String {
         case username = "Username"
         case signInButton = "Sign In"
@@ -698,12 +697,11 @@ public enum Locators {
             case deleteForMeButton = "Delete Meeting for Me Button"
             case joinButton = "Join Button"
         }
-=======
+    }
+    
     public enum SessionExpiredPage: String {
 
         case alertTitle = "Your session expired"
         case okButton = "OK"
->>>>>>> 89b2142dbe (fix: session expired alert not shown when removed from team - WPB-24285 (#5213))
-
     }
 }
