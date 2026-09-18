1d712165f3e513c48c9fd7a5e6f4d1f78595bb69
François Benaiteau <francois.benaiteau@wire.com>
2026-09-18 14:19:52 +0200

    fix swift format

--- wire-ios/Wire-iOS/Sources/UserInterface/GroupDetails/Sections/MessageProtocolSectionController.swift
+++ wire-ios/Wire-iOS/Sources/UserInterface/GroupDetails/Sections/MessageProtocolSectionController.swift
@@ -176,8 +176,7 @@ final class MessageProtocolSectionController: GroupDetailsSectionController {
         requestMLSMigration()
     }
 
-    @MainActor
-    private var canTriggerManualMLSMigration: Bool {
+    @MainActor private var canTriggerManualMLSMigration: Bool {
         get async {
             guard messageProtocol == .proteus || messageProtocol == .mixed else {
                 Self.logger
