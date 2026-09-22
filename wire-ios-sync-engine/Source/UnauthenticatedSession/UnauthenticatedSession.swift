fcaf39ccc1d5635bcc03ef8aa51220fd2f4876b4
François Benaiteau <netbe@users.noreply.github.com>
2026-09-22 10:56:52 +0200

    Update wire-ios-sync-engine/Source/UnauthenticatedSession/UnauthenticatedSession.swift
    Co-authored-by: Christoph Aldrian <caldrian@users.noreply.github.com>

--- wire-ios-sync-engine/Source/UnauthenticatedSession/UnauthenticatedSession.swift
+++ wire-ios-sync-engine/Source/UnauthenticatedSession/UnauthenticatedSession.swift
@@ -184,7 +184,7 @@ extension UnauthenticatedSession: UserInfoParser {
     public func upgradeToAuthenticatedSession(
         with userInfo: UserInfo,
         newEnvironment: NewEnvironment,
-        multiIngressIdentityProviderID: UUID? = nil
+        multiIngressIdentityProviderID: UUID?
     ) {
         let account = Account(userName: "", userIdentifier: userInfo.identifier)
         account.lastSSOIdentityProviderID = multiIngressIdentityProviderID
