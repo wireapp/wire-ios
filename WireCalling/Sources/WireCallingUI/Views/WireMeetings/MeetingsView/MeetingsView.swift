8ee7fceb3f27e729ed8dbc0654e5f06b58aade5b
Christoph Aldrian <christoph.aldrian@wire.com>
2026-09-24 10:10:01 +0200

    add task.yield

--- WireCalling/Sources/WireCallingUI/Views/WireMeetings/MeetingsView/MeetingsView.swift
+++ WireCalling/Sources/WireCallingUI/Views/WireMeetings/MeetingsView/MeetingsView.swift
@@ -163,6 +163,7 @@ struct MeetingsView: View {
         .scrollContentBackground(.hidden)
         .background(ColorTheme.Backgrounds.surface.color)
         .refreshable {
+            await Task.yield()
             await viewModel.loadInitialData()
         }
         .alert(
