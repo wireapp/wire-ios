70c7d8a703770d845cf569fd4fe9e72649b031c1
Manish Singh <31733022+findms@users.noreply.github.com>
2026-09-25 18:37:39 +0200

    fix(UITest): simulator not showing default pre seeded images to send on in-house runner - WPB-29027 (#5308)

--- wire-ios/WireUITests/Pages/ActiveConversationPage.swift
+++ wire-ios/WireUITests/Pages/ActiveConversationPage.swift
@@ -602,29 +602,43 @@ class ActiveConversationPage: PageModel {
         return self
     }
 
-    func openPhotos() throws -> ActiveConversationPage {
-        photoButton.waitAndTap()
-        return self
-    }
-
-    func selectImageAndSend(at index: Int = 3) throws -> ActiveConversationPage {
-        if !imageToChoose(at: index).waitForExistence(timeout: 2) {
+    func selectImageAndSend() throws -> ActiveConversationPage {
+        if !cameraRollButton.waitForExistence(timeout: 3) {
             photoButton.waitAndTap()
         }
-        imageToChoose(at: index).waitAndTap()
+
+        XCTAssertTrue(
+            cameraRollButton.waitAndTap(),
+            "cameraRollButton did not show up"
+        )
+
+        let image = app.images.matching(NSPredicate(
+            format: "identifier == %@ AND NOT (label BEGINSWITH %@)",
+            Locators.PhotosAppPage.imageTile.rawValue,
+            "Video"
+        )).firstMatch
+        XCTAssertTrue(
+            image.waitForExistence(timeout: 10),
+            "No image found in camera roll"
+        )
+        image.coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5)).tap()
 
         XCTAssertTrue(
             okToSend.waitForExistence(timeout: 3),
-            "OK button did not appear after selecting media"
+            "Seems like image is not tapped"
         )
         okToSend.waitAndTap()
         return self
     }
 
     func selectImageAndSendInDriveEnabledConversation(at index: Int = 3) throws -> ActiveConversationPage {
-        if !imageToChoose(at: index).waitForExistence(timeout: 2) {
+        if !imageToChoose(at: index).waitForExistence(timeout: 5) {
             photoButton.waitAndTap()
         }
+        XCTAssertTrue(
+            imageToChoose(at: index).waitForExistence(timeout: 5),
+            "No image found in simulator photo library"
+        )
         imageToChoose(at: index).waitAndTap()
 
         XCTAssertTrue(
